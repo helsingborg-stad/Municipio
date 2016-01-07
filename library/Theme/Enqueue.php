72565bada6b156bbc367d4309459f932a9463b3e
Kristoffer Svanmark <kristoffer.svanmark@lexiconitkonsult.se>
2016-01-07 16:45:54 +0100

    Enqueue theme specific css

--- library/Theme/Enqueue.php
+++ library/Theme/Enqueue.php
@@ -26,6 +26,9 @@ class Enqueue
     {
         wp_register_style('hbg-prime', 'http://helsingborg-stad.github.io/styleguide-web/dist/css/hbg-prime.min.css', '', '1.0.0');
         wp_enqueue_style('hbg-prime');
+
+        wp_register_style('helsingborg-se', get_template_directory_uri() . '/assets/dist/css/app.min.css', '', '1.0.0');
+        wp_enqueue_style('helsingborg-se');
     }
 
     /**
