76820f0569ef4649969c12331298225da2313177
Kristoffer Svanmark <kristoffer.svanmark@lexiconitkonsult.se>
2016-01-11 12:15:13 +0100

    Search template

---
+++ search.php
@@ -0,0 +1,13 @@
+<?php get_header(); ?>
+
+<section class="creamy gutter-vertical gutter-lg clearfix">
+    <div class="container">
+        <div class="gid">
+            <div class="grid-lg-12">
+                <?php get_search_form(); ?>
+            </div>
+        </div>
+    </div>
+</section>
+
+<?php get_footer(); ?>
--- searchform.php
+++ searchform.php
@@ -3,7 +3,12 @@
     $searchFormNode = ($searchFormNode) ? $searchFormNode+1 : 1;
 ?>
 <form class="search" method="get" action="/">
+    <?php if (is_front_page()) : ?>
     <label class="label label-lg label-red-4" for="searchkeyword-<?php echo $searchFormNode; ?>">Sök på Helsingborg.se</label>
+    <?php else : ?>
+    <label for="searchkeyword-<?php echo $searchFormNode; ?>" class="sr-only">Sök på Helsingborg.se</label>
+    <?php endif; ?>
+
     <div class="input-group input-group-lg">
         <input id="searchkeyword-<?php echo $searchFormNode; ?>" autocomplete="off" class="form-control form-control-lg" type="search" name="s" placeholder="Vad letar du efter?" value="<?php echo (isset($_GET['s']) && strlen($_GET['s']) > 0) ? urldecode(stripslashes($_GET['s'])) : ''; ?>">
         <span class="input-group-addon-btn">
