c20b33e5d5bc4a7f730664bf1f729fa33715b0c3
Kristoffer Svanmark <kristoffer.svanmark@lexiconitkonsult.se>
2016-01-13 14:47:02 +0100

    Add blade templates filter

--- library/Theme/BladerunnerSettings.php
+++ library/Theme/BladerunnerSettings.php
@@ -7,9 +7,35 @@ class BladerunnerSettings
     public function __construct()
     {
         $this->createCacheFolder();
+        $this->templates();
 
         add_filter('bladerunner/cache_path', array($this, 'cachePath'));
         add_filter('bladerunner/cache', array($this, 'cachePath'));
+    }
+
+    public function templates()
+    {
+        add_filter('bladerunner/template_types', function () {
+            return array(
+                'index'      => 'index.blade.php',
+                'home'       => 'index.blade.php',
+                'single'     => 'single.blade.php',
+                'page'       => 'page.blade.php',
+                '404'        => '404.blade.php',
+                'archive'    => 'archive.blade.php',
+                'author'     => 'author.blade.php',
+                'category'   => 'category.blade.php',
+                'tag'        => 'tag.blade.php',
+                'taxonomy'   => 'taxonomy.blade.php',
+                'date'       => 'date.blade.php',
+                'front-page' => 'front-page.blade.php',
+                'paged'      => 'paged.blade.php',
+                'search'     => 'search.blade.php',
+                'single'     => 'single.blade.php',
+                'singular'   => 'singular.blade.php',
+                'attachment' => 'attachment.blade.php',
+            );
+        });
     }
 
     public function cachePath()
