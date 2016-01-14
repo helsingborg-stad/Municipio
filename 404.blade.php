2f557349f24d3764f59d86b83cad80f7542acd90
Kristoffer Svanmark <kristoffer.svanmark@lexiconitkonsult.se>
2016-01-14 12:06:48 +0100

    Archive, category and 404 templates

---
+++ 404.blade.php
@@ -0,0 +1,15 @@
+@extends('templates.scaffolding')
+
+@section('content')
+
+<div class="container">
+    @include('templates.partials.breadcrumbs')
+
+    <div class="grid no-margin-top">
+        <article class="grid-md-12">
+            <h1>404</h1>
+        </article>
+    </div>
+</div>
+
+@stop
