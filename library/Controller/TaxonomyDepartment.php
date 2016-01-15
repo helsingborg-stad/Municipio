f03b3b9797c132dde77cfe0f0d9812fe7860cdd9
Kristoffer Svanmark <kristoffer.svanmark@lexiconitkonsult.se>
2016-01-15 14:21:45 +0100

    Send taxnony data to view

--- library/Controller/TaxonomyDepartment.php
+++ library/Controller/TaxonomyDepartment.php
@@ -6,6 +6,6 @@ class TaxonomyDepartment extends \Municipio\Controller\BaseController
 {
     public function init()
     {
-        $this->data['term'] = 'Hello World!';
+        $this->data['term'] = get_term_by('slug', get_query_var('term'), get_query_var('taxonomy'));
     }
 }
