478bd7d4c7ec228f8ea3acb1c9496b422b7dcfe0
Kristoffer Svanmark <kristoffer.svanmark@lexiconitkonsult.se>
2016-01-15 14:12:34 +0100

    Removes unwanted semicolon

--- templates/partials/hero.blade.php
+++ templates/partials/hero.blade.php
@@ -1,6 +1,6 @@
 <div class="hero hidden-xs hidden-sm" style="background-image:url('http://www.helsingborg.se/wp-content/uploads/2015/11/skolflicka_i_kattarp_1800x600_foto_anna_alexander_olsson.jpg');">
     @if (is_front_page())
-        @include('templates.partials.stripe');
+        @include('templates.partials.stripe')
     @endif
 
     <?php get_search_form(); ?>
