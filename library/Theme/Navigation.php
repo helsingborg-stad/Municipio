19f5af3c2c4767c78d8662ed72c0ae83ee1f6ca6
Kristoffer Svanmark <kristoffer.svanmark@lexiconitkonsult.se>
2016-01-18 11:21:13 +0100

    Breadcrumb

--- library/Theme/Navigation.php
+++ library/Theme/Navigation.php
@@ -15,4 +15,62 @@ class Navigation
             'main-menu' => __('Main menu', 'municipio')
         ));
     }
+
+    /**
+     * Outputs the html for the breadcrumb
+     * @return void
+     */
+    public static function outputBreadcrumbs()
+    {
+        global $post;
+
+        $title = get_the_title();
+        $output = '';
+
+        echo '<ol class="breadcrumbs" itemscope itemtype="http://schema.org/BreadcrumbList">';
+
+        if (!is_front_page()) {
+            if (is_category() || is_single()) {
+                echo '<li>';
+                the_category('<li>');
+
+                if (is_single()) {
+                    echo '<li>';
+                    the_title();
+                    echo '</li>';
+                }
+            } elseif (is_page()) {
+                if ($post->post_parent) {
+                    $anc = get_post_ancestors($post->ID);
+                    $title = get_the_title();
+
+                    $int = 1;
+                    foreach ($anc as $ancestor) {
+                        if (get_post_status($ancestor) != 'private') {
+                            $output = '<li itemscope itemprop="itemListElement" itemtype="http://schema.org/ListItem">
+                                            <a itemprop="item" href="' . get_permalink($ancestor) . '" title="' . get_the_title($ancestor) . '">
+                                                <span itemprop="name">' . get_the_title($ancestor) . '</span>
+                                                <meta itemprop="position" content="' . $int . '" />
+                                            </a>
+                                       </li>' . $output;
+
+                            $int++;
+                        }
+                    }
+
+                    echo $output;
+                    echo '<li itemscope itemprop="itemListElement" itemtype="http://schema.org/ListItem">
+                            <span itemprop="name" class="breadcrumbs-current" title="' . $title . '">' . $title . '</span>
+                            <meta itemprop="position" content="' . ($int+1) . '" />
+                          </li>';
+                } else {
+                    echo '<li itemscope itemprop="itemListElement" itemtype="http://schema.org/ListItem">
+                            <span itemprop="name" class="breadcrumbs-current">' . get_the_title() . '</span>
+                            <meta itemprop="position" content="1" />
+                          </li>';
+                }
+            }
+        }
+        echo '</ol>';
+    }
 }
--- templates/partials/breadcrumbs.blade.php
+++ templates/partials/breadcrumbs.blade.php
@@ -1,9 +1,5 @@
 <div class="grid">
     <div class="grid-lg-12">
-        <ul class="breadcrumbs">
-            <li><a href="#">Parent node</a></li>
-            <li><a href="#">First child node</a></li>
-            <li>Current node</li>
-        </ul>
+        {{ \Municipio\Theme\Navigation::outputBreadcrumbs() }}
     </div>
 </div>
