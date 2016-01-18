6fe617e75cf9a4c9265bd66b62fdd6d6f975489d
Kristoffer Svanmark <kristoffer.svanmark@lexiconitkonsult.se>
2016-01-18 09:31:22 +0100

    Updated sidebar areas

--- library/Theme/Sidebars.php
+++ library/Theme/Sidebars.php
@@ -16,8 +16,8 @@ class Sidebars
          */
         register_sidebar(array(
             'id'            => 'footer-area',
-            'name'          => __('Footerarea', 'Helsingborg'),
-            'description'   => __('Arean längst ner på sidan', 'Helsingborg'),
+            'name'          => __('Footer', 'municipio'),
+            'description'   => __('The footer area', 'municipio'),
             'before_widget' => '<div class="left large-6 medium-6 print-6 columns"><div class="footer-content">',
             'after_widget'  => '</div></div>',
             'before_title'  => '<h2 class="footer-title">',
@@ -29,8 +29,8 @@ class Sidebars
          */
         register_sidebar(array(
             'id'            => 'slider-area',
-            'name'          => __('Topparea', 'Helsingborg'),
-            'description'   => __('Visas under huvudmenyn', 'Helsingborg'),
+            'name'          => __('Hero', 'municipio'),
+            'description'   => __('The hero area', 'municipio'),
             'before_widget' => '<div class="large-12 medium-12 small-12 print-12 columns widget">' .
                                '<div class="box %2$s">',
             'after_widget'  => '</div></div>',
@@ -43,8 +43,8 @@ class Sidebars
          */
         register_sidebar(array(
             'id'            => 'content-area',
-            'name'          => __('Innehållsarea', 'Helsingborg'),
-            'description'   => __('Visas strax under en artikels brödtext', 'Helsingborg'),
+            'name'          => __('Below content', 'municipio'),
+            'description'   => __('The area below the content', 'municipio'),
             'before_widget' => '<div class="box box-outlined widget %2$s">',
             'after_widget'  => '</div>'
         ));
@@ -54,32 +54,10 @@ class Sidebars
          */
         register_sidebar(array(
             'id'            => 'content-area-bottom',
-            'name'          => __('Innehåll bottenarea', 'Helsingborg'),
-            'description'   => __('Visas under vänstermeny och artikel (fullbredd) ', 'Helsingborg'),
+            'name'          => __('Below main container', 'municipio'),
+            'description'   => __('The area below the main container', 'municipio'),
             'before_widget' => '<div class="large-12 medium-12 small-12 print-12 columns widget">' .
                                '<div class="box box-outlined %2$s">',
-            'after_widget'  => '</div>'
-        ));
-
-        /**
-         * Service Area
-         */
-        register_sidebar(array(
-            'id'            => 'service-area',
-            'name'          => __('Servicearea', 'Helsingborg'),
-            'description'   => __('De service-länkar som visas i grått fält på startsidan', 'Helsingborg'),
-            'before_widget' => '<div class="widget columns large-4 medium-4 small-12 pprint-4">',
-            'after_widget'  => '</div>'
-        ));
-
-        /**
-         * Fun Facts Area
-         */
-        register_sidebar(array(
-            'id'            => 'fun-facts-area',
-            'name'          => __('Fakta', 'Helsingborg'),
-            'description'   => __('Faktarutor som visas innan footer (visar tre slumpmässiga).', 'Helsingborg'),
-            'before_widget' => '<div class="widget columns large-3 medium-3 print-3 left">',
             'after_widget'  => '</div>'
         ));
 
@@ -88,8 +66,8 @@ class Sidebars
          */
         register_sidebar(array(
             'id'            => 'left-sidebar',
-            'name'          => __('Vänster area', 'Helsingborg'),
-            'description'   => __('Visas ovanför vänstermenyn.', 'Helsingborg'),
+            'name'          => __('Left sidebar', 'municipio'),
+            'description'   => __('The left sidebar area', 'municipio'),
             'before_widget' => '<div class="large-12 medium-12 small-12 print-12 columns widget">' .
                                '<div class="box box-filled widget %2$s">',
             'after_widget'  => '</div></div>',
@@ -102,8 +80,8 @@ class Sidebars
          */
         register_sidebar(array(
             'id'            => 'left-sidebar-bottom',
-            'name'          => __('Vänster bottenarea', 'Helsingborg'),
-            'description'   => __('Visas under vänstermenyn.', 'Helsingborg'),
+            'name'          => __('Left sidebar bottom', 'municipio'),
+            'description'   => __('The area below the left sidebar content', 'municipio'),
             'before_widget' => '<div class="large-12 medium-12 small-12 print-12 columns widget">' .
                                '<div class="box box-filled widget %2$s">',
             'after_widget'  => '</div></div>',
@@ -116,8 +94,8 @@ class Sidebars
          */
         register_sidebar(array(
             'id'            => 'right-sidebar',
-            'name'          => __('Höger area', 'Helsingborg'),
-            'description'   => __('Visas i högerspalten.', 'Helsingborg'),
+            'name'          => __('Right sidebar', 'municipio'),
+            'description'   => __('The right sidebar area', 'municipio'),
             'before_widget' => '<div class="large-12 medium-12 small-12 print-12 columns widget">' .
                                '<div class="box box-filled widget %2$s">',
             'after_widget'  => '</div></div>',
