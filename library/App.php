ea6641a71fcace1b6bc24cd82bd58d562f1d8c68
Kristoffer Svanmark <kristoffer.svanmark@lexiconitkonsult.se>
2016-01-20 16:48:52 +0100

    Theme options with google search

--- library/App.php
+++ library/App.php
@@ -6,11 +6,27 @@ class App
 {
     public function __construct()
     {
+        /**
+         * Helpers
+         */
+        new \Municipio\Helper\Acf();
+
+        /**
+         * Template
+         */
         new \Municipio\Template();
 
+        /**
+         * Theme
+         */
         new \Municipio\Theme\Enqueue();
         new \Municipio\Theme\Support();
         new \Municipio\Theme\Sidebars();
         new \Municipio\Theme\Navigation();
+
+        /**
+         * Admin
+         */
+        new \Municipio\Admin\Options\Theme();
     }
 }
