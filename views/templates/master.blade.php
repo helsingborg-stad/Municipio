a6804f25d4a6cd03ce580ac783cbde8db935438c
Kristoffer Svanmark <kristoffer.svanmark@lexiconitkonsult.se>
2016-01-21 16:48:43 +0100

    Use blade instead of php

--- views/templates/master.blade.php
+++ views/templates/master.blade.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html class="no-js" <?php language_attributes(); ?>>
+<html class="no-js" {!! language_attributes() !!}>
 <head>
     <meta http-equiv="X-UA-Compatible" content="IE=EDGE">
 
@@ -10,20 +10,20 @@
 
     <meta name="description" content="">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name="pubdate" content="<?php echo the_time('d M Y'); ?>">
-    <meta name="moddate" content="<?php echo the_modified_time('d M Y'); ?>">
+    <meta name="pubdate" content="{{ the_time('d M Y') }}">
+    <meta name="moddate" content="{{ the_modified_time('d M Y') }}">
 
     <meta name="google-translate-customization" content="10edc883cb199c91-cbfc59690263b16d-gf15574b8983c6459-12">
 
-    <link rel="icon" href="<?php echo get_template_directory_uri(); ?>/assets/images/icons/favicon.ico" type="image/x-icon">
+    <link rel="icon" href="{!! get_template_directory_uri() !!}/assets/images/icons/favicon.ico" type="image/x-icon">
 
-    <link rel="apple-touch-icon-precomposed" sizes="144x144" href="<?php echo get_template_directory_uri(); ?>/assets/images/icons/apple-touch-icon-144x144-precomposed.png">
-    <link rel="apple-touch-icon-precomposed" sizes="114x114" href="<?php echo get_template_directory_uri(); ?>/assets/images/icons/apple-touch-icon-114x114-precomposed.png">
-    <link rel="apple-touch-icon-precomposed" sizes="72x72" href="<?php echo get_template_directory_uri(); ?>/assets/images/icons/apple-touch-icon-72x72-precomposed.png">
-    <link rel="apple-touch-icon-precomposed" href="<?php echo get_template_directory_uri(); ?>/assets/images/icons/apple-touch-icon-precomposed.png">
+    <link rel="apple-touch-icon-precomposed" sizes="144x144" href="{!! get_template_directory_uri() !!}/assets/images/icons/apple-touch-icon-144x144-precomposed.png">
+    <link rel="apple-touch-icon-precomposed" sizes="114x114" href="{!! get_template_directory_uri() !!}/assets/images/icons/apple-touch-icon-114x114-precomposed.png">
+    <link rel="apple-touch-icon-precomposed" sizes="72x72" href="{!! get_template_directory_uri() !!}/assets/images/icons/apple-touch-icon-72x72-precomposed.png">
+    <link rel="apple-touch-icon-precomposed" href="{!! get_template_directory_uri() !!}/assets/images/icons/apple-touch-icon-precomposed.png">
 
     <script>
-        var ajaxurl = '<?php echo admin_url('admin-ajax.php'); ?>';
+        var ajaxurl = '{!! admin_url('admin-ajax.php'); !!}';
     </script>
 
     <!--[if lt IE 9]>
@@ -39,9 +39,9 @@
     <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
     <![endif]-->
 
-    <?php wp_head(); ?>
+    {!! wp_head() !!}
 </head>
-<body <?php body_class(); ?>>
+<body {!! body_class() !!}>
     <div id="wrapper">
         @if (isset($notice) && !empty($notice))
             @include('views.partials.notice')
@@ -53,7 +53,8 @@
 
         @include('views.partials.footer')
      </div>
-    <?php wp_footer(); ?>
+
+    {!! wp_footer() !!}
 
 </body>
 </html>
