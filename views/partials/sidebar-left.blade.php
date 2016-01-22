c1ceddb9344bd2a6b3552e4193d6278f88554ad4
Kristoffer Svanmark <kristoffer.svanmark@lexiconitkonsult.se>
2016-01-22 11:49:21 +0100

    Small bugfix

--- views/partials/sidebar-left.blade.php
+++ views/partials/sidebar-left.blade.php
@@ -1,9 +1,10 @@
 <div class="grid-md-4 grid-lg-3">
     <?php
     global $post;
+    $childOf = isset(array_reverse(get_post_ancestors($post))[1]) ? array_reverse(get_post_ancestors($post))[1] : get_option('page_on_front');
     $menu = wp_list_pages(array(
         'title_li' => '',
-        'child_of' => array_reverse(get_post_ancestors($post))[1],
+        'child_of' => $childOf,
         'echo'     => 0,
         'walker'   => new \Municipio\Walker\Navigation(),
         'include'  => \Municipio\Helper\Navigation::getNavigationPages($post, 'csv')
