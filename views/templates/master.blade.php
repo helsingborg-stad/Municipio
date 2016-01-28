7b0beeaecb2fc7c1d2f0a93c6e01af973211d0d7
Kristoffer Svanmark <kristoffer.svanmark@lexiconitkonsult.se>
2016-01-28 10:44:13 +0100

    Use site name as <title>

--- views/templates/master.blade.php
+++ views/templates/master.blade.php
@@ -6,7 +6,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-    <title>Helsingborg stad</title>
+    <title>{{ get_bloginfo('name') }}</title>
 
     <meta name="description" content="">
     <meta name="viewport" content="width=device-width, initial-scale=1">
