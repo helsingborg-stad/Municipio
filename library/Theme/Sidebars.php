35bda2f872744086e7a2b49bcf943976dc79d2e7
Kristoffer Svanmark <kristoffer.svanmark@lexiconitkonsult.se>
2016-01-28 14:12:38 +0100

    Sidebars before and after widget markup

--- library/Theme/Sidebars.php
+++ library/Theme/Sidebars.php
@@ -18,8 +18,8 @@ class Sidebars
             'id'            => 'footer-area',
             'name'          => __('Footer', 'municipio'),
             'description'   => __('The footer area', 'municipio'),
-            'before_widget' => '<div class="left large-6 medium-6 print-6 columns"><div class="footer-content">',
-            'after_widget'  => '</div></div>',
+            'before_widget' => '<div class="grid-lg-4 %2$s">',
+            'after_widget'  => '</div>',
             'before_title'  => '<h2 class="footer-title">',
             'after_title'   => '</h2>'
         ));
@@ -31,7 +31,7 @@ class Sidebars
             'id'            => 'slider-area',
             'name'          => __('Hero', 'municipio'),
             'description'   => __('The hero area', 'municipio'),
-            'before_widget' => '<div class="widget">',
+            'before_widget' => '<div class="%2$s">',
             'after_widget'  => '</div>',
             'before_title'  => '<h3>',
             'after_title'   => '</h3>'
@@ -44,7 +44,7 @@ class Sidebars
             'id'            => 'content-area',
             'name'          => __('Content area', 'municipio'),
             'description'   => __('The area below the content', 'municipio'),
-            'before_widget' => '<div class="box box-outlined widget %2$s">',
+            'before_widget' => '<div class="grid-sm-12 %2$s">',
             'after_widget'  => '</div>'
         ));
 
@@ -55,8 +55,7 @@ class Sidebars
             'id'            => 'content-area-bottom',
             'name'          => __('Main container bottom', 'municipio'),
             'description'   => __('The area below the main container', 'municipio'),
-            'before_widget' => '<div class="large-12 medium-12 small-12 print-12 columns widget">' .
-                               '<div class="box box-outlined %2$s">',
+            'before_widget' => '<div class="grid-sm-12 grid-md-6 grid-lg-6 %2$s">',
             'after_widget'  => '</div>'
         ));
 
@@ -67,9 +66,8 @@ class Sidebars
             'id'            => 'left-sidebar',
             'name'          => __('Left sidebar', 'municipio'),
             'description'   => __('The left sidebar area', 'municipio'),
-            'before_widget' => '<div class="large-12 medium-12 small-12 print-12 columns widget">' .
-                               '<div class="box box-filled widget %2$s">',
-            'after_widget'  => '</div></div>',
+            'before_widget' => '<div class="grid-lg-12 %2$s">',
+            'after_widget'  => '</div>',
             'before_title'  => '<h2>',
             'after_title'   => '</h2>'
         ));
@@ -81,9 +79,8 @@ class Sidebars
             'id'            => 'left-sidebar-bottom',
             'name'          => __('Left sidebar bottom', 'municipio'),
             'description'   => __('The area below the left sidebar content', 'municipio'),
-            'before_widget' => '<div class="large-12 medium-12 small-12 print-12 columns widget">' .
-                               '<div class="box box-filled widget %2$s">',
-            'after_widget'  => '</div></div>',
+            'before_widget' => '<div class="grid-lg-12 %2$s">',
+            'after_widget'  => '</div>',
             'before_title'  => '<h2>',
             'after_title'   => '</h2>'
         ));
@@ -95,9 +92,8 @@ class Sidebars
             'id'            => 'right-sidebar',
             'name'          => __('Right sidebar', 'municipio'),
             'description'   => __('The right sidebar area', 'municipio'),
-            'before_widget' => '<div class="large-12 medium-12 small-12 print-12 columns widget">' .
-                               '<div class="box box-filled widget %2$s">',
-            'after_widget'  => '</div></div>',
+            'before_widget' => '<div class="grid-lg-12">',
+            'after_widget'  => '</div>',
             'before_title'  => '<h2>',
             'after_title'   => '</h2>'
         ));
