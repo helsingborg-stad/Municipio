1a7f52ea2b6c9801a6e5718fb9c140b3f497d07f
Sebastian Thulin <sebastian.thulin@gmail.com>
2016-02-01 13:49:35 +0100

    Added the ability to add footer icons. Print markup and saved json.

--- views/partials/footer.blade.php
+++ views/partials/footer.blade.php
@@ -1,10 +1,14 @@
 <footer class="main-footer">
     <div class="container">
+
+        <!-- Logotype -->
         <div class="grid">
             <div class="grid-lg-12">
-                <a href="/" class="logotype"><img src="http://www.helsingborg.se/wp-content/themes/This-is-Helsingborg/assets/images/helsingborg-neg.svg" alt="Helsingborg Stad" width="239" height="68"></a>
+                {!! municipio_get_logotype(get_field('footer_logotype', 'option')) !!}
             </div>
         </div>
+
+        <!-- Widgets -->
         <div class="grid">
             <div class="grid-lg-4">
                 <ul>
@@ -23,5 +27,25 @@
                 </ul>
             </div>
         </div>
+
+        <!-- Footer links -->
+        @if(function_exists('have_rows'))
+            @if(have_rows('footer_icons_repeater', 'option'))
+                <div class="grid">
+                    <div class="grid-xs-12">
+                        <ul class="icons-list text-center gutter-margin">
+                            @foreach(get_field('footer_icons_repeater', 'option') as $link)
+                                <li>
+                                    <a href="{{ $link['link_url'] }}" target="_blank" class="link-item-light">
+                                        {!! $link['link_icon'] !!}
+                                    </a>
+                                </li>
+                            @endforeach
+                        </ul>
+                    </div>
+                </div>
+            @endif
+        @endif
+
     </div>
 </footer>
