a993672cafa7fd0c3a5bd626b9339b7f49e0bfc0
Sebastian Thulin <sebastian.thulin@gmail.com>
2016-02-01 16:55:51 +0100

    Added footer -area.

--- views/partials/footer.blade.php
+++ views/partials/footer.blade.php
@@ -10,22 +10,7 @@
 
         <!-- Widgets -->
         <div class="grid">
-            <div class="grid-lg-4">
-                <ul>
-                    <li><strong>Telefonnummer</strong></li>
-                    <li>Helsingborg kontaktcenter: 042-10 50 00</li>
-                </ul>
-                <ul>
-                    <li><strong>E-post</strong></li>
-                    <li><a href="mailto:kontaktcenter@helsingborg.se" class="link-item link-item-light">kontaktcenter@helsingborg.se</a></li>
-                </ul>
-            </div>
-            <div class="grid-lg-4">
-                <ul>
-                    <li><strong>Postadress</strong></li>
-                    <li>Namn på verksamheten<br>251 89 Helsingborg</li>
-                </ul>
-            </div>
+            {!! dynamic_sidebar('footer-area'); !!}
         </div>
 
         <!-- Footer links -->
