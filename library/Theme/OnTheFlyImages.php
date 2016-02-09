dba6cb21a9c9df2582bc70051e5981e6a354444f
Sebastian Thulin <sebastian.thulin@gmail.com>
2016-02-09 14:58:48 +0100

    New upscale function

--- library/Theme/OnTheFlyImages.php
+++ library/Theme/OnTheFlyImages.php
@@ -84,20 +84,35 @@ class OnTheFlyImages
      * @return Array                  Array with new dimension
      */
 
-    public function upscaleThumbnail($orig_w, $orig_h, $new_w, $new_h, $crop)
+    public function upscaleThumbnail($default, $orig_w, $orig_h, $new_w, $new_h, $crop)
     {
-        if (!$crop) {
-            return null;
-        }
+        if(!$crop)
+            return null; // let the wordpress default function handle this
 
-        $aspect_ratio = $orig_w / $orig_h;
         $size_ratio = max($new_w / $orig_w, $new_h / $orig_h);
 
         $crop_w = round($new_w / $size_ratio);
         $crop_h = round($new_h / $size_ratio);
 
-        $s_x = floor(($orig_w - $crop_w) / 2);
-        $s_y = floor(($orig_h - $crop_h) / 2);
+        $s_x = floor( ($orig_w - $crop_w) / 2 );
+        $s_y = floor( ($orig_h - $crop_h) / 2 );
+
+        if(is_array($crop)) {
+
+            //Handles left, right and center (no change)
+            if($crop[ 0 ] === 'left') {
+                $s_x = 0;
+            } else if($crop[ 0 ] === 'right') {
+                $s_x = $orig_w - $crop_w;
+            }
+
+            //Handles top, bottom and center (no change)
+            if($crop[ 1 ] === 'top') {
+                $s_y = 0;
+            } else if($crop[ 1 ] === 'bottom') {
+                $s_y = $orig_h - $crop_h;
+            }
+        }
 
         return array( 0, 0, (int) $s_x, (int) $s_y, (int) $new_w, (int) $new_h, (int) $crop_w, (int) $crop_h );
     }
