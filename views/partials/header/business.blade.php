22c8fdc49ada411e9593f995372c691666d81aef
Kristoffer Svanmark <kristoffer.svanmark@lexiconitkonsult.se>
2016-02-10 16:36:41 +0100

    Mobile menu fixes

--- views/partials/header/business.blade.php
+++ views/partials/header/business.blade.php
@@ -3,7 +3,7 @@
         <div class="grid">
             <div class="grid-md-6 text-center-xs text-center-sm">
                 {!! municipio_get_logotype(get_field('header_logotype', 'option'), get_field('logotype_tooltip', 'option')) !!}
-                <a href="#mobile-menu" class="hidden-md hidden-lg menu-trigger" data-target="#mobile-menu"><span class="menu-icon"></span></a>
+                <a href="#mobile-menu" class="hidden-md hidden-lg menu-trigger" data-target="#mobile-menu"><span class="menu-icon"></span> Meny</a>
             </div>
             <div class="grid-md-6 text-center-sm text-center-xs text-right">
                 <nav>
