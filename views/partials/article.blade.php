e0204aef142487925ec748e321ea42559d7ba8fc
Kristoffer Svanmark <kristoffer.svanmark@lexiconitkonsult.se>
2016-02-10 10:17:16 +0100

    Article template update

--- views/partials/article.blade.php
+++ views/partials/article.blade.php
@@ -2,7 +2,7 @@
 <article>
     <h1>{{ the_title() }}</h1>
 
-    @if (isset(get_extended($post->post_content)['main']) && strlen(get_extended($post->post_content)['main']) > 0)
+    @if (isset(get_extended($post->post_content)['main']) && strlen(get_extended($post->post_content)['main']) > 0 && isset(get_extended($post->post_content)['extended']) && strlen(get_extended($post->post_content)['extended']) > 0)
 
         {!! apply_filters('the_lead', get_extended($post->post_content)['main']) !!}
         {!! apply_filters('the_content', get_extended($post->post_content)['extended']) !!}
