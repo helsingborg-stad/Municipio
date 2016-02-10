a3f2ee3ff853684ada5b4c62fd895a28dcd5fef3
Kristoffer Svanmark <kristoffer.svanmark@lexiconitkonsult.se>
2016-02-10 16:47:13 +0100

    Casual header updated with new mobile menu markup

--- views/partials/header/casual.blade.php
+++ views/partials/header/casual.blade.php
@@ -24,29 +24,30 @@
                     !!}
                 </div>
                 <div class="grid-xs-2 hidden-md hidden-lg text-right">
-                    <nav>
-                        <a href="#menu-open" id="menu-open" class=" hidden-md hidden-lg menu-trigger"><span class="menu-icon"></span></a>
-                        {!!
-                            wp_nav_menu(array(
-                                'theme_location' => 'main-menu',
-                                'container' => false,
-                                'container_class' => 'menu-{menu-slug}-container',
-                                'container_id' => '',
-                                'menu_class' => 'nav-mobile hidden',
-                                'menu_id' => 'mobile-menu',
-                                'echo' => false,
-                                'before' => '',
-                                'after' => '',
-                                'link_before' => '',
-                                'link_after' => '',
-                                'items_wrap' => '<ul id="%1$s" class="%2$s">%3$s</ul>',
-                                'depth' => -1,
-                            ));
-                        !!}
-                    </nav>
+                    <a href="#mobile-menu" class="hidden-md hidden-lg menu-trigger" data-target="#mobile-menu"><span class="menu-icon"></span></a>
                 </div>
             </div>
         </div>
+    </nav>
+
+    <nav id="mobile-menu" class="nav-mobile-menu nav-toggle hidden-md hidden-lg">
+        {!!
+            wp_nav_menu(array(
+                'theme_location' => 'main-menu',
+                'container' => false,
+                'container_class' => 'menu-{menu-slug}-container',
+                'container_id' => '',
+                'menu_class' => 'nav-mobile',
+                'menu_id' => '',
+                'echo' => false,
+                'before' => '',
+                'after' => '',
+                'link_before' => '',
+                'link_after' => '',
+                'items_wrap' => '<ul id="%1$s" class="%2$s">%3$s</ul>',
+                'depth' => -1,
+            ));
+        !!}
     </nav>
 </header>
 
