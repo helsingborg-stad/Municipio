fe0175d7e60d01e04af7351b164c49165b75ea00
Kristoffer Svanmark <kristoffer.svanmark@lexiconitkonsult.se>
2016-02-11 13:49:44 +0100

    Change old browser notice to lt IE 9

--- views/templates/master.blade.php
+++ views/templates/master.blade.php
@@ -42,7 +42,7 @@
     {!! wp_head() !!}
 </head>
 <body {!! body_class() !!}>
-    <!--[if lt IE 8]>
+    <!--[if lt IE 9]>
         <div class="notice info browserupgrade">
             <div class="container"><div class="grid-table grid-va-middle">
                 <div class="grid-col-icon">
