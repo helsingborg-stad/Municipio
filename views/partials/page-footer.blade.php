2983b770e8427e0d895787494da8b78f27fc3fd9
Kristoffer Svanmark <kristoffer.svanmark@lexiconitkonsult.se>
2016-02-17 09:48:44 +0100

    Share links master switch

--- views/partials/page-footer.blade.php
+++ views/partials/page-footer.blade.php
@@ -28,7 +28,7 @@
     </div>
 
     <div class="grid-md-6 text-right">
-        @if (get_field('show_share', get_the_id()) != 'false')
+        @if (get_field('show_share', get_the_id()) != 'false' && get_field('show_share_master', 'option') != 'false')
             @include('partials.social-share')
         @endif
     </div>
