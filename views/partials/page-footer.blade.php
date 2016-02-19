315097f640ab684fdaaa9a83ea734dcb547ceed9
Kristoffer Svanmark <kristoffer.svanmark@lexiconitkonsult.se>
2016-02-19 10:40:36 +0100

    Put timestamps in its own template file

--- views/partials/page-footer.blade.php
+++ views/partials/page-footer.blade.php
@@ -1,30 +1,6 @@
 <footer class="grid">
     <div class="grid-md-6">
-        @if (get_the_modified_time() != get_the_time())
-        <ul class="article-timestamps">
-            <li>
-                <strong>Publicerad:</strong>
-                <time datetime="<?php echo the_time('Y-m-d H:i'); ?>">
-                    <?php the_time('j F Y'); ?> kl. <?php the_time('H:i'); ?>
-                </time>
-            </li>
-            <li>
-                <strong>Senast ändrad:</strong>
-                <time datetime="<?php echo the_modified_time('Y-m-d H:i'); ?>">
-                    <?php the_modified_time('j F Y'); ?> kl. <?php the_modified_time('H:i'); ?>
-                </time>
-            </li>
-        </ul>
-        @else
-        <ul class="article-timestamps">
-            <li>
-                <strong>Publicerad:</strong>
-                <time datetime="<?php echo the_time('Y-m-d H:i'); ?>">
-                    <?php the_time('j F Y'); ?> kl <?php the_time('H:i'); ?>
-                </time>
-            </li>
-        </ul>
-        @endif
+        @include('partials.timestamps')
     </div>
 
     <div class="grid-md-6 text-right">
