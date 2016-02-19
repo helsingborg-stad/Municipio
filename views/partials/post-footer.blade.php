d84e4d1b748b440c4fcb3d1f5e9ee9b86678961f
Kristoffer Svanmark <kristoffer.svanmark@lexiconitkonsult.se>
2016-02-19 11:18:34 +0100

    Adds comments links to blog post

--- views/partials/post-footer.blade.php
+++ views/partials/post-footer.blade.php
@@ -1,11 +1,15 @@
-<footer class="grid-table grid-va-middle">
-    <div class="grid-md-6">
+<footer class="page-footer grid-table grid-va-middle">
+    <div class="grid-md-5">
         @include('partials.timestamps')
     </div>
 
-    <div class="grid-md-6 text-right">
-        @if (get_field('show_share', get_the_id()) != 'false' && get_field('show_share_master', 'option') != 'false')
-            @include('partials.social-share')
-        @endif
+    <div class="grid-md-5 {{ get_field('show_share', get_the_id()) == 'false' ? 'text-right' : '' }}">
+        <a href="{{ comments_link() }}">Kommentarer ({{ comments_number('0', '1', '%') }})</a>
     </div>
+
+    @if (get_field('show_share', get_the_id()) != 'false' && get_field('show_share_master', 'option') != 'false')
+    <div class="grid-md-2 text-right">
+        @include('partials.social-share')
+    </div>
+    @endif
 </footer>
