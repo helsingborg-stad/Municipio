43842b04cc51423383efc25a4fac71042b930b62
Kristoffer Svanmark <kristoffer.svanmark@lexiconitkonsult.se>
2016-02-22 13:59:34 +0100

    Hide hero if no image is set

--- views/partials/hero.blade.php
+++ views/partials/hero.blade.php
@@ -1,12 +1,11 @@
+@if (is_active_sidebar('slider-area'))
 <div class="hero hidden-xs hidden-sm">
-    @if (is_active_sidebar('slider-area'))
-        {{ dynamic_sidebar('slider-area') }}
-    @endif
+    {{ dynamic_sidebar('slider-area') }}
 
     @include('partials.stripe')
 
     @if (is_front_page() && get_field('front_page_hero_search', 'option') === true)
         {{ get_search_form() }}
     @endif
-
 </div>
+@endif
