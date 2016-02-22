b17303683e29fc545cf0239ef5a532070d85f577
Sebastian Thulin <sebastian.thulin@gmail.com>
2016-02-22 10:30:44 +0100

    Updated with the posibility to add / remove posts timestamp from admin panel.

--- views/partials/timestamps.blade.php
+++ views/partials/timestamps.blade.php
@@ -1,25 +1,40 @@
 @if (get_the_modified_time() != get_the_time())
-<ul class="article-timestamps">
-    <li>
-        <strong>Publicerad:</strong>
-        <time datetime="<?php echo the_time('Y-m-d H:i'); ?>">
-            <?php the_time('j F Y'); ?> kl. <?php the_time('H:i'); ?>
-        </time>
-    </li>
-    <li>
-        <strong>Senast ändrad:</strong>
-        <time datetime="<?php echo the_modified_time('Y-m-d H:i'); ?>">
-            <?php the_modified_time('j F Y'); ?> kl. <?php the_modified_time('H:i'); ?>
-        </time>
-    </li>
-</ul>
+
+    @if( !in_array(get_post_type(get_the_id()),get_field('show_date_updated','option')) && !in_array(get_post_type(get_the_id()),get_field('show_date_published','option')))
+        <ul class="article-timestamps">
+
+            @if(in_array(get_post_type(get_the_id()),get_field('show_date_published','option')))
+            <li>
+                <strong>Publicerad:</strong>
+                <time datetime="<?php echo the_time('Y-m-d H:i'); ?>">
+                    <?php the_time('j F Y'); ?> kl. <?php the_time('H:i'); ?>
+                </time>
+            </li>
+            @endif
+
+            @if(in_array(get_post_type(get_the_id()),get_field('show_date_updated','option')))
+            <li>
+                <strong>Senast ändrad:</strong>
+                <time datetime="<?php echo the_modified_time('Y-m-d H:i'); ?>">
+                    <?php the_modified_time('j F Y'); ?> kl. <?php the_modified_time('H:i'); ?>
+                </time>
+            </li>
+            @endif
+
+        </ul>
+    @endif
+
 @else
-<ul class="article-timestamps">
-    <li>
-        <strong>Publicerad:</strong>
-        <time datetime="<?php echo the_time('Y-m-d H:i'); ?>">
-            <?php the_time('j F Y'); ?> kl <?php the_time('H:i'); ?>
-        </time>
-    </li>
-</ul>
+
+    @if(in_array(get_post_type(get_the_id()),get_field('show_date_published','option')))
+        <ul class="article-timestamps">
+            <li>
+                <strong>Publicerad:</strong>
+                <time datetime="<?php echo the_time('Y-m-d H:i'); ?>">
+                    <?php the_time('j F Y'); ?> kl <?php the_time('H:i'); ?>
+                </time>
+            </li>
+        </ul>
+    @endif
+
 @endif
