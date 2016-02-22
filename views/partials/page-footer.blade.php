2181b8cecc5d62b78a919d59e3c3eee382d8cbd4
Kristoffer Svanmark <kristoffer.svanmark@lexiconitkonsult.se>
2016-02-22 11:25:24 +0100

    Page display settings

--- views/partials/page-footer.blade.php
+++ views/partials/page-footer.blade.php
@@ -4,7 +4,7 @@
     </div>
 
     <div class="grid-md-6 text-right">
-        @if (get_field('show_share', get_the_id()) != 'false' && get_field('show_share_master', 'option') != 'false')
+        @if (get_field('show_share', get_the_id()) !== false && get_field('show_share_master', 'option') !== false)
             @include('partials.social-share')
         @endif
     </div>
