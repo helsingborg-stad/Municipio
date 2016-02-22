b4d392c3798217498c636bf1293c5c3dedb1949b
Kristoffer Svanmark <kristoffer.svanmark@lexiconitkonsult.se>
2016-02-22 10:29:21 +0100

    Add id to comments

--- views/partials/blog/comments.blade.php
+++ views/partials/blog/comments.blade.php
@@ -1,4 +1,4 @@
-<ul class="comments">
+<ul id="comments" class="comments">
     @foreach($comments as $comment)
         <li class="comment">
             <header><em>{{ $comment->comment_author }}</em> <time data-tooltip="{{ date('Y-m-d \k\l\. H:i', strtotime($comment->comment_date)) }}" data-tooltip-right datetime="{{ $comment->comment_date_gmt }}">{{ municipio_human_datediff($comment->comment_date) }} sedan</time></header>
