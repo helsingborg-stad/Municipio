6586307e98ef3d78556ff5cd0012656ccebc286f
Sebastian Thulin <sebastian.thulin@gmail.com>
2016-02-25 11:45:33 +0100

    Do not show breadcrumbs on first level sub pages.

--- views/partials/breadcrumbs.blade.php
+++ views/partials/breadcrumbs.blade.php
@@ -1,5 +1,7 @@
-<div class="grid">
+@if (wp_get_post_parent_id(get_the_id()) != 0)
+<div class="grid" id="breadcrumb-wrapper">
     <div class="grid-lg-12">
         {{ \Municipio\Theme\Navigation::outputBreadcrumbs() }}
     </div>
 </div>
+@endif
