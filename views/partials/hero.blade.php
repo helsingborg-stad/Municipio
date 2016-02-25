920f8885cb94d86650de815492a61c916a674d94
Sebastian Thulin <sebastian.thulin@gmail.com>
2016-02-25 14:43:22 +0100

    Trying to get rid of false-positive.

--- views/partials/hero.blade.php
+++ views/partials/hero.blade.php
@@ -1,11 +1,11 @@
-@if (is_active_sidebar('slider-area'))
-<div class="hero hidden-xs hidden-sm">
-    {{ dynamic_sidebar('slider-area') }}
+@if (is_active_sidebar('slider-area') === true )
+    <div class="hero hidden-xs hidden-sm">
+        {{ dynamic_sidebar('slider-area') }}
 
-    @include('partials.stripe')
+        @include('partials.stripe')
 
-    @if (is_front_page() && get_field('front_page_hero_search', 'option') === true)
-        {{ get_search_form() }}
-    @endif
-</div>
+        @if (is_front_page() && get_field('front_page_hero_search', 'option') === true)
+            {{ get_search_form() }}
+        @endif
+    </div>
 @endif
