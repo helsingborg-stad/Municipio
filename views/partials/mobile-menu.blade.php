7c14234bfd44632ac63980e72b855e66b54fcc79
Kristoffer Svanmark <kristoffer.svanmark@lexiconitkonsult.se>
2016-03-02 13:37:19 +0100

    Mobile search form

--- views/partials/mobile-menu.blade.php
+++ views/partials/mobile-menu.blade.php
@@ -1,4 +1,13 @@
-{!! get_search_form() !!}
+<form class="search" method="get" action="/">
+    <label for="searchkeyword-mobile" class="sr-only">{{ get_field('search_label_text', 'option') ? get_field('search_label_text', 'option') : 'Search' }}</label>
+
+    <div class="input-group">
+        <input id="searchkeyword-mobile" autocomplete="off" class="form-control" type="search" name="s" placeholder="{{ get_field('search_placeholder_text', 'option') ? get_field('search_placeholder_text', 'option') : 'What are you looking for?' }}" value="<?php echo (isset($_GET['s']) && strlen($_GET['s']) > 0) ? urldecode(stripslashes($_GET['s'])) : ''; ?>">
+        <span class="input-group-addon-btn">
+            <input type="submit" class="btn btn-primary" value="{{ get_field('search_button_text', 'option') ? get_field('search_button_text', 'option') : 'Search' }}">
+        </span>
+    </div>
+</form>
 
 <ul class="nav-mobile">
     <?php
