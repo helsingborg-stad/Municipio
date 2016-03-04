9420dd881b956cdd62cafba996ab5f63d9ebc412
Kristoffer Svanmark <kristoffer.svanmark@lexiconitkonsult.se>
2016-03-04 11:13:18 +0100

    Main to div since the main element is in the master template

--- views/page.blade.php
+++ views/page.blade.php
@@ -9,7 +9,7 @@
     <div class="grid {{ (wp_get_post_parent_id(get_the_id()) != 0) ? 'no-margin-top' : '' }}">
         @include('partials.sidebar-left')
 
-        <main class="grid-md-8 grid-lg-6" id="readspeaker-read">
+        <div class="grid-md-8 grid-lg-6" id="readspeaker-read">
             @while(have_posts())
                 {!! the_post() !!}
 
@@ -23,7 +23,7 @@
             @endif
 
             @include('partials.page-footer')
-        </main>
+        </div>
 
         @include('partials.sidebar-right')
     </div>
