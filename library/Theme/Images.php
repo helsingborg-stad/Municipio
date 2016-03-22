ba18fab80f020b440cee1d215252b248df19934b
Sebastian Thulin <sebastian.thulin@gmail.com>
2016-03-22 13:02:14 +0100

    Custom image size in hero area.
    1800 x 350 px

---
+++ library/Theme/Images.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Municipio\Theme;
+
+class ImageSizeFilter
+{
+    public function __construct()
+    {
+        add_filter('modularity/image/slider', array($this, 'filterHeroImageSize'), 100, 2);
+    }
+
+    public function filterHeroImageSize($orginal_size, $args)
+    {
+
+        //If slider is shown in top area
+        if ($args['id'] == "sidebar-slider-area") {
+            return array(1800,350);
+        }
+
+        //Default value
+        return $orginal_size;
+    }
+}
