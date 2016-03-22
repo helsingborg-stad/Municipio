3bde778ca1ddbc522421034db7cd77389579877e
Sebastian Thulin <sebastian.thulin@gmail.com>
2016-03-22 14:18:57 +0100

    Added ability to remove image size filter

--- library/Theme/ImageSizeFilter.php
+++ library/Theme/ImageSizeFilter.php
@@ -9,7 +9,7 @@ class ImageSizeFilter
         add_filter('modularity/image/slider', array($this, 'filterHeroImageSize'), 100, 2);
     }
 
-    public static function filterHeroImageSize($orginal_size, $args)
+    public function filterHeroImageSize($orginal_size, $args)
     {
 
         //If slider is shown in top area
@@ -19,5 +19,28 @@ class ImageSizeFilter
 
         //Default value
         return $orginal_size;
+    }
+
+    public function removeFilter($hook_name = '', $method_name = '', $priority = 0)
+    {
+        global $wp_filter;
+
+        // Take only filters on right hook name and priority
+        if (!isset($wp_filter[$hook_name][$priority]) || !is_array($wp_filter[$hook_name][$priority])) {
+            return false;
+        }
+
+        // Loop on filters registered
+        foreach ((array) $wp_filter[$hook_name][$priority] as $unique_id => $filter_array) {
+            // Test if filter is an array ! (always for class/method)
+            if (isset($filter_array['function']) && is_array($filter_array['function'])) {
+                // Test if object is a class and method is equal to param !
+                if (is_object($filter_array['function'][0]) && get_class($filter_array['function'][0]) && $filter_array['function'][1] == $method_name) {
+                    unset($wp_filter[$hook_name][$priority][$unique_id]);
+                }
+            }
+        }
+
+        return false;
     }
 }
