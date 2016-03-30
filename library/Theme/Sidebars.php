1c803ba2d152b477d067327c26b5e39b7e27a654
Kristoffer Svanmark <kristoffer.svanmark@lexiconitkonsult.se>
2016-03-30 16:01:58 +0200

    Module classes

--- library/Theme/Sidebars.php
+++ library/Theme/Sidebars.php
@@ -7,6 +7,35 @@ class Sidebars
     public function __construct()
     {
         add_action('widgets_init', array($this, 'register'));
+        add_filter('Modularity/Module/Classes', array($this, 'moduleClasses'), 10, 3);
+    }
+
+    /**
+     * Modify module classes in different areas
+     * @param  array $classes      Default classes
+     * @param  string $moduleType  Module type
+     * @param  array $sidebarArgs  Sidebar arguments
+     * @return array               Modified list of classes
+     */
+    public function moduleClasses($classes, $moduleType, $sidebarArgs)
+    {
+        // Box panel in content area and content area bottom
+        if (in_array($sidebarArgs['id'], array('content-area', 'content-area-bottom')) && in_array('box-panel', $classes)) {
+            $classes[] = 'box-panel-secondary';
+        }
+
+        // Inline box panels
+        if (is_numeric($sidebarArgs['id']) && in_array('box-panel', $classes)) {
+            $classes[] = 'box-panel-secondary';
+        }
+
+        // Sidebar boxes (should be filled)
+        if (in_array($sidebarArgs['id'], array('left-sidebar-bottom', 'left-sidebar', 'right-sidebar')) && in_array('box-panel', $classes)) {
+            unset($classes[array_search('box-panel', $classes)]);
+            $classes[] = 'box-filled';
+        }
+
+        return $classes;
     }
 
     public function register()
@@ -18,8 +47,8 @@ class Sidebars
             'id'            => 'footer-area',
             'name'          => __('Footer', 'municipio'),
             'description'   => __('The footer area', 'municipio'),
-            'before_widget' => '<div class="grid-lg-4 %2$s">',
-            'after_widget'  => '</div>',
+            'before_widget' => '<div class="grid-lg-4"><div class="%2$s">',
+            'after_widget'  => '</div></div>',
             'before_title'  => '<h2 class="footer-title">',
             'after_title'   => '</h2>'
         ));
@@ -55,8 +84,8 @@ class Sidebars
             'id'            => 'content-area-bottom',
             'name'          => __('Main container bottom', 'municipio'),
             'description'   => __('The area below the main container', 'municipio'),
-            'before_widget' => '<div class="grid-sm-12 grid-md-6 grid-lg-6 %2$s">',
-            'after_widget'  => '</div>'
+            'before_widget' => '<div class="grid-sm-12 grid-md-6 grid-lg-6"><div class="%2$s">',
+            'after_widget'  => '</div></div>'
         ));
 
         /**
@@ -66,8 +95,8 @@ class Sidebars
             'id'            => 'left-sidebar',
             'name'          => __('Left sidebar', 'municipio'),
             'description'   => __('The left sidebar area', 'municipio'),
-            'before_widget' => '<div class="grid-lg-12 %2$s">',
-            'after_widget'  => '</div>',
+            'before_widget' => '<div class="grid-lg-12"><div class="%2$s">',
+            'after_widget'  => '</div></div>',
             'before_title'  => '<h2>',
             'after_title'   => '</h2>'
         ));
@@ -79,8 +108,8 @@ class Sidebars
             'id'            => 'left-sidebar-bottom',
             'name'          => __('Left sidebar bottom', 'municipio'),
             'description'   => __('The area below the left sidebar content', 'municipio'),
-            'before_widget' => '<div class="grid-lg-12 %2$s">',
-            'after_widget'  => '</div>',
+            'before_widget' => '<div class="grid-lg-12"><div class="%2$s">',
+            'after_widget'  => '</div></div>',
             'before_title'  => '<h2>',
             'after_title'   => '</h2>'
         ));
@@ -92,8 +121,8 @@ class Sidebars
             'id'            => 'right-sidebar',
             'name'          => __('Right sidebar', 'municipio'),
             'description'   => __('The right sidebar area', 'municipio'),
-            'before_widget' => '<div class="grid-lg-12">',
-            'after_widget'  => '</div>',
+            'before_widget' => '<div class="grid-lg-12"><div class="%2$s">',
+            'after_widget'  => '</div></div>',
             'before_title'  => '<h2>',
             'after_title'   => '</h2>'
         ));
