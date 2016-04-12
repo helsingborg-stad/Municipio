6709260c0ed27022f461a525def73b78f0097e5c
Kristoffer Svanmark <kristoffer.svanmark@lexiconitkonsult.se>
2016-04-12 10:02:25 +0200

    Add uploads support for ms office filetypes

--- library/Theme/FileUploads.php
+++ library/Theme/FileUploads.php
@@ -38,6 +38,34 @@ class FileUploads
         $mimes['mp4'] = 'video/mp4';
         $mimes['ogg'] = 'video/ogg';
 
+        // MS Office
+        $mimes['doc'] = 'application/msword';
+        $mimes['pot|pps|ppt'] = 'application/vnd.ms-powerpoint';
+        $mimes['wri'] = 'application/vnd.ms-write';
+        $mimes['xla|xls|xlt|xlw'] = 'application/vnd.ms-excel';
+        $mimes['mdb'] = 'application/vnd.ms-access';
+        $mimes['mpp'] = 'application/vnd.ms-project';
+        $mimes['docx'] = 'application/vnd.openxmlformats-officedocument.wordprocessingml.document';
+        $mimes['docm'] = 'application/vnd.ms-word.document.macroEnabled.12';
+        $mimes['dotx'] = 'application/vnd.openxmlformats-officedocument.wordprocessingml.template';
+        $mimes['dotm'] = 'application/vnd.ms-word.template.macroEnabled.12';
+        $mimes['xlsx'] = 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet';
+        $mimes['xlsm'] = 'application/vnd.ms-excel.sheet.macroEnabled.12';
+        $mimes['xlsb'] = 'application/vnd.ms-excel.sheet.binary.macroEnabled.12';
+        $mimes['xltx'] = 'application/vnd.openxmlformats-officedocument.spreadsheetml.template';
+        $mimes['xltm'] = 'application/vnd.ms-excel.template.macroEnabled.12';
+        $mimes['xlam'] = 'application/vnd.ms-excel.addin.macroEnabled.12';
+        $mimes['pptx'] = 'application/vnd.openxmlformats-officedocument.presentationml.presentation';
+        $mimes['pptm'] = 'application/vnd.ms-powerpoint.presentation.macroEnabled.12';
+        $mimes['ppsx'] = 'application/vnd.openxmlformats-officedocument.presentationml.slideshow';
+        $mimes['ppsm'] = 'application/vnd.ms-powerpoint.slideshow.macroEnabled.12';
+        $mimes['potx'] = 'application/vnd.openxmlformats-officedocument.presentationml.template';
+        $mimes['potm'] = 'application/vnd.ms-powerpoint.template.macroEnabled.12';
+        $mimes['ppam'] = 'application/vnd.ms-powerpoint.addin.macroEnabled.12';
+        $mimes['sldx'] = 'application/vnd.openxmlformats-officedocument.presentationml.slide';
+        $mimes['sldm'] = 'application/vnd.ms-powerpoint.slide.macroEnabled.12';
+        $mimes['onetoc|onetoc2|onetmp|onepkg'] = 'application/onenote';
+
         return $mimes;
     }
 }
