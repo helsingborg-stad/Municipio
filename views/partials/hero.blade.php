3f2a602077b5ffb532e07fa41f6f95636fa14f9f
Kristoffer Svanmark <kristoffer.svanmark@lexiconitkonsult.se>
2016-04-14 11:27:27 +0200

    Do not hide hero in smaller screens

--- views/partials/hero.blade.php
+++ views/partials/hero.blade.php
@@ -1,5 +1,5 @@
 @if (is_active_sidebar('slider-area') === true )
-    <div class="hero has-stripe hidden-xs hidden-sm sidebar-slider-area">
+    <div class="hero has-stripe sidebar-slider-area">
         <div class="grid no-gutter">
             <?php dynamic_sidebar('slider-area'); ?>
         </div>
