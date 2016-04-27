8d3a5ee1083db975eb5b6a20c885bc88f926f8c8
Kristoffer Svanmark <kristoffer.svanmark@lexiconitkonsult.se>
2016-04-27 13:35:08 +0200

    Adds print-button to accessability menu

--- views/partials/accessibility-menu.blade.php
+++ views/partials/accessibility-menu.blade.php
@@ -1,10 +1,13 @@
 @if (function_exists('ReadSpeakerHelper_playButton'))
-<ul class="nav nav-pills gutter gutter-bottom">
+<ul class="nav nav-pills nav-horizontal gutter gutter-bottom">
     @if (function_exists('ReadSpeakerHelper_playButton'))
     <li>
         {!! ReadSpeakerHelper_playButton() !!}
     </li>
     @endif
+    <li>
+        <a href="#" onclick="window.print();return false;" class=""><i class="fa fa-print"></i> Skriv ut</a>
+    </li>
 </ul>
 @endif
 
