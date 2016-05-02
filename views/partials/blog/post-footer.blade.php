70b109aa70ca78d082faf0b0f0b97eb4a4c7bef0
Kristoffer Svanmark <kristoffer.svanmark@lexiconitkonsult.se>
2016-05-02 10:18:42 +0200

    Grid correction for post footer

--- views/partials/blog/post-footer.blade.php
+++ views/partials/blog/post-footer.blade.php
@@ -1,5 +1,5 @@
 <footer class="post-footer grid-table grid-va-top">
-    <div class="grid-md-6">
+    <div class="grid-md-8">
         @if (in_array('tags', (array)get_field('archive_' . sanitize_title(get_post_type()) . '_post_display_info', 'option')))
             @include('partials.blog.post-tags')
         @endif
@@ -10,7 +10,7 @@
     </div>
 
     @if (get_field('post_show_share', get_the_id()) !== false && get_field('page_show_share', 'option') !== false)
-    <div class="grid-md-2 text-right">
+    <div class="grid-md-4 text-right">
         @include('partials.social-share')
     </div>
     @endif
