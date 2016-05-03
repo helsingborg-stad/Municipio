2baa5fe348965647a3a96c73da5b9fa6aeba9df6
Kristoffer Svanmark <kristoffer.svanmark@lexiconitkonsult.se>
2016-05-03 14:34:39 +0200

    Add textdomain to translation tag

--- views/partials/blog/taxonomy-filters.blade.php
+++ views/partials/blog/taxonomy-filters.blade.php
@@ -7,7 +7,7 @@
                 <h4 class="box-title">{{ $taxonomyVars->labels->name }}</h4>
                 <div class="box-content">
                     <ul>
-                        <li><a href=""><?php _e('Show all'); ?></a></li>
+                        <li><a href=""><?php _e('Show all', 'municipio'); ?></a></li>
                     @foreach ($taxs as $tax)
                         <li><a class="{{ isset($_GET['tax']) && $_GET['tax'] == $taxonomyVars->name && isset($_GET['term']) && $_GET['term'] == $tax->slug ? 'active' : '' }}" href="?{{ \Municipio\Helper\Url::queryStringExclude($_SERVER['QUERY_STRING'], array('tax', 'term'), '&amp;') }}tax={{ $taxonomyVars->name }}&amp;term={{ $tax->slug }}">{{ $tax->name }}</a></li>
                     @endforeach
