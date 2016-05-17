0a52792e785be22d6014f72ba829e6933b80a74b
Kristoffer Svanmark <kristoffer.svanmark@lexiconitkonsult.se>
2016-05-17 14:43:40 +0200

    Handle frontend login/logout events

---
+++ wp-content/themes/intranet/library/User/Login.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace Intranet\User;
+
+class Login
+{
+    public function __construct()
+    {
+        add_action('wp_login_failed', array($this, 'frontendLoginFailed'));
+        add_action('wp_logout', array($this, 'frontendLogout'));
+    }
+
+    /**
+     * Handles logout from frontend
+     * @return void
+     */
+    public function frontendLogout()
+    {
+        $referrer = $_SERVER['HTTP_REFERER'];
+
+        if (!empty($referrer) && !strstr($referrer, 'wp-login') && !strstr($referrer, 'wp-admin')) {
+            // let's append some information (loggedout=true) to the URL for the theme to use
+            wp_redirect(strstr($referrer, '?loggedout=true') ? $referrer : $referrer . '?loggedout=true');
+            exit;
+        }
+
+        die($referrer);
+    }
+
+    /**
+     * Handles incorrect logins on frontend
+     * @param  string $username
+     * @return void
+     */
+    public function frontendLoginFailed($username)
+    {
+        // Where did the submit come from
+        $referrer = $_SERVER['HTTP_REFERER'];
+
+        // If there's a valid referrer, and it's not the default log-in screen
+        if (!empty($referrer) && !strstr($referrer, 'wp-login') && !strstr($referrer, 'wp-admin')) {
+            // let's append some information (login=show) to the URL for the theme to use
+            wp_redirect(strstr($referrer, '?login=failed') ? $referrer : $referrer . '?login=failed');
+            exit;
+        }
+
+        die($referrer);
+    }
+}
