a1ce49e1f37df35155d6475d37901abedb12aafc
Kristoffer Svanmark <kristoffer.svanmark@lexiconitkonsult.se>
2016-05-18 08:37:36 +0200

    Change url strucutre for author page

--- wp-content/themes/intranet/library/User/Profile.php
+++ wp-content/themes/intranet/library/User/Profile.php
@@ -4,5 +4,15 @@ namespace Intranet\User;
 
 class Profile
 {
+    public function __construct()
+    {
+        add_action('init', array($this, 'changeUrlRewrite'));
+    }
 
+    public function changeUrlRewrite()
+    {
+        global $wp_rewrite;
+        $wp_rewrite->author_base = 'user';
+        $wp_rewrite->author_structure = '/' . $wp_rewrite->author_base . '/%author%';
+    }
 }
