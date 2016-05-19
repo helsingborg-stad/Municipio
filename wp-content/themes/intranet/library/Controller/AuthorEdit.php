c51721232687002096f7fba45d8956295cf06b7f
Kristoffer Svanmark <kristoffer.svanmark@lexiconitkonsult.se>
2016-05-19 10:56:42 +0200

    Save the user settings

--- wp-content/themes/intranet/library/Controller/AuthorEdit.php
+++ wp-content/themes/intranet/library/Controller/AuthorEdit.php
@@ -9,6 +9,10 @@ class AuthorEdit extends \Municipio\Controller\BaseController
         global $wp_query;
         global $authordata;
 
+        // Save form if posted
+        $this->saveForm();
+
+        // Get other data
         $currentUser = wp_get_current_user();
         $user = get_user_by('slug', $wp_query->query['author_name']);
 
@@ -18,5 +22,28 @@ class AuthorEdit extends \Municipio\Controller\BaseController
 
         $this->data['currentUser'] = $currentUser;
         $this->data['user'] = $user;
+    }
+
+    private function saveForm()
+    {
+        global $wp_query;
+
+        if (!isset($_POST['_wpnonce'])) {
+            return;
+        }
+
+        $currentUser = wp_get_current_user();
+        $user = get_user_by('slug', $wp_query->query['author_name']);
+
+        if (!wp_verify_nonce($_REQUEST['_wpnonce'], 'user_settings_update_' . $user->ID)) {
+            return;
+        }
+
+        update_user_meta($user->ID, 'user_work_title', sanitize_text_field($_POST['user_work_title']));
+        update_user_meta($user->ID, 'user_phone', sanitize_text_field($_POST['user_phone']));
+        update_user_meta($user->ID, 'user_administration_unit', sanitize_text_field($_POST['user_administration_unit']));
+        update_user_meta($user->ID, 'user_department', sanitize_text_field($_POST['user_department']));
+
+        return true;
     }
 }
