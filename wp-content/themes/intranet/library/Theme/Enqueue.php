73e6de002cbed791a5daec39f9f7207b68704d9f
Kristoffer Svanmark <kristoffer.svanmark@lexiconitkonsult.se>
2016-05-20 15:29:26 +0200

    Corrected hbgprime url

--- wp-content/themes/intranet/library/Theme/Enqueue.php
+++ wp-content/themes/intranet/library/Theme/Enqueue.php
@@ -20,7 +20,12 @@ class Enqueue
      */
     public function style()
     {
-        wp_register_style('hbg-prime', 'http://hbgprime.dev/dist/css/hbg-prime.min.css', '', '1.0.0');
+        if (defined('HBG_PRIME_CSS_URL') && strlen(HBG_PRIME_CSS_URL) > 0) {
+            wp_register_style('hbg-prime', HBG_PRIME_CSS_URL, '', '1.0.0');
+        } else {
+            wp_register_style('hbg-prime', 'http://helsingborg-stad.github.io/styleguide-web-cdn/styleguide.dev/dist/css/hbg-prime.min.css', '', '1.0.0');
+        }
+
         wp_enqueue_style('hbg-prime');
 
         wp_enqueue_style('intranet', get_stylesheet_directory_uri(). '/assets/dist/css/app.min.css', '', filemtime(get_stylesheet_directory() . '/assets/dist/css/app.min.css'));
