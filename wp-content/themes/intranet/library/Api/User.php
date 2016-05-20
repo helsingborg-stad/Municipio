0ce8752db648a4d3d931209bdf9a4e456359e146
Kristoffer Svanmark <kristoffer.svanmark@lexiconitkonsult.se>
2016-05-20 12:10:34 +0200

    Public functions in Api folder

---
+++ wp-content/themes/intranet/library/Api/User.php
@@ -0,0 +1,32 @@
+<?php
+
+if (!function_exists('municipio_intranet_get_user_profile_url')) {
+    /**
+     * Get profile url
+     * @param  mixed $user User id or login name, default is current logged in user
+     * @return string
+     */
+    function municipio_intranet_get_user_profile_url($user = null) {
+        if (is_null($user)) {
+            $user = wp_get_current_user();
+        } elseif (is_numeric($user)) {
+            $user = get_user_by('ID', $user);
+        } elseif (is_string($user)) {
+            $user = get_user_by('slug', $user);
+        }
+
+        return network_site_url('user/' . $user->data->user_login);
+    }
+}
+
+if (!function_exists('municipio_intranet_get_user_profile_edit_url')) {
+    /**
+     * Get edit profile url
+     * @param  mixed $user User id or login name, default is current logged in user
+     * @return string
+     */
+    function municipio_intranet_get_user_profile_edit_url($user = null) {
+        $url = municipio_intranet_get_user_profile_url($user);
+        return $url . '/edit';
+    }
+}
---
+++ wp-content/themes/intranet/library/Public.php
@@ -0,0 +1,3 @@
+<?php
+
+require_once('Api/User.php');
