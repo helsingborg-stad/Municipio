d7d2e8a2da43d2198c7d18b453f1518695ba458c
Kristoffer Svanmark <kristoffer.svanmark@lexiconitkonsult.se>
2016-05-20 15:53:17 +0200

    Hopefully fixes broken "session timeout" login modal

--- wp-content/themes/intranet/library/User/Login.php
+++ wp-content/themes/intranet/library/User/Login.php
@@ -35,11 +35,8 @@ class Login
 
         // If there's a valid referrer, and it's not the default log-in screen
         if (!empty($referrer) && !strstr($referrer, 'wp-login') && !strstr($referrer, 'wp-admin')) {
-            // let's append some information (login=show) to the URL for the theme to use
+            // let's append some information (login=failed) to the URL for the theme to use
             wp_redirect(strstr($referrer, '?login=failed') ? $referrer : $referrer . '?login=failed');
-            exit;
         }
-
-        die($referrer);
     }
 }
