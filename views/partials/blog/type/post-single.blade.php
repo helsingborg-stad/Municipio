528119f423ca00e5abb8caa2072bdf988df646bf
Kristoffer Svanmark <kristoffer.svanmark@lexiconitkonsult.se>
2016-05-23 13:29:03 +0200

    User page footer for single blog posts

--- views/partials/blog/type/post-single.blade.php
+++ views/partials/blog/type/post-single.blade.php
@@ -1,17 +1,21 @@
 <?php global $post; ?>
-<div class="post post-single">
-    @include('partials.blog.post-header')
+<div class="grid">
+    <div class="grid-xs-12">
+        <div class="post post-single">
+            @include('partials.blog.post-header')
 
-    <article>
-        @if (isset(get_extended($post->post_content)['main']) && strlen(get_extended($post->post_content)['main']) > 0 && isset(get_extended($post->post_content)['extended']) && strlen(get_extended($post->post_content)['extended']) > 0)
+            <article>
+                @if (isset(get_extended($post->post_content)['main']) && strlen(get_extended($post->post_content)['main']) > 0 && isset(get_extended($post->post_content)['extended']) && strlen(get_extended($post->post_content)['extended']) > 0)
 
-            {!! apply_filters('the_lead', get_extended($post->post_content)['main']) !!}
-            {!! apply_filters('the_content', get_extended($post->post_content)['extended']) !!}
+                    {!! apply_filters('the_lead', get_extended($post->post_content)['main']) !!}
+                    {!! apply_filters('the_content', get_extended($post->post_content)['extended']) !!}
 
-        @else
-            {!! the_content() !!}
-        @endif
-    </article>
-
-    @include('partials.blog.post-footer')
+                @else
+                    {!! the_content() !!}
+                @endif
+            </article>
+        </div>
+    </div>
 </div>
+
+@include('partials.page-footer')
