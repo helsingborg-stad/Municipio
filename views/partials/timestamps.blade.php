4b8fd2ad62c6c300f011c9d39fee0975c1369741
Kristoffer Svanmark <kristoffer.svanmark@lexiconitkonsult.se>
2016-05-23 13:58:46 +0200

    Remove sizes thing

--- views/partials/timestamps.blade.php
+++ views/partials/timestamps.blade.php
@@ -4,7 +4,7 @@
             <strong><?php _e('Published by', 'municipio'); ?>:</strong>
             <span class="post-author post-author-margin-left">
                 @if (get_field('page_show_author_image', 'option') !== false && get_field('post_show_author_image', get_the_id()) !== false && !empty(get_field('user_profile_picture', 'user_' . get_the_author_meta('ID'))))
-                    <span class="post-author-image" style="background-image:url('{{ get_field('user_profile_picture', 'user_' . get_the_author_meta('ID'))['sizes']['thumbnail'] }}');"><img src="{{ get_field('user_profile_picture', 'user_' . get_the_author_meta('ID'))['sizes']['thumbnail'] }}" alt="{{ (!empty(get_the_author_meta('first_name')) && !empty(get_the_author_meta('last_name'))) ? get_the_author_meta('first_name') . ' ' . get_the_author_meta('last_name')  : get_the_author() }}"></span>
+                    <span class="post-author-image" style="background-image:url('{{ get_field('user_profile_picture', 'user_' . get_the_author_meta('ID')) }}');"><img src="{{ get_field('user_profile_picture', 'user_' . get_the_author_meta('ID')) }}" alt="{{ (!empty(get_the_author_meta('first_name')) && !empty(get_the_author_meta('last_name'))) ? get_the_author_meta('first_name') . ' ' . get_the_author_meta('last_name')  : get_the_author() }}"></span>
                 @endif
 
                 @if (!empty(get_the_author_meta('first_name')) && !empty(get_the_author_meta('last_name')))
