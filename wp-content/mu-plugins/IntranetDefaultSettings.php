6246f707a0fdacfa2a745a3de5e2c3bf3ccb48da
Kristoffer Svanmark <kristoffer.svanmark@lexiconitkonsult.se>
2016-05-25 11:37:41 +0200

    Default blog settings adjusted

--- wp-content/mu-plugins/IntranetDefaultSettings.php
+++ wp-content/mu-plugins/IntranetDefaultSettings.php
@@ -6,20 +6,79 @@ class IntranetDefaultSettings
 {
     public function __construct()
     {
-        add_action('wpmu_new_blog', array($this, 'setHeaderLayout'));
+        // Blogs
+        add_action('wpmu_new_blog', array($this, 'createFrontPage'));
+        add_action('wpmu_new_blog', array($this, 'setDefaultSiteSettings'));
+
+        // Portal
         add_action('admin_init', array($this, 'createSiteListPage'));
     }
 
-    public function setHeaderLayout($blogId)
+    /**
+     * Sets the default site settings
+     * Add new settings with $key => $value structure
+     * @param [type] $blogId [description]
+     */
+    public function setDefaultSiteSettings($blogId)
     {
-        // Set the intranet header as default header style
-        update_blog_option($blogId, 'options_header_layout', 'intranet');
+        $defaultBlogOptions = array(
 
-        // Show date published and date modified per default
-        update_blog_option($blogId, 'options_show_date_updated', array('post', 'page', 'intranet-news'));
-        update_blog_option($blogId, 'options_show_date_published', array('post', 'page', 'intranet-news'));
+             // Header style
+             'options_header_layout'             => 'intranet',
+
+             // Show date published and date modified per default
+             'options_show_date_updated'         => array('post', 'page', 'intranet-news'),
+             'options_show_date_published'       => array('post', 'page', 'intranet-news'),
+
+             // Show page author
+             'options_page_show_author'          => 1,
+             'options_page_show_author_image'    => 1,
+
+             // Navigation settings (primary disabled, secondary enabled width settings)
+             'options_nav_primary_enable'        => 0,
+             'options_nav_sub_enable'            => 1,
+             'options_nav_sub_type'              => 'auto',
+             'options_nav_sub_depth'             => 0,
+             'options_nav_sub_include_top_level' => 1,
+             'options_nav_sub_render'            => 'active'
+
+        );
+
+        foreach ($defaultBlogOptions as $key => $value) {
+            update_blog_option($blogId, $key, $value);
+        }
     }
 
+    /**
+     * Creates a static frontpage for the blog
+     * @param  integer $blogId Blog id
+     * @return void
+     */
+    public function createFrontPage($blogId)
+    {
+        switch_to_blog($blogId);
+
+        // Delete "sample page"
+        $defaultPage = get_page_by_title(__('Sample Page'));
+        wp_delete_post($defaultPage->ID);
+
+        // Add new front page
+        $frontPageId = wp_insert_post(array(
+            'post_type' => 'page',
+            'post_status' => 'publish',
+            'post_title' => __('Front page')
+        ));
+
+        update_blog_option($blogId, 'show_on_front', 'page');
+        update_blog_option($blogId, 'page_on_front', $frontPageId);
+
+        restore_current_blog();
+    }
+
+    /**
+     * Creates a page that holds a list of all sites in the network
+     * @return void
+     */
     public function createSiteListPage()
     {
         global $current_site;
@@ -55,4 +114,3 @@ class IntranetDefaultSettings
 if (defined('MULTISITE') && MULTISITE) {
     new \IntranetDefaultSettings\IntranetDefaultSettings();
 }
-
