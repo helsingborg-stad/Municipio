efb1b1c9ec9a8afa95602418a63f54d019534f1a
Kristoffer Svanmark <kristoffer.svanmark@lexiconitkonsult.se>
2016-05-27 10:21:35 +0200

    Hide adminbar if user can't edit posts

--- library/Admin/Roles/General.php
+++ library/Admin/Roles/General.php
@@ -7,6 +7,16 @@ class General
     public function __construct()
     {
         add_action('admin_init', array($this, 'removeUnusedRoles'));
+        add_action('set_current_user', array($this, 'hideAdminBarForUsersWhoCantCreatePosts'));
+    }
+
+    public function hideAdminBarForUsersWhoCantEditPosts()
+    {
+        if (current_user_can('edit_posts')) {
+            return;
+        }
+
+        show_admin_bar(false);
     }
 
     public function removeUnusedRoles()
