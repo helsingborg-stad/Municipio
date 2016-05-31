c15b3df6cfae74aba50075da42858c2dcfb9d16c
Kristoffer Svanmark <kristoffer.svanmark@lexiconitkonsult.se>
2016-05-31 13:13:25 +0200

    Get querystring method to url helper

--- library/Helper/Url.php
+++ library/Helper/Url.php
@@ -40,4 +40,21 @@ class Url
 
         return $queryString;
     }
+
+    /**
+     * Get array with querystring params and values in current url
+     * @return array
+     */
+    public static function getQueryString()
+    {
+        $querystringsDefault = explode('&', $_SERVER['QUERY_STRING']);
+        $querystrings = array();
+
+        foreach ($querystringsDefault as $querystring) {
+            $querystring = explode('=', $querystring);
+            $querystrings[$querystring[0]] = isset($querystring[1]) ? $querystring[1] : null;
+        }
+
+        return $querystrings;
+    }
 }
