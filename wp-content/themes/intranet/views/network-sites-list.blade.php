280db0f5688262aeb9222ed685310d44c18db8e5
Kristoffer Svanmark <kristoffer.svanmark@lexiconitkonsult.se>
2016-05-31 11:17:02 +0200

    Format site name correclt in sites list

--- wp-content/themes/intranet/views/network-sites-list.blade.php
+++ wp-content/themes/intranet/views/network-sites-list.blade.php
@@ -9,7 +9,7 @@
             <div class="grid-md-4">
                 <a href="{{ $site['path'] }}" class="box box-index">
                     <div class="box-content">
-                        <h5 class="box-index-title link-item">{{ $site['name'] }}</h5>
+                        <h5 class="box-index-title link-item">{!! municipio_intranet_format_site_name($site) !!}</h5>
                         @if (!empty($site['description']))
                         <p>{{ $site['description'] }}</p>
                         @endif
