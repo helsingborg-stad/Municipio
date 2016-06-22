4d4dda9503022fe89c0fce23eba563ee47e7b117
Kristoffer Svanmark <kristoffer.svanmark@lexiconitkonsult.se>
2016-06-22 13:48:58 +0200

    Show "lathund" link on admin dashboard

--- assets/dist/js/admin.js
+++ assets/dist/js/admin.js
@@ -1,1 +1,7 @@
 var Municipio = {};
+
+jQuery('.index-php #screen-meta-links').append('\
+    <div id="screen-options-show-lathund-wrap" class="hide-if-no-js screen-meta-toggle">\
+        <a href="http://lathund.helsingborg.se" id="show-lathund" target="_blank" class="button show-settings">Lathund</a>\
+    </div>\
+');
