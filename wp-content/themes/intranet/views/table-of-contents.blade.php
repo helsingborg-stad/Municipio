61c09066dbfe8321a0183eec72a8f29f355f648d
Kristoffer Svanmark <kristoffer.svanmark@lexiconitkonsult.se>
2016-06-27 09:35:01 +0200

    Added filtering for the table-of-contents function

--- wp-content/themes/intranet/views/table-of-contents.blade.php
+++ wp-content/themes/intranet/views/table-of-contents.blade.php
@@ -9,11 +9,43 @@
 
             <div class="grid">
                 <div class="grid-xs-12">
-                    <ul class="pagination pagination-lg">
-                    @foreach ($tableOfContents as $key => $pages)
-                         <li><a href="#index-{{ $key }}">{{ strtoupper($key) }}</a></li>
-                    @endforeach;
-                    </ul>
+                    <form action="" method="get" class="form-horizontal-block">
+                        <div class="grid">
+                            <div class="grid-md-5">
+                                <div class="form-group">
+                                    <label for="table-of-contents-department"><?php _e('Department', 'municipio-intranet'); ?></label>
+                                    <select id="table-of-contents-department" name="department">
+                                        <option value=""><?php _e('All', 'municipio-intranet'); ?></option>
+                                        @foreach (\Intranet\Helper\Multisite::getSitesList(true) as $site)
+                                            <option value="{{ $site['blog_id'] }}" {{ selected($selectedDepartment, $site['blog_id']) }}>{{ $site['name'] }}</option>
+                                        @endforeach
+                                    </select>
+                                </div>
+                            </div>
+                            <div class="grid-md-5">
+                                <div class="form-group">
+                                    <label for="table-of-contents-text-search"><?php _e('Text search', 'municipio-intranet'); ?></label>
+                                    <input name="title" id="table-of-contents-text-search" type="search" placeholder="Sök i listan…" value="{{ $titleQuery }}">
+                                </div>
+                            </div>
+                            <div class="grid-md-2">
+                                <input class="btn btn-primary btn-block" type="submit" value="<?php _e('Search'); ?>">
+                            </div>
+                        </div>
+                    </form>
+                </div>
+            </div>
+
+            <div class="grid">
+                <div class="grid-xs-12">
+                    <div class="form-group">
+                        <label>Hoppa till bokstav</label>
+                        <ul class="pagination pagination-lg">
+                        @foreach ($tableOfContents as $key => $pages)
+                             <li><a href="#index-{{ $key }}">{{ strtoupper($key) }}</a></li>
+                        @endforeach;
+                        </ul>
+                    </div>
                 </div>
             </div>
 
