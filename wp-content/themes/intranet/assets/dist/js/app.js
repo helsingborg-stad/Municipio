ae0ca6ddcc5abb377956278e89fc70d4bcfafbd5
Kristoffer Svanmark <kristoffer.svanmark@lexiconitkonsult.se>
2016-06-30 14:54:34 +0200

    Preparation of js class for the responsibility input

--- wp-content/themes/intranet/assets/dist/js/app.js
+++ wp-content/themes/intranet/assets/dist/js/app.js
@@ -166,6 +166,21 @@ Intranet.Stripe = (function ($) {
 
 var Intranet;
 
+//
+// @name Modal
+// @description  Show accodrion dropdown, make linkable by updating adress bar
+//
+Intranet = Intranet || {};
+Intranet.ResponsibilitySelect = (function ($) {
+
+    function ResponsibilitySelect() {
+
+    }
+
+    return new ResponsibilitySelect();
+
+})(jQuery);
+
 Intranet = Intranet || {};
 Intranet.Search = Intranet.Search || {};
 
---
+++ wp-content/themes/intranet/assets/source/js/Components/ResponsibilitySelect.js
@@ -0,0 +1,14 @@
+//
+// @name Modal
+// @description  Show accodrion dropdown, make linkable by updating adress bar
+//
+Intranet = Intranet || {};
+Intranet.ResponsibilitySelect = (function ($) {
+
+    function ResponsibilitySelect() {
+
+    }
+
+    return new ResponsibilitySelect();
+
+})(jQuery);
