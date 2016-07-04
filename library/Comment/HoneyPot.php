acff5c5a26f92846303babf8926a7fa0fe52c615
Sebastian Thulin <sebastian.thulin@gmail.com>
2016-07-04 15:01:37 +0200

    Fake image cookie check
    Sets a cookie when an image url is loaded. Check this on form-post.

--- library/Comment/HoneyPot.php
+++ library/Comment/HoneyPot.php
@@ -19,14 +19,49 @@ class HoneyPot
         add_filter('comment_form_default_fields', array($this, 'addHoneyPotFieldFilled'));
         add_filter('comment_form_default_fields', array($this, 'addHoneyPotFieldBlank'));
 
+        //Print cookie method
+        add_action('init', array($this, 'fakeImage'));
+
         //Cactch fields
         add_filter('preprocess_comment', array($this, 'honeyPotValidateFieldContent'));
+        add_filter('preprocess_comment', array($this, 'fakeImageCookieCheck'));
 
         //Add styling to hide field
         add_filter('comment_form', array($this, 'printFakeHideBox'));
+        add_filter('comment_form', array($this, 'printFakeImage'));
     }
 
-    public function printFakeHideBox() {
+    public function fakeImage()
+    {
+        if (isset($_GET[$this->field_name]) && $_GET[$this->field_name] == $this->field_content) {
+
+            //Send svg header
+            header('Content-type: image/svg+xml');
+
+            //Set cookie
+            setcookie($this->field_name, $this->field_content, time() + 3600, "/");
+
+            //Return something tha looks like a image
+            die('<svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"><circle cx="50" cy="50" r="40" stroke="black" stroke-width="3" fill="red" /></svg>');
+        }
+    }
+
+    public function fakeImageCookieCheck($data)
+    {
+        if (isset($_COOKIE[$this->field_name]) && $_COOKIE[$this->field_name] == $this->field_content) {
+            return $data;
+        } else {
+            wp_die(__("Could not verify that you are human (image check).", 'municipio'));
+        }
+    }
+
+    public function printFakeImage()
+    {
+        echo '<div class="fake-hide"><img src="' . get_permalink() . '?' . $this->field_name . '=' . $this->field_content . '"/></div>';
+    }
+
+    public function printFakeHideBox()
+    {
         echo '<style>.fake-hide {width: 1px; height: 1px; opacity: 0.0001; position: absolute;}</style>';
     }
 
