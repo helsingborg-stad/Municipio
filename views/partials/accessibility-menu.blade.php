b4a899c2a0493831853cee040113e72ae048c353
Kristoffer Svanmark <kristoffer.svanmark@lexiconitkonsult.se>
2016-07-06 13:18:18 +0200

    Accessibility menu fix

--- views/partials/accessibility-menu.blade.php
+++ views/partials/accessibility-menu.blade.php
@@ -1,4 +1,3 @@
-@if (function_exists('ReadSpeakerHelper_playButton'))
 <ul class="nav nav-pills nav-horizontal gutter gutter-bottom hidden-print">
     @if (function_exists('ReadSpeakerHelper_playButton'))
     <li>
@@ -9,7 +8,6 @@
         <a href="#" onclick="window.print();return false;" class=""><i class="fa fa-print"></i> Skriv ut</a>
     </li>
 </ul>
-@endif
 
 @if (function_exists('ReadSpeakerHelper_player'))
     {!! ReadSpeakerHelper_player() !!}
