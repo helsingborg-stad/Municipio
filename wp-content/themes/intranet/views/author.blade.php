3e782e8757eceae0ab1622e9cf647cf9aa76cb83
Kristoffer Svanmark <kristoffer.svanmark@lexiconitkonsult.se>
2016-08-08 13:44:30 +0200

    New and fresh user profile layoutt

--- wp-content/themes/intranet/views/author.blade.php
+++ wp-content/themes/intranet/views/author.blade.php
@@ -3,7 +3,7 @@
 @section('content')
 
 <header class="profile-header">
-    <div class="profile-header-background" style="background-image:url('{{ !empty(get_the_author_meta('user_profile_picture')) ? get_the_author_meta('user_profile_picture') : 'http://www.helsingborg.se/wp-content/uploads/2016/05/varen_2016_2_1800x350.jpg' }}');"></div>
+    <div class="profile-header-background hidden" style="background-image:url('{{ !empty(get_the_author_meta('user_profile_picture')) ? get_the_author_meta('user_profile_picture') : 'http://www.helsingborg.se/wp-content/uploads/2016/05/varen_2016_2_1800x350.jpg' }}');"></div>
 
     <div class="container">
         <div class="grid">
@@ -13,32 +13,72 @@
                     <div class="profile-image" style="background-image:url('{{ get_the_author_meta('user_profile_picture') }}');"></div>
                     @endif
 
-                    <div class="profile-basics">
-                        <h1 class="profile-fullname">{{ municipio_intranet_get_user_full_name(get_the_author_meta('ID')) }}</h1>
+                    <h1 class="profile-fullname">{{ municipio_intranet_get_user_full_name(get_the_author_meta('ID')) }}</h1>
+                    @if (!empty(get_the_author_meta('ad_title')))
+                         <span class="profile-title">{{ get_the_author_meta('ad_title') }}</span>
+                    @elseif (!empty(get_the_author_meta('user_work_title')))
+                        <span class="profile-title">{{ get_the_author_meta('user_work_title') }}</span>
+                    @endif
 
-                        @if (!empty(get_the_author_meta('ad_title')))
-                             <span class="profile-title">{{ get_the_author_meta('ad_title') }}</span>
-                        @elseif (!empty(get_the_author_meta('user_work_title')))
-                            <span class="profile-title">{{ get_the_author_meta('user_work_title') }}</span>
-                        @endif
+                    @if (!empty(get_the_author_meta('user_administration_unit')) || !empty(get_the_author_meta('user_department')))
+                        <span class="profile-department">
+                            {{ !empty(get_the_author_meta('user_administration_unit')) ? municipio_intranet_get_administration_unit_name(get_the_author_meta('user_administration_unit')) : '' }}{{ !empty(get_the_author_meta('user_administration_unit')) && !empty(get_the_author_meta('user_department')) ? ',' : '' }}
+                            {{ !empty(get_the_author_meta('user_department')) ? get_the_author_meta('user_department') : '' }}
+                        </span>
+                    @endif
 
-                        @if (!empty(get_the_author_meta('user_administration_unit')) || !empty(get_the_author_meta('user_department')))
-                            <span class="profile-department">
-                                {{ !empty(get_the_author_meta('user_administration_unit')) ? municipio_intranet_get_administration_unit_name(get_the_author_meta('user_administration_unit')) : '' }}{{ !empty(get_the_author_meta('user_administration_unit')) && !empty(get_the_author_meta('user_department')) ? ',' : '' }}
-                                {{ !empty(get_the_author_meta('user_department')) ? get_the_author_meta('user_department') : '' }}
-                            </span>
-                        @endif
-                    </div>
+                    <ul class="profile-social-networks nav-horizontal">
+                        <li><a href="#" data-tooltip="<?php _e('My profile on Facebook', 'municipio-intranet'); ?>"><i class="fa fa-facebook"></i><span class="sr-only"><?php _e('My profile on Facebook', 'municipio-intranet'); ?></span></a></li>
+                        <li><a href="#" data-tooltip="<?php _e('My profile on Instagram', 'municipio-intranet'); ?>"><i class="fa fa-instagram"><span class="sr-only"><?php _e('My profile on Instagram', 'municipio-intranet'); ?></span></i></a></li>
+                        <li><a href="#" data-tooltip="<?php _e('My profile on Twitter', 'municipio-intranet'); ?>"><i class="fa fa-twitter"></i><span class="sr-only"><?php _e('My profile on Twitter', 'municipio-intranet'); ?></span></a></li>
+                        <li><a href="#" data-tooltip="<?php _e('My profile on Snapchat', 'municipio-intranet'); ?>"><i class="fa fa-snapchat"></i><span class="sr-only"><?php _e('My profile on Snapchat', 'municipio-intranet'); ?></span></a></li>
+                    </ul>
 
                     @if (get_current_user_id() == get_the_author_meta('ID') || is_super_admin())
                     <ul class="profile-actions">
-                        <li><a href="{{ municipio_intranet_get_user_profile_edit_url(get_the_author_meta('user_login')) }}" class="btn btn-primary"><i class="fa fa-wrench"></i> <?php _e('Edit settings', 'municipio-intranet'); ?></a></li>
+                        <li><a href="{{ municipio_intranet_get_user_profile_edit_url(get_the_author_meta('user_login')) }}"><i class="fa fa-wrench"></i> <?php _e('Edit settings', 'municipio-intranet'); ?></a></li>
                     </ul>
                     @endif
                 </div>
             </div>
         </div>
     </div>
+
+    <ul class="profile-contact nav-horizontal">
+        @if (!empty(get_the_author_meta('user_phone')))
+        <li>
+            <a href="tel:{{ get_the_author_meta('user_phone') }}">
+                <i class="fa fa-phone fa-2x"></i>
+                <span class="value">
+                    {{ get_the_author_meta('user_phone') }}
+                    <span class="value-label"><i class="fa fa-phone"></i> <?php _e('Phone number', 'municipio-intranet'); ?></span>
+                </span>
+            </a>
+        </li>
+        @endif
+
+        @if (!empty(get_the_author_meta('user_phone')))
+        <li>
+            <a href="mailto:{{ get_the_author_meta('email') }}">
+                <i class="fa fa-envelope-o fa-2x"></i>
+                <span class="value">
+                    {{ get_the_author_meta('email') }}
+                    <span class="value-label"><i class="fa fa-envelope-o"></i> <?php _e('Email', 'municipio-intranet'); ?></span>
+                </span>
+            </a>
+        </li>
+        @endif
+
+        <li>
+            <a href="mailto:{{ get_the_author_meta('email') }}">
+                <i class="fa fa-map-marker fa-2x"></i>
+                <span class="value">
+                    Kontaktcenter
+                    <span class="value-label"><i class="fa fa-map-marker"></i> <?php _e('Office', 'municipio-intranet'); ?></span>
+                </span>
+            </a>
+        </li>
+    </ul>
 </header>
 
 <div class="container main-container">
@@ -52,30 +92,6 @@
 
         <aside class="grid-md-4 sidebar-right-sidebar">
             <div class="grid">
-                <div class="grid-xs-12">
-                    <div class="box box-filled">
-                        <h4 class="box-title"><?php _e('Contact information', 'municipio-intranet'); ?></h4>
-                        <div class="box-content">
-                            <p>
-                                <strong><?php _e('Email', 'municipio-intranet'); ?></strong><br>
-                                <a href="mailto:{{ get_the_author_meta('email') }}">{{ get_the_author_meta('email') }}</a>
-                            </p>
-                            <p>
-                                <strong><?php _e('Phone number', 'municipio-intranet'); ?></strong><br>
-                                @if (!empty(get_the_author_meta('user_phone')))
-                                <a href="tel:{{ get_the_author_meta('user_phone') }}">{{ get_the_author_meta('user_phone') }}</a>
-                                @else
-                                <?php _e('No phone number given', 'municipio-intranet'); ?>
-                                @endif
-                            </p>
-                            <p>
-                                <strong><?php _e('Office', 'municipio-intranet'); ?></strong><br>
-                                Kontaktcenter
-                            </p>
-                        </div>
-                    </div>
-                </div>
-
                 @if (count($userSkills) > 0)
                 <div class="grid-xs-12">
                     <div class="box box-filled">
