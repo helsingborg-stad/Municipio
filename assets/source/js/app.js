8f108b69172736cf26a4e64b67cccaddf54a78dc
Kristoffer Svanmark <kristoffer.svanmark@lexiconitkonsult.se>
2016-08-09 16:12:48 +0200

    Load Google Translate on-demand

--- assets/source/js/app.js
+++ assets/source/js/app.js
@@ -1,5 +1,15 @@
 var Municipio = {};
 
+var googleTranslateLoaded = false;
+
+if (location.href.indexOf('translate=true') > -1) {
+    loadGoogleTranslate();
+}
+
+$('[href="#translate"]').on('click', function (e) {
+    loadGoogleTranslate();
+});
+
 function googleTranslateElementInit() {
     new google.translate.TranslateElement({
         pageLanguage: "sv",
@@ -7,4 +17,38 @@ function googleTranslateElementInit() {
         gaTrack: HbgPrimeArgs.googleTranslate.gaTrack,
         gaId: HbgPrimeArgs.googleTranslate.gaUA
     }, "google-translate-element");
+}
+
+function loadGoogleTranslate() {
+    if (googleTranslateLoaded) {
+        return;
+    }
+
+    $.getScript('//translate.google.com/translate_a/element.js?cb=googleTranslateElementInit', function() {
+        $('a').each(function () {
+            var hrefUrl = $(this).attr('href');
+
+            // Check if external or non valid url (do not add querystring)
+            if (hrefUrl.indexOf(location.origin) === -1 || hrefUrl.substr(0, 1) === '#') {
+                return;
+            }
+
+            hrefUrl = updateQueryStringParameter(hrefUrl, 'translate', 'true');
+
+            $(this).attr('href', hrefUrl);
+        });
+
+        googleTranslateLoaded = true;
+    });
+}
+
+function updateQueryStringParameter(uri, key, value) {
+    var re = new RegExp("([?&])" + key + "=.*?(&|$)", "i");
+    var separator = uri.indexOf('?') !== -1 ? "&" : "?";
+
+    if (uri.match(re)) {
+        return uri.replace(re, '$1' + key + "=" + value + '$2');
+    }
+
+    return uri + separator + key + "=" + value;
 }
