8f4710d84e24005db3eacd70075ea6c771cbc3a8
Kristoffer Svanmark <kristoffer.svanmark@lexiconitkonsult.se>
2016-08-15 15:59:37 +0200

    Revert "Updated share style"
    This reverts commit 5556ca67b7719e33df5b8fa26dd333ce2ded72d8.

--- views/partials/page-footer.blade.php
+++ views/partials/page-footer.blade.php
@@ -1,19 +1,13 @@
 <?php do_action('customer-feedback'); ?>
 
 <footer class="page-footer grid">
-    <div class="grid-xs-12">
-        @if (get_field('show_share', get_the_id()) !== false && get_field('page_show_share', 'option') !== false)
-            <div class="grid">
-                <div class="grid-xs-12">
-                    @include('partials.social-share')
-                </div>
-            </div>
-        @endif
+    <div class="grid-md-8">
+        @include('partials.timestamps')
+    </div>
 
-        <div class="grid">
-            <div class="grid-xs-12">
-                @include('partials.timestamps')
-            </div>
-        </div>
+    <div class="grid-md-4 text-right-lg text-right-md text-left-sm text-left-xs">
+        @if (get_field('show_share', get_the_id()) !== false && get_field('page_show_share', 'option') !== false)
+            @include('partials.social-share')
+        @endif
     </div>
 </footer>
