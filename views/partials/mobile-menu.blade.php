5d5bed7742064d41051e2736294ad019879b0716
Kristoffer Svanmark <kristoffer.svanmark@lexiconitkonsult.se>
2016-08-19 10:40:16 +0200

    Translation fix

--- views/partials/mobile-menu.blade.php
+++ views/partials/mobile-menu.blade.php
@@ -2,7 +2,7 @@
     <label for="searchkeyword-mobile" class="sr-only">{{ get_field('search_label_text', 'option') ? get_field('search_label_text', 'option') : 'Search' }}</label>
 
     <div class="input-group">
-        <input id="searchkeyword-mobile" autocomplete="off" class="form-control" type="search" name="s" placeholder="{{ get_field('search_placeholder_text', 'option') ? get_field('search_placeholder_text', 'option') : 'What are you looking for?' }}" value="<?php echo (!empty(get_search_query())) ? get_search_query() : ''; ?>">
+        <input id="searchkeyword-mobile" autocomplete="off" class="form-control" type="search" name="s" placeholder="<?php echo get_field('search_placeholder_text', 'option') ? get_field('search_placeholder_text', 'option') : __('What are you looking for?', 'municipio'); ?>" value="<?php echo (!empty(get_search_query())) ? get_search_query() : ''; ?>">
         <span class="input-group-addon-btn">
             <input type="submit" class="btn btn-primary" value="{{ get_field('search_button_text', 'option') ? get_field('search_button_text', 'option') : 'Search' }}">
         </span>
