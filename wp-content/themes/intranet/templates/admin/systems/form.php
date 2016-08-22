633014358c044458210e0f59d5becb91289bebce
Kristoffer Svanmark <kristoffer.svanmark@lexiconitkonsult.se>
2016-08-22 08:53:37 +0200

    Hide system list admin column for "description" if small screen

--- wp-content/themes/intranet/templates/admin/systems/form.php
+++ wp-content/themes/intranet/templates/admin/systems/form.php
@@ -70,7 +70,7 @@
                                     <thead>
                                         <tr>
                                             <th><?php _e('Name', 'municipio-intranet'); ?></th>
-                                            <th><?php _e('Description', 'municipio-intranet'); ?></th>
+                                            <th class="system-hidden"><?php _e('Description', 'municipio-intranet'); ?></th>
                                             <th><?php _e('Url', 'municipio-intranet'); ?></th>
                                             <th width="50"></th>
                                         </tr>
@@ -79,7 +79,7 @@
                                         <?php foreach (\Intranet\User\Systems::getAvailabelSystems() as $system) : ?>
                                         <tr>
                                             <td><?php echo $system->name; ?></td>
-                                            <td><?php echo $system->description; ?></td>
+                                            <td class="system-hidden"><?php echo $system->description; ?></td>
                                             <td><?php echo $system->url; ?></td>
                                             <td class="text-right">
                                                 <a title="<?php _e('Edit'); ?>" href="<?php echo admin_url('admin.php?page=user-systems&edit=' . $system->id); ?>" class="btn-plain municipio-edit"><span class="dashicons dashicons-edit"></span></a>
@@ -138,7 +138,7 @@
                                     <thead>
                                         <tr>
                                             <th><?php _e('Name', 'municipio-intranet'); ?></th>
-                                            <th><?php _e('Description', 'municipio-intranet'); ?></th>
+                                            <th class="system-hidden"><?php _e('Description', 'municipio-intranet'); ?></th>
                                             <th><?php _e('Url', 'municipio-intranet'); ?></th>
                                             <th style="text-align:center;"><?php _e('Selectable', 'municipio-intranet'); ?></th>
                                             <th style="text-align:center;"><?php _e('Forced', 'municipio-intranet'); ?></th>
@@ -148,7 +148,7 @@
                                         <?php foreach (\Intranet\User\Systems::getAvailabelSystems($unit->id) as $system) : ?>
                                         <tr>
                                             <td><?php echo $system->name; ?></td>
-                                            <td><?php echo $system->description; ?></td>
+                                            <td class="system-hidden"><?php echo $system->description; ?></td>
                                             <td><?php echo $system->url; ?></td>
                                             <td style="text-align:center;"><input type="checkbox" name="selectable[<?php echo $unit->id; ?>][]" value="<?php echo $system->id; ?>" <?php checked(true, $system->selectable); ?>></td>
                                             <td style="text-align:center;"><input type="checkbox" name="forced[<?php echo $unit->id; ?>][]" value="<?php echo $system->id; ?>" <?php checked(true, $system->forced); ?>></td>
