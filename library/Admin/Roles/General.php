8e1099f0e2252086afcd5483b2fc77bba58aab7e
Kristoffer Svanmark <kristoffer.svanmark@lexiconitkonsult.se>
2016-08-29 16:27:29 +0200

    Add back author role

--- library/Admin/Roles/General.php
+++ library/Admin/Roles/General.php
@@ -7,6 +7,8 @@ class General
     public function __construct()
     {
         add_action('admin_init', array($this, 'removeUnusedRoles'));
+        add_action('admin_init', array($this, 'addMissingRoles'));
+
         add_action('set_current_user', array($this, 'hideAdminBarForUsersWhoCantEditPosts'));
     }
 
@@ -19,9 +21,50 @@ class General
         show_admin_bar(false);
     }
 
+    /**
+     * Adds back missing author role
+     */
+    public function addMissingRoles()
+    {
+        if (!get_role('author')) {
+            add_role(
+                'author',
+                'Author',
+                array(
+                    'upload_files'           => true,
+                    'edit_posts'             => true,
+                    'edit_published_posts'   => true,
+                    'publish_posts'          => true,
+                    'read'                   => true,
+                    'level_2'                => true,
+                    'level_1'                => true,
+                    'level_0'                => true,
+                    'delete_posts'           => true,
+                    'delete_published_posts' => true
+                )
+            );
+
+            delete_option('_author_role_bkp');
+        }
+    }
+
+    /**
+     * Remove unwanted roles
+     * @return void
+     */
     public function removeUnusedRoles()
     {
-        remove_role('author');
-        remove_role('contributor');
+        $removeRoles = array(
+            'contributor'
+        );
+
+        foreach ($removeRoles as $role) {
+            if (!get_role($role)) {
+                continue;
+            }
+
+            update_option('_' . $role . '_role_bkp', get_role('author'));
+            remove_role($role);
+        }
     }
 }
