1eaaf1ca2398383fb79d6e7d7e53aa15c93731b7
Sebastian Thulin <sebastian.thulin@gmail.com>
2016-08-29 09:15:18 +0200

    Added ssl support for local prime assets.

--- library/Theme/Enqueue.php
+++ library/Theme/Enqueue.php
@@ -74,7 +74,7 @@ class Enqueue
 
         //Custom
         if (defined('DEV_MODE') && DEV_MODE === true) {
-            wp_register_script('hbg-prime', 'http://hbgprime.dev/dist/js/hbg-prime.min.js', '', '1.0.0', true);
+            wp_register_script('hbg-prime', '//hbgprime.dev/dist/js/hbg-prime.min.js', '', '1.0.0', true);
         } else {
             wp_register_script('hbg-prime', '//helsingborg-stad.github.io/styleguide-web-cdn/styleguide.dev/dist/js/hbg-prime.min.js', '', '1.0.0', true);
         }
