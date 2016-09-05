4e8c356227b589258f65709b693fb6d072999e28
Kristoffer Svanmark <kristoffer.svanmark@lexiconitkonsult.se>
2016-09-05 15:30:15 +0200

    Facebook login

--- wp-content/themes/intranet/assets/dist/js/app.js
+++ wp-content/themes/intranet/assets/dist/js/app.js
@@ -612,6 +612,49 @@ Intranet.Search.Sites = (function ($) {
 })(jQuery);
 
 Intranet = Intranet || {};
+Intranet.SocialLogin = Intranet.SocialLogin || {};
+
+Intranet.SocialLogin.Facebook = (function ($) {
+    function Facebook() {
+        // Facebook SDK needs #fb-root div, set it up
+        $('body').prepend('<div id="fb-root"></div>');
+
+        // Load the Facebook SDK
+        (function(d, s, id) {
+            var js, fjs = d.getElementsByTagName(s)[0];
+            if (d.getElementById(id)) return;
+            js = d.createElement(s); js.id = id;
+            js.src = "//connect.facebook.net/sv_SE/sdk.js#xfbml=1&version=v2.7&appId=530769533785659";
+            fjs.parentNode.insertBefore(js, fjs);
+        }(document, 'script', 'facebook-jssdk'));
+    }
+
+    Facebook.prototype.checkStatus = function() {
+        FB.getLoginStatus(function(response) {
+            if (response.status === 'connected') {
+                Intranet.SocialLogin.Facebook.getDetails();
+            }
+            else {
+                FB.login();
+            }
+        });
+    };
+
+    Facebook.prototype.getDetails = function() {
+        FB.api('/me', {fields: 'about, bio, birthday, gender, hometown, location'}, function (response) {
+            console.log(response);
+        });
+    };
+
+    return new Facebook();
+
+})(jQuery);
+
+function facebookLoginDone() {
+    Intranet.SocialLogin.Facebook.checkStatus();
+}
+
+Intranet = Intranet || {};
 Intranet.User = Intranet.User || {};
 
 Intranet.User.Links = (function ($) {
