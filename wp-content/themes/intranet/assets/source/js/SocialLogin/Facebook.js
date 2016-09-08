a8e1db3b739039702678a24cbdf31c8a7c6aa302
Kristoffer Svanmark <kristoffer.svanmark@lexiconitkonsult.se>
2016-09-08 14:50:59 +0200

    Facebook App ID switch

--- wp-content/themes/intranet/assets/source/js/SocialLogin/Facebook.js
+++ wp-content/themes/intranet/assets/source/js/SocialLogin/Facebook.js
@@ -11,7 +11,7 @@ Intranet.SocialLogin.Facebook = (function ($) {
             var js, fjs = d.getElementsByTagName(s)[0];
             if (d.getElementById(id)) return;
             js = d.createElement(s); js.id = id;
-            js.src = "//connect.facebook.net/sv_SE/sdk.js#xfbml=1&version=v2.7&appId=530769533785659";
+            js.src = "//connect.facebook.net/sv_SE/sdk.js#xfbml=1&version=v2.7&appId=1604603396447959";
             fjs.parentNode.insertBefore(js, fjs);
         }(document, 'script', 'facebook-jssdk'));
     }
