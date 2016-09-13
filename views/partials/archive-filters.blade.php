afbd0c07951c29bf94e23830b367c8750eb21b2e
Kristoffer Svanmark <kristoffer.svanmark@lexiconitkonsult.se>
2016-09-13 16:02:52 +0200

    Updated archive search filter ui

--- views/partials/archive-filters.blade.php
+++ views/partials/archive-filters.blade.php
@@ -4,17 +4,17 @@
         <div class="grid grid-table">
             @if (in_array('text_search', $enabledHeaderFilters))
             <div class="grid-auto">
-                <label for="filter-keyword" class="text-sm"><strong><?php _e('Title', 'municipio'); ?>:</strong></label>
+                <label for="filter-keyword" class="text-sm sr-only"><strong><?php _e('Search', 'municipio'); ?>:</strong></label>
                 <div class="input-group">
                     <span class="input-group-addon"><i class="fa fa-search"></i></span>
-                    <input type="text" name="s" id="filter-keyword" class="form-control" value="{{ !empty(get_search_query()) ? get_search_query() : '' }}">
+                    <input type="text" name="s" id="filter-keyword" class="form-control" value="{{ !empty(get_search_query()) ? get_search_query() : '' }}" placeholder="<?php _e('Search', 'municipio'); ?>">
                 </div>
             </div>
             @endif
 
             @if (in_array('date_range', $enabledHeaderFilters))
             <div class="grid-auto">
-                <label for="filter-date-from" class="text-sm"><strong><?php _e('Date published', 'municipio'); ?>:</strong></label>
+                <label for="filter-date-from" class="text-sm sr-only"><strong><?php _e('Date published', 'municipio'); ?>:</strong></label>
                 <div class="input-group">
                     <span class="input-group-addon"><?php _e('From', 'municipio'); ?>:</span>
                     <input type="text" name="from" placeholder="<?php _e('From date', 'municipio'); ?>…" id="filter-date-from" class="form-control datepicker-range datepicker-range-from" value="{{ isset($_GET['from']) && !empty($_GET['from']) ? sanitize_text_field($_GET['from']) : '' }}" readonly>
@@ -24,43 +24,42 @@
             </div>
             @endif
 
-            @if (!empty($enabledTaxonomyFilters))
             <div class="grid-fit-content">
-                <label>&nbsp;</label>
-                <div class="pos-relative">
-                    <button class="btn" data-dropdown=".filter-dropdown" type="button">Välj taxonomier</button>
-                    <div class="dropdown-menu filter-dropdown dropdown-menu-arrow dropdown-menu-arrow-right dropdown-overflow" style="right:0;">
-                        <div class="accordion accordion-icon accordion-list">
-                            <?php $i = 0; ?>
-                            @foreach ($enabledTaxonomyFilters as $taxKey => $taxonomy)
-                            <?php $i++; ?>
-                             <section class="accordion-section">
-                                <input type="radio" name="active-section" id="accordion-taxonomy-{{ $taxKey }}">
-                                <label class="accordion-toggle" for="accordion-taxonomy-{{ $taxKey }}">
-                                    <h5>{{ $taxonomy->label }}</h5>
-                                </label>
-                                <div class="accordion-content text-columns-2">
-                                    @foreach ($taxonomy->values as $term)
-                                    <label class="checkbox">
-                                        <input type="checkbox" name="term[]" value="{{ $taxKey }}|{{ $term->slug }}" {{ checked(true, isset($_GET['term']) && is_array($_GET['term']) && in_array($taxKey . '|' . $term->slug, $_GET['term'])) }}> {{ $term->name }}
-                                    </label>
-                                    @endforeach
-                                </div>
-                            </section>
-                            @endforeach
-                        </div>
-                    </div>
-                </div>
+                <input type="submit" value="<?php _e('Search', 'municipio'); ?>" class="btn btn-primary btn-block">
             </div>
-            @endif
         </div>
 
-        <div class="grid">
-            <div class="grid-md-2">
-                 <label for="filter-date-from" class="text-sm">&nbsp;</label>
-                <input type="submit" value="<?php _e('Filter', 'municipio'); ?>" class="btn btn-primary btn-block">
+        @if (!empty($enabledTaxonomyFilters))
+        <div class="grid" id="options" style="display: none;">
+            <div class="grid-sm-12 grid-md-6">
+                <div class="accordion accordion-icon accordion-list no-shadow">
+                    @foreach ($enabledTaxonomyFilters as $taxKey => $taxonomy)
+                     <section class="accordion-section">
+                        <input type="radio" name="active-section" id="accordion-taxonomy-{{ $taxKey }}">
+                        <label class="accordion-toggle" for="accordion-taxonomy-{{ $taxKey }}">
+                            <h6>{{ $taxonomy->label }}</h6>
+                        </label>
+                        <div class="accordion-content text-columns-2">
+                            @foreach ($taxonomy->values as $term)
+                            <label class="checkbox block-level">
+                                <input type="checkbox" name="term[]" value="{{ $taxKey }}|{{ $term->slug }}" {{ checked(true, isset($_GET['term']) && is_array($_GET['term']) && in_array($taxKey . '|' . $term->slug, $_GET['term'])) }}> {{ $term->name }}
+                            </label>
+                            @endforeach
+                        </div>
+                    </section>
+                    @endforeach
+                </div>
             </div>
         </div>
+        @endif
+
+        @if (!empty($enabledTaxonomyFilters))
+        <div class="grid no-margin gutter gutter-top gutter-sm">
+            <div class="grid-xs-12">
+                <button type="button" data-toggle="#options" class="btn btn-plain pricon pricon-plus-o pricon-space-right" data-toggle-text="Visa färre sökalternativ…" data-toggle-class="btn btn-plain pricon pricon-minus-o pricon-space-right">Visa fler sökalternativ…</a>
+            </div>
+        </div>
+        @endif
     </form>
 </section>
 @endif
