24f06acd6b508c6b64442ea96cbca1181fddf5e1
Kristoffer Svanmark <kristoffer.svanmark@lexiconitkonsult.se>
2016-09-13 13:32:35 +0200

    Updated single template

--- views/single.blade.php
+++ views/single.blade.php
@@ -7,41 +7,38 @@
 
     <div class="grid">
         <div class="grid-md-12 grid-lg-9">
-            @while(have_posts())
+            @if (is_single() && is_active_sidebar('content-area-top'))
+                <div class="grid sidebar-content-area sidebar-content-area-top">
+                    <?php dynamic_sidebar('content-area-top'); ?>
+                </div>
+            @endif
 
-                @if (is_single() && is_active_sidebar('content-area-top'))
-                    <div class="grid sidebar-content-area sidebar-content-area-top">
-                        <?php dynamic_sidebar('content-area-top'); ?>
-                    </div>
-                @endif
+            <div class="grid">
+                <div class="grid-sm-12">
+                        {!! the_post() !!}
 
+                        @include('partials.blog.type.post-single')
+                </div>
+            </div>
+
+            @if (is_single() && is_active_sidebar('content-area'))
+                <div class="grid sidebar-content-area sidebar-content-area-bottom">
+                    <?php dynamic_sidebar('content-area'); ?>
+                </div>
+            @endif
+
+            @if (is_single() && comments_open())
                 <div class="grid">
                     <div class="grid-sm-12">
-                            {!! the_post() !!}
-
-                            @include('partials.blog.type.post-single')
+                        @include('partials.blog.comments-form')
                     </div>
                 </div>
-
-                @if (is_single() && is_active_sidebar('content-area'))
-                    <div class="grid sidebar-content-area sidebar-content-area-bottom">
-                        <?php dynamic_sidebar('content-area'); ?>
+                <div class="grid">
+                    <div class="grid-sm-12">
+                        @include('partials.blog.comments')
                     </div>
-                @endif
-
-                @if (is_single() && comments_open())
-                    <div class="grid">
-                        <div class="grid-sm-12">
-                            @include('partials.blog.comments-form')
-                        </div>
-                    </div>
-                    <div class="grid">
-                        <div class="grid-sm-12">
-                            @include('partials.blog.comments')
-                        </div>
-                    </div>
-                @endif
-            @endwhile
+                </div>
+            @endif
         </div>
 
         @include('partials.sidebar-right')
