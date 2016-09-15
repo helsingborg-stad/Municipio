85f1d5a5cda559462706ace1785a2d9d3099140c
Kristoffer Svanmark <kristoffer.svanmark@lexiconitkonsult.se>
2016-09-15 13:13:28 +0200

    Updated "incident" module ui

--- wp-content/themes/intranet/templates/module/modularity-mod-intranet-inciden.php
+++ wp-content/themes/intranet/templates/module/modularity-mod-intranet-inciden.php
@@ -5,19 +5,17 @@ $length = get_field('length', $module->ID);
 
 $incidents = \Intranet\CustomPostType\Incidents::getIncidents($sites, $level, $length);
 ?>
-<div class="<?php echo implode(' ', apply_filters('Modularity/Module/Classes', array('box', 'box-index'), $module->post_type, $args)); ?>">
-    <h4 class="box-title gutter gutter-sm" style="padding-bottom:0;"><?php echo $module->post_title ? $module->post_title : __('Incidents', 'municipio-intranet'); ?></h4>
-    <div class="box-content">
-        <ul class="list-item-spacing">
-            <?php foreach ($incidents as $incident) : ?>
-            <li><a href="<?php echo get_blog_permalink($incident->blog_id, $incident->ID); ?>" class="notice <?php echo $incident->incident_level; ?> pricon pricon-notice-<?php echo $incident->incident_level; ?> pricon-space-right"><?php echo $incident->post_title; ?></a></li>
-            <?php endforeach; ?>
-        </ul>
-    </div>
+<div class="box box-plain">
+    <ul class="list-item-spacing">
+        <?php foreach ($incidents as $incident) : ?>
+        <li><a href="<?php echo get_blog_permalink($incident->blog_id, $incident->ID); ?>" class="notice <?php echo $incident->incident_level; ?> pricon pricon-notice-<?php echo $incident->incident_level; ?> pricon-space-right"><?php echo $incident->post_title; ?></a></li>
+        <?php endforeach; ?>
+    </ul>
 
     <?php if (get_field('link_to_archive', $module->ID)) : ?>
-    <div class="gutter gutter-sm gutter-top">
-        <a href="<?php echo get_post_type_archive_link('incidents'); ?>" class="read-more"><?php _e('Show all incidents', 'municipio-intranet'); ?></a>
-    </div>
+        <div class="">
+            <a href="<?php echo get_post_type_archive_link('incidents'); ?>" class="pricon pricon-plus-o pricon-space-right text-sm"><?php _e('Show all incidents', 'municipio-intranet'); ?></a>
+        </div>
     <?php endif; ?>
 </div>
+
