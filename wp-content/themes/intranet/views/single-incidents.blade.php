40ce557ff69e82525351e764252c6c0f295b9889
Kristoffer Svanmark <kristoffer.svanmark@lexiconitkonsult.se>
2016-09-15 10:42:36 +0200

    Incident single template

--- wp-content/themes/intranet/views/single-incidents.blade.php
+++ wp-content/themes/intranet/views/single-incidents.blade.php
@@ -17,33 +17,35 @@
 
             <div class="grid">
                 <div class="grid-sm-12">
-                    {!! the_post() !!}
+                    @while(have_posts())
+                        {!! the_post() !!}
 
-                    <?php global $post; ?>
-                    <div class="grid">
-                        <div class="grid-xs-12">
-                            <div class="post post-single">
-                                <header class="post-header">
-                                    <h1 style="padding-bottom: 0.5em;" class="pricon pricon-notice-{{ $post->incident_level }} pricon-space-right notice notice-inline-block notice-lg {{ $post->incident_level }}">{{ the_title() }}</h1>
+                        <?php global $post; ?>
+                        <div class="grid">
+                            <div class="grid-xs-12">
+                                <div class="post post-single">
+                                    <header class="post-header">
+                                        <h1 style="padding-bottom: 0.5em;" class="pricon pricon-notice-{{ $post->incident_level }} pricon-space-right notice notice-inline-block notice-lg {{ $post->incident_level }}">{{ the_title() }}</h1>
 
-                                    @include('partials.blog.post-info')
-                                </header>
+                                        @include('partials.blog.post-info')
+                                    </header>
 
-                                <article>
-                                    @if (isset(get_extended($post->post_content)['main']) && strlen(get_extended($post->post_content)['main']) > 0 && isset(get_extended($post->post_content)['extended']) && strlen(get_extended($post->post_content)['extended']) > 0)
+                                    <article>
+                                        @if (isset(get_extended($post->post_content)['main']) && strlen(get_extended($post->post_content)['main']) > 0 && isset(get_extended($post->post_content)['extended']) && strlen(get_extended($post->post_content)['extended']) > 0)
 
-                                        {!! apply_filters('the_lead', get_extended($post->post_content)['main']) !!}
-                                        {!! apply_filters('the_content', get_extended($post->post_content)['extended']) !!}
+                                            {!! apply_filters('the_lead', get_extended($post->post_content)['main']) !!}
+                                            {!! apply_filters('the_content', get_extended($post->post_content)['extended']) !!}
 
-                                    @else
-                                        {!! the_content() !!}
-                                    @endif
-                                </article>
+                                        @else
+                                            {!! the_content() !!}
+                                        @endif
+                                    </article>
+                                </div>
                             </div>
                         </div>
-                    </div>
 
-                    @include('partials.blog.post-footer')
+                        @include('partials.blog.post-footer')
+                    @endwhile
                 </div>
             </div>
 
