49c8ca89d342a1b668ea99000d3a11017e181c34
Kristoffer Svanmark <kristoffer.svanmark@lexiconitkonsult.se>
2016-10-06 14:19:54 +0200

    Archive filter grid fix

--- views/partials/archive-filters.blade.php
+++ views/partials/archive-filters.blade.php
@@ -1,7 +1,7 @@
 @if (!empty($enabledHeaderFilters))
 <section class="creamy creamy-border-bottom gutter-lg gutter-vertical sidebar-content-area archive-filters">
     <form method="get" action="{{ get_post_type_archive_link(get_post_type()) }}" class="container" id="archive-filter">
-        <div class="grid grid-table">
+        <div class="grid">
             @if (in_array('text_search', $enabledHeaderFilters))
             <div class="grid-sm-12 grid-md-auto">
                 <label for="filter-keyword" class="text-sm sr-only"><strong><?php _e('Search', 'municipio'); ?>:</strong></label>
