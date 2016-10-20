e34510fb06b37ada08233d45b6035cdb1e7ae82a
Kristoffer Svanmark <kristoffer.svanmark@lexiconitkonsult.se>
2016-10-20 09:21:37 +0200

    News ui

--- wp-content/themes/intranet/templates/module/modularity-mod-intranet-news.php
+++ wp-content/themes/intranet/templates/module/modularity-mod-intranet-news.php
@@ -75,11 +75,9 @@
                     </div>
                 <?php endif; ?>
                 <div class="box-content">
+                <span class="label label-sm label-purple-5 network-title"><?php echo municipio_intranet_format_site_name(\Intranet\Helper\Multisite::getSite($item->blog_id), 'long'); ?></span>
                     <time class="pricon pricon-clock pricon-space-right" datetime="<?php echo mysql2date('Y-m-d H:i:s', strtotime($item->post_date)); ?>"><?php echo mysql2date(get_option('date_format'), $item->post_date); ?></time>
                     <h3 class="box-title text-highlight"><?php echo apply_filters('the_title', $item->post_title); ?></h3>
-                    <div class="sub-heading">
-                        <?php echo municipio_intranet_format_site_name(\Intranet\Helper\Multisite::getSite($item->blog_id), 'long'); ?>
-                    </div>
 
                     <article class="clearfix">
                         <?php if (in_array($args['id'], array('content-area', 'content-area-top')) && ($hasImages || (!get_field('placeholders', $module->ID) && $item->thumbnail_image))) : ?>
@@ -90,9 +88,18 @@
                             <?php endif; ?>
                         <?php endif; ?>
 
-                        <p><?php echo isset(get_extended($item->post_content)['extended']) && !empty(get_extended($item->post_content)['extended']) ? wp_strip_all_tags(get_extended($item->post_content)['main']) : wp_trim_words($item->post_content, 50, ''); ?></p>
+                        <p>
+                            <?php
+                            if (isset(get_extended($item->post_content)['extended']) && !empty(get_extended($item->post_content)['extended'])) {
+                                echo wp_strip_all_tags(get_extended($item->post_content)['main']);
+                            } else {
+                                echo wp_trim_words($item->post_content, 50, '');
+                            }
+                            ?>
+                        </p>
+
+                        <span class="read-more btn-md inline-block"><?php _e('Read more', 'modularity'); ?></span>
                     </article>
-                    <div><span class="read-more inline-block"><?php _e('Read more', 'modularity'); ?></span></div>
                 </div>
             </a>
         </div>
