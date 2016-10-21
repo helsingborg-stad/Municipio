0905512b84e9216f3e85cdd8d580f56725c5b948
Anders Fajerson <anders@perifer.se>
2016-10-21 22:14:02 +0200

    Fix warning if no Post type functions are selected

--- library/Content/CustomPostType.php
+++ library/Content/CustomPostType.php
@@ -64,6 +64,10 @@ class CustomPostType
                         'not_found_in_trash' => sprintf(__('No %s found in trash', 'municipio'), $type_definition['post_type_name'])
                     );
 
+                    $supports = array('title');
+                    if (!empty($type_definition['supports'])) {
+                        array_merge($type_definition['supports'], array('title'));
+                    }
                     $args = array(
                         'labels'             => $labels,
                         'description'        => __('Auto generated cpt from user iterface.', 'municipio'),
@@ -75,7 +79,7 @@ class CustomPostType
                                                 ),
                         'capability_type'    => 'post',
                         'hierarchical'       => $type_definition['hierarchical'],
-                        'supports'           => array_merge($type_definition['supports'], array('title')),
+                        'supports'           => $supports,
                         'menu_position'      => (int) $type_definition['menu_position'],
                         'has_archive'        => true,
                     );
