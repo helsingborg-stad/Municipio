0d379a42a0d5f6cf65eebfdca6cc1fd764d1d49f
Anders Fajerson <anders@perifer.se>
2016-10-25 13:09:08 +0200

    Fix social share setting for pages

--- views/partials/page-footer.blade.php
+++ views/partials/page-footer.blade.php
@@ -1,7 +1,7 @@
 <?php do_action('customer-feedback'); ?>
 
 <footer class="page-footer">
-    @if (get_field('show_share', get_the_id()) !== false && get_field('page_show_share', 'option') !== false)
+    @if (get_field('post_show_share', get_the_id()) !== false && get_field('page_show_share', 'option') !== false)
     <div class="grid">
         <div class="grid-xs-12">
             <div class="box box-border gutter gutter-horizontal no-margin hidden-print">
