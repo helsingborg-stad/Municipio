29c5bc0e31c6aaeca74f81a7bbd1e294809a1313
Kristoffer Svanmark <kristoffer.svanmark@lexiconitkonsult.se>
2016-10-27 16:19:35 +0200

    Filter to decide if show breadcrumbs or not

--- views/partials/breadcrumbs.blade.php
+++ views/partials/breadcrumbs.blade.php
@@ -1,4 +1,4 @@
-@if (wp_get_post_parent_id(get_the_id()) != 0)
+@if (apply_filters('Municipio/show_breadcrumbs', wp_get_post_parent_id(get_the_id()), get_queried_object()) != 0)
 <div class="grid breadcrumbs-wrapper">
     <div class="grid-lg-12">
         {{ \Municipio\Theme\Navigation::outputBreadcrumbs() }}
