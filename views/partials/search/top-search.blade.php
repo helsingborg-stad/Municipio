045b8f58ab12dcdabb161e6f2a1c8443801ab6f7
Sebastian Thulin <sebastian.thulin@gmail.com>
2016-11-22 11:21:41 +0100

    Do not break page.

--- views/partials/search/top-search.blade.php
+++ views/partials/search/top-search.blade.php
@@ -2,7 +2,7 @@
     global $searchFormNode;
     $searchFormNode = ($searchFormNode) ? $searchFormNode+1 : 1;
 ?>
-<form class="search top-search hidden-xs hidden-sm" method="get" action="{{ apply_filters('Municipio/search_form/action', home_url()); }}">
+<form class="search top-search hidden-xs hidden-sm" method="get" action="{{ apply_filters('Municipio/search_form/action', home_url()) }}">
     <label for="searchkeyword-{{ $searchFormNode }}" class="sr-only">{{ get_field('search_label_text', 'option') ? get_field('search_label_text', 'option') : __('Search', 'municipio') }}</label>
     <div class="input-group">
         <input id="searchkeyword-{{ $searchFormNode }}" autocomplete="off" class="form-control" type="search" name="s" placeholder="{{ get_field('search_placeholder_text', 'option') ? get_field('search_placeholder_text', 'option') : 'What are you looking for?' }}" value="<?php echo (!empty(get_search_query())) ? get_search_query() : ''; ?>">
