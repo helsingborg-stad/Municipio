2a844cebca17081e53746cb055cebaab9b5b18fb
Kristoffer Svanmark <kristoffer.svanmark@lexiconitkonsult.se>
2016-11-22 08:44:23 +0100

    Get private posts in wp editor link tool

--- wp-content/themes/intranet/library/Admin/Filters.php
+++ wp-content/themes/intranet/library/Admin/Filters.php
@@ -7,8 +7,13 @@ class Filters
     public function __construct()
     {
         add_action('admin_init', array($this, 'removeNewsFromPageForPostTypes'), 11);
+        add_filter('wp_link_query_args', array($this, 'privatePostsInMceLink'));
     }
 
+    /**
+     * Removes the selector in "page for post types" for "intranet news" post type
+     * @return void
+     */
     public function removeNewsFromPageForPostTypes()
     {
         global $wp_settings_fields;
@@ -16,5 +21,16 @@ class Filters
         if (isset($wp_settings_fields['reading']['page_for_post_type']['page_for_intranet-news'])) {
             unset($wp_settings_fields['reading']['page_for_post_type']['page_for_intranet-news']);
         }
+    }
+
+    /**
+     * Get private posts in the editor link tool
+     * @param  array $query Query args
+     * @return array
+     */
+    public function privatePostsInMceLink($args)
+    {
+        $args['post_status'] = array('publish', 'private');
+        return $args;
     }
 }
