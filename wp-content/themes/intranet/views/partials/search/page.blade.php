884c29bc8124e830f3393569d2e0fc877d2e3156
Kristoffer Svanmark <kristoffer.svanmark@lexiconitkonsult.se>
2016-11-22 08:15:06 +0100

    Show attachment descriptions in search

--- wp-content/themes/intranet/views/partials/search/page.blade.php
+++ wp-content/themes/intranet/views/partials/search/page.blade.php
@@ -10,8 +10,9 @@
                 <h3><a href="{{ apply_filters('Municipio/search_result/permalink_url', get_blog_permalink($post->site_id, $post->ID), get_post()) }}" class="{{ municipio_get_mime_link_item($post->post_mime_type) }}">{{ apply_filters('Municipio/search_result/title', get_the_title() ? get_the_title() : __('Unknown media', 'municipio-intranet'), get_post()) }}</a></h3>
             @else
                 <h3><a href="{{ apply_filters('Municipio/search_result/permalink_url', get_blog_permalink($post->site_id, $post->ID), get_post()) }}">{{ apply_filters('Municipio/search_result/title', get_the_title(), get_post()) }}</a></h3>
-                <p>{!! apply_filters('Municipio/search_result/excerpt', get_the_excerpt(), get_post()) !!}</p>
             @endif
+
+            <p>{!! apply_filters('Municipio/search_result/excerpt', get_the_excerpt(), get_post()) !!}</p>
 
             <div class="search-result-info">
                 <span class="search-result-url"><i class="fa fa-globe"></i> <a href="{{ apply_filters('Municipio/search_result/permalink_url', get_blog_permalink($post->site_id, $post->ID), get_post()) }}">{{ apply_filters('Municipio/search_result/permalink_text', get_blog_permalink($post->site_id, $post->ID), get_post()) }}</a></span>
