e6bdebffa34cb0c4a8b4603a7af6f2d5422176ad
Kristoffer Svanmark <kristoffer.svanmark@lexiconitkonsult.se>
2016-11-23 08:24:30 +0100

    Do not get posts with parents in archive if the post type is in the menu

---
+++ library/Theme/Archive.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Municipio\Theme;
+
+class Archive
+{
+    public function __construct()
+    {
+        add_action('pre_get_posts', array($this, 'onlyFirstLevel'));
+    }
+
+    public function onlyFirstLevel($query)
+    {
+        if (!is_archive() || !$query->is_main_query()) {
+            return;
+        }
+
+        $inMenu = false;
+        foreach (get_field('avabile_dynamic_post_types', 'options') as $type) {
+            if ($type['slug'] !== $query->post_type) {
+                continue;
+            }
+
+            if (!$type['show_posts_in_sidebar_menu']) {
+                return;
+            }
+        }
+
+        $query->set('post_parent', 0);
+    }
+}
