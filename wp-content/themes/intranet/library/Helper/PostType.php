183e8bd4a11e2cd99c0075c67a08743cb112e817
Sebastian Thulin <sebastian.thulin@gmail.com>
2016-11-25 16:54:45 +0100

    Not associative

--- wp-content/themes/intranet/library/Helper/PostType.php
+++ wp-content/themes/intranet/library/Helper/PostType.php
@@ -8,7 +8,7 @@ class PostType
     {
         $postTypes = get_post_types(array('public' => true));
         if (is_array($postTypes)) {
-            return array_diff($postTypes, array('nav_menu_item','revision','hbg-alarm','incidents'));
+            return array_values(array_diff($postTypes, array('nav_menu_item','revision','hbg-alarm','incidents')));
         }
         return array();
     }
