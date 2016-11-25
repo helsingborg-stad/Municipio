749ccc4df7e516cb1d72651ef54b020d630d8014
Sebastian Thulin <sebastian.thulin@gmail.com>
2016-11-25 13:29:51 +0100

    Show error notice.

--- wp-content/themes/intranet/views/partials/404/login.blade.php
+++ wp-content/themes/intranet/views/partials/404/login.blade.php
@@ -2,13 +2,20 @@
     <div class="grid">
         <div class="grid-lg-3 grid-md-2 grid-sm-12 hidden-xs hidden-sm no-margin no-padding"></div>
         <div class="grid-lg-6 grid-md-8 grid-sm-12">
-           <span class="h1 no-margin no-padding">Du måste logga in för att se den här sidan</span>
+           <span class="h1">Du måste logga in för att se den här sidan</span>
+
+            @if (isset($_GET['login']) && $_GET['login'] === 'failed')
+                <div class="notice info margin-top">
+                     <i class="pricon pricon-notice-warning"></i> Det användarnamn eller lösenord som du angav är felaktigt.
+                </div>
+            @endif
+
         </div>
     </div>
 </div>
 
 <div class="container main-container">
-    <div class="grid">
+    <div class="grid no-margin no-padding">
         <div class="grid-lg-3 grid-md-2 grid-sm-12 hidden-xs hidden-sm no-margin no-padding"></div>
         <div class="grid-lg-6 grid-md-8 grid-sm-12">
             @include('partials.user.loginform')
