00eaf0ac53d2b4e1ff5b8545cb8bd0bb8de69925
Kristoffer Svanmark <kristoffer.svanmark@lexiconitkonsult.se>
2016-11-28 09:39:14 +0100

    Open attachment links in new window from search

--- wp-content/themes/intranet/views/partials/search/page.blade.php
+++ wp-content/themes/intranet/views/partials/search/page.blade.php
@@ -7,7 +7,7 @@
             <span class="network-title label label-sm label-purple-5">{!! municipio_intranet_format_site_name(\Intranet\Helper\Multisite::getSite($post->site_id), 'long') !!}</span>
 
             @if (get_post_type() === 'attachment')
-                <h3><a href="{{ apply_filters('Municipio/search_result/permalink_url', get_blog_permalink($post->site_id, $post->ID), get_post()) }}" class="{{ municipio_get_mime_link_item($post->post_mime_type) }} link-item-lg">{{ apply_filters('Municipio/search_result/title', get_the_title() ? get_the_title() : __('Unknown media', 'municipio-intranet'), get_post()) }}</a></h3>
+                <h3><a href="{{ apply_filters('Municipio/search_result/permalink_url', get_blog_permalink($post->site_id, $post->ID), get_post()) }}" class="{{ municipio_get_mime_link_item($post->post_mime_type) }} link-item-lg" target="_blank">{{ apply_filters('Municipio/search_result/title', get_the_title() ? get_the_title() : __('Unknown media', 'municipio-intranet'), get_post()) }}</a></h3>
             @else
                 <h3><a href="{{ apply_filters('Municipio/search_result/permalink_url', get_blog_permalink($post->site_id, $post->ID), get_post()) }}">{{ apply_filters('Municipio/search_result/title', get_the_title(), get_post()) }}</a></h3>
             @endif
@@ -15,7 +15,7 @@
             <p>{!! apply_filters('Municipio/search_result/excerpt', get_the_excerpt(), get_post()) !!}</p>
 
             <div class="search-result-info">
-                <span class="search-result-url"><i class="fa fa-globe"></i> <a href="{{ apply_filters('Municipio/search_result/permalink_url', get_blog_permalink($post->site_id, $post->ID), get_post()) }}">{{ apply_filters('Municipio/search_result/permalink_text', get_blog_permalink($post->site_id, $post->ID), get_post()) }}</a></span>
+                <span class="search-result-url"><i class="fa fa-globe"></i> <a href="{{ apply_filters('Municipio/search_result/permalink_url', get_blog_permalink($post->site_id, $post->ID), get_post()) }}" {{ get_post_type() === 'attachment' ? 'taget="_blank"' : '' }}>{{ apply_filters('Municipio/search_result/permalink_text', get_blog_permalink($post->site_id, $post->ID), get_post()) }}</a></span>
             </div>
         </div>
     </li>
