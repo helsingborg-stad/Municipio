0c61c8e4d30c1eca69fd76d7b67dde464000b059
Sebastian Thulin <sebastian.thulin@gmail.com>
2016-11-28 10:31:07 +0100

    Exclude from search.

--- wp-content/themes/intranet/library/Helper/PostType.php
+++ wp-content/themes/intranet/library/Helper/PostType.php
@@ -6,7 +6,7 @@ class PostType
 {
     public static function getPublic()
     {
-        $postTypes = get_post_types(array('public' => true));
+        $postTypes = get_post_types(array('public' => true, 'exclude_from_search' => false));
         if (is_array($postTypes)) {
             return array_values(array_diff($postTypes, array('nav_menu_item','revision','hbg-alarm','incidents')));
         }
