319bdfa54ea9be2c3b8b98e079f5e0d157b08f0b
Kristoffer Svanmark <kristoffer.svanmark@lexiconitkonsult.se>
2016-11-30 15:18:43 +0100

    SSO Ad sync

--- wp-content/plugins/saml-20-single-sign-on/lib/classes/saml_client.php
+++ wp-content/plugins/saml-20-single-sign-on/lib/classes/saml_client.php
@@ -1,23 +1,22 @@
 <?php
 class SAML_Client
 {
-  private $saml;
-  private $opt;
+    private $saml;
+    private $opt;
 
-  function __construct()
-  {
-    $this->settings = new SAML_Settings();
+    public function __construct()
+    {
+        $this->settings = new SAML_Settings();
 
-    require_once(constant('SAMLAUTH_ROOT') . '/saml/lib/_autoload.php');
-		if( $this->settings->get_enabled() )
-		{
-			$this->saml = new SimpleSAML_Auth_Simple((string)get_current_blog_id());
+        require_once(constant('SAMLAUTH_ROOT') . '/saml/lib/_autoload.php');
+        if ($this->settings->get_enabled()) {
+            $this->saml = new SimpleSAML_Auth_Simple((string)get_current_blog_id());
 
-			add_action('wp_authenticate',array($this,'authenticate'));
-	    add_action('wp_logout',array($this,'logout'));
-      add_action('login_form', array($this, 'modify_login_form'));
-		}
-  }
+            add_action('wp_authenticate', array($this, 'authenticate'));
+            add_action('wp_logout', array($this, 'logout'));
+            add_action('login_form', array($this, 'modify_login_form'));
+        }
+    }
 
   /**
    *  Authenticates the user using SAML
@@ -26,38 +25,26 @@ class SAML_Client
    */
   public function authenticate()
   {
-    if( isset($_GET['loggedout']) && $_GET['loggedout'] == 'true' )
-    {
-      header('Location: ' . get_option('siteurl'));
-      exit();
-    }
-    elseif ( $this->settings->get_allow_sso_bypass() == true  && (( isset($_GET['use_sso']) && $_GET['use_sso'] == 'false' ) || ( isset($_POST['use_sso']) && $_POST['use_sso'] == 'false' )) )
-    {
-      // User wants native WP login, do nothing
-    }
-    else
-    {
-
-      $redirect_url = (array_key_exists('redirect_to', $_GET)) ? wp_login_url( $_GET['redirect_to']) : get_admin_url();
-      $this->saml->requireAuth( array('ReturnTo' => $redirect_url ) );
-      $attrs = $this->saml->getAttributes();
-      if(array_key_exists($this->settings->get_attribute('username'), $attrs) )
-      {
-        $username = $attrs[$this->settings->get_attribute('username')][0];
-        if(get_user_by('login',$username))
-        {
-          $this->simulate_signon($username);
-        }
-        else
-        {
-          $this->new_user($attrs);
-        }
+      if (isset($_GET['loggedout']) && $_GET['loggedout'] == 'true') {
+          header('Location: ' . get_option('siteurl'));
+          exit();
+      } elseif ($this->settings->get_allow_sso_bypass() == true  && ((isset($_GET['use_sso']) && $_GET['use_sso'] == 'false') || (isset($_POST['use_sso']) && $_POST['use_sso'] == 'false'))) {
+          // User wants native WP login, do nothing
+      } else {
+          $redirect_url = (array_key_exists('redirect_to', $_GET)) ? wp_login_url($_GET['redirect_to']) : get_admin_url();
+          $this->saml->requireAuth(array('ReturnTo' => $redirect_url ));
+          $attrs = $this->saml->getAttributes();
+          if (array_key_exists($this->settings->get_attribute('username'), $attrs)) {
+              $username = $attrs[$this->settings->get_attribute('username')][0];
+              if (get_user_by('login', $username)) {
+                  $this->simulate_signon($username);
+              } else {
+                  $this->new_user($attrs);
+              }
+          } else {
+              die('A username was not provided.');
+          }
       }
-      else
-      {
-        die('A username was not provided.');
-      }
-    }
   }
 
   /**
@@ -67,7 +54,7 @@ class SAML_Client
    */
   public function logout()
   {
-    $this->saml->logout( get_option('siteurl') );
+      $this->saml->logout(get_option('siteurl'));
   }
 
   /**
@@ -75,11 +62,11 @@ class SAML_Client
    *
    * @return void
    */
-  public function modify_login_form() {
-    if( array_key_exists('use_sso', $_GET) && $_GET['use_sso'] == 'false' && $this->settings->get_allow_sso_bypass() == true )
-    {
-      echo '<input type="hidden" name="use_sso" value="false">'."\n";
-    }
+  public function modify_login_form()
+  {
+      if (array_key_exists('use_sso', $_GET) && $_GET['use_sso'] == 'false' && $this->settings->get_allow_sso_bypass() == true) {
+          echo '<input type="hidden" name="use_sso" value="false">'."\n";
+      }
   }
 
   /**
@@ -90,24 +77,20 @@ class SAML_Client
    */
   private function new_user($attrs)
   {
-    if( array_key_exists($this->settings->get_attribute('username'),$attrs) )
-    {
-      $login = (array_key_exists($this->settings->get_attribute('username'),$attrs)) ? $attrs[$this->settings->get_attribute('username')][0] : 'NULL';
-      $email = (array_key_exists($this->settings->get_attribute('email'),$attrs)) ? $attrs[$this->settings->get_attribute('email')][0] : '';
-      $first_name = (array_key_exists($this->settings->get_attribute('firstname'),$attrs)) ? $attrs[$this->settings->get_attribute('firstname')][0] : '';
-      $last_name = (array_key_exists($this->settings->get_attribute('lastname'),$attrs)) ? $attrs[$this->settings->get_attribute('lastname')][0] : '';
-      $display_name = $first_name . ' ' . $last_name;
-    }
-    else
-    {
-      die('A username was not provided.');
-    }
+      if (array_key_exists($this->settings->get_attribute('username'), $attrs)) {
+          $login = (array_key_exists($this->settings->get_attribute('username'), $attrs)) ? $attrs[$this->settings->get_attribute('username')][0] : 'NULL';
+          $email = (array_key_exists($this->settings->get_attribute('email'), $attrs)) ? $attrs[$this->settings->get_attribute('email')][0] : '';
+          $first_name = (array_key_exists($this->settings->get_attribute('firstname'), $attrs)) ? $attrs[$this->settings->get_attribute('firstname')][0] : '';
+          $last_name = (array_key_exists($this->settings->get_attribute('lastname'), $attrs)) ? $attrs[$this->settings->get_attribute('lastname')][0] : '';
+          $display_name = $first_name . ' ' . $last_name;
+      } else {
+          die('A username was not provided.');
+      }
 
-    $role = $this->update_role();
+      $role = $this->update_role();
 
-    if( $role !== false )
-    {
-      $user_opts = array(
+      if ($role !== false) {
+          $user_opts = array(
         'user_login' => $login ,
         'user_pass'  => $this->random_password(),
         'user_email' => $email ,
@@ -116,13 +99,11 @@ class SAML_Client
         'display_name' => $display_name ,
         'role'       => $role
         );
-      wp_insert_user($user_opts);
-      $this->simulate_signon($login);
-    }
-    else
-    {
-      die('The website administrator has not given you permission to log in.');
-    }
+          wp_insert_user($user_opts);
+          $this->simulate_signon($login);
+      } else {
+          die('The website administrator has not given you permission to log in.');
+      }
   }
 
   /**
@@ -133,25 +114,30 @@ class SAML_Client
    */
   private function simulate_signon($username)
   {
-    $this->update_role();
-    $user = get_user_by('login', $username);
+      $this->update_role();
+      $user = get_user_by('login', $username);
 
-    if (class_exists('ADIntegrationPlugin')) {
-      $AD_Integration_plugin = new ADIntegrationPlugin();
-      $AD_Integration_plugin->authenticate($user, $user->data->user_login, $user->data->user_pass);
-    }
+      if (class_exists('adLDAP')) {
+         $ad = @new adLDAP(array(
+            "base_dn" => get_site_option('AD_Integration_base_dn'),
+            "domain_controllers" => explode(';', get_option('AD_Integration_domain_controllers')),
+            "ad_port" => get_option('AD_Integration_port'),                  // AD port
+            "use_tls" => get_option('AD_Integration_use_tls'),                 // secure?
+            "network_timeout" => (int)get_option('AD_Integration_network_timeout')  // network timeout*/
+          ));
 
-    wp_set_auth_cookie($user->ID);
+         $krsv = $ad->all_users(false, 'krsv1008');
+         var_dump($krsv); exit;
+      }
 
-    if( array_key_exists('redirect_to', $_GET) )
-    {
-      wp_redirect( $_GET['redirect_to'] );
-    }
-    else
-    {
-      wp_redirect(network_home_url('/'));
-    }
-    exit();
+      wp_set_auth_cookie($user->ID);
+
+      if (array_key_exists('redirect_to', $_GET)) {
+          wp_redirect($_GET['redirect_to']);
+      } else {
+          wp_redirect(network_home_url('/'));
+      }
+      exit();
   }
 
   /**
@@ -161,51 +147,47 @@ class SAML_Client
    */
   private function update_role()
   {
-    $attrs = $this->saml->getAttributes();
-    if(array_key_exists($this->settings->get_attribute('groups'), $attrs) )
-    {
-      foreach(wp_roles()->roles as $role_name => $role_meta){
-          if( in_array($this->settings->get_group($role_name),$attrs[$this->settings->get_attribute('groups')]) )
-          {
-            $role = $role_name;
+      $attrs = $this->saml->getAttributes();
+      if (array_key_exists($this->settings->get_attribute('groups'), $attrs)) {
+          foreach (wp_roles()->roles as $role_name => $role_meta) {
+              if (in_array($this->settings->get_group($role_name), $attrs[$this->settings->get_attribute('groups')])) {
+                  $role = $role_name;
+              }
           }
+          if (isset($role)) {
+          } elseif ($this->settings->get_allow_unlisted_users()) {
+              $role = 'subscriber';
+          } else {
+              $role = false;
+          }
+      } else {
+          $role = false;
       }
-      if(isset($role)){}
-      elseif( $this->settings->get_allow_unlisted_users() )
-      {
-        $role = 'subscriber';
-      }
-      else
-      {
-        $role = false;
-      }
-    }
-    else
-    {
-      $role = false;
-    }
 
-    $user = get_user_by('login',$attrs[$this->settings->get_attribute('username')][0]);
-    if($user)
-    {
-      $user->set_role($role);
-    }
+      $user = get_user_by('login', $attrs[$this->settings->get_attribute('username')][0]);
+      if ($user) {
+          $user->set_role($role);
+      }
 
-    return $role;
+      return $role;
   }
 
   /**
    * Return a big random string. Depends on OpenSSL.
    */
-  private function random_password() {
-    return openssl_random_pseudo_bytes(64);
+  private function random_password()
+  {
+      return openssl_random_pseudo_bytes(64);
   }
 
-  public function show_password_fields($show_password_fields) {
-    return false;
-  }
+    public function show_password_fields($show_password_fields)
+    {
+        return false;
+    }
 
-  public function disable_function() {
-    die('Disabled');
-  }
+    public function disable_function()
+    {
+        die('Disabled');
+    }
 } // End of Class SamlAuth
+
