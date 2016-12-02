a522e2e31e730b510a0a339483dfc3ec6118367f
Sebastian Thulin <sebastian.thulin@gmail.com>
2016-12-02 15:00:40 +0100

    Cose admin interface
    Planning to switch servers.

--- wp-content/mu-plugins/BeforeLiveTemporaryAdmin.php
+++ wp-content/mu-plugins/BeforeLiveTemporaryAdmin.php
@@ -17,8 +17,14 @@ class BeforeLiveTemporaryAdmin
     public function __construct()
     {
         if (in_array($_SERVER['HTTP_HOST'], array('beta.intranat.helsingborg.se'))) {
-            if (is_admin() && (! defined('DOING_AJAX') || (defined('DOING_AJAX') && !DOING_AJAX))) {
+            if (is_admin() && (!defined('DOING_AJAX') || (defined('DOING_AJAX') && !DOING_AJAX))) {
                 wp_die('Vi har flyttat intanätet hit: <a href="https://intranat.helsingborg.se/">intranat.helsingborg.se</a>.');
+            }
+        }
+
+        if (in_array($_SERVER['HTTP_HOST'], array('intranat.helsingborg.se'))) {
+            if (is_admin() && (!defined('DOING_AJAX') || (defined('DOING_AJAX') && !DOING_AJAX))) {
+                wp_die('På grund av byte av server kan du just nu inte redigera information på intranätet. Om du har frågor kring detta, vänligen kontakta servicedesk.');
             }
         }
     }
