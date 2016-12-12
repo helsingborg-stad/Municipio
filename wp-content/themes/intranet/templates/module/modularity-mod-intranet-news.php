4fd90021ab19b7922156ed5d1fa6a99584e149f2
Kristoffer Svanmark <kristoffer.svanmark@lexiconitkonsult.se>
2016-12-12 09:22:17 +0100

    Use intranet-news as cache key for news feed

--- wp-content/themes/intranet/templates/module/modularity-mod-intranet-news.php
+++ wp-content/themes/intranet/templates/module/modularity-mod-intranet-news.php
@@ -29,7 +29,7 @@
             break;
     }
 
-    $cache = new Municipio\Helper\Cache($module->post_type, array(
+    $cache = new Municipio\Helper\Cache('intranet-news', array(
         Intranet\User\Subscription::getSubscriptions(get_current_user_id()),
         $display,
         $limit,
