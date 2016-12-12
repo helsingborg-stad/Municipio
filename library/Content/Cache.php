27db49282f45c699fd0d048534cc96ff6c6f642e
Kristoffer Svanmark <kristoffer.svanmark@lexiconitkonsult.se>
2016-12-12 13:24:40 +0100

    Empty cache on post delete and post trash

--- library/Content/Cache.php
+++ library/Content/Cache.php
@@ -7,5 +7,11 @@ class Cache
     public function __construct()
     {
         add_action('save_post', '\Municipio\Helper\Cache::clearCache', 10, 2);
+        add_action('pre_delete_post', '\Municipio\Helper\Cache::clearCache', 10, 2);
+
+        add_action('trashed_post', function ($postId) {
+            $post = get_post($postId);
+            \Municipio\Helper\Cache::clearCache($postId, $post);
+        });
     }
 }
