14ef963602157af7bfe42b60b1ee0f1ed977bb3e
Kristoffer Svanmark <kristoffer.svanmark@lexiconitkonsult.se>
2016-12-12 14:08:00 +0100

    Updated translations

--- wp-content/themes/intranet/views/partials/404/login.blade.php
+++ wp-content/themes/intranet/views/partials/404/login.blade.php
@@ -2,11 +2,11 @@
     <div class="grid">
         <div class="grid-lg-3 grid-md-2 grid-sm-12 hidden-xs hidden-sm no-margin no-padding"></div>
         <div class="grid-lg-6 grid-md-8 grid-sm-12">
-           <span class="h1">Du måste logga in för att se den här sidan</span>
+           <span class="h1"><?php _e('You need to login to be able to view this page', 'municipio-intranet'); ?>Du måste logga in för att se den här sidan</span>
 
             @if (isset($_GET['login']) && $_GET['login'] === 'failed')
                 <div class="notice info margin-top">
-                     <i class="pricon pricon-notice-warning"></i> Det användarnamn eller lösenord som du angav är felaktigt.
+                     <i class="pricon pricon-notice-warning"></i> <?php _e('Username or password is incorrect.', 'municipio-intranet'); ?>
                 </div>
             @endif
 
