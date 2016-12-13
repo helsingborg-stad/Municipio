362002f96641d7bff2e0c1e7c2cb839daf0aa56d
Kristoffer Svanmark <kristoffer.svanmark@lexiconitkonsult.se>
2016-12-13 13:14:36 +0100

    Fix pre_post_delete cache deletion

--- library/Content/Cache.php
+++ library/Content/Cache.php
@@ -7,7 +7,11 @@ class Cache
     public function __construct()
     {
         add_action('save_post', '\Municipio\Helper\Cache::clearCache', 10, 2);
-        add_action('pre_delete_post', '\Municipio\Helper\Cache::clearCache', 10, 2);
+
+        add_filter('pre_delete_post', function ($delete, $post) {
+            \Municipio\Helper\Cache::clearCache($post->ID, $post);
+            return $delete;
+        }, 10, 2);
 
         add_action('trashed_post', function ($postId) {
             $post = get_post($postId);
