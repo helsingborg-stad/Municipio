15ba869171f98bd1e89d59094d92a0ff3733fde8
Kristoffer Svanmark <kristoffer.svanmark@lexiconitkonsult.se>
2016-12-20 14:31:17 +0100

    Insert empty row

--- wp-content/themes/intranet/library/User/Login.php
+++ wp-content/themes/intranet/library/User/Login.php
@@ -9,6 +9,7 @@ class Login
         add_action('wp_authenticate', function () {
             $_GET['redirect_to'] = network_home_url('/');
         }, 9);
+
         add_action('wp_login', array($this, 'adMapping'), 10, 2);
         add_action('wp_login_failed', array($this, 'frontendLoginFailed'));
         add_action('wp_logout', array($this, 'frontendLogout'), 9);
