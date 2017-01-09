1f18e121d7857691cff9c63a3c88410738dfe43e
Kristoffer Svanmark <kristoffer.svanmark@lexiconitkonsult.se>
2017-01-09 16:24:30 +0100

    Save public post types in a site option to be able to get them for search usage

--- wp-content/themes/intranet/library/Helper/PostType.php
+++ wp-content/themes/intranet/library/Helper/PostType.php
@@ -4,20 +4,44 @@ namespace Intranet\Helper;
 
 class PostType
 {
-    public static function getPublic($filter = null)
+    public function __construct()
     {
-        $postTypes = get_post_types(array(
-            'public' => true,
-            'exclude_from_search' => false
-        ));
+        add_action('registered_post_type', array($this, 'registeredPostType'), 10, 2);
+    }
 
-        // Filters out given post types
-        if ($filter) {
-            $postTypes = array_filter($postTypes, function ($postType) use ($filter) {
-                return !in_array($postType, (array) $filter);
-            });
+    public function registeredPostType($postType, $postTypeObject)
+    {
+        if (!$postTypeObject->public || $postTypeObject->exclude_from_search) {
+            return;
         }
 
-        return array_values(array_diff($postTypes, array('nav_menu_item', 'revision', 'hbg-alarm', 'incidents')));
+        $postTypes = get_site_option('public_post_types', array());
+        $postTypes[] = $postType;
+
+        update_site_option('public_post_types', array_unique($postTypes));
+    }
+
+    public static function getPublic($filter = null, $useSiteOption = true)
+    {
+        $postTypes = array();
+
+        if ($useSiteOption && is_multisite()) {
+            $postTypes = get_site_option('public_post_types', array());
+        } else {
+            $args = array(
+                'public' => true,
+                'exclude_from_search' => false
+            );
+
+            $postTypes = get_post_types($args);
+        }
+
+        // Filters out given post types
+        $filter = array_merge(
+            (array) $filter,
+            array('nav_menu_item', 'revision', 'hbg-alarm', 'incidents')
+        );
+
+        return array_values(array_diff($postTypes, $filter));
     }
 }
