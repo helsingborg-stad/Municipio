34aa994f5bdff0454be2eefcf606124b23d68095
Kristoffer Svanmark <kristoffer.svanmark@lexiconitkonsult.se>
2017-01-23 16:40:28 +0100

    Inline styled term template

--- views/partials/blog/post-terms.blade.php
+++ views/partials/blog/post-terms.blade.php
@@ -1,5 +1,5 @@
-<h5>{{ get_taxonomy($taxonomy)->label }}</h5>
-<ul class="tags">
+<strong>{{ get_taxonomy($taxonomy)->label }}:</strong>
+<ul class="inline-block nav-horizontal tags">
     @foreach ($terms as $term)
     <li><a href="{{ get_term_link($term, $taxonomy) }}" class="tag tag-{{ $term->taxonomy }} tag-{{ $term->slug }}">{{ $term->name }}</a></li>
     @endforeach
