51d599174118c3b1d5ad712e6b3e47ddd9626ff1
Kristoffer Svanmark <kristoffer.svanmark@lexiconitkonsult.se>
2017-01-24 16:15:24 +0100

    Post terms gutte

--- views/partials/blog/post-terms.blade.php
+++ views/partials/blog/post-terms.blade.php
@@ -1,6 +1,8 @@
-<strong>{{ get_taxonomy($taxonomy)->label }}:</strong>
-<ul class="inline-block nav-horizontal tags">
-    @foreach ($terms as $term)
-    <li><a href="{{ get_term_link($term, $taxonomy) }}" class="tag tag-{{ $term->taxonomy }} tag-{{ $term->slug }}">{{ $term->name }}</a></li>
-    @endforeach
-</ul>
+<div class="gutter gutter-top">
+    <strong>{{ get_taxonomy($taxonomy)->label }}:</strong>
+    <ul class="inline-block nav-horizontal tags">
+        @foreach ($terms as $term)
+        <li><a href="{{ get_term_link($term, $taxonomy) }}" class="tag tag-{{ $term->taxonomy }} tag-{{ $term->slug }}">{{ $term->name }}</a></li>
+        @endforeach
+    </ul>
+</div>
