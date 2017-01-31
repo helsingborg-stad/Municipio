8d25726e5c14c6b8f297c6b3ff03c674dee4635d
Kristoffer Svanmark <kristoffer.svanmark@lexiconitkonsult.se>
2017-01-31 11:47:43 +0100

    Full width template available for all post types

--- library/Theme/CustomTemplates.php
+++ library/Theme/CustomTemplates.php
@@ -8,7 +8,8 @@ class CustomTemplates
     {
         \Municipio\Helper\Template::add(
             __('Full width', 'municipio'),
-            \Municipio\Helper\Template::locateTemplate('full-width.blade.php')
+            \Municipio\Helper\Template::locateTemplate('full-width.blade.php'),
+            'all'
         );
     }
 }
