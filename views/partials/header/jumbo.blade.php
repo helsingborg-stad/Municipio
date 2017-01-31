467df7971dc45163242d20ac394cbdc951b21e65
Kristoffer Svanmark <kristoffer.svanmark@lexiconitkonsult.se>
2017-01-31 09:45:25 +0100

    Centered jumbo header

--- views/partials/header/jumbo.blade.php
+++ views/partials/header/jumbo.blade.php
@@ -13,13 +13,13 @@
         <div class="grid">
             <div class="grid-xs-12 {!! apply_filters('Municipio/header_grid_size', 'grid-md-12'); !!}">
                 <div class="grid">
-                    <div class="grid-sm-12 grid-md-4">
+                    <div class="{{ get_field('header_centered', 'option') ? 'grid-md-12' : 'grid-sm-12 grid-md-4' }}">
                         {!! municipio_get_logotype(get_field('header_logotype', 'option'), get_field('logotype_tooltip', 'option')) !!}
                         <a href="#mobile-menu" data-target="#mobile-menu" class="{!! apply_filters('Municipio/mobile_menu_breakpoint', 'hidden-md hidden-lg'); !!} menu-trigger"><span class="menu-icon"></span></a>
                     </div>
 
                     @if (get_field('nav_primary_enable', 'option') === true)
-                    <div class="grid-md-8 text-right {!! apply_filters('Municipio/desktop_menu_breakpoint', 'hidden-xs hidden-sm'); !!}">
+                    <div class="{{ get_field('header_centered', 'option') ? 'grid-md-12' : 'grid-md-8 text-right' }} {!! apply_filters('Municipio/desktop_menu_breakpoint', 'hidden-xs hidden-sm'); !!}">
                         <nav class="nav-group-overflow" data-btn-width="100">
                             {!! $navigation['mainMenu'] !!}
 
