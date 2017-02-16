5d7a2cbd03eb4636b9ec2705acd2e5a6629e6b12
Kristoffer Svanmark <kristoffer.svanmark@knowit.se>
2017-02-16 09:09:50 +0100

    Altering grid for archive

--- library/Controller/Archive.php
+++ library/Controller/Archive.php
@@ -4,11 +4,110 @@ namespace Municipio\Controller;
 
 class Archive extends \Municipio\Controller\BaseController
 {
+    private static $gridSize;
+
+    private static $randomGridBase = array();
+    private static $gridRow = array();
+    private static $gridColumns = array();
+
+
     public function init()
     {
         $this->data['postType'] = get_post_type();
         $this->data['template'] = !empty(get_field('archive_' . sanitize_title(get_post_type()) . '_post_style', 'option')) ? get_field('archive_' . sanitize_title(get_post_type()) . '_post_style', 'option') : 'collapsed';
         $this->data['grid_size'] = !empty(get_field('archive_' . sanitize_title(get_post_type()) . '_grid_columns', 'option')) ? get_field('archive_' . sanitize_title(get_post_type()) . '_grid_columns', 'option') : 'grid-md-6';
+
         $this->data['grid_alter'] = get_field('archive_' . sanitize_title(get_post_type()) . '_grid_columns_alter', 'option') ? true : false;
+        $this->data['gridSize'] = (int)str_replace('-', '', filter_var($this->data['grid_size'], FILTER_SANITIZE_NUMBER_INT));
+        self::$gridSize = $this->data['gridSize'];
+
+        if ($this->data['grid_alter']) {
+            $this->gridAlterColumns();
+        }
+    }
+
+    public function gridAlterColumns()
+    {
+        $gridRand = array();
+        switch ($this->data['gridSize']) {
+            case 12:
+                $gridRand = array(
+                    array(12)
+                );
+                break;
+
+            case 6:
+                $gridRand = array(
+                    array(12),
+                    array(6, 6),
+                    array(6, 6)
+                );
+                break;
+
+            case 4:
+                $gridRand = array(
+                    array(8, 4),
+                    array(4, 4, 4),
+                    array(4, 8)
+                );
+                break;
+
+            case 3:
+                $gridRand = array(
+                    array(6, 3, 3),
+                    array(3, 3, 3, 3),
+                    array(3, 3, 6),
+                    array(3, 3, 3, 3),
+                    array(3, 6, 3)
+                );
+                break;
+
+            default:
+                $gridRand = array(
+                    array(12)
+                );
+                break;
+        }
+
+        self::$randomGridBase = $gridRand;
+    }
+
+    public static function getColumnSize()
+    {
+        if (empty(self::$gridRow)) {
+            self::$gridRow = self::$randomGridBase;
+        }
+
+        if (empty(self::$gridColumns)) {
+            self::$gridColumns = self::$gridRow[0];
+            array_shift(self::$gridRow);
+        }
+
+        $columnSize = 'grid-md-' . self::$gridColumns[0];
+        array_shift(self::$gridColumns);
+
+        return $columnSize;
+    }
+
+    public static function getColumnHeight()
+    {
+        switch (self::$gridSize) {
+            case 3:
+                return '280px';
+
+            case 4:
+                return '400px';
+
+            case 6:
+                return '500px';
+
+            case 12:
+                return '500px';
+
+            default:
+                return false;
+        }
+
+        return false;
     }
 }
--- views/partials/blog/type/post-grid.blade.php
+++ views/partials/blog/type/post-grid.blade.php
@@ -6,38 +6,11 @@ $thumbnail = municipio_get_thumbnail_source(
     array(500, 500)
 );
 
-$gridSizeInt = (int)str_replace('-', '', filter_var($grid_size, FILTER_SANITIZE_NUMBER_INT));
-$gridAlterSize = $gridSizeInt * 2;
-
-$columnSize = 'grid-md-' . $gridSizeInt;
-$columnHeight = false;
-
-if ($grid_alter) {
-    switch ($gridSizeInt) {
-        case 3:
-            $columnHeight = '280px';
-            break;
-
-        case 4:
-            $columnHeight = '400px';
-            break;
-
-        case 6:
-            $columnHeight = '500px';
-            break;
-
-        default:
-            $columnHeight = false;
-            break;
-    }
-
-    if ($gridSizeInt !== 12 && floor($postNum % (12 / $gridSizeInt) === 0)) {
-        $columnSize = 'grid-md-' . $gridAlterSize;
-    }
-}
+$columnSize = \Municipio\Controller\Archive::getColumnSize();
+$columnHeight = \Municipio\Controller\Archive::getColumnHeight();
 ?>
 <div class="{{ $columnSize }}">
-    <a href="{{ the_permalink() }}" class="box box-post-brick" <?php echo ($columnHeight) ? 'style="padding-bottom:0;height:' . $columnHeight . '"' : ''; ?>>
+    <a href="{{ the_permalink() }}" class="box box-post-brick" <?php echo $columnHeight ? 'style="padding-bottom:0;height:' . $columnHeight . '"' : ''; ?>>
         @if ($thumbnail)
         <div class="box-image" {!! $thumbnail ? 'style="background-image:url(' . $thumbnail . ');"' : '' !!}>
             <img src="{{ municipio_get_thumbnail_source(null,array(500,500)) }}" alt="{{ the_title() }}">
