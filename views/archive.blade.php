ab33dd8a5f2c6d010e347e0a9c9e1b3e47d0499f
Kristoffer Svanmark <kristoffer.svanmark@knowit.se>
2017-02-16 16:07:28 +0100

    Fix indentation

--- views/archive.blade.php
+++ views/archive.blade.php
@@ -28,19 +28,19 @@
                 <div class="grid-xs-12">
                     @if (get_field('archive_' . sanitize_title($postType) . '_title', 'option'))
                         @if (is_category())
-                        <h1>{{ get_field('archive_' . sanitize_title($postType) . '_title', 'option') }}: {{ single_cat_title() }}</h1>
+                            <h1>{{ get_field('archive_' . sanitize_title($postType) . '_title', 'option') }}: {{ single_cat_title() }}</h1>
                         {!! category_description() !!}
                         @elseif (is_date())
-                        <h1>{{ get_field('archive_' . sanitize_title($postType) . '_title', 'option') }}: {{ the_archive_title() }}</h1>
+                            <h1>{{ get_field('archive_' . sanitize_title($postType) . '_title', 'option') }}: {{ the_archive_title() }}</h1>
                         @else
-                        <h1>{{ get_field('archive_' . sanitize_title($postType) . '_title', 'option') }}</h1>
+                            <h1>{{ get_field('archive_' . sanitize_title($postType) . '_title', 'option') }}</h1>
                         @endif
                     @else
                         @if (is_category())
-                        <h1>{{ single_cat_title() }}</h1>
+                            <h1>{{ single_cat_title() }}</h1>
                         {!! category_description() !!}
                         @elseif (is_date())
-                        <h1>{{ the_archive_title() }}</h1>
+                            <h1>{{ the_archive_title() }}</h1>
                         @endif
                     @endif
                 </div>
