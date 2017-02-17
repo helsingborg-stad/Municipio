96023af884cc0f0240f168c9a5af7bd1f8c45042
Kristoffer Svanmark <kristoffer.svanmark@knowit.se>
2017-02-17 15:48:29 +0100

    Editor pricon dialog

--- assets/source/mce-js/mce-pricons.js
+++ assets/source/mce-js/mce-pricons.js
@@ -5,9 +5,51 @@
             icon: 'pricon-smiley-cool',
             context: 'insert',
             tooltip: 'Pricon',
-            onclick: function(e) {
+            cmd: 'openInsertPiconModal'
+        });
 
-            }
+
+        editor.addCommand('openInsertPiconModal', function() {
+            editor.windowManager.open({
+                title: 'Pricons',
+                url: url + '/mce-picons.php',
+                width: 500,
+                height: 400,
+                buttons: [
+                    {
+                        text: 'Insert',
+                        onclick: function(e) {
+                            var $iframe = jQuery('.mce-container-body.mce-window-body.mce-abs-layout iframe').contents();
+                            var size = $iframe.find('[name="pricon-size"]').val();
+                            var color = $iframe.find('[name="pricon-color"]').val();
+                            var icon = $iframe.find('[name="pricon-icon"]').val();
+
+                            if (!icon.length) {
+                                editor.windowManager.close();
+                                return false;
+                            }
+
+                            var shortcode = '[pricon icon="' + icon + '"';
+
+                            if (color.length) {
+                                shortcode = shortcode + ' color="' + color + '"';
+                            }
+
+                            if (size.length) {
+                                shortcode = shortcode + ' size="' + size + '"';
+                            }
+
+                            shortcode = shortcode + ']';
+
+                            editor.insertContent(shortcode);
+
+                            editor.windowManager.close();
+                            return true;
+                        }
+                    }
+                ]
+            });
+
         });
     });
 })();
