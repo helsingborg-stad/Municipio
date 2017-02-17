55f52231ce810334c51df02c7687e12627bc3efa
Kristoffer Svanmark <kristoffer.svanmark@knowit.se>
2017-02-17 09:55:03 +0100

    Pricon shortcode

--- library/Content/ShortCode.php
+++ library/Content/ShortCode.php
@@ -13,6 +13,44 @@ class ShortCode
     {
         add_shortcode('meta', array($this,'displayMetaValue'));
         add_shortcode('explain', array($this, 'explain'));
+        add_shortcode('pricon', array($this, 'pricon'));
+    }
+
+    /**
+     * Insert icons with shortcode
+     * Example: [pricon icon="dog" color="#ff0000" size="5"]
+     * @param  array $atts     Shortcode attributes
+     * @param  string $content
+     * @return string          Rendered shortcode html
+     */
+    public function pricon($atts, $content = '')
+    {
+        if (!isset($atts['icon'])) {
+            return '';
+        }
+
+        $classes = array(
+            'pricon',
+            'pricon-' . $atts['icon']
+        );
+
+        if (isset($atts['size'])) {
+            if (is_numeric($atts['size'])) {
+                $classes[] = 'pricon-' . $atts['size'] . 'x';
+            } else {
+                $classes[] = 'pricon-' . $atts['size'];
+            }
+        }
+
+        $html = '<i class="' . implode(' ', $classes) . '"';
+
+        if (isset($atts['color']) && !empty($atts['color'])) {
+            $html .= ' style="color: ' . $atts['color'] . ';"';
+        }
+
+        $html .= '></i>';
+
+        return $html;
     }
 
     /**
