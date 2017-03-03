3b517e09224f8c4e3e71003f53e6237a8a650555
Kristoffer Svanmark <kristoffer.svanmark@knowit.se>
2017-03-03 08:54:34 +0100

    Remove placeholder option from news module

--- wp-content/themes/intranet/templates/module/modularity-mod-intranet-news-item.php
+++ wp-content/themes/intranet/templates/module/modularity-mod-intranet-news-item.php
@@ -1,11 +1,9 @@
 <div class="grid-lg-12">
     <a <?php if (is_super_admin()) : ?>title="Rank: <?php echo round($item->rank_percent, 3); ?>%"<?php endif; ?> href="<?php echo get_blog_permalink($item->blog_id, $item->ID); ?>" class="<?php echo implode(' ', apply_filters('Modularity/Module/Classes', array('box', 'box-news', 'box-news-horizontal'), $module->post_type, $args)); ?> <?php echo $item->is_sticky ? 'is-sticky' : ''; ?>">
-        <?php if (!in_array($args['id'], array('content-area', 'content-area-top')) && ($hasImages || (!get_field('placeholders', $module->ID) && $item->thumbnail_image))) : ?>
+        <?php if (!in_array($args['id'], array('content-area', 'content-area-top')) && $item->thumbnail_image) : ?>
             <div class="box-image-container">
                 <?php if ($item->thumbnail_image && $item->image) : ?>
                 <img class="box-image" src="<?php echo $i === 1 ? $item->image[0] : $item->thumbnail_image[0]; ?>" alt="<?php echo $item->post_title; ?>">
-                <?php else : ?>
-                <figure class="image-placeholder"></figure>
                 <?php endif; ?>
             </div>
         <?php endif; ?>
@@ -17,12 +15,8 @@
             <h3 class="box-title text-highlight"><?php echo apply_filters('the_title', $item->post_title); ?></h3>
 
             <article class="clearfix">
-                <?php if (in_array($args['id'], array('content-area', 'content-area-top')) && ((isset($hasImages) && $hasImages) || (!get_field('placeholders', $module->ID) && $item->thumbnail_image))) : ?>
-                    <?php if ($item->thumbnail_image) : ?>
+                <?php if (in_array($args['id'], array('content-area', 'content-area-top')) && $item->thumbnail_image) : ?>
                     <img src="<?php echo $item->thumbnail_image[0]; ?>" alt="<?php echo $item->post_title; ?>">
-                    <?php else : ?>
-                    <figure class="image-placeholder"></figure>
-                    <?php endif; ?>
                 <?php endif; ?>
 
                 <p>
