ed172c7ffceed18ca7101a8e77e8c0950a0ab352
Jonatan Hanson <jonatan.hanson@hiq.se>
2017-04-21 09:19:44 +0200

    Show breadcrumbs on custom post type pages

--- library/Theme/Navigation.php
+++ library/Theme/Navigation.php
@@ -175,46 +175,67 @@ class Navigation
     public static function outputBreadcrumbs()
     {
         global $post;
-
         $title = get_the_title();
+        $post_type = get_post_type_object($post->post_type);
         $output = array();
 
         echo '<ol class="breadcrumbs" itemscope itemtype="http://schema.org/BreadcrumbList">';
 
         if (!is_front_page()) {
-            if (is_single()) {
-                $output[] = '<li>' . get_the_title() . '</li>';
-            } elseif (is_category()) {
-                $output[] = '<li>' . get_the_category() . '</li>';
-            } elseif (is_page()) {
+            $int = 1;
+            $output[] = '<li itemscope itemprop="itemListElement" itemtype="http://schema.org/ListItem">
+                            <a itemprop="item" href="' . get_home_url() . '" title="' . __('Home') . '">
+                            <span itemprop="name">' . __('Home') . '</span><meta itemprop="position" content="' . $int++ . '"></a>
+                        </li>';
+
+            if (is_single() && get_post_type_archive_link($post->post_type)) {
+                $title = get_the_title();
+                $archive_title = $post_type->label;
+                $output[] = '<li itemscope itemprop="itemListElement" itemtype="http://schema.org/ListItem">
+                                <a itemprop="item" href="' . get_post_type_archive_link($post->post_type) . '" title="' . $archive_title . '">
+                                <span itemprop="name">' . $archive_title . '</span><meta itemprop="position" content="' . $int++ . '"></a>
+                            </li>';
+            }
+
+            if (is_page() || (is_single() && $post_type->hierarchical == true)) {
                 if ($post->post_parent) {
                     $anc = array_reverse(get_post_ancestors($post->ID));
                     $title = get_the_title();
 
-                    $int = 1;
                     foreach ($anc as $ancestor) {
                         if (get_post_status($ancestor) != 'private') {
                             $output[] = '<li itemscope itemprop="itemListElement" itemtype="http://schema.org/ListItem">
                                             <a itemprop="item" href="' . get_permalink($ancestor) . '" title="' . get_the_title($ancestor) . '">
                                                 <span itemprop="name">' . get_the_title($ancestor) . '</span>
-                                                <meta itemprop="position" content="' . $int . '" />
+                                                <meta itemprop="position" content="' . $int++ . '" />
                                             </a>
                                        </li>';
-
-                            $int++;
                         }
                     }
 
                     $output[] = '<li itemscope itemprop="itemListElement" itemtype="http://schema.org/ListItem">
-                            <span itemprop="name" class="breadcrumbs-current" title="' . $title . '">' . $title . '</span>
-                            <meta itemprop="position" content="' . ($int+1) . '" />
-                          </li>';
+                                    <span itemprop="name" class="breadcrumbs-current" title="' . $title . '">' . $title . '</span>
+                                    <meta itemprop="position" content="' . ($int++) . '" />
+                                </li>';
                 } else {
                     $output[] = '<li itemscope itemprop="itemListElement" itemtype="http://schema.org/ListItem">
-                            <span itemprop="name" class="breadcrumbs-current">' . get_the_title() . '</span>
-                            <meta itemprop="position" content="1" />
-                          </li>';
+                                    <span itemprop="name" class="breadcrumbs-current">' . get_the_title() . '</span>
+                                    <meta itemprop="position" content="1" />
+                                </li>';
                 }
+            } else {
+                if (is_home()) {
+                    $title = single_post_title();
+                } elseif (is_category()) {
+                    $title = get_the_category();
+                } elseif (is_archive()) {
+                    $title = post_type_archive_title(null, false);
+                } else {
+                    $title = get_the_title();
+                }
+                $output[] = '<li itemscope itemprop="itemListElement" itemtype="http://schema.org/ListItem">
+                                <span itemprop="name">' . $title . '</span><meta itemprop="position" content="' . $int++ . '" />
+                            </li>';
             }
         }
 
