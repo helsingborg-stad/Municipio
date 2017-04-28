d25a93740df97d153c40be198f67480abd45e704
Jonatan Hanson <jonatan.hanson@hiq.se>
2017-04-28 13:18:56 +0200

    Title on event archives

--- views/archive-event.blade.php
+++ views/archive-event.blade.php
@@ -23,6 +23,30 @@
 
         <div class="{{ $cols }}">
 
+            @if (get_field('archive_' . sanitize_title($postType) . '_title', 'option') || is_category() || is_date())
+            <div class="grid">
+                <div class="grid-xs-12">
+                    @if (get_field('archive_' . sanitize_title($postType) . '_title', 'option'))
+                        @if (is_category())
+                            <h1>{{ get_field('archive_' . sanitize_title($postType) . '_title', 'option') }}: {{ single_cat_title() }}</h1>
+                        {!! category_description() !!}
+                        @elseif (is_date())
+                            <h1>{{ get_field('archive_' . sanitize_title($postType) . '_title', 'option') }}: {{ the_archive_title() }}</h1>
+                        @else
+                            <h1>{{ get_field('archive_' . sanitize_title($postType) . '_title', 'option') }}</h1>
+                        @endif
+                    @else
+                        @if (is_category())
+                            <h1>{{ single_cat_title() }}</h1>
+                        {!! category_description() !!}
+                        @elseif (is_date())
+                            <h1>{{ the_archive_title() }}</h1>
+                        @endif
+                    @endif
+                </div>
+            </div>
+            @endif
+
             @if (is_active_sidebar('content-area-top'))
                 <div class="grid sidebar-content-area sidebar-content-area-top">
                     <?php dynamic_sidebar('content-area-top'); ?>
