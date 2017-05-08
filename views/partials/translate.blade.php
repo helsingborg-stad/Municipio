af37255b553d8c29164867ec618d6126d7dcfb27
Kristoffer Svanmark <kristoffer.svanmark@knowit.se>
2017-05-08 10:03:15 +0200

    Use home_url instead of site_url in translate

--- views/partials/translate.blade.php
+++ views/partials/translate.blade.php
@@ -4,7 +4,7 @@
             <div class="grid-sm-12">
                 <h3>Translate</h3>
                 <p>
-                    You can use Google Translate to translate the contents of {{ explode('://', get_site_url())[1] }}. To do that, select the language you would like to translate into in the lost below.
+                    You can use Google Translate to translate the contents of {{ explode('://', get_home_url())[1] }}. To do that, select the language you would like to translate into in the lost below.
                     <br><small>Please bear in mind, since the Google Translate is an automatically generated translation, we do not take any responsibility for errors in the text.</small>
                 </p>
                 <div id="google-translate-element" class="gutter gutter-vertical"></div>
