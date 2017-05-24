0d51891902c59d6ee8f29b538ecb5df278119191
Kristoffer Svanmark <kristoffer.svanmark@knowit.se>
2017-05-24 11:15:34 +0200

    NavigationTree fixes for ajax

--- library/Helper/NavigationTree.php
+++ library/Helper/NavigationTree.php
@@ -250,6 +250,7 @@ class NavigationTree
         if (count($children) > 0) {
             $hasChildren = true;
             $attributes['class'][] = 'has-children';
+            $attributes['class'][] = 'has-sub-menu';
             $attributes['data-page-id'] = $pageId;
         }
 
@@ -477,7 +478,7 @@ class NavigationTree
         }
 
         $this->addOutput(sprintf(
-            '<li%1$s><a href="%2$s">%3$s</a>',
+            '<li%1$s><span><a href="%2$s">%3$s</a>',
             $this->attributes($attributes),
             $href,
             $title
@@ -509,7 +510,7 @@ class NavigationTree
             return;
         }
 
-        $this->addOutput('</li>');
+        $this->addOutput('</span></li>');
     }
 
     /**
