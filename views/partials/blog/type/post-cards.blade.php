b4c991ee4b7dc9b969c742cadba6d92ba00ba5ab
Jonatan Hanson <jonatan.hanson@hiq.se>
2017-07-07 10:06:50 +0200

    Post card background cover area

--- views/partials/blog/type/post-cards.blade.php
+++ views/partials/blog/type/post-cards.blade.php
@@ -3,7 +3,7 @@
     <a href="{{ the_permalink() }}" class="box box-index box-card-post">
         <div class="box-container" data-equal-item>
             @if (municipio_get_thumbnail_source(null,array(400,225)))
-            <div class="box-image ratio-16-9" style="background-image:url('{{ municipio_get_thumbnail_source(null,array(400,225)) }}');">
+            <div class="box-image ratio-16-9" style="background:url('{{ municipio_get_thumbnail_source(null,array(400,225)) }}') no-repeat center center; background-size: cover;">
                 @if (in_array('category', (array)get_field('archive_' . sanitize_title(get_post_type()) . '_post_display_info', 'option')) && isset(get_the_category()[0]->name))
                 <span class="label-category label label-theme">{{ get_the_category()[0]->name }}</span>
                 @endif
