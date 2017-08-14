04112123e2baa387faf03fdfd6669ee5bb48184b
Nikolas Ramstedt <nikolas.ramstedt@gmail.com>
2017-08-14 14:12:35 +0200

    Controller - AuthorEdit - Fix typo

--- wp-content/themes/intranet/library/Controller/AuthorEdit.php
+++ wp-content/themes/intranet/library/Controller/AuthorEdit.php
@@ -24,7 +24,7 @@ class AuthorEdit extends \Intranet\Controller\BaseController
         $this->data['userSkills'] = is_array(get_the_author_meta('user_skills', $user->ID)) ? get_the_author_meta('user_skills', $user->ID) : array();
         $this->data['administrationUnits'] = \Intranet\User\AdministrationUnits::getAdministrationUnits();
         $this->data['targetGroups'] = \Intranet\User\TargetGroups::getAvailableGroups(false, get_current_user_id());
-        $this->data['profile_img'] =   $this->getProfileImgUrl($user->ID);
+        $this->data['profile_img'] =   $this->getProfileImageUrl($user->ID);
         $this->data['profile_cover'] = $this->getCoverUrl($user->ID);
     }
 
