c6de55e5fdd4a1708d2ed5bdc4b5990f682e417f
Sebastian Thulin <sebastian.thulin@gmail.com>
2017-08-15 11:43:22 +0200

    Check by $hasLeftSidebar

--- views/partials/sidebar-right.blade.php
+++ views/partials/sidebar-right.blade.php
@@ -1,5 +1,5 @@
 @if ($hasRightSidebar)
-<aside class="grid-lg-3 grid-md-12 sidebar-right-sidebar @if(is_active_sidebar('left-sidebar'))  hidden-xs hidden-sm hidden-md @endif">
+<aside class="grid-lg-3 grid-md-12 sidebar-right-sidebar @if($hasLeftSidebar)  hidden-xs hidden-sm hidden-md @endif">
     @if (is_active_sidebar('right-sidebar') || (isset($enabledSidebarFilters) && is_array($enabledSidebarFilters)))
     <div class="grid">
 
