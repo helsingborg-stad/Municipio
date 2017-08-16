2fb0c14a45c23420c7e4b39167d863eac041ba92
Sebastian Thulin <sebastian.thulin@gmail.com>
2017-08-16 10:00:56 +0200

    Use shortlinks instead of permalinks in social share.

--- views/partials/social-share.blade.php
+++ views/partials/social-share.blade.php
@@ -1,18 +1,18 @@
 <ul class="share share-social share-social-icon-md share-horizontal share-no-labels hidden-print inline-block">
     <li>
-        <a class="share-social-facebook" data-action="share-popup" href="https://www.facebook.com/sharer/sharer.php?u={{ get_the_permalink() }}" data-tooltip="<?php _e('Share on', 'municipio'); ?> Facebook">
+        <a class="share-social-facebook" data-action="share-popup" href="https://www.facebook.com/sharer/sharer.php?u={!! urlencode(wp_get_shortlink()) !!}" data-tooltip="<?php _e('Share on', 'municipio'); ?> Facebook">
             <i class="pricon pricon-facebook"></i>
             <span><?php _e('Share on', 'municipio'); ?> Facebook</span>
         </a>
     </li>
     <li>
-        <a class="share-social-twitter" data-action="share-popup" href="http://twitter.com/share?url=<?php echo urlencode(wp_get_shortlink()); ?>" data-tooltip="<?php _e('Share on', 'municipio'); ?> Twitter">
+        <a class="share-social-twitter" data-action="share-popup" href="http://twitter.com/share?url={!! urlencode(wp_get_shortlink()) !!}" data-tooltip="<?php _e('Share on', 'municipio'); ?> Twitter">
             <i class="pricon pricon-twitter"></i>
             <span><?php _e('Share on', 'municipio'); ?> Twitter</span>
         </a>
     </li>
     <li>
-        <a class="share-social-linkedin" data-action="share-popup" href="https://www.linkedin.com/shareArticle?mini=true&amp;url={{ get_the_permalink() }}&amp;title={{ get_the_title() }}&amp;summary={{ get_the_excerpt() }}&amp;source={{ bloginfo('site_name') }}" data-tooltip="<?php _e('Share on', 'municipio'); ?> LinkedIn">
+        <a class="share-social-linkedin" data-action="share-popup" href="https://www.linkedin.com/shareArticle?mini=true&amp;url={!! urlencode(wp_get_shortlink()) !!}&amp;title={{ get_the_title() }}&amp;summary={{ get_the_excerpt() }}&amp;source={{ bloginfo('site_name') }}" data-tooltip="<?php _e('Share on', 'municipio'); ?> LinkedIn">
             <i class="pricon pricon-linkedin"></i>
             <span><?php _e('Share on', 'municipio'); ?> LinkedIn</span>
         </a>
