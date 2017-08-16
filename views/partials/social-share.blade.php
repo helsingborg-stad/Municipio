f8eeb53cf5a5e5274c4d57d4eba812bc5c0d95c5
Sebastian Thulin <sebastian.thulin@gmail.com>
2017-08-16 10:47:55 +0200

    Only title & url linkedin.
    IE support

--- views/partials/social-share.blade.php
+++ views/partials/social-share.blade.php
@@ -12,7 +12,7 @@
         </a>
     </li>
     <li>
-        <a class="share-social-linkedin" data-action="share-popup" href="https://www.linkedin.com/shareArticle?mini=true&amp;url={!! urlencode(wp_get_shortlink()) !!}&amp;title={{ get_the_title() }}&amp;summary={{ get_the_excerpt() }}&amp;source={{ bloginfo('site_name') }}" data-tooltip="<?php _e('Share on', 'municipio'); ?> LinkedIn">
+        <a class="share-social-linkedin" data-action="share-popup" href="https://www.linkedin.com/shareArticle?mini=true&amp;url={!! urlencode(wp_get_shortlink()) !!}&amp;title={{ get_the_title() }}" data-tooltip="<?php _e('Share on', 'municipio'); ?> LinkedIn">
             <i class="pricon pricon-linkedin"></i>
             <span><?php _e('Share on', 'municipio'); ?> LinkedIn</span>
         </a>
