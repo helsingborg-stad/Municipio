d5fce04fcd0a6d19ed15640d389f6a8959a03007
Nikolas Ramstedt <nikolas.ramstedt@gmail.com>
2017-08-16 13:26:10 +0200

    Added author image and link to profile on comments

--- views/partials/blog/comments.blade.php
+++ views/partials/blog/comments.blade.php
@@ -1,16 +1,30 @@
 <ul id="comments" class="comments">
     @foreach($comments as $comment)
-
         @if($comment->comment_parent == 0)
             <li class="comment">
                 <header>
-                    <em>{{ $comment->comment_author }}</em>
+                    <div class="author-image">
+                        <a href="{{ municipio_intranet_get_user_profile_url($comment->user_id) }}">
+                            @if(get_the_author_meta('user_profile_picture', $comment->user_id))
+                            <img src="{{ get_the_author_meta('user_profile_picture', $comment->user_id) }}">
+                            @else
+                            <i class="pricon pricon-2x pricon-user-o"></i>
+                            @endif
+                        </a>
+                    </div>
+
+                    <div class="author-name">
+                        <a class="author-link" href="{{ municipio_intranet_get_user_profile_url($comment->user_id) }}">
+                            <em>{{ $comment->comment_author }}</em>
+                        </a>
+                    </div>
+
                     <time data-tooltip="{{ date('Y-m-d \k\l\. H:i', strtotime($comment->comment_date)) }}" data-tooltip-right datetime="{{ $comment->comment_date_gmt }}">
                         {{ municipio_human_datediff($comment->comment_date) }} sedan
                     </time>
                 </header>
                 <div class="comment-text">
-                    {!! $comment->comment_content !!}
+                    {!! comment_text($comment->comment_ID); !!}
                 </div>
 
                 @foreach($comments as $answer)
@@ -18,13 +32,28 @@
                         <ul class="answers">
                             <li>
                                 <header>
-                                    <em>{{ $answer->comment_author }}</em>
+                                    <div class="author-image">
+                                        <a href="{{ municipio_intranet_get_user_profile_url($answer->user_id) }}">
+                                            @if(get_the_author_meta('user_profile_picture', $answer->user_id))
+                                            <img src="{{ get_the_author_meta('user_profile_picture', $answer->user_id) }}">
+                                            @else
+                                            <i class="pricon pricon-2x pricon-user-o"></i>
+                                            @endif
+                                        </a>
+                                    </div>
+
+                                    <div class="author-name">
+                                        <a class="author-link" href="{{ municipio_intranet_get_user_profile_url($answer->user_id) }}">
+                                            <em>{{ $answer->comment_author }}</em>
+                                        </a>
+                                    </div>
+
                                     <time data-tooltip="{{ date('Y-m-d \k\l\. H:i', strtotime($answer->comment_date)) }}" data-tooltip-right datetime="{{ $answer->comment_date_gmt }}">
-                                        {{ municipio_human_datediff($answer->comment_date) }} sedan
+                                        {{ municipio_human_datediff($comment->comment_date) }} sedan
                                     </time>
                                 </header>
                                 <div class="comment-text">
-                                    {!! $answer->comment_content !!}
+                                    {!! comment_text($answer->comment_ID); !!}
                                 </div>
                             </li>
                         </ul>
