9ff3259a7d724469f4fa57783f28fdf70b2f6ecf
Sebastian Thulin <sebastian.thulin@gmail.com>
2017-08-23 15:06:19 +0200

    Gulped

--- wp-content/themes/intranet/assets/dist/js/app.js
+++ wp-content/themes/intranet/assets/dist/js/app.js
@@ -170,6 +170,101 @@ Intranet.Stripe = (function ($) {
 var Intranet;
 
 Intranet = Intranet || {};
+Intranet.Misc = Intranet.Misc || {};
+
+Intranet.Misc.News = (function ($) {
+    function News() {
+
+        //Init
+        this.container  = $('.modularity-mod-intranet-news .intranet-news');
+        this.button     = $('.modularity-mod-intranet-news [data-action="intranet-news-load-more"]');
+        this.category   = $('.modularity-mod-intranet-news select[name="cat"]');
+
+        //Enable disabled button
+        this.button.prop('disabled', false);
+
+        //Load more click
+        this.button.on('click', function (e) {
+            this.loadMore(this.container, this.button);
+        }.bind(this));
+
+        //Category switcher
+        this.category.on('change', function (e) {
+            this.container.empty();
+            this.loadMore(this.container, this.button);
+        }.bind(this));
+    }
+
+    News.prototype.showLoader = function(button) {
+        button.hide();
+        button.after('<div class="loading"><div></div><div></div><div></div><div></div></div>');
+    };
+
+    News.prototype.hideLoader = function(button) {
+        button.parent().find('.loading').remove();
+        button.show();
+    };
+
+    News.prototype.loadMore = function(container, button) {
+        var callbackUrl     = container.attr('data-infinite-scroll-callback');
+        var pagesize        = container.attr('data-infinite-scroll-pagesize');
+        var sites           = container.attr('data-infinite-scroll-sites');
+        var offset          = container.find('a.box-news').length ? container.find('a.box-news').length + 1 : 0;
+        var module          = container.attr('data-module');
+        var category        = this.category.val();
+        var args            = container.attr('data-args');
+
+        this.showLoader(button);
+
+        if(!isNaN(parseFloat(category)) && isFinite(category)) {
+            var url = callbackUrl + pagesize + '/' + offset + '/' + sites + '/' + category;
+        } else {
+            var url = callbackUrl + pagesize + '/' + offset + '/' + sites + '/0';
+        }
+
+        $.ajax({
+            url: url,
+            method: 'POST',
+            data: {
+                module: module,
+                args: args
+            },
+            dataType: 'JSON',
+            beforeSend: function (xhr) {
+                xhr.setRequestHeader('X-WP-Nonce', municipioIntranet.wpapi.nonce);
+            }
+        }).done(function (res) {
+            if (res.length === 0) {
+                this.noMore(container, button);
+                return;
+            }
+
+            this.output(container, res);
+            this.hideLoader(button);
+
+            if (res.length < pagesize) {
+                this.noMore(container, button);
+            }
+        }.bind(this));
+    };
+
+    News.prototype.noMore = function(container, button) {
+        this.hideLoader(button);
+        button.text(municipioIntranet.no_more_news).prop('disabled', true);
+    };
+
+    News.prototype.output = function(container, news) {
+        $.each(news, function (index, item) {
+            container.append(item.markup);
+        });
+    };
+
+    return new News();
+
+})(jQuery);
+
+
+Intranet = Intranet || {};
 Intranet.Helper = Intranet.Helper || {};
 
 Intranet.Helper.MissingData = (function ($) {
@@ -337,6 +432,38 @@ Intranet.Helper.Walkthrough = (function ($) {
     return new Walkthrough();
 
 })(jQuery);
+
+/*
+Intranet = Intranet || {};
+Intranet.SocialLogin = Intranet.SocialLogin || {};
+
+Intranet.SocialLogin.Facebook = (function ($) {
+    function Facebook() {
+        // Facebook SDK needs #fb-root div, set it up
+        $('body').prepend('<div id="fb-root"></div>');
+
+        // Load the Facebook SDK
+        (function(d, s, id) {
+            var js, fjs = d.getElementsByTagName(s)[0];
+            if (d.getElementById(id)) return;
+            js = d.createElement(s); js.id = id;
+            js.src = "//connect.facebook.net/sv_SE/sdk.js#xfbml=1&version=v2.7&appId=1604603396447959";
+            fjs.parentNode.insertBefore(js, fjs);
+        }(document, 'script', 'facebook-jssdk'));
+    }
+
+    Facebook.prototype.checkStatus = function() {
+        FB.getLoginStatus(function(response) {
+            if (response.status !== 'connected') {
+                FB.login();
+            }
+        });
+    };
+
+    return new Facebook();
+
+})(jQuery);
+*/
 
 Intranet = Intranet || {};
 Intranet.Search = Intranet.Search || {};
@@ -702,133 +829,6 @@ Intranet.Search.Sites = (function ($) {
     return new Sites();
 
 })(jQuery);
-
-Intranet = Intranet || {};
-Intranet.Misc = Intranet.Misc || {};
-
-Intranet.Misc.News = (function ($) {
-    function News() {
-
-        //Init
-        this.container  = $('.modularity-mod-intranet-news .intranet-news');
-        this.button     = $('.modularity-mod-intranet-news [data-action="intranet-news-load-more"]');
-        this.category   = $('.modularity-mod-intranet-news select[name="cat"]');
-
-        //Enable disabled button
-        this.button.prop('disabled', false);
-
-        //Load more click
-        this.button.on('click', function (e) {
-            this.loadMore(this.container, this.button);
-        }.bind(this));
-
-        //Category switcher
-        this.category.on('change', function (e) {
-            this.container.empty();
-            this.loadMore(this.container, this.button);
-        }.bind(this));
-    }
-
-    News.prototype.showLoader = function(button) {
-        button.hide();
-        button.after('<div class="loading"><div></div><div></div><div></div><div></div></div>');
-    };
-
-    News.prototype.hideLoader = function(button) {
-        button.parent().find('.loading').remove();
-        button.show();
-    };
-
-    News.prototype.loadMore = function(container, button) {
-        var callbackUrl     = container.attr('data-infinite-scroll-callback');
-        var pagesize        = container.attr('data-infinite-scroll-pagesize');
-        var sites           = container.attr('data-infinite-scroll-sites');
-        var offset          = container.find('a.box-news').length ? container.find('a.box-news').length + 1 : 0;
-        var module          = container.attr('data-module');
-        var category        = this.category.val();
-        var args            = container.attr('data-args');
-
-        this.showLoader(button);
-
-        if(!isNaN(parseFloat(category)) && isFinite(category)) {
-            var url = callbackUrl + pagesize + '/' + offset + '/' + sites + '/' + category;
-        } else {
-            var url = callbackUrl + pagesize + '/' + offset + '/' + sites + '/0';
-        }
-
-        $.ajax({
-            url: url,
-            method: 'POST',
-            data: {
-                module: module,
-                args: args
-            },
-            dataType: 'JSON',
-            beforeSend: function (xhr) {
-                xhr.setRequestHeader('X-WP-Nonce', municipioIntranet.wpapi.nonce);
-            }
-        }).done(function (res) {
-            if (res.length === 0) {
-                this.noMore(container, button);
-                return;
-            }
-
-            this.output(container, res);
-            this.hideLoader(button);
-
-            if (res.length < pagesize) {
-                this.noMore(container, button);
-            }
-        }.bind(this));
-    };
-
-    News.prototype.noMore = function(container, button) {
-        this.hideLoader(button);
-        button.text(municipioIntranet.no_more_news).prop('disabled', true);
-    };
-
-    News.prototype.output = function(container, news) {
-        $.each(news, function (index, item) {
-            container.append(item.markup);
-        });
-    };
-
-    return new News();
-
-})(jQuery);
-
-
-/*
-Intranet = Intranet || {};
-Intranet.SocialLogin = Intranet.SocialLogin || {};
-
-Intranet.SocialLogin.Facebook = (function ($) {
-    function Facebook() {
-        // Facebook SDK needs #fb-root div, set it up
-        $('body').prepend('<div id="fb-root"></div>');
-
-        // Load the Facebook SDK
-        (function(d, s, id) {
-            var js, fjs = d.getElementsByTagName(s)[0];
-            if (d.getElementById(id)) return;
-            js = d.createElement(s); js.id = id;
-            js.src = "//connect.facebook.net/sv_SE/sdk.js#xfbml=1&version=v2.7&appId=1604603396447959";
-            fjs.parentNode.insertBefore(js, fjs);
-        }(document, 'script', 'facebook-jssdk'));
-    }
-
-    Facebook.prototype.checkStatus = function() {
-        FB.getLoginStatus(function(response) {
-            if (response.status !== 'connected') {
-                FB.login();
-            }
-        });
-    };
-
-    return new Facebook();
-
-})(jQuery);
-*/
 
 Intranet = Intranet || {};
 Intranet.User = Intranet.User || {};
