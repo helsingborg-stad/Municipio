818cf17bedccd1528580ccb9d7725d4de64aa6dc
Nikolas Ramstedt <nikolas.ramstedt@gmail.com>
2017-08-30 15:39:59 +0200

    Typo Fix

--- views/partials/vertical-menu.blade.php
+++ views/partials/vertical-menu.blade.php
@@ -1,4 +1,4 @@
-@if(isset($verticalNav)) && is_array($verticalNav) && !empty($verticalNav) && count($verticalNav) > 1)
+@if(isset($verticalNav) && is_array($verticalNav) && !empty($verticalNav) && count($verticalNav) > 1)
     <ul class="scroll-dots">
         @foreach($verticalNav as $navItem)
             <li><a href="{!! $navItem['link'] !!}" data-link-tooltip="{{ $navItem['title'] }}"></a></li>
