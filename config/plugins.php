ddf1cd7b6153c7935a735cc6027527c750a222c7
Jonatan Hanson <jonatan.hanson@hiq.se>
2017-08-31 16:17:50 +0200

    Set google recaptcha constants to config

--- config/plugins.php
+++ config/plugins.php
@@ -9,3 +9,9 @@
  * Share search notices across the network
  */
  define('SEARCH_NOTICES_NETWORK', true);
+
+/**
+ * Google reCAPTCHA keys, used in modularity-guides
+ */
+define('G_RECAPTCHA_KEY', '6LfTLCUUAAAAABa7ndIUwuAsjiJxzBXkl83YsHtt');
+define('G_RECAPTCHA_SECRET', '6LfTLCUUAAAAAFH-sRUGsS1w2hdr2esOwcWcHVS6');
