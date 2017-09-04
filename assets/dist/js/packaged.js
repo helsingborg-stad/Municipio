4924e25e7b054339f49388c352b51c4e7cd1244f
Nikolas Ramstedt <nikolas.ramstedt@gmail.com>
2017-09-04 13:31:44 +0200

    Gulp

--- assets/dist/js/packaged.js
+++ assets/dist/js/packaged.js
@@ -95,19 +95,6 @@ Municipio.Helper.MainContainer = (function ($) {
 
 })(jQuery);
 
-var Municipio = {};
-
-jQuery('.index-php #screen-meta-links').append('\
-    <div id="screen-options-show-lathund-wrap" class="hide-if-no-js screen-meta-toggle">\
-        <a href="http://lathund.helsingborg.se" id="show-lathund" target="_blank" class="button show-settings">Lathund</a>\
-    </div>\
-');
-
-jQuery(document).ready(function () {
-    jQuery('.acf-field-url input[type="url"]').parents('form').attr('novalidate', 'novalidate');
-});
-
-
 Muncipio = Muncipio || {};
 Muncipio.Ajax = Muncipio.Ajax || {};
 
@@ -161,3 +148,16 @@ Muncipio.Ajax.LikeButton = (function ($) {
     return new Like();
 
 })($);
+
+var Municipio = {};
+
+jQuery('.index-php #screen-meta-links').append('\
+    <div id="screen-options-show-lathund-wrap" class="hide-if-no-js screen-meta-toggle">\
+        <a href="http://lathund.helsingborg.se" id="show-lathund" target="_blank" class="button show-settings">Lathund</a>\
+    </div>\
+');
+
+jQuery(document).ready(function () {
+    jQuery('.acf-field-url input[type="url"]').parents('form').attr('novalidate', 'novalidate');
+});
+
