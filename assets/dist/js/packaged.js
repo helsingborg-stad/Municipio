18e1a82e8e538834f32c89759bbfb0748d39f631
Sebastian Thulin <sebastian.thulin@gmail.com>
2017-09-05 09:08:05 +0200

    Move vertical navbar to the right if screen is small

--- assets/dist/js/packaged.js
+++ assets/dist/js/packaged.js
@@ -95,6 +95,19 @@ Municipio.Helper.MainContainer = (function ($) {
 
 })(jQuery);
 
+var Municipio = {};
+
+jQuery('.index-php #screen-meta-links').append('\
+    <div id="screen-options-show-lathund-wrap" class="hide-if-no-js screen-meta-toggle">\
+        <a href="http://lathund.helsingborg.se" id="show-lathund" target="_blank" class="button show-settings">Lathund</a>\
+    </div>\
+');
+
+jQuery(document).ready(function () {
+    jQuery('.acf-field-url input[type="url"]').parents('form').attr('novalidate', 'novalidate');
+});
+
+
 Muncipio = Muncipio || {};
 Muncipio.Ajax = Muncipio.Ajax || {};
 
@@ -148,16 +161,3 @@ Muncipio.Ajax.LikeButton = (function ($) {
     return new Like();
 
 })($);
-
-var Municipio = {};
-
-jQuery('.index-php #screen-meta-links').append('\
-    <div id="screen-options-show-lathund-wrap" class="hide-if-no-js screen-meta-toggle">\
-        <a href="http://lathund.helsingborg.se" id="show-lathund" target="_blank" class="button show-settings">Lathund</a>\
-    </div>\
-');
-
-jQuery(document).ready(function () {
-    jQuery('.acf-field-url input[type="url"]').parents('form').attr('novalidate', 'novalidate');
-});
-
