ffb05d274de38e247ef2324f7463904603da1524
Sebastian Thulin <sebastian.thulin@gmail.com>
2017-09-07 12:59:15 +0200

    Fixes admin url’s

--- wp-content/mu-plugins/AdminUrlBugFix.php
+++ wp-content/mu-plugins/AdminUrlBugFix.php
@@ -12,15 +12,26 @@ class AdminUrlBugFix
 {
     public function __construct()
     {
+        //Redirect correct url
         $this->redirectToCorrectUrl((isset($_SERVER['HTTPS']) ? "https" : "http") . "://" . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI']);
+
+        //Filter genereated url's
+        add_filter('admin_url', function ($url, $path, $blog_id = null) {
+            return $this->fixUrl($url);
+        }, 9999, 3);
     }
 
     public function redirectToCorrectUrl($url)
     {
         if (preg_match("~/wp/wp-admin/post.php/wp/wp-admin/~i", $url)) {
-            header("Location: " . str_replace("/wp/wp-admin/post.php/wp/wp-admin/", "/wp/wp-admin/", $url));
+            header("Location: " . $this->fixUrl($url));
             exit;
         }
+    }
+
+    public function fixUrl($url)
+    {
+        return str_replace("/wp/wp-admin/post.php/wp/wp-admin/", "/wp/wp-admin/", $url);
     }
 }
 new \AdminUrlBugFix\AdminUrlBugFix();
