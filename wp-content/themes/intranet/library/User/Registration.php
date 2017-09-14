6e103111098bba97ed9f37862adf29c1342a23e2
Sebastian Thulin <sebastian.thulin@gmail.com>
2017-09-14 16:49:02 +0200

    Most of these removed functions is moved to the ad-integration plugin.

--- wp-content/themes/intranet/library/User/Registration.php
+++ wp-content/themes/intranet/library/User/Registration.php
@@ -14,16 +14,6 @@ class Registration
         // Ban new user account registration if email is used as username
         add_filter('pre_user_login', array($this, 'disallowAccountRegistration'));
 
-        // Add new user to all sites
-        add_action('wpmu_activate_user', array($this, 'addDefaultRole'), 10, 1);
-        add_action('wpmu_new_user', array($this, 'addDefaultRole'), 10, 1);
-        add_action('user_register', array($this, 'addDefaultRole'), 10, 1);
-
-        // Add existing users to new or activated sites
-        add_action('wpmu_new_blog', array($this, 'addUsersToNewBlog'));
-        add_action('wpmu_new_blog_cron', array($this, 'addUsersToNewBlogCron'), 10, 1);
-        add_action('wpmu_activate_blog', array($this, 'activateBlogUser'), 10, 2);
-
         // Set default display name
         add_action('network_user_new_form', array($this, 'addNameFieldsToUserRegistration'));
         add_action('wpmu_new_user', array($this, 'saveUserRegistrationName'), 10, 1);
@@ -41,7 +31,6 @@ class Registration
      */
     public function disallowedUsers($userLogin)
     {
-
         if (wp_doing_cron()) {
             return $userLogin;
         }
@@ -156,74 +145,5 @@ class Registration
             'ID' => $userId,
             'display_name' => $firstName . ' ' . $lastName
         ));
-    }
-
-    /**
-     * Adds all users to the blog when
-     * @param integer $blogId The newly created blog's ID
-     */
-    public function addUsersToNewBlog($blogId)
-    {
-        if (function_exists('count_users') && count_users()['total_users'] > 100) {
-            wp_schedule_single_event(time() + 5, 'wpmu_new_blog_cron', array($blogId));
-            return true;
-        }
-
-        $this->addUsersToNewBlogCron($blogId);
-        return true;
-    }
-
-    public function addUsersToNewBlogCron($blogId)
-    {
-        set_time_limit(600);
-
-        global $wpdb;
-        $users = $wpdb->get_results("SELECT ID FROM $wpdb->users");
-
-        foreach ($users as $user) {
-            $this->addDefaultRole($user->ID, $blogId);
-        }
-    }
-
-    /**
-     * Add user role when activated
-     * @param  integer $blogId Blog id
-     * @param  integer $userId User id
-     * @return boolean
-     */
-    public function activateBlogUser($blogId, $userId)
-    {
-        return $this->addDefaultRole($userId, $blogId);
-    }
-
-    /**
-     * Adds the specified userid to a specified or all blogs
-     * @param integer $userId User id to add
-     * @param integer $blogId Specific blog_id (leave null for all)
-     */
-    public function addDefaultRole($userId, $blogId = null)
-    {
-        // Single
-        if ($blogId) {
-            if (is_user_member_of_blog($userId, $blogId)) {
-                return false;
-            }
-
-            add_user_to_blog($blogId, $userId, $this->defaultRole);
-            return true;
-        }
-
-        // Multiple
-        $sites = \Intranet\Helper\Multisite::getSitesList(true);
-
-        foreach ($sites as $site) {
-            if (is_user_member_of_blog($userId, $site->blog_id)) {
-                continue;
-            }
-
-            add_user_to_blog($site->blog_id, $userId, $this->defaultRole);
-        }
-
-        return true;
     }
 }
