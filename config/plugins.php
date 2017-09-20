82ebd7686f5d3ae6ae5d9adfc5f3b00001ba40d0
Jonatan Hanson <jonatan.hanson@hiq.se>
2017-09-20 15:04:53 +0200

    Removed captcha constants

--- config/plugins.php
+++ config/plugins.php
@@ -9,9 +9,3 @@
  * Share search notices across the network
  */
  define('SEARCH_NOTICES_NETWORK', true);
-
-/**
- * Google reCAPTCHA keys, used in modularity-guides
- */
-define('G_RECAPTCHA_KEY', '6Lc7xSkTAAAAAJLXT2LbsFDPPp81rKmqzNj0jcH0');
-define('G_RECAPTCHA_SECRET', '6Lc7xSkTAAAAAIhub2lk0HY9EvxlP81vpen2AfDG');
