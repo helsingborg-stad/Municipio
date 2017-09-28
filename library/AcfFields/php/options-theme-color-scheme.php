d65143c13fdda585f232dc87ecabc9298700eb78
Jonatan Hanson <jonatan.hanson@hiq.se>
2017-09-28 12:51:09 +0200

    Resaved acf color scheme

--- library/AcfFields/php/options-theme-color-scheme.php
+++ library/AcfFields/php/options-theme-color-scheme.php
@@ -1,30 +1,13 @@
 <?php 
-if (function_exists('acf_add_local_field_group')) {
 
-if (function_exists('acf_add_local_field_group')) {
-    acf_add_local_field_group(array(
+if (function_exists('acf_add_local_field_group')) {
+    acf_add_local_field_group(array(
     'key' => 'group_56a0a7dcb5c09',
-    'title' => 'Color scheme',
+    'title' => __('Color scheme', 'municipio'),
     'fields' => array(
         0 => array(
-            'layout' => 'vertical',
-            'choices' => array(
-                'gray' => 'Gray',
-                'red' => 'Red',
-                'blue' => 'Blue',
-                'green' => 'Green',
-                'purple' => 'Purple',
-                'familjen' => 'Familjen helsingborg',
-                'astorp' => 'Åstorps kommun',
-                'hultsfred' => 'Hultsfreds kommun',
-            ),
-            'default_value' => 'gray',
-            'other_choice' => 0,
-            'save_other_choice' => 0,
-            'allow_null' => 0,
-            'return_format' => 'value',
             'key' => 'field_56a0a7e36365b',
-            'label' => 'Color scheme',
+            'label' => __('Color scheme', 'municipio'),
             'name' => 'color_scheme',
             'type' => 'radio',
             'instructions' => '',
@@ -35,6 +18,22 @@ if (function_exists('acf_add_local_field_group')) {
                 'class' => '',
                 'id' => '',
             ),
+            'choices' => array(
+                'gray' => __('Gray', 'municipio'),
+                'red' => __('Red', 'municipio'),
+                'blue' => __('Blue', 'municipio'),
+                'green' => __('Green', 'municipio'),
+                'purple' => __('Purple', 'municipio'),
+                'familjen' => __('Familjen helsingborg', 'municipio'),
+                'astorp' => __('Åstorps kommun', 'municipio'),
+                'hultsfred' => __('Hultsfreds kommun', 'municipio'),
+            ),
+            'allow_null' => 0,
+            'other_choice' => 0,
+            'save_other_choice' => 0,
+            'default_value' => 'gray',
+            'layout' => 'vertical',
+            'return_format' => 'value',
         ),
     ),
     'location' => array(
@@ -54,6 +53,5 @@ if (function_exists('acf_add_local_field_group')) {
     'hide_on_screen' => '',
     'active' => 1,
     'description' => '',
-    'local' => 'php',
 ));
-}
+}
