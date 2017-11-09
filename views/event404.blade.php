c5c5489c908599bb9cff2d39e09298c0280e2c1a
karl <philippe@nerdsofsweden.se>
2017-11-09 10:24:27 +0100

    landingpage for passed events

---
+++ views/event404.blade.php
@@ -0,0 +1,35 @@
+<div class="creamy">
+    <div class="container">
+        <div class="grid-lg-12 text-center">
+            <div class="gutter gutter-xl">
+                <h1><?php
+
+_e('The event could not be found', 'municipio');
+
+?></h1>
+            </div>
+        </div>
+    </div>
+</div>
+
+<div class="container main-container">
+    <div class="grid">
+        <div class="grid-lg-8" style="margin: 0 auto;">
+           
+
+            <ul class="actions">
+                
+                <li><a href="{{ $event_redirect }}" class="link-item"><?php _e('Events archive', 'municipio'); ?></a></li>
+                
+            </ul>
+        </div>
+    </div>
+</div>
+
+
+
+
+
+
+
+
