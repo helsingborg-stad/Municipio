db0f471e0e56a166828df266b4980a7f476deae5
karl <philippe@nerdsofsweden.se>
2017-11-21 16:13:14 +0100

    Remove permission test

--- wp-content/themes/intranet/library/User/TargetGroups.php
+++ wp-content/themes/intranet/library/User/TargetGroups.php
@@ -23,7 +23,7 @@ class TargetGroups
         add_action('save_post', array($this, 'saveGroupRestrictions'));
 
         // Limit access to posts based on groups
-        add_action('pre_get_posts', array($this, 'doGroupRestriction'));
+        //add_action('pre_get_posts', array($this, 'doGroupRestriction'));
 
         // Restruct modules
         add_filter('Modularity/Display/Markup', array($this, 'restrictModules'), 10, 2);
