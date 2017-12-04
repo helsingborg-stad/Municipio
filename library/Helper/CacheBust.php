31095363801bcfd1806e206aec90ec2292aca9cf
Nikolas Ramstedt <nikolas.ramstedt@gmail.com>
2017-12-04 16:31:10 +0100

    add child theme support & assets path filter @ cachebust

--- library/Helper/CacheBust.php
+++ library/Helper/CacheBust.php
@@ -6,18 +6,46 @@ class CacheBust
 {
     /**
      * Returns the revved/cache-busted file name of an asset.
+     * @param string $name Asset name (array key) from rev-mainfest.json
+     * @param boolean $childTheme Set child or parent theme path (defaults to parent)
+     * @return string filename of the asset (including directory above)
      */
-    static function name($name) {
-        static $revManifest;
-        if (!isset($revManifest)) {
-            $revManifestPath = get_template_directory(). '/assets/dist/rev-manifest.json';
-            if (file_exists($revManifestPath)) {
-                $revManifest = json_decode(file_get_contents($revManifestPath), true);
-            }
-            elseif(WP_DEBUG) {
-                echo '<div style="color:red">Error: Assets not built. Go to ' . get_template_directory() . ' and run gulp. See '. get_template_directory() . '/README.md for more info.</div>';
-            }
+    public static function name($name, $childTheme = false)
+    {
+        static $revManifestParent;
+        static $revManifestChild;
+
+        $themePath = ($childTheme == true) ? get_stylesheet_directory() : get_template_directory();
+
+        if ($childTheme == true && !isset($revManifestChild[$name])) {
+            $revManifestChild = self::getRevManifest($childTheme);
+        } elseif ($childTheme == false && !isset($revManifestParent[$name])) {
+            $revManifestParent = self::getRevManifest($childTheme);
         }
+
+        $revManifest = ($childTheme == true) ? $revManifestChild : $revManifestParent;
+
+        if (!isset($revManifest[$name])) {
+            return;
+        }
+
         return $revManifest[$name];
+    }
+
+    /**
+     * Decode assets json to array
+     * @param boolean $childTheme Set child or parent theme path (defaults to parent)
+     * @return array containg assets filenames
+     */
+    public static function getRevManifest($childTheme = false)
+    {
+        $themePath = ($childTheme == true) ? get_stylesheet_directory() : get_template_directory();
+        $jsonPath = $themePath . apply_filters('Municipio/Helper/CacheBust/RevManifestPath', '/assets/dist/rev-manifest.json');
+
+        if (file_exists($jsonPath)) {
+            return json_decode(file_get_contents($jsonPath), true);
+        } elseif (WP_DEBUG) {
+            echo '<div style="color:red">Error: Assets not built. Go to ' . $themePath . ' and run gulp. See '. $themePath . '/README.md for more info.</div>';
+        }
     }
 }
