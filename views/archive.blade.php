52b4d8abe297392217b792aee1d3ebfc41812d02
Jonatan Hanson <jonatan.hanson@hiq.se>
2017-12-18 14:16:45 +0100

    Wrap horizontal-card with grid

--- views/archive.blade.php
+++ views/archive.blade.php
@@ -69,7 +69,7 @@
                         @while(have_posts())
                             {!! the_post() !!}
 
-                            @if (in_array($template, array('full', 'compressed', 'collapsed')))
+                            @if (in_array($template, array('full', 'compressed', 'collapsed', 'horizontal-cards')))
                                 <div class="grid-xs-12 post">
                                     @include('partials.blog.type.post-' . $template)
                                 </div>
