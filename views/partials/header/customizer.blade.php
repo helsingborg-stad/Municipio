ba7e53af0d6acae101be191f3df00079302ee1db
Nikolas Ramstedt <nikolas.ramstedt@gmail.com>
2018-01-16 14:08:49 +0100

    edit component class names

--- views/partials/header/customizer.blade.php
+++ views/partials/header/customizer.blade.php
@@ -1,15 +1,17 @@
-@if (isset($headerLayout['navbars']) && $headerLayout['navbars'] && !empty($headerLayout['navbars']))
-    @foreach ($headerLayout['navbars'] as $navbar)
-        <div class="{{$navbar['classes']}}" {{$navbar['attributes']}}>
-            @if (isset($navbar['sections']) && !empty($navbar['sections']))
-                <div class="{{$navbar['bodyClasses']}}">
-                    @foreach ($navbar['sections'] as $section)
-                        <div class="{{$section['classes']}}">
-                            <?php dynamic_sidebar($section['id']); ?>
-                        </div>
-                    @endforeach
-                </div>
-            @endif
-        </div>
-    @endforeach
+@if (isset($headerLayout['panels']) && $headerLayout['panels'] && !empty($headerLayout['panels']))
+    <header class="{{$headerLayout['classes']}}" {{$headerLayout['attributes']}}>
+        @foreach ($headerLayout['panels'] as $panel)
+            <div class="{{$panel['classes']}}" {{$panel['attributes']}}>
+                @if (isset($panel['items']) && !empty($panel['items']))
+                    <div class="{{$panel['bodyClasses']}}">
+                        @foreach ($panel['items'] as $item)
+                            <div class="{{$item['classes']}}">
+                                <?php dynamic_sidebar($item['id']); ?>
+                            </div>
+                        @endforeach
+                    </div>
+                @endif
+            </div>
+        @endforeach
+    </header>
 @endif
