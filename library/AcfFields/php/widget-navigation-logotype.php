c7f57886c6ecf94e1ad4affdaf6bcd8dbc39ab27
Nikolas Ramstedt <nikolas.ramstedt@gmail.com>
2018-01-16 11:51:04 +0100

    add acf fields to logo widget

---
+++ library/AcfFields/php/widget-navigation-logotype.php
@@ -0,0 +1,51 @@
+<?php 
+
+if (function_exists('acf_add_local_field_group')) {
+    acf_add_local_field_group(array(
+    'key' => 'group_5a5ca31651f08',
+    'title' => __('Widget - Navigation Logotype', 'municipio'),
+    'fields' => array(
+        0 => array(
+            'key' => 'field_5a5ca31c83a1c',
+            'label' => __('Upload logotype', 'municipio'),
+            'name' => 'widget_navigation_logotype',
+            'type' => 'image',
+            'instructions' => '',
+            'required' => 0,
+            'conditional_logic' => 0,
+            'wrapper' => array(
+                'width' => '',
+                'class' => '',
+                'id' => '',
+            ),
+            'return_format' => 'array',
+            'preview_size' => 'medium',
+            'library' => 'all',
+            'min_width' => '',
+            'min_height' => '',
+            'min_size' => '',
+            'max_width' => '',
+            'max_height' => '',
+            'max_size' => '',
+            'mime_types' => 'svg,png',
+        ),
+    ),
+    'location' => array(
+        0 => array(
+            0 => array(
+                'param' => 'widget',
+                'operator' => '==',
+                'value' => 'navigation_logo',
+            ),
+        ),
+    ),
+    'menu_order' => 0,
+    'position' => 'normal',
+    'style' => 'default',
+    'label_placement' => 'top',
+    'instruction_placement' => 'label',
+    'hide_on_screen' => '',
+    'active' => 1,
+    'description' => '',
+));
+}
