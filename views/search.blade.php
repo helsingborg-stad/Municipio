5be2782bf1ec6f58174cc4c9ec5f50fbfe36bccc
Sebastian Thulin <sebastian.thulin@gmail.com>
2018-01-22 14:28:03 +0100

    Sarch by algolia view

--- views/search.blade.php
+++ views/search.blade.php
@@ -4,6 +4,8 @@
 
     @if (get_field('use_google_search', 'option') === true)
         @include('partials.search.google')
+    @elseif(get_field('use_algolia_search', 'option') === true)
+        @include('partials.search.algolia')
     @else
         @include('partials.search.wp')
     @endif
