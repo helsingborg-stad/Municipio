ef51488f59235fcb6f16d40e802e912152d898c0
Nikolas Ramstedt <nikolas.ramstedt@gmail.com>
2018-02-01 11:28:52 +0100

    add before & after text

--- views/widget/header-links.blade.php
+++ views/widget/header-links.blade.php
@@ -5,10 +5,12 @@
             @foreach ($links as $link)
                 <li class="c-navbar__item">
                     @if (isset($link['url']))
-                        <a class="{{$link['classes']}}" {!!$link['attributes']!!} href="{{$link['url']}}">
+                        <a class="{{$link['classes']}}" {!!$link['attributes']!!} href="{{$link['url']}}" aria-label="{{$link['text']}}">
+                            @if (isset($link['beforeText'])){!! $link['beforeText'] !!}@endif
                             @if (isset($link['hide_text']) && $link['hide_text'])<span class="hidden">@endif
                                 {{$link['text']}}
                             @if (isset($link['hide_text']) && $link['hide_text'])</span>@endif
+                            @if (isset($link['afterText'])){!! $link['beforeText'] !!}@endif
                         </a>
                     @endif
                 </li>
