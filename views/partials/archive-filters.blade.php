bfccce2b8229541c54bf52eb61c578ad3cf79d2a
Sebastian Thulin <sebastian.thulin@gmail.com>
2018-02-06 16:29:06 +0100

    Take up full with in content.

--- views/partials/archive-filters.blade.php
+++ views/partials/archive-filters.blade.php
@@ -1,7 +1,7 @@
 @if (!empty($enabledHeaderFilters))
 
 @if (get_field('archive_' . sanitize_title($postType) . '_filter_position', 'option') == 'content')
-    <section class="gutter sidebar-content-area archive-filters">
+    <section class="gutter sidebar-content-area archive-filters grid-xs-12">
 @else
     <section class="creamy creamy-border-bottom gutter-lg gutter-vertical sidebar-content-area archive-filters">
 @endif
