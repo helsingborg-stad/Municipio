b3902468e1ba5e6d9fafbea0fe03b3f9f3f30674
Sebastian Thulin <sebastian.thulin@gmail.com>
2018-02-07 15:35:12 +0100

    Add body class to BODY not HTML

--- views/widget/header-logo.blade.php
+++ views/widget/header-logo.blade.php
@@ -1,6 +1,6 @@
 @extends('widget.header-widget')
     @section('widget')
-        <div class="c-header__logo t-header__logo">
+        <div class="c-header__logo t-familjen-helsingborg">
             <a href="{{$home}}">
                 {!! $logotype !!}
             </a>
