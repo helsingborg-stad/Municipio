cba321fd381bf0bd9222fcadec71eacd33d5a1ef
Nikolas Ramstedt <nikolas.ramstedt@gmail.com>
2018-02-15 14:49:44 +0100

    remove section

--- views/widget/header-links/partials/link.blade.php
+++ views/widget/header-links/partials/link.blade.php
@@ -1,5 +1,5 @@
 <a class="{{$link['classes']}}" href="{{$link['url']}}" aria-label="{{$link['text']}}" {!! $link['attributes'] !!}>
-    @section('content')
+
         @if (isset($link['hide_text']) && $link['hide_text'])
             <span class="hidden">
                 {{$link['text']}}
@@ -7,5 +7,5 @@
         @else
             {{$link['text']}}
         @endif
-    @show
+
 </a>
