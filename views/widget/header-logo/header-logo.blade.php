2598716717322ae61a54ad117177916c1268addf
Sebastian Thulin <sebastian.thulin@gmail.com>
2018-02-21 11:07:54 +0100

    Only use min-width when needed
    This is a bugfix for internet explorer who can’t calculate svg origin with max-width.

--- views/widget/header-logo/header-logo.blade.php
+++ views/widget/header-logo/header-logo.blade.php
@@ -3,7 +3,12 @@
     <style scoped>
         .c-header__logo.t-municipio a img,
         .c-header__logo.t-municipio a svg {
-            max-width: {{ $maxWidth }}px;
+            @media (max-width: 599px) {
+                max-width: {{ $maxWidth }}px;
+            }
+            @media (min-width: 600px) {
+                width: {{ $maxWidth }}px;
+            }
         }
     </style>
     <div class="c-header__logo {{$themeClass}}" data-tooltip="{{ $language['logoLabel'] }}">
