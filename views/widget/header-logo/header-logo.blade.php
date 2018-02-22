1be63275ffeabc4f251976d9bb665333869e67a0
Sebastian Thulin <sebastian.thulin@gmail.com>
2018-02-22 10:53:38 +0100

    Adjust logo size depending on screen size
    Calc from user setting.

--- views/widget/header-logo/header-logo.blade.php
+++ views/widget/header-logo/header-logo.blade.php
@@ -3,11 +3,11 @@
     <style scoped>
         .c-header__logo.t-municipio a img,
         .c-header__logo.t-municipio a svg {
-            max-width: 100%;
-            width: {{ $maxWidth }}px;
+            max-width: {{ $maxWidth }}px;
         }
         @if($imageRatio)
             .c-header__logo.t-municipio a {
+                position: relative;
                 padding-top: {{ $imageRatio }}%;
                 max-width: 100%;
                 width: {{ $maxWidth }}px;
@@ -18,6 +18,16 @@
                 right: 0;
                 bottom: 0;
                 left: 0;
+            }
+            @media (max-width: 599px) {
+                .c-header__logo.t-municipio a {
+                    width: {{ ($maxWidth/2) }}px;
+                }
+            }
+            @media (min-width: 600px) and (max-width: 799px) {
+                .c-header__logo.t-municipio a {
+                    width: {{ ($maxWidth/3)*2 }}px;
+                }
             }
         @endif
     </style>
