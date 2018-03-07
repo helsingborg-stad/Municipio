1ea3b1a943ac5533a3cd83dac19e9f9aefb54f9a
Carl Johan Silvergrund <johan.silvergrund@hiq.se>
2018-03-07 13:27:43 +0100

    changed share link from permalink to get_page_link

--- views/partials/social-share.blade.php
+++ views/partials/social-share.blade.php
@@ -1,18 +1,18 @@
 <ul class="share share-social share-social-icon-md share-horizontal share-no-labels hidden-print inline-block">
     <li>
-        <a class="share-social-facebook" data-action="share-popup" href="https://www.facebook.com/sharer/sharer.php?u={!! urlencode(get_permalink()) !!}" data-tooltip="<?php _e('Share on', 'municipio'); ?> Facebook">
+        <a class="share-social-facebook" data-action="share-popup" href="https://www.facebook.com/sharer/sharer.php?u={!! urlencode(get_page_link()) !!}" data-tooltip="<?php _e('Share on', 'municipio'); ?> Facebook">
             <i class="pricon pricon-facebook"></i>
             <span><?php _e('Share on', 'municipio'); ?> Facebook</span>
         </a>
     </li>
     <li>
-        <a class="share-social-twitter" data-action="share-popup" href="http://twitter.com/share?url={!! urlencode(get_permalink()) !!}" data-tooltip="<?php _e('Share on', 'municipio'); ?> Twitter">
+        <a class="share-social-twitter" data-action="share-popup" href="http://twitter.com/share?url={!! urlencode(get_page_link()) !!}" data-tooltip="<?php _e('Share on', 'municipio'); ?> Twitter">
             <i class="pricon pricon-twitter"></i>
             <span><?php _e('Share on', 'municipio'); ?> Twitter</span>
         </a>
     </li>
     <li>
-        <a class="share-social-linkedin" data-action="share-popup" href="https://www.linkedin.com/shareArticle?mini=true&amp;url={!! urlencode(get_permalink()) !!}&amp;title={{ urlencode(get_the_title()) }}" data-tooltip="<?php _e('Share on', 'municipio'); ?> LinkedIn">
+        <a class="share-social-linkedin" data-action="share-popup" href="https://www.linkedin.com/shareArticle?mini=true&amp;url={!! urlencode(get_page_link()) !!}&amp;title={{ urlencode(get_the_title()) }}" data-tooltip="<?php _e('Share on', 'municipio'); ?> LinkedIn">
             <i class="pricon pricon-linkedin"></i>
             <span><?php _e('Share on', 'municipio'); ?> LinkedIn</span>
         </a>
