018b0ffe20b5f8d79842dc04b4290f15ac34f71c
Sebastian Thulin <sebastian.thulin@gmail.com>
2018-03-09 09:47:14 +0100

    Adds a sample 404 view.

--- bem-views/404.blade.php
+++ bem-views/404.blade.php
@@ -1,1 +1,12 @@
-BEM!
+@extends('templates.master')
+
+@section('content')
+
+    @if(file_exists(MUNICIPIO_PATH . '/views/partials/404/' . $post_type . '.blade.php'))
+        @include('partials.404.' . $post_type)
+    @else
+        @include('partials.404.default')
+    @endif
+
+@stop
+{{ /* THIS IS A SAMPLE VIEW */ }}
