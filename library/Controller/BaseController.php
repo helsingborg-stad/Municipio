b76e9580db793af1fb51350b9825cdc6ec5377d4
Sebastian Thulin <sebastian.thulin@gmail.com>
2018-03-13 08:58:11 +0100

    Removes the depricated folder

--- library/Controller/BaseController.php
+++ library/Controller/BaseController.php
@@ -115,7 +115,7 @@ class BaseController
     {
         $this->data = array_merge(
             $this->data,
-            apply_filters('Municipio/controller/base/view_data', $this->data)
+            apply_filters_deprecated('Municipio/controller/base/view_data', array($this->data), "2.0", 'Municipio/ViewData')
         );
     }
 
