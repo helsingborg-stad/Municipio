cf0107ac661ae2fe85bf505a7017b2556177deda
Sebastian Thulin <sebastian.thulin@gmail.com>
2018-03-13 11:44:20 +0100

    Formatting of comment like function

--- library/Comment/LikeButton.php
+++ library/Comment/LikeButton.php
@@ -4,10 +4,11 @@ namespace Municipio\Comment;
 
 class LikeButton extends \Municipio\Helper\Ajax
 {
-    public function __construct() {
+    public function __construct()
+    {
         //Data
-        $this->data['ajax_url'] = admin_url( 'admin-ajax.php' );
-        $this->data['nonce'] = wp_create_nonce( 'likeNonce' );
+        $this->data['ajax_url'] = admin_url('admin-ajax.php');
+        $this->data['nonce'] = wp_create_nonce('likeNonce');
 
         //Localize
         $this->localize('likeButtonData');
@@ -22,12 +23,12 @@ class LikeButton extends \Municipio\Helper\Ajax
      */
     public function ajaxLikeMethod()
     {
-        if ( ! defined( 'DOING_AJAX' ) && ! DOING_AJAX ) {
+        if (! defined('DOING_AJAX') && ! DOING_AJAX) {
             return false;
         }
 
-        if (! wp_verify_nonce( $_POST['nonce'], 'likeNonce' ) ) {
-            die ( 'Busted!');
+        if (! wp_verify_nonce($_POST['nonce'], 'likeNonce')) {
+            die('Busted!');
         }
 
         ignore_user_abort(true);
@@ -35,17 +36,17 @@ class LikeButton extends \Municipio\Helper\Ajax
         $commentId = $_REQUEST['comment_id'];
         $like = array();
 
-        if(is_array(get_comment_meta( $commentId, '_likes', true )) == true) {
-            $like = array_merge($like, get_comment_meta( $commentId, '_likes', true ));
+        if (is_array(get_comment_meta($commentId, '_likes', true)) == true) {
+            $like = array_merge($like, get_comment_meta($commentId, '_likes', true));
         }
-        if(in_array(get_current_user_id(), $like)) {
+        if (in_array(get_current_user_id(), $like)) {
             $index = array_search(get_current_user_id(), $like);
             unset($like[$index]);
         } else {
             $like[] = get_current_user_id();
         }
 
-        update_comment_meta( $commentId, '_likes', $like );
+        update_comment_meta($commentId, '_likes', $like);
 
         return true;
     }
