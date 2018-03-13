328ec8228696146f6ce8cc4f9a25a87e7423f27e
Sebastian Thulin <sebastian.thulin@gmail.com>
2018-03-13 09:59:43 +0100

    Update ACF nag

--- library/Bootstrap.php
+++ library/Bootstrap.php
@@ -107,21 +107,8 @@ if (function_exists('get_field')) {
     new Municipio\App();
 } else {
     if (!(defined('WP_CLI') && WP_CLI)) {
-
-        //Be shure to enable ACF
-        if (!is_admin()) {
-            if (is_user_logged_in()) {
-                wp_redirect(admin_url('plugins.php'));
-                exit;
-            } else {
-                if (!in_array($GLOBALS['pagenow'], array('wp-login.php', 'wp-register.php'))) {
-                    auth_redirect();
-                }
-            }
-        } else {
-            add_action('admin_notices', function () {
-                echo '<div class="notice notice-error"><p>Please active ACF (PRO) to proceed.</p></div>';
-            });
-        }
+        add_action('admin_notices', function () {
+            echo '<div class="notice notice-error"><p>To get the full expirience of the Event Manager Integration plugin, please activate the <a href="http://www.advancedcustomfields.com/pro/">Advanced Custom Fields Pro plugin</a>.</p></div>';
+        });
     }
 }
