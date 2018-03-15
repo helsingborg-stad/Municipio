2e1a8bbee265ac4afebbbbd53b9321d2d15b07d7
Sebastian Thulin <sebastian.thulin@gmail.com>
2018-03-15 11:37:20 +0100

    Use inclide first instead of looping in view 404

--- bem-views/404.blade.php
+++ bem-views/404.blade.php
@@ -1,12 +1,6 @@
 @extends('templates.master')
 
 @section('content')
-
-    @if(file_exists(MUNICIPIO_PATH . '/views/partials/404/' . $post_type . '.blade.php'))
-        @include('partials.404.' . $post_type)
-    @else
-        @include('partials.404.default')
-    @endif
-
+    @includeFirst(['partials.404.' . $post_type, 'partials.404.default'])
 @stop
 {{ /* THIS IS A SAMPLE VIEW */ }}
