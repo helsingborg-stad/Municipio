e46f09c51965f966df14cd5ca76ab05966a703de
Sebastian Thulin <sebastian.thulin@gmail.com>
2018-03-19 14:07:55 +0100

    Smarter date view.

--- bem-views/utilities/date.blade.php
+++ bem-views/utilities/date.blade.php
@@ -1,1 +1,5 @@
-<time data-tooltip="{{$date}}" data-tooltip-right datetime="{{$date}}">{{$date}}</time>
+@if($elapsed == true)
+<time data-tooltip="{{$date}}" data-tooltip-right datetime="{{$date}}">{{municipio_human_datediff($date)}} {{$lang['ago']}}</time>
+@else
+<time data-tooltip="{{municipio_human_datediff($date)}} {{$lang['ago']}}" data-tooltip-right datetime="{{$date}}">{{$date}}</time>
+@endif
