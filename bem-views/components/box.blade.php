6c1e0a1783d8b30e0b7043e26c3f74b71fb337b9
Sebastian Thulin <sebastian.thulin@gmail.com>
2018-03-19 14:55:35 +0100

    Test box item

--- bem-views/components/box.blade.php
+++ bem-views/components/box.blade.php
@@ -1,1 +1,6 @@
-<div class=""></div>
+<div class="card" style="width: 18rem;">
+  <div class="card-body">
+    <h5 class="card-title">{{$title}}</h5>
+    <p class="card-text">{{$text}}</p>
+  </div>
+</div>
