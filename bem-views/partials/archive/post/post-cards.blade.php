5b213b4ff5b0517cf5b2f6f69e25b87d34f2eda5
Nikolas Ramstedt <nikolas.ramstedt@gmail.com>
2018-03-28 09:07:57 +0200

    bemify post-cards

--- bem-views/partials/archive/post/post-cards.blade.php
+++ bem-views/partials/archive/post/post-cards.blade.php
@@ -1,31 +1,18 @@
 <?php global $post; ?>
-<div class="{{ $grid_size }}">
-    <a href="{{ the_permalink() }}" class="box box-index box-card-post">
-        <div class="box-container" data-equal-item>
-            @if (municipio_get_thumbnail_source(null,array(400,225)))
-            <div class="box-image ratio-16-9" style="background-image:url('{{ municipio_get_thumbnail_source(null,array(400,225)) }}');">
-                @if (in_array('category', (array)get_field('archive_' . sanitize_title(get_post_type()) . '_post_display_info', 'option')) && isset(get_the_category()[0]->name))
-                <span class="label-category label label-theme">{{ get_the_category()[0]->name }}</span>
-                @endif
-            </div>
-            @else
-                @if (in_array('category', (array)get_field('archive_' . sanitize_title(get_post_type()) . '_post_display_info', 'option')) && isset(get_the_category()[0]->name))
-                <span class="label-category label label-theme">{{ get_the_category()[0]->name }}</span>
-                @endif
+
+    <a href="{{ the_permalink() }}" class="c-card c-card--action">
+        @if (municipio_get_thumbnail_source(null,array(400,225)))
+        <img class="c-card__image" src="{{ municipio_get_thumbnail_source(null,array(400,225)) }}">
+        @endif
+        <div class="c-card__body" data-equal-item>
+            <h3 class="c-card__title">{{ the_title() }}</h3>
+            @if (get_field('archive_' . sanitize_title(get_post_type()) . '_feed_date_published', 'option') != 'false')
+            <time class="c-card__sub o-text-secondary o-text-small">
+                {{ in_array(get_field('archive_' . sanitize_title(get_post_type()) . '_feed_date_published', 'option'), array('datetime', 'date')) ? the_time(get_option('date_format')) : '' }}
+                {{ in_array(get_field('archive_' . sanitize_title(get_post_type()) . '_feed_date_published', 'option'), array('datetime', 'time')) ? the_time(get_option('time_format')) : '' }}
+            </time>
             @endif
-
-            <div class="box-content">
-                <h3 class="text-highlight">{{ the_title() }}</h3>
-
-                @if (get_field('archive_' . sanitize_title(get_post_type()) . '_feed_date_published', 'option') != 'false')
-                <time>
-                    {{ in_array(get_field('archive_' . sanitize_title(get_post_type()) . '_feed_date_published', 'option'), array('datetime', 'date')) ? the_time(get_option('date_format')) : '' }}
-                    {{ in_array(get_field('archive_' . sanitize_title(get_post_type()) . '_feed_date_published', 'option'), array('datetime', 'time')) ? the_time(get_option('time_format')) : '' }}
-                </time>
-                @endif
-
-                {{ the_excerpt() }}
-            </div>
+            <p class="c-card__text">{{ the_excerpt() }}</p>
         </div>
     </a>
-</div>
+
