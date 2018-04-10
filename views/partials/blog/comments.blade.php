6bda1d469226f4b0e5a0585a18a23eeabd4e4a17
Jonatan Hanson <jonatan.hanson@hiq.se>
2018-04-10 15:06:47 +0200

    Delete action dropdown

--- views/partials/blog/comments.blade.php
+++ views/partials/blog/comments.blade.php
@@ -49,6 +49,18 @@
                                     {{comment_reply_link($replyArgs,$comment->comment_ID,$comment->comment_post_ID)}}
                                 </span>
                             @endif
+                            @if($comment->user_id == get_current_user_id())
+                            <span class="actions">
+                                <a href="#" data-dropdown=".comment-actions-{{ $comment->comment_ID }}">
+                                    <i class="pricon pricon-menu-dots"></i>
+                                </a>
+                                <ul class="comment-actions-{{ $comment->comment_ID }} dropdown-menu dropdown-menu-arrow dropdown-menu-arrow-left">
+                                    <li>
+                                        <a href="#" id="delete-comment" data-comment-nonce="{{ wp_create_nonce("delete-comment_$comment->comment_ID") }}" data-comment-id="{{ $comment->comment_ID }}"><i class="pricon pricon-minus-o pricon-space-right"></i> <?php _e('Remove', 'municipio'); ?></a>
+                                    </li>
+                                </ul>
+                            </span>
+                            @endif
                         </div>
                     </div>
 
@@ -96,6 +108,18 @@
                                             @if (Municipio\Controller\Single::likeButton($answer->comment_ID) !== null )
                                             <span class="like">
                                                 {!! Municipio\Controller\Single::likeButton($answer->comment_ID) !!}
+                                            </span>
+                                            @endif
+                                            @if($answer->user_id == get_current_user_id())
+                                            <span class="actions">
+                                                <a href="#" data-dropdown=".comment-actions-{{ $answer->comment_ID }}">
+                                                    <i class="pricon pricon-menu-dots"></i>
+                                                </a>
+                                                <ul class="comment-actions-{{ $answer->comment_ID }} dropdown-menu dropdown-menu-arrow dropdown-menu-arrow-left">
+                                                    <li>
+                                                        <a href="#" id="delete-comment" data-comment-nonce="{{ wp_create_nonce("delete-comment_$answer->comment_ID") }}" data-comment-id="{{ $answer->comment_ID }}"><i class="pricon pricon-minus-o pricon-space-right"></i> <?php _e('Remove', 'municipio'); ?></a>
+                                                    </li>
+                                                </ul>
                                             </span>
                                             @endif
                                         </div>
