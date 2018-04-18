229bece908ff776dd6ea3cfe937bb7c73312d19b
Nikolas Ramstedt <nikolas.ramstedt@gmail.com>
2018-04-18 16:18:44 +0200

    add field to span footer columns within theme options / footer

---
+++ library/AcfFields/php/options-theme-footer-customizer.php
@@ -0,0 +1,47 @@
+<?php 
+
+if (function_exists('acf_add_local_field_group')) {
+    acf_add_local_field_group(array(
+    'key' => 'group_5acf485bbf000',
+    'title' => __('Customizer footer', 'municipio'),
+    'fields' => array(
+        0 => array(
+            'key' => 'field_5acf48608921c',
+            'label' => __('Footer sidebar columns', 'municipio'),
+            'name' => 'customizer_footer_sidebars',
+            'type' => 'range',
+            'instructions' => '',
+            'required' => 0,
+            'conditional_logic' => 0,
+            'wrapper' => array(
+                'width' => '',
+                'class' => '',
+                'id' => '',
+            ),
+            'default_value' => 4,
+            'min' => 1,
+            'max' => 12,
+            'step' => '',
+            'prepend' => '',
+            'append' => '',
+        ),
+    ),
+    'location' => array(
+        0 => array(
+            0 => array(
+                'param' => 'options_page',
+                'operator' => '==',
+                'value' => 'acf-options-footer',
+            ),
+        ),
+    ),
+    'menu_order' => 0,
+    'position' => 'normal',
+    'style' => 'default',
+    'label_placement' => 'top',
+    'instruction_placement' => 'label',
+    'hide_on_screen' => '',
+    'active' => 1,
+    'description' => '',
+));
+}
--- library/Bootstrap.php
+++ library/Bootstrap.php
@@ -96,6 +96,7 @@ add_action('init', function () {
         'widget-header-logotype'           => 'group_5a5ca31651f08',
         'widget-header-links'              => 'group_5a6744018083f',
         'theme-version-preview'            => 'group_5aa14b41551ae',
+        'options-theme-footer-customizer'  => 'group_5acf485bbf000'
     ));
     $acfExportManager->import();
 });
