739b44518b47f914bbc717e5e45f6c615da0669e
Jonatan Hanson <jonatan.hanson@hiq.se>
2018-05-23 08:51:03 +0200

    Corrected 404 message

--- views/partials/404/event.blade.php
+++ views/partials/404/event.blade.php
@@ -2,9 +2,9 @@
     <div class="container">
         <div class="grid-lg-12 text-center">
             <div class="gutter gutter-xl">
-                <h1><?php _e('The ticket could not be found', 'municipio');?></h1>
+                <h1><?php _e('The event could not be found', 'municipio');?></h1>
 
-                <a href="{{ $post_type_permalink }}" class="btn btn-md"><?php _e('View all tickets', 'municipio'); ?></a>
+                <a href="{{ $post_type_permalink }}" class="btn btn-md"><?php _e('View all events', 'municipio'); ?></a>
             </div>
         </div>
     </div>
