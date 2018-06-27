dd44788f574670f8eee8b064ce584037b9179b0b
Jonatan Hanson <jonatan.hanson@hiq.se>
2018-06-27 09:11:13 +0200

    Fixed errors in translations files

--- library/AcfFields/php/theme-version-preview.php
+++ library/AcfFields/php/theme-version-preview.php
@@ -1,7 +1,9 @@
-<?php 
+<?php
 
-if (function_exists('acf_add_local_field_group')) {
-    acf_add_local_field_group(array(
+
+if (function_exists('acf_add_local_field_group')) {
+
+    acf_add_local_field_group(array(
     'key' => 'group_5aa14b41551ae',
     'title' => __('2.0 Enabler', 'municipio'),
     'fields' => array(
@@ -18,12 +20,7 @@
                 'class' => '',
                 'id' => '',
             ),
-            'message' => __('In the upcoming months we are moving to a complete BEM style of our frontend. This is eased in and hidden by default (enable checkbox below to preview). Out main focus will be: 
-
-- Enabling the use of the customizer
-- Introduce BEM in all views
-
-Our amibition is that all functionality should be fully functional in this mode. But i\'t may break existing modifications.', 'municipio'),
+            'message' => __('In the upcoming months we are moving to a complete BEM style of our frontend. This is eased in and hidden by default (enable checkbox below to preview). Out main focus will be: 1. Enabling the use of the customizer 2. Introduce BEM in all views. Our amibition is that all functionality should be fully functional in this mode. But it may break existing modifications.', 'municipio'),
             'new_lines' => 'br',
             'esc_html' => 0,
         ),
@@ -73,4 +70,5 @@ Our amibition is that all functionality should be fully functional in this mode.
     'active' => 1,
     'description' => '',
 ));
-}
+
+}
--- views/partials/404/event.blade.php
+++ views/partials/404/event.blade.php
@@ -2,8 +2,7 @@
     <div class="container">
         <div class="grid-lg-12 text-center">
             <div class="gutter gutter-xl">
-                <h1><?php _e('The event could not be found', 'municipio');?></h1>
-
+                <h1><?php _e('The event could not be found', 'municipio'); ?></h1>
                 <a href="{{ $post_type_permalink }}" class="btn btn-md"><?php _e('View all events', 'municipio'); ?></a>
             </div>
         </div>
