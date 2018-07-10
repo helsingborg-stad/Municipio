a245fc89e56867515df7d2ace4b4a8a0d7a55dd3
Nikolas Ramstedt <nikolas.ramstedt@gmail.com>
2018-07-10 19:10:19 +0200

    Comment to distinguish sections

--- bem-views/templates/master.blade.php
+++ bem-views/templates/master.blade.php
@@ -22,6 +22,7 @@
 </head>
 <body class="{{ $bodyClass }}">
 
+    {{-- Site header --}}
     @section('header')
         @if (isset($headerLayout['template']))
             @includeIf('partials.header.' . $headerLayout['template'])
@@ -29,49 +30,54 @@
     @show
 
     <main id="main" class="c-main s-main">
-
+        {{-- Before page layout --}}
         @yield('before-layout')
 
+        {{-- Page layout --}}
         @section('layout')
             <div class="container">
                 <div class="grid grid--columns">
-
+                    {{-- Above --}}
                     @hasSection('above')
                         <div class="grid-xs-12 s-above">
                             @yield('above')
                         </div>
                     @endif
 
+                    {{-- Sidebar left --}}
                     @hasSection('sidebar-left')
                         <div class="{{$layout['sidebarLeft']}} s-sidebar-left">
                             @yield('sidebar-left')
                         </div>
                     @endif
 
+                    {{-- Content --}}
                     <div class="{{$layout['content']}} s-content">
                         @yield('content')
                     </div>
 
+                    {{-- Sidebar right --}}
                     @hasSection('sidebar-right')
                         <div class="{{$layout['sidebarRight']}} s-sidebar-right">
                             @yield('sidebar-right')
                         </div>
                     @endif
 
+                    {{-- Below --}}
                     @hasSection('below')
                         <div class="grid-xs-12 s-below">
                             @yield('below')
                         </div>
                     @endif
-
                 </div>
             </div>
         @show
 
+        {{-- After page layout --}}
         @yield('after-layout')
-
     </main>
 
+    {{-- Site footer --}}
     @section('footer')
         @if (isset($footerLayout['template']))
             @includeIf('partials.footer.' . $footerLayout['template'])
