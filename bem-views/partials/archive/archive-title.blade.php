1b63dc2cb3198b5d4ff53b3f2d957d3add34d94c
Nikolas Ramstedt <nikolas.ramstedt@gmail.com>
2018-07-16 07:48:31 +0200

    archive title

--- bem-views/partials/archive/archive-title.blade.php
+++ bem-views/partials/archive/archive-title.blade.php
@@ -1,17 +1,17 @@
 @if (get_field('archive_' . sanitize_title($postType) . '_title', 'option'))
     @if (is_category())
-        <h1>{{ get_field('archive_' . sanitize_title($postType) . '_title', 'option') }}: {{ single_cat_title() }}</h1>
+        <h1 class="archive__title">{{ get_field('archive_' . sanitize_title($postType) . '_title', 'option') }}: {{ single_cat_title() }}</h1>
     {!! category_description() !!}
     @elseif (is_date())
-        <h1>{{ get_field('archive_' . sanitize_title($postType) . '_title', 'option') }}: {{ the_archive_title() }}</h1>
+        <h1 class="archive__title">{{ get_field('archive_' . sanitize_title($postType) . '_title', 'option') }}: {{ the_archive_title() }}</h1>
     @else
-        <h1>{{ get_field('archive_' . sanitize_title($postType) . '_title', 'option') }}</h1>
+        <h1 class="archive__title">{{ get_field('archive_' . sanitize_title($postType) . '_title', 'option') }}</h1>
     @endif
 @else
     @if (is_category())
-        <h1>{{ single_cat_title() }}</h1>
+        <h1 class="archive__title">{{ single_cat_title() }}</h1>
     {!! category_description() !!}
     @elseif (is_date())
-        <h1>{{ the_archive_title() }}</h1>
+        <h1 class="archive__title">{{ the_archive_title() }}</h1>
     @endif
 @endif
