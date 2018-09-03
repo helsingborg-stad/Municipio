a63dd7ecdc05fdbd82ac3f95851ddeb67a481420
Sebastian Thulin <sebastian.thulin@gmail.com>
2018-09-03 14:18:21 +0200

    Simplifies honeytpot check
    Since we are relying on google robot check more, we are simplifiying this function to remove some code.

--- library/Comment/HoneyPot.php
+++ library/Comment/HoneyPot.php
@@ -25,64 +25,11 @@ class HoneyPot
         add_filter('comment_form_logged_in_after', array($this, 'addHoneyPotFieldBlank'));
         add_filter('comment_form_after_fields', array($this, 'addHoneyPotFieldBlank'));
 
-        //Print cookie method
-        add_action('init', array($this, 'fakeImage'));
-
         //Catch fields
         add_filter('preprocess_comment', array($this, 'honeyPotValidateFieldContent'));
-        add_filter('preprocess_comment', array($this, 'fakeImageCookieCheck'));
 
         //Add styling to hide field
         add_filter('comment_form', array($this, 'printFakeHideBox'));
-        add_filter('comment_form', array($this, 'printFakeImage'));
-    }
-
-    /**
-     * Outputs honey pot fake image
-     */
-    public function fakeImage()
-    {
-
-        if (isset($_GET[$this->field_name]) && $_GET[$this->field_name] == $this->field_content) {
-
-            if (!session_id()) {
-                session_start();
-            }
-
-            //Send svg header
-            header('Content-type: image/svg+xml');
-
-            //Do not cache this response
-            header("Cache-Control: no-store, no-cache, must-revalidate, max-age=0");
-            header("Cache-Control: post-check=0, pre-check=0", false);
-            header("Pragma: no-cache");
-
-            //Set cookie
-            $_SESSION[$this->field_name] = $this->field_content;
-
-            //Return something tha looks like a image
-            die('<svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"><circle cx="50" cy="50" r="40" stroke="black" stroke-width="3" fill="red" /></svg>');
-        }
-    }
-
-    /**
-     * Outputs honey pot fake image check
-     */
-    public function fakeImageCookieCheck($data)
-    {
-        if (isset($_SESSION[$this->field_name]) && $_SESSION[$this->field_name] == $this->field_content) {
-            return $data;
-        }
-
-        wp_die(__("Could not verify that you are human (image check).", 'municipio'));
-    }
-
-    /**
-     * Outputs honey pot fake image
-     */
-    public function printFakeImage()
-    {
-        echo '<div class="fake-hide"><img src="' . get_permalink() . '?' . $this->field_name . '=' . $this->field_content . '"></div>';
     }
 
     /**
@@ -98,7 +45,7 @@ class HoneyPot
      */
     public function addHoneyPotFieldFilled()
     {
-        echo '<div class="fake-hide"><input name="'.$this->field_name.'_fi" type="text" value="'.$this->field_content.'" size="30"></div>';
+        echo '<div class="fake-hide"><input name="'.$this->field_name.'_fi" type="text" value="'.$this->field_content.'" size="30" autocomplete="off" tabIndex="-1"></div>';
     }
 
     /**
@@ -106,7 +53,7 @@ class HoneyPot
      */
     public function addHoneyPotFieldBlank()
     {
-        echo '<div class="fake-hide"><input class="hidden" name="'.$this->field_name.'_bl" type="text" value="" size="30"></div>';
+        echo '<div class="fake-hide"><input class="hidden" name="'.$this->field_name.'_bl" type="text" value="" size="30" autocomplete="off" tabIndex="-1"></div>';
     }
 
     /**
