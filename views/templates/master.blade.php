11ca6bacc58d482a146afd10b80359bc6d33aea3
Sebastian Thulin <sebastian.thulin@gmail.com>
2018-09-05 14:23:04 +0200

    REmoves support for lt IE 9.
    This stuff is way too old now.

--- views/templates/master.blade.php
+++ views/templates/master.blade.php
@@ -20,36 +20,9 @@
         var ajaxurl = '{!! apply_filters('Municipio/ajax_url_in_head', admin_url('admin-ajax.php')) !!}';
     </script>
 
-    <!--[if lt IE 9]>
-        <script type="text/javascript">
-            document.createElement('header');
-            document.createElement('nav');
-            document.createElement('section');
-            document.createElement('article');
-            document.createElement('aside');
-            document.createElement('footer');
-            document.createElement('hgroup');
-        </script>
-        <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
-    <![endif]-->
-
     {!! wp_head() !!}
 </head>
 <body {!! body_class('no-js') !!}>
-    <!--[if lt IE 9]>
-        <div class="notice info browserupgrade">
-            <div class="container">
-                <div class="grid-table grid-va-middle">
-                    <div class="grid-fit-content">
-                        <i class="fa fa-plug"></i>
-                    </div>
-                    <div class="grid-auto">
-                        <strong>Du använder en gammal webbläsare.</strong> För att hemsidan ska fungera så bra som möjligt bör du byta till en modernare webbläsare. På <a href="http://browsehappy.com">browsehappy.com</a> kan du få hjälp att hitta en ny modern webbläsare.
-                    </div>
-                </div>
-            </div>
-        </div>
-    <![endif]-->
 
     <a href="#main-menu" class="btn btn-default btn-block btn-lg btn-offcanvas" tabindex="1"><?php _e('Jump to the main menu', 'municipio'); ?></a>
     <a href="#main-content" class="btn btn-default btn-block btn-lg btn-offcanvas" tabindex="2"><?php _e('Jump to the main content', 'municipio'); ?></a>
