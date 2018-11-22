c91812c7d4723dbfefdb5dd341ce3df3ee77f657
Nikolas Ramstedt <nikolas.ramstedt@gmail.com>
2018-11-22 10:28:07 +0100

    Enable password protection for BEM-views

--- bem-views/partials/article.blade.php
+++ bem-views/partials/article.blade.php
@@ -8,18 +8,22 @@
         <img src="{{ municipio_get_thumbnail_source(null, array(700,700)) }}" alt="{{ the_title() }}">
     @endif
 
-    @if (isset(get_extended($post->post_content)['main']) && strlen(get_extended($post->post_content)['main']) > 0 && isset(get_extended($post->post_content)['extended']) && strlen(get_extended($post->post_content)['extended']) > 0)
-
-        {!! apply_filters('the_lead', get_extended($post->post_content)['main']) !!}
-        {!! apply_filters('the_content', get_extended($post->post_content)['extended']) !!}
-
+    @if (post_password_required($post))
+        {!! get_the_password_form() !!}
     @else
-        @if (substr($post->post_content, -11) == '<!--more-->')
-        {!! apply_filters('the_lead', get_extended($post->post_content)['main']) !!}
-        @else
-        {!! the_content() !!}
-        @endif
+        @if (isset(get_extended($post->post_content)['main']) && strlen(get_extended($post->post_content)['main']) > 0 && isset(get_extended($post->post_content)['extended']) && strlen(get_extended($post->post_content)['extended']) > 0)
 
+            {!! apply_filters('the_lead', get_extended($post->post_content)['main']) !!}
+            {!! apply_filters('the_content', get_extended($post->post_content)['extended']) !!}
+
+        @else
+            @if (substr($post->post_content, -11) == '<!--more-->')
+            {!! apply_filters('the_lead', get_extended($post->post_content)['main']) !!}
+            @else
+            {!! the_content() !!}
+            @endif
+
+        @endif
     @endif
 
 </article>
