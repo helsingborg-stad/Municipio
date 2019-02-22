0a9684609bbb70fdb4e708973ca7f7f132d038db
Sebastian Thulin <sebastian.thulin@helsingborg.se>
2019-02-22 13:28:24 +0100

    Adds comments functionality to pages

--- views/page.blade.php
+++ views/page.blade.php
@@ -29,11 +29,40 @@
                 </div>
             @endif
 
+            @if (is_singular() && comments_open() && get_option('comment_registration') == 0 || is_singular() && comments_open() && is_user_logged_in())
+                @if(get_option('comment_order') == 'desc')
+                    <div class="grid">
+                        <div class="grid-sm-12">
+                            @include('partials.blog.comments-form')
+                        </div>
+                    </div>
+                    @if(isset($comments) && ! empty($comments))
+                        <div class="grid">
+                            <div class="grid-sm-12">
+                                @include('partials.blog.comments')
+                            </div>
+                        </div>
+                    @endif
+                @else
+                    @if(isset($comments) && ! empty($comments))
+                        <div class="grid">
+                            <div class="grid-sm-12">
+                                @include('partials.blog.comments')
+                            </div>
+                        </div>
+                    @endif
+                    <div class="grid">
+                        <div class="grid-sm-12">
+                            @include('partials.blog.comments-form')
+                        </div>
+                    </div>
+                @endif
+            @endif
+
             <div class="hidden-xs hidden-sm hidden-print">
                 @include('partials.page-footer')
             </div>
         </div>
-
 
         @include('partials.sidebar-right')
     </div>
