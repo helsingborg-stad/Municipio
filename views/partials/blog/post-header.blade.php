a0bfa68e63010491541f8fed974ec3d01e9e9e8e
Sebastian Thulin <sebastian.thulin@helsingborg.se>
2019-11-13 10:56:57 +0100

    Add h1 class to header for blog

--- views/partials/blog/post-header.blade.php
+++ views/partials/blog/post-header.blade.php
@@ -1,5 +1,5 @@
 <header class="post-header">
-    <h1><a href="{{ the_permalink() }}">{{ the_title() }}</a></h1>
+    <h1><a class="h1" href="{{ the_permalink() }}">{{ the_title() }}</a></h1>
 
     @include('partials.blog.post-info')
 
