1476a1b87ca5553b1f4da14e0e9ac3c36857900d
Sebastian Thulin <sebastian.thulin@helsingborg.se>
2020-02-11 10:20:12 +0100

    Remove IE 8/9 support tag

--- views/v3/templates/master.blade.php
+++ views/v3/templates/master.blade.php
@@ -1,8 +1,6 @@
 <!DOCTYPE html>
 <html {!! $languageAttributes !!}>
 <head>
-    <meta http-equiv="X-UA-Compatible" content="IE=EDGE">
-
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
