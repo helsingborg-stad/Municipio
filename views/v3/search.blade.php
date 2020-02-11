115f70d17149b2f463f1fa874673cd415a23075d
Sebastian Thulin <sebastian.thulin@helsingborg.se>
2020-02-11 08:09:26 +0100

    Remove algolia alternative searches.

--- views/v3/search.blade.php
+++ views/v3/search.blade.php
@@ -4,14 +4,8 @@
         @case("google")
             @include('partials.search.google')
             @break
-        @case("algoliacustom")
-            @include('partials.search.algolia-customsearch')
-            @break
         @case("algolia")
             @include('partials.search.algolia')
-            @break
-        @case("algoliainstant")
-            @include('partials.search.algolia-instantsearch')
             @break
         @default
             @include('partials.search.wp')
