685f48f5ec4cc67d469a143666c310f22516a8bc
Silvergrund <johan.silvergrund@hiq.se>
2020-02-21 11:27:31 +0100

    post-grid

--- views/v3/partials/archive/post/post-grid.blade.php
+++ views/v3/partials/archive/post/post-grid.blade.php
@@ -5,7 +5,7 @@ $thumbnail = municipio_get_thumbnail_source(
     $post->ID,
     array(500, 500)
 );
-;
+
 @endphp
 @link([
     'href' =>  the_permalink(),
@@ -17,57 +17,55 @@ $thumbnail = municipio_get_thumbnail_source(
 ])
 
 
-        @if ($thumbnail)
-            <div class="box-image" {!! $thumbnail ? 'style="background-image:url(' . $thumbnail . ');"' : '' !!}>
+    @if ($thumbnail)
+        <div class="box-image" {!! $thumbnail ? 'style="background-image:url(' . $thumbnail . ');"' : '' !!}>
 
-                @image([
-                    'src'=> municipio_get_thumbnail_source(null,array(500,500)),
-                    'alt' => the_title()
-                ])
-                @endimage
-
-            </div>
-        @endif
-
-        <div class="box-content">
-
-            @if (in_array('category', (array)get_field('archive_' . sanitize_title(get_post_type()) . '_post_display_info', 'option')) && isset(get_the_category()[0]->name))
-
-                @typography([
-                    'variant'=> 'span',
-                    'classList' => ['box-post-brick-category']
-                ])
-                    {{get_the_category()[0]->name}}
-                @endtypography
-
-            @endif
-
-            @if (get_field('archive_' . sanitize_title(get_post_type()) . '_feed_date_published', 'option') != 'false')
-            <span class="box-post-brick-date u-mb-3">
-
-                {{-- TODO: $post->dateObject  something from them post object - use right var --}}
-                @date([
-                    'action' => 'formatDate',
-                    'timestamp' =>  $post->dateObject
-                ])
-                @enddate
-
-            </span>
-            @endif
-
-            <h3 class="post-title">{{ the_title() }}</h3>
-
-            <ul class="tags">
-                @foreach (municipio_post_taxonomies_to_display(get_the_id()) as $taxonomy => $terms)
-                    @foreach ($terms as $term)
-                        <li class="tag tag-{{ $term->taxonomy }} tag-{{ $term->slug }}">{{ $term->name }}</li>
-                    @endforeach
-                @endforeach
-            </ul>
+            @image([
+                'src'=> municipio_get_thumbnail_source(null,array(500,500)),
+                'alt' => the_title()
+            ])
+            @endimage
 
         </div>
-        <div class="box-post-brick-lead">
-            {{ the_excerpt() }}
-        </div>
+    @endif
+
+
+
+    @if (in_array('category', (array)get_field('archive_' . sanitize_title(get_post_type()) . '_post_display_info', 'option')) && isset(get_the_category()[0]->name))
+
+        @typography([
+            'variant'=> 'span',
+            'classList' => ['box-post-brick-category']
+        ])
+            {{get_the_category()[0]->name}}
+        @endtypography
+
+    @endif
+
+    @if (get_field('archive_' . sanitize_title(get_post_type()) . '_feed_date_published', 'option') != 'false')
+        {{-- TODO: $post->dateObject  something from them post object - use right var --}}
+        @date([
+            'action' => 'formatDate',
+            'timestamp' =>  $post->dateObject
+        ])
+        @enddate
+    @endif
+
+    @typography([
+    'element'=> 'h3'
+    ])
+        {{ the_title() }}
+    @endtypography
+
+    <ul class="tags">
+        @foreach (municipio_post_taxonomies_to_display(get_the_id()) as $taxonomy => $terms)
+            @foreach ($terms as $term)
+                <li class="tag tag-{{ $term->taxonomy }} tag-{{ $term->slug }}">{{ $term->name }}</li>
+            @endforeach
+        @endforeach
+    </ul>
+
+    {{ the_excerpt() }}
+
 @endlink
 
