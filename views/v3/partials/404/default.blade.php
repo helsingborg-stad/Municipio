61076081769d4edf922bb87aef6578f0bebcaea9
Silvergrund <johan.silvergrund@hiq.se>
2020-02-25 10:55:13 +0100

    views - Component dir obsolete

--- views/v3/partials/404/default.blade.php
+++ views/v3/partials/404/default.blade.php
@@ -1,17 +1,18 @@
 @typography([
     "element" => "h1",
-    ])
+])
     {{ $heading }}
 @endtypography
 
 @typography([
     "element" => "span",
-    ])
+])
     {{ $subheading }}
 @endtypography
 
 <ul class="actions">
-    @if (is_array(get_field('404_display', 'option')) && in_array('search', get_field('404_display', 'option')))
+    @if (is_array(get_field('404_display', 'option')) &&
+        in_array('search', get_field('404_display', 'option')))
         <li>
             @link([
                 'href' => {{ home_url() }}?s={{ $keyword }}
@@ -22,22 +23,26 @@
         </li>
     @endif
 
-    @if (is_array(get_field('404_display', 'option')) && in_array('home', get_field('404_display', 'option')))
+    @if (is_array(get_field('404_display', 'option')) &&
+            in_array('home', get_field('404_display', 'option')))
         <li>
             @link([
                 'href' => {{ home_url() }}
             ])
-            {{ get_field('404_home_link_text', 'option') ? get_field('404_home_link_text', 'option') : 'Go to home' }}
+                {{ get_field('404_home_link_text', 'option') ?
+                        get_field('404_home_link_text', 'option') : 'Go to home' }}
             @endlink
         </li>
     @endif
 
-    @if (is_array(get_field('404_display', 'option')) && in_array('back', get_field('404_display', 'option')))
+    @if (is_array(get_field('404_display', 'option')) &&
+        in_array('back', get_field('404_display', 'option')))
         <li>
             @link([
                 'href' => 'javascript:history.go(-1);'
             ])
-                {{ get_field('404_back_button_text', 'option') ? get_field('404_back_button_text', 'option') : 'Go back' }}
+                {{ get_field('404_back_button_text', 'option') ?
+                        get_field('404_back_button_text', 'option') : 'Go back' }}
             @endlink
         </li>
     @endif
@@ -46,7 +51,7 @@
 @if($errorMessage)
     @typography([
         "element" => "h3",
-        ])
+    ])
         {{ $debugHeading }}
     @endtypography
 
