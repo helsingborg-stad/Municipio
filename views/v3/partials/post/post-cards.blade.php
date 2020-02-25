7eb63d8fbd09b95df80850b140ca7cdceab3ad5f
Silvergrund <johan.silvergrund@hiq.se>
2020-02-25 10:07:26 +0100

    view - cleanup

--- views/v3/partials/post/post-cards.blade.php
+++ views/v3/partials/post/post-cards.blade.php
@@ -1,34 +1,23 @@
-<?php global $post; ?>
-{{-- TODO:  Temporary - Maybe move logic to some Controller --}}
+
 @php
 
     if (municipio_get_thumbnail_source(null,array(400,225))) {
         $image = municipio_get_thumbnail_source(null,array(400,225));
     }
 
-    if (get_field('archive_' . sanitize_title(get_post_type()) . '_feed_date_published',
-    'option') != 'false') {
 
-        $publishedDate = in_array(get_field('archive_' . sanitize_title(get_post_type()) .
-        '_feed_date_published','option'), array('datetime', 'date')) ?
-            the_time(get_option('date_format')) : '';
-
-        $publishedTime = in_array(get_field('archive_' . sanitize_title(get_post_type()) .
-                '_feed_date_published', 'option'), array('datetime', 'time')) ?
-                the_time(get_option('time_format')) : '';
-    }
 
  @endphp
 
 @card([
-        'href' => the_permalink(),
-        'image' => $image,
+        'href' => $post->permalink,
+        'image' => $post-thumbnail,
         'title' => [
-            'text' => the_title(),
+            'text' => $post->postTitle,
             'position' => 'body'
         ],
         'classList' => ['c-card--shadow-on-hover'],
-        'byline' => ['text' => $publishedDate . '' . $publishedTime, 'position' => 'body'],
+        'byline' => ['text' => $post->postDate, 'position' => 'body'],
         'content' => the_excerpt(),
         'hasRipple' => false
     ])
--- views/v3/singular.blade.php
+++ views/v3/singular.blade.php
@@ -1,1 +1,10 @@
 @extends('templates.single')
+
+
+@php
+
+        echo "<pre>";
+        var_dump($post);
+        echo "</pre>";
+
+@endphp
