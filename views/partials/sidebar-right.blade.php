4590e978a31d98137f55b0d0eed22bdced8281b1
nRamstedt <nikolas.ramstedt@gmail.com>
2020-04-01 09:50:57 +0200

    Fix righ sidebar not showing

--- views/partials/sidebar-right.blade.php
+++ views/partials/sidebar-right.blade.php
@@ -1,5 +1,5 @@
 @if ($hasRightSidebar)
-<aside class="grid-lg-3 grid-md-12 sidebar-right-sidebar hidden-xs hidden-sm hidden-md">
+<aside class="grid-lg-3 grid-md-12 sidebar-right-sidebar @if (is_page()) hidden-xs hidden-sm hidden-md @endif">
     @if (is_active_sidebar('right-sidebar') || (isset($enabledSidebarFilters) && is_array($enabledSidebarFilters)))
     <div class="grid grid--columns">
 
