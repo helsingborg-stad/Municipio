7a95b1a5dabb2f86d8b11dc04e930e323ef04e41
Sebastian Thulin <sebastian.thulin@helsingborg.se>
2020-06-16 14:24:08 +0200

    Do not use js to load fonts, use swaploading from google web fonts.

--- library/Theme/Font.php
+++ library/Theme/Font.php
@@ -6,64 +6,34 @@ class Font
 {
     public function __construct()
     {
-        if (defined('WEB_FONT') && WEB_FONT != "") {
-            add_action('wp_head', array($this, 'renderFontJS'), 10);
-            add_action('wp_head', array($this, 'renderFontVar'), 10);
-            add_action('wp_head', array($this, 'renderFontLibrary'), 5);
-        }
+        add_action('wp_head', array($this, 'renderFont'), 10);
     }
 
     /**
-     * Print current font
+     * Print current font from google web fonts
+     * 
      * @return void
      */
-    public function renderFontVar()
+    public function renderFont()
     {
+        //Disable inline print
         if (defined('WEB_FONT_DISABLE_INLINE') && WEB_FONT_DISABLE_INLINE != "") {
+            return;
+        }
+
+        //What webfont to load (name)
+        if (!defined('WEB_FONT') || (defined('WEB_FONT') && WEB_FONT == "")) { 
             return;
         }
 
         echo "
             <style>
-            body {
-                font-family: " . WEB_FONT . ",system,Segoe UI,Tahoma,-apple-system;
-            }
+                body {
+                    font-family: " . WEB_FONT . ",system,Segoe UI,Tahoma,-apple-system;
+                }
             </style>
         ";
-    }
 
-    /**
-     * Print js-function in header
-     * @param  string $fontFamily Font family to save
-     * @return void
-     */
-    public function renderFontJS()
-    {
-        echo "
-            <script>
-            WebFont.load({
-                google: {
-                  families: ['". WEB_FONT .":300,400,400i,500,500i,700,700i,900']
-                },
-                timeout: 2000
-              });
-            </script>
-        ";
-    }
-
-    /**
-     * Print js-library in header
-     * @param  string $fontFamily Font family to save
-     * @return void
-     */
-    public function renderFontLibrary()
-    {
-        if (!defined('WEB_FONT_REMOTE') && file_exists(MUNICIPIO_PATH . '/assets/source/js/font.js')) {
-            echo '<script>';
-            readfile(MUNICIPIO_PATH . '/assets/source/js/font.js');
-            echo '</script>';
-        } else {
-            wp_enqueue_script('webfont-loader', 'https://ajax.googleapis.com/ajax/libs/webfont/1.6.26/webfont.js', array(), '1.0.0', false);
-        }
+        wp_enqueue_style('municipio-font', 'https://fonts.googleapis.com/css2?family=' . WEB_FONT . ':ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap'); 
     }
 }
