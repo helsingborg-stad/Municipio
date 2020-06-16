e1105a0592aa1b63507974b273269d9ee30cabe3
Ehsan Zilaei <zzilaei@gmail.com>
2020-06-16 15:15:16 +0200

    Order select dropdown by name, ascending alphabetic order (#77)

--- views/partials/archive-filters/select.blade.php
+++ views/partials/archive-filters/select.blade.php
@@ -2,11 +2,12 @@
     $selected = isset($_GET['filter'][$taxKey]) && $_GET['filter'][$taxKey] !== '-1' ? $_GET['filter'][$taxKey] : null;
 
     wp_dropdown_categories(array(
-        'taxonomy' => $taxKey,
-        'hide_empty' => false,
-        'hierarchical' => true,
-        'name' => 'filter[' . $taxKey . ']',
-        'show_option_none' => sprintf(__('Select') . ' %s…', $tax->label),
-        'value_field' => 'slug',
-        'selected' => $selected
+        'taxonomy'          => $taxKey,
+        'hide_empty'        => false,
+        'hierarchical'      => true,
+        'name'              => 'filter[' . $taxKey . ']',
+        'show_option_none'  => sprintf(__('Select') . ' %s…', $tax->label),
+        'value_field'       => 'slug',
+        'selected'          => $selected,
+        'orderby'           => 'name'
     ));
