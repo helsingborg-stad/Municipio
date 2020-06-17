cc87ac6134156bc3a163db2c66e7be51ae6bfdb7
Sebastian Thulin <sebastian.thulin@helsingborg.se>
2020-06-17 14:52:17 +0200

    Get go back link if is archive page 404

--- views/v3/partials/404/default.blade.php
+++ views/v3/partials/404/default.blade.php
@@ -1,16 +1,20 @@
-@typography(["element" => "h1"])
-    {{ $heading }}
-@endtypography
 
-@typography(["element" => "span"])
-    {{ $subheading }}
-@endtypography
+    @typography(["element" => "h1", "id" => "header404", "classList" => ["c-typhography--404-heading"]])
+        {{ $heading }}
+    @endtypography
 
-@foreach($actionButtons as $button) 
-    @button([
-        'text' => $button->label,
-        'color' => 'primary',
-        'style' => 'basic',
-        'href' => $button->href
-    ])@endbutton
-@endforeach
+    @typography(["element" => "span", "id" => "content404", "classList" => ["c-typhography--404-subheading"]])
+        {{ $subheading }}
+    @endtypography
+
+    @grid(["col_gap" => 2,"row_gap" => 8, 'container' => true])
+        @foreach($actionButtons as $button) 
+            @button([
+                'text' => $button->label,
+                'href' => $button->href,
+                'color' => $loop->first ? 'primary' : 'secondary',
+                'style' => 'filled'
+            ])
+            @endbutton
+        @endforeach
+    @endgrid
