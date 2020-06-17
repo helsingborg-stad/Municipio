d5d4e658c0b840fff255f6b7d75a2deeb3570f5b
Sebastian Thulin <sebastian.thulin@helsingborg.se>
2020-06-17 15:20:51 +0200

    Indentation

--- views/v3/partials/404/default.blade.php
+++ views/v3/partials/404/default.blade.php
@@ -1,20 +1,19 @@
+@typography(["element" => "h1", "id" => "header404", "classList" => ["c-typhography--404-heading"]])
+    {{ $heading }}
+@endtypography
 
-    @typography(["element" => "h1", "id" => "header404", "classList" => ["c-typhography--404-heading"]])
-        {{ $heading }}
-    @endtypography
+@typography(["element" => "span", "id" => "content404", "classList" => ["c-typhography--404-subheading"]])
+    {{ $subheading }}
+@endtypography
 
-    @typography(["element" => "span", "id" => "content404", "classList" => ["c-typhography--404-subheading"]])
-        {{ $subheading }}
-    @endtypography
-
-    @grid(["col_gap" => 2,"row_gap" => 8, 'container' => true])
-        @foreach($actionButtons as $button) 
-            @button([
-                'text' => $button->label,
-                'href' => $button->href,
-                'color' => $loop->first ? 'primary' : 'secondary',
-                'style' => 'filled'
-            ])
-            @endbutton
-        @endforeach
-    @endgrid
+@grid(["col_gap" => 2,"row_gap" => 8, 'container' => true])
+    @foreach($actionButtons as $button) 
+        @button([
+            'text' => $button->label,
+            'href' => $button->href,
+            'color' => $loop->first ? 'primary' : 'secondary',
+            'style' => 'filled'
+        ])
+        @endbutton
+    @endforeach
+@endgrid
