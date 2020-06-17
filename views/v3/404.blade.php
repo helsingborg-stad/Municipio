f6ebecab11bd83fa349578beeaacd3cb7243fdcd
Sebastian Thulin <sebastian.thulin@helsingborg.se>
2020-06-17 14:12:03 +0200

    Fixes undefined variable

--- views/v3/404.blade.php
+++ views/v3/404.blade.php
@@ -1,5 +1,5 @@
 @extends('templates.master')
 
 @section('content')
-    @includeFirst(['partials.404.' . $post_type, 'partials.404.default'])
+    @includeFirst(['partials.404.' . $postType, 'partials.404.default'])
 @stop
