e37e8cf584ec9c4daeef2264f9d170dd063aa3d8
Sebastian Thulin <sebastian.thulin@helsingborg.se>
2020-06-17 11:21:05 +0200

    Separate font enqueue & inline css.

--- library/Theme/Font.php
+++ library/Theme/Font.php
@@ -6,22 +6,23 @@ class Font
 {
     public function __construct()
     {
-        add_action('wp_head', array($this, 'renderFont'), 10);
+        add_action('wp_head', array($this, 'renderInlineFontCss'), 10);
+        add_action('wp_head', array($this, 'enqueueFontLoad'), 10);
     }
 
     /**
-     * Print current font from google web fonts
+     * Print current font inline in head
      * 
      * @return void
      */
-    public function renderFont()
+    public function renderInlineFontCss()
     {
         //Disable inline print
         if (defined('WEB_FONT_DISABLE_INLINE') && WEB_FONT_DISABLE_INLINE != "") {
             return;
         }
 
-        //What webfont to load (name)
+        //Disable font if not defined. 
         if (!defined('WEB_FONT') || (defined('WEB_FONT') && WEB_FONT == "")) { 
             return;
         }
@@ -34,6 +35,21 @@ class Font
             </style>
         ";
 
+    }
+
+    /**
+     * Enqueue webfont from google web fonts with fontswapping
+     *
+     * @return void
+     */
+    public function enqueueFontLoad() {
+
+        //Disable font if not defined. 
+        if (!defined('WEB_FONT') || (defined('WEB_FONT') && WEB_FONT == "")) { 
+            return;
+        }
+
         wp_enqueue_style('municipio-font', 'https://fonts.googleapis.com/css2?family=' . WEB_FONT . ':ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap'); 
+
     }
 }
