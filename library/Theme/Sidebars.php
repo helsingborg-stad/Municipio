da54c374808a7f8f88e9960891365338070f77e4
Sebastian Thulin <sebastian.thulin@helsingborg.se>
2020-07-08 14:54:24 +0200

    Remove old classes & html form specific sidebar areas.

--- library/Theme/Sidebars.php
+++ library/Theme/Sidebars.php
@@ -7,70 +7,10 @@ class Sidebars
     public function __construct()
     {
         add_action('widgets_init', array($this, 'register'));
-        add_filter('Modularity/Module/Classes', array($this, 'moduleClasses'), 10, 3);
-        add_filter('Modularity/Module/WpWidget/before', array($this, 'wpWidgetBefore'), 10, 3);
         add_filter('Modularity/Editor/SidebarIncompability', array($this, 'moduleSidebarIncompability'), 10, 2);
 
         add_filter('Modularity/Module/Container/Sidebars', array($this, 'registerSidebarWithContainerSupport'));
         add_filter('Modularity/Module/Container/Modules', array($this, 'registerModulesWithContainerSupport'));
-    }
-
-    public function wpWidgetBefore($before, $sidebarArgs, $module)
-    {
-        if (get_field('mod_standard_widget_type', $module->ID) == 'WP_Widget_Search') {
-            return '<div>';
-        }
-
-        // Box panel in content area and content area bottom
-        if (in_array($sidebarArgs['id'], array('content-area', 'content-area-bottom')) && !is_front_page()) {
-            $before = '<div class="box box-panel box-panel-secondary">';
-        }
-
-        // Sidebar boxes (should be filled)
-        if (in_array($sidebarArgs['id'], array('left-sidebar-bottom', 'left-sidebar', 'right-sidebar'))) {
-            $before = '<div class="box box-filled">';
-        }
-
-        return $before;
-    }
-
-    /**
-     * Modify module classes in different areas
-     * @param  array $classes      Default classes
-     * @param  string $moduleType  Module type
-     * @param  array $sidebarArgs  Sidebar arguments
-     * @return array               Modified list of classes
-     */
-    public function moduleClasses($classes, $moduleType, $sidebarArgs)
-    {
-        // Box panel in content area and content area bottom
-        if (in_array($sidebarArgs['id'], array('content-area', 'content-area-bottom')) && in_array('box-panel', $classes) && !is_front_page()) {
-            $classes[] = 'box-panel-secondary';
-        }
-
-        // Inline box panels
-        if (is_numeric($sidebarArgs['id']) && in_array('box-panel', $classes)) {
-            $classes[] = 'box-panel-secondary';
-        }
-
-        // Sidebar box-panel (should be filled)
-        if (in_array($sidebarArgs['id'], array('left-sidebar-bottom', 'left-sidebar', 'right-sidebar')) && in_array('box-panel', $classes)) {
-            unset($classes[array_search('box-panel', $classes)]);
-            $classes[] = 'box-filled';
-        }
-
-        // Sidebar box-index (should be filled)
-        if (in_array($sidebarArgs['id'], array('left-sidebar-bottom', 'left-sidebar', 'right-sidebar')) && in_array('box-index', $classes)) {
-            unset($classes[array_search('box-index', $classes)]);
-            $classes[] = 'box-filled';
-        }
-
-        // Sidebar box-news-horizontal (should be only box-news in sidebar)
-        if (in_array($sidebarArgs['id'], array('left-sidebar-bottom', 'left-sidebar', 'right-sidebar')) && in_array('box-news-horizontal', $classes)) {
-            unset($classes[array_search('box-news-horizontal', $classes)]);
-        }
-
-        return $classes;
     }
 
     public function register()
@@ -103,7 +43,9 @@ class Sidebars
         register_sidebar(array(
             'id'            => 'content-area-top',
             'name'          => __('Content area (above article)', 'municipio'),
-            'description'   => __('The area above the content', 'municipio')
+            'description'   => __('The area above the content', 'municipio'),
+            'before_title'  => '<h2>',
+            'after_title'   => '</h2>'
         ));
 
         /**
@@ -112,7 +54,9 @@ class Sidebars
         register_sidebar(array(
             'id'            => 'content-area',
             'name'          => __('Content area (below article)', 'municipio'),
-            'description'   => __('The area below the content', 'municipio')
+            'description'   => __('The area below the content', 'municipio'),
+            'before_title'  => '<h2>',
+            'after_title'   => '</h2>'
         ));
 
         /**
@@ -121,7 +65,9 @@ class Sidebars
         register_sidebar(array(
             'id'            => 'content-area-bottom',
             'name'          => __('Main container bottom', 'municipio'),
-            'description'   => __('The area below the main container', 'municipio')
+            'description'   => __('The area below the main container', 'municipio'),
+            'before_title'  => '<h2>',
+            'after_title'   => '</h2>'
         ));
 
         /**
