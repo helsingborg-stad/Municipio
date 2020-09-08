0fe1fe262fe4108ba6a686372851c5218b67c7a8
Joel Bernerman <joel.bernerman@helsingborg.se>
2020-09-08 17:41:30 +0200

    Added github action yaml and changed build script to php

--- build-everything.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-npm install
-npm run build
-composer install --prefer-dist --no-progress --no-suggest 
-
-# any unique commands for this plugin
---
+++ build.php
@@ -0,0 +1,42 @@
+#!/bin/php
+<?php
+// Only allow run from cli.
+if (php_sapi_name() !== 'cli') {
+    exit(0);
+}
+
+// Any command needed to run and build plugin assets when newly cheched out of repo.
+$buildCommands = [
+    'npm install',
+    'npm run build',
+    'composer install --prefer-dist --no-progress --no-suggest'
+];
+
+// Files and directories not suitable for prod to be removed.
+$removables = [
+    '.git',
+    '.gitignore',
+    '.github',
+    'build.php',
+    '.npmrc',
+    'composer.json',
+    'env-example',
+    'gulpfile.js',
+    'gulpfile.old.js',
+    'webpack.config.js',
+    'node_modules'
+];
+
+// Run all build commands.
+foreach ($buildCommands as $buildCommand) {
+    print "Running build command $buildCommand.\n";
+    shell_exec($buildCommand);
+}
+
+// Remove files and directories.
+foreach ($removables as $removable) {
+    if (file_exists($removable)) {
+        print "Removing $removable\n";
+        shell_exec("rm -rf $removable");
+    }
+}
