72d21e4c76674c56356c3786175bc30b3dcee5d4
Silvergrund <johan.silvergrund@hiq.se>
2020-09-23 14:32:10 +0200

    mce-bug
    Lifted mce-dir from 2.0 to 3.0
    A’dded js/mce': './assets/source/3.0/mce-js/mce.js', to webpack

--- webpack.config.js
+++ webpack.config.js
@@ -31,7 +31,7 @@ module.exports = {
         'js/municipio': './assets/source/3.0/js/municipio.js',
         /* Legacy 2.0  */
         /* TODO: Fix TinyMce Scripts */
-        // 'js/mce': './assets/source/2.0/mce-js/mce.js',
+        'js/mce': './assets/source/3.0/mce-js/mce.js',
     },
     /**
      * Output settings
