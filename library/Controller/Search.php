ea48aaa218b4c814426775f50ded6193c7bc0b4a
Sebastian Thulin <sebastian.thulin@helsingborg.se>
2020-09-25 13:26:37 +0200

    Enable the ability to replace search page

--- library/Controller/Search.php
+++ library/Controller/Search.php
@@ -11,22 +11,23 @@ class Search extends \Municipio\Controller\Archive
         parent::init();
 
         //Translations
-        $this->data['lang']['allPages']         = __("All pages", 'municipio'); 
-        $this->data['lang']['noResult']         = __("The searchquery did not match any content.", 'municipio'); 
-        $this->data['lang']['found']            = __("Found", 'municipio'); 
-        $this->data['lang']['results']          = __("results", 'municipio'); 
-        $this->data['lang']['searchFor']        = __("Search for", 'municipio'); 
-        $this->data['lang']['viewPage']         = __("View page", 'municipio'); 
+        $this->data['lang']['allPages']             = __("All pages", 'municipio'); 
+        $this->data['lang']['noResult']             = __("The searchquery did not match any content.", 'municipio'); 
+        $this->data['lang']['found']                = __("Found", 'municipio'); 
+        $this->data['lang']['results']              = __("results", 'municipio'); 
+        $this->data['lang']['searchFor']            = __("Search for", 'municipio'); 
+        $this->data['lang']['viewPage']             = __("View page", 'municipio'); 
 
         //Search general data  
-        $this->data['resultCount']              = $this->wpQuery->found_posts;
-        $this->data['keyword']                  = isset($_GET['s']) ? $_GET['s'] : "";
+        $this->data['resultCount']                  = $this->wpQuery->found_posts;
+        $this->data['keyword']                      = isset($_GET['s']) ? $_GET['s'] : "";
 
         //Result
-        $this->data['posts']                    = $this->getSearchResult($this->wpQuery->posts);
+        $this->data['posts']                        = $this->getSearchResult($this->wpQuery->posts);
 
         //Hooks 
-        $this->data['hook']->searchNotices      = $this->hook('search_notices'); 
+        $this->data['hook']->searchNotices          = $this->hook('search_notices'); 
+        $this->data['hook']->customSearchPage   = $this->hook('custom_search_page'); 
     }
     /**
      * Default wordpress search
--- views/v3/search.blade.php
+++ views/v3/search.blade.php
@@ -1,124 +1,137 @@
 @extends('templates.master')
 @section('layout')
+
+    @if($hook->customSearchPage) 
+        <section class="o-container t-customsearchpage u-margin__top--6">
+            <div class="o-row">
+                <div class="o-col-12">
+                    {!! $hook->customSearchPage !!}
+                </div>
+            </div>
+        </section>
+    @else 
     
-    <section class="o-container t-searchform u-margin__top--6">
-
-        <div class="o-row">
-            
-            <div class="o-col-12">
-                @form([
-                    'method' => 'get',
-                    'action' => $homeUrl,
-                    'classList' => []
-                ])
-                    @field([
-                        'type' => 'text',
-                        'value' => $keyword,
-                        'attributeList' => [
-                            'type' => 'search',
-                            'name' => 's',
-                            'required' => true,
-                        ],
-                        'label' => $lang['searchFor']
-                    ])
-                    @endfield
-
-                    @button([
-                        'id' => 'search-form--submit',
-                        'text' => __('Search', 'municipio'),
-                        'color' => 'primary',
-                        'type' => 'basic',
-                        'size' => 'lg',
-                        'attributeList' => [
-                            'id' => 'search-form--submit'
-                        ]
-                    ])
-                    @endbutton 
-
-                @endform
-
-            </div>
-
-            <div class="o-col-12">
-                <div class="search-result-count">
-                    @typography(['variant' => 'meta', 'element' => 'span'])
-                        {{ $lang['found'] }} {{ $resultCount }} {{ $lang['results'] }} 
-                    @endtypography
-                </div>
-            </div>
-
-        </div>
-
-    </section>
-
-    {!! $hook->searchNotices !!}
-
-    @if (!$resultCount)
-
         <section class="o-container t-searchform u-margin__top--6">
+
             <div class="o-row">
+                
                 <div class="o-col-12">
-                    @notice([
-                        'type' => 'info',
-                        'message' => [
-                            'text' => $lang['noResult'],
-                            'size' => 'md'
-                        ]
+                    @form([
+                        'method' => 'get',
+                        'action' => $homeUrl,
+                        'classList' => []
                     ])
-                    @endnotice
-                </div>
-            </div>
-        </section>
-        
-    @else
-
-        <section class="o-container t-searchresult u-margin__top--6">
-            <div class="o-row">
-                <div class="o-col-12">
-
-                    {!! $hook->loopStart !!}
-
-                    @foreach($posts as $post) 
-
-                        @card([
-                            'heading' => $post->postTitleFiltered,
-                            'subHeading' => $siteName,
-                            'content' => $post->postExcerpt,
-                            'link' => $post->permalink,
-                            'classList' => ['u-margin__top--4']
+                        @field([
+                            'type' => 'text',
+                            'value' => $keyword,
+                            'attributeList' => [
+                                'type' => 'search',
+                                'name' => 's',
+                                'required' => true,
+                            ],
+                            'label' => $lang['searchFor']
                         ])
-                        
-                        @typography(["variant" => "meta"])
+                        @endfield
 
-                            @icon(['icon' => 'link', 'size' => 'inherit'])
-                            @endicon
+                        @button([
+                            'id' => 'search-form--submit',
+                            'text' => __('Search', 'municipio'),
+                            'color' => 'primary',
+                            'type' => 'basic',
+                            'size' => 'lg',
+                            'attributeList' => [
+                                'id' => 'search-form--submit'
+                            ]
+                        ])
+                        @endbutton 
 
-                            {{ $post->permalink }}
+                    @endform
 
-                        @endtypography
-
-                        @endcard
-
-                    @endforeach
-
-                    {!! $hook->loopEnd !!}
-                    
                 </div>
+
+                <div class="o-col-12">
+                    <div class="search-result-count">
+                        @typography(['variant' => 'meta', 'element' => 'span'])
+                            {{ $lang['found'] }} {{ $resultCount }} {{ $lang['results'] }} 
+                        @endtypography
+                    </div>
+                </div>
+
             </div>
-        </section>
-
-        <section class="t-searchpagination u-mt-0 u-margin__top--2">
-
-            @if ($paginationList)
-                @pagination([
-                    'list' => $paginationList,
-                    'current' => $currentPagePagination,
-                    'classList' => ['u-margin__top--8', 'u-margin__left--auto']
-                ])
-                @endpagination
-            @endif
 
         </section>
+
+        {!! $hook->searchNotices !!}
+
+
+        @if (!$resultCount)
+
+            <section class="o-container t-searchform u-margin__top--6">
+                <div class="o-row">
+                    <div class="o-col-12">
+                        @notice([
+                            'type' => 'info',
+                            'message' => [
+                                'text' => $lang['noResult'],
+                                'size' => 'md'
+                            ]
+                        ])
+                        @endnotice
+                    </div>
+                </div>
+            </section>
+            
+        @else
+
+            <section class="o-container t-searchresult u-margin__top--6">
+                <div class="o-row">
+                    <div class="o-col-12">
+
+                        {!! $hook->loopStart !!}
+
+                        @foreach($posts as $post) 
+
+                            @card([
+                                'heading' => $post->postTitleFiltered,
+                                'subHeading' => $siteName,
+                                'content' => $post->postExcerpt,
+                                'link' => $post->permalink,
+                                'classList' => ['u-margin__top--4']
+                            ])
+                            
+                            @typography(["variant" => "meta"])
+
+                                @icon(['icon' => 'link', 'size' => 'inherit'])
+                                @endicon
+
+                                {{ $post->permalink }}
+
+                            @endtypography
+
+                            @endcard
+
+                        @endforeach
+
+                        {!! $hook->loopEnd !!}
+                        
+                    </div>
+                </div>
+            </section>
+
+            <section class="t-searchpagination u-mt-0 u-margin__top--2">
+
+                @if ($paginationList)
+                    @pagination([
+                        'list' => $paginationList,
+                        'current' => $currentPagePagination,
+                        'classList' => ['u-margin__top--8', 'u-margin__left--auto']
+                    ])
+                    @endpagination
+                @endif
+
+            </section>
+
+        @endif
 
     @endif
 
