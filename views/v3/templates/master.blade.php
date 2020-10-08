6a299eb5d17fe0ef8f4ae45d8506c269b82302aa
Sebastian Thulin <sebastian.thulin@helsingborg.se>
2020-10-08 13:52:01 +0200

    Fix ”html” in page title

--- views/v3/templates/master.blade.php
+++ views/v3/templates/master.blade.php
@@ -5,7 +5,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-    <title>{{ $pageTitle }}</title>
+    <title>{!! $pageTitle !!}</title>
 
     <meta name="pubdate" content="{{ $pagePublished }}">
     <meta name="moddate" content="{{ $pageModified }}">
