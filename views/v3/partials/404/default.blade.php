e8bc5d6a3acc5c2f79e9289717e3585461aec8aa
Sebastian Thulin <sebastian.thulin@helsingborg.se>
2020-10-09 11:06:14 +0200

    Styles for 404

--- views/v3/partials/404/default.blade.php
+++ views/v3/partials/404/default.blade.php
@@ -1,19 +1,30 @@
-@typography(["element" => "h1", "id" => "header404", "classList" => ["c-typhography--404-heading"]])
-    {{ $heading }}
-@endtypography
+<div class="t-404">
+    @typography(["element" => "h1", "id" => "header404", "classList" => ["c-typhography--404-heading"]])
+        {{ $heading }}
+    @endtypography
 
-@typography(["element" => "span", "id" => "content404", "classList" => ["c-typhography--404-subheading"]])
-    {{ $subheading }}
-@endtypography
+    @typography(["element" => "span", "id" => "content404", "classList" => ["c-typhography--404-subheading"]])
+        {{ $subheading }}
+    @endtypography
 
-@grid(["col_gap" => 2,"row_gap" => 8, 'container' => true])
-    @foreach($actionButtons as $button) 
-        @button([
-            'text' => $button->label,
-            'href' => $button->href,
-            'color' => $loop->first ? 'primary' : 'secondary',
-            'style' => 'filled'
-        ])
-        @endbutton
-    @endforeach
-@endgrid
+    <div class="t-404__buttons">
+        @foreach($actionButtons as $button) 
+            @button([
+                'text' => $button->label,
+                'href' => $button->href,
+                'color' => $button->color,
+                'style' => $button->style,
+                'classList' => [
+                    'u-margin__right--2', 
+                    'u-margin__bottom--2', 
+                    'u-margin__right--2', 
+                    'u-display--block@xs'
+                ],
+                'size' => 'lg',
+                'icon' => $button->icon,
+                'reversePositions' => true,
+            ])
+            @endbutton
+        @endforeach
+    </div>
+</div>
