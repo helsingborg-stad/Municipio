ad3a69f6f130352e7bddac24e4ebbf440b0b34ce
Sebastian Thulin <sebastian.thulin@helsingborg.se>
2020-10-30 08:47:55 +0100

    Updated hero search form.

--- views/v3/partials/search/hero-search-form.blade.php
+++ views/v3/partials/search/hero-search-form.blade.php
@@ -1,40 +1,36 @@
 @form([
     'id'        => 'hero-search-form',
     'method'    => 'get',
-    'action'    => esc_url( home_url( '/' ) ),
+    'action'    => $homeUrl,
     'classList' => ['c-form--hidden']
 ])
-    @typography([
-        "element" => "h1",
-        'classList' => [
-            'u-color__text--primary'
-        ]
-    ])
-        {{ $lang->search }} {{ $siteName }}
-    @endtypography
-
-    @field([
-        'id' => 'search-form--field',
-        'type' => 'text',
-        'value' => get_search_query(),
-        'attributeList' => [
-            'type' => 'search',
-            'name' => 's',
-            'required' => false,
-        ],
-        'label' => $lang->search . " " . $siteName
-    ])
-    @endfield
-
-    @button([
-        'id' => 'search-form--submit',
-        'text' => $lang->search,
-        'color' => 'primary',
-        'type' => 'basic',
-        'size' => 'md',
-        'attributeList' => [
-            'id' => 'search-form--submit'
-        ]
-    ])
-    @endbutton 
+    <div class="o-grid">
+        <div class="o-grid-auto">
+            @field([
+                'id' => 'search-form--field',
+                'type' => 'text',
+                'value' => get_search_query(),
+                'attributeList' => [
+                    'type' => 'search',
+                    'name' => 's',
+                    'required' => false,
+                ],
+                'label' => $lang['search'] . " " . $siteName
+            ])
+            @endfield
+        </div>
+        <div class="o-grid-fit">
+            @button([
+                'id' => 'search-form--submit',
+                'text' => $lang['search'],
+                'color' => 'primary',
+                'type' => 'basic',
+                'size' => 'md',
+                'attributeList' => [
+                    'id' => 'search-form--submit'
+                ]
+            ])
+            @endbutton
+        </div>
+    </div>
 @endform
