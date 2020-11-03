ba9d505e51101cab84166cd05c21fae7bfef196f
Nikolas Ramstedt <nikolas.ramstedt@gmail.com>
2020-11-03 13:56:21 +0100

    Add drawer close button

--- views/v3/partials/navigation/drawer.blade.php
+++ views/v3/partials/navigation/drawer.blade.php
@@ -1,4 +1,15 @@
 <div class="c-drawer c-drawer--right js-drawer" data-js-toggle-item="js-drawer">
+    <div class="c-drawer__header">
+        <button class="hamburger hamburger--drawer hamburger--stacked@sm hamburger--reverse@md hamburger--slider is-active js-close-drawer" type="button"
+        aria-label="Menu" aria-controls="navigation">
+            <span class="hamburger-box">
+                <span class="hamburger-inner"></span>
+            </span>
+            <span class="hamburger-label">
+                Stäng
+            </span>
+        </button>
+    </div>
     <div class="c-drawer__body">
         @nav([
             'classList' => ['c-nav--drawer'],
@@ -10,4 +21,4 @@
         @endnav
     </div>
 </div>
-<div class="overlay js-close-drawer"></div>
+<div class="drawer-overlay js-close-drawer"></div>
