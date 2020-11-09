658fb699a929d6ab28d4474074906f512b624b6f
Nikolas Ramstedt <nikolas.ramstedt@gmail.com>
2020-11-09 10:55:14 +0100

    Add: Render endpoint for navigation

--- library/Api/Navigation.php
+++ library/Api/Navigation.php
@@ -6,15 +6,43 @@ class Navigation
 {
     public function __construct()
     {
-        add_action('rest_api_init', array($this, 'registerEndpoint'));
+        add_action('rest_api_init', array($this, 'registerEndpoints'));
     }
 
-    public function registerEndpoint()
+    public function registerEndpoints()
     {
         register_rest_route('municipio/v1', '/navigation/children', array(
             'methods' => 'GET',
             'callback' => array($this, 'getPostChildren'),
         ));
+
+        register_rest_route('municipio/v1', '/navigation/render', array(
+            'methods' => 'GET',
+            'callback' => array($this, 'renderPostChildren'),
+        ));
+    }
+
+    public function renderPostChildren()
+    {
+        if (isset($data->get_params()['pageId']) && is_numeric($data->get_params()['pageId'])) {
+            $parentId = $data->get_params()['pageId'];
+            $viewPath = $data->get_params()['viewPath'];
+
+
+            if (isset($parentId)) {
+                $NavigationInstance = new \Municipio\Helper\Navigation();
+                $items = $NavigationInstance->getPostChildren($parentId);
+                
+                return array(
+                    'markup' => render_blade_view($viewPath ?: 'partials.navigation.mobile', [
+                        'menuItems' => $items,
+                        'homeUrl' => esc_url(get_home_url())
+                    ])
+                );
+            }
+        }
+
+        return [];
     }
 
     public function getPostChildren($data)
