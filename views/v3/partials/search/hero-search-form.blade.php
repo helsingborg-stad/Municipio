d5696c03729258b70de17f6e2a711f1b37a38e22
Sebastian Thulin <sebastianthulin@users.noreply.github.com>
2020-11-11 15:35:22 +0100

    Feature/3.0/group (#132)
    * Implement backend group component.
    
    * Add group component
    
    * Implement HTML tidy
    
    Makes output more readable.
    
    * Add grow to input field.
    
    Co-authored-by: Sebastian Thulin <sebastian.thulin@helsingborg.se>

--- views/v3/partials/search/hero-search-form.blade.php
+++ views/v3/partials/search/hero-search-form.blade.php
@@ -4,33 +4,30 @@
     'action'    => $homeUrl,
     'classList' => ['c-form--hidden']
 ])
-    <div class="o-grid">
-        <div class="o-grid-auto">
-            @field([
-                'id' => 'search-form--field',
-                'type' => 'text',
-                'value' => get_search_query(),
-                'attributeList' => [
-                    'type' => 'search',
-                    'name' => 's',
-                    'required' => false,
-                ],
-                'label' => $lang->search . " " . $siteName
-            ])
-            @endfield
-        </div>
-        <div class="o-grid-fit">
-            @button([
-                'id' => 'search-form--submit',
-                'text' => $lang->search,
-                'color' => 'primary',
-                'type' => 'basic',
-                'size' => 'md',
-                'attributeList' => [
-                    'id' => 'search-form--submit'
-                ]
-            ])
-            @endbutton
-        </div>
-    </div>
+    @group(['direction' => 'horizontal'])
+        @field([
+            'id' => 'search-form--field',
+            'type' => 'text',
+            'value' => get_search_query(),
+            'attributeList' => [
+                'type' => 'search',
+                'name' => 's',
+                'required' => false,
+            ],
+            'label' => $lang->search . " " . $siteName,
+            'classList' => ['u-flex-grow--1']
+        ])
+        @endfield
+        @button([
+            'id' => 'search-form--submit',
+            'text' => $lang->search,
+            'color' => 'primary',
+            'type' => 'basic',
+            'size' => 'md',
+            'attributeList' => [
+                'id' => 'search-form--submit'
+            ]
+        ])
+        @endbutton
+    @endgroup
 @endform
