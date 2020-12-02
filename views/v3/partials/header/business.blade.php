9f1b3e9f751dad149cba5ac4f0e895fe918e8c5d
Sebastian Thulin <sebastian.thulin@helsingborg.se>
2020-12-02 09:55:46 +0100

    Move searchform in header

--- views/v3/partials/header/business.blade.php
+++ views/v3/partials/header/business.blade.php
@@ -43,14 +43,11 @@
                             ])
                             @endbutton
                         @endforeach
-
-                        {{-- Search form in header --}}
-                        @includeWhen($showHeaderSearch, 'partials.search.header-search-form')
-                        
                     @endgroup
                 </nav>
 
-               
+               {{-- Search form in header --}}
+               @includeWhen($showHeaderSearch, 'partials.search.header-search-form')
                 
             </div>
 
