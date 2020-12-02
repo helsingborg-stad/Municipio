a304bb71102fec3b81033225858b921b8d0b2d5c
Sebastian Thulin <sebastian.thulin@helsingborg.se>
2020-12-02 09:56:33 +0100

    Utilize new features in header search (small formtype)

--- views/v3/partials/search/header-search-form.blade.php
+++ views/v3/partials/search/header-search-form.blade.php
@@ -1,10 +1,38 @@
 
-@button([
-    'icon' => 'search',
-    'reversePositions' => true,
-    'text' => 'Search',
-    'style' => 'filled',
-    'color' => 'secondary',
-    'size' => 'sm'
+@form([
+    'id'        => 'header-search-form',
+    'method'    => 'get',
+    'action'    => $homeUrl,
+    'classList' => ['u-margin__left--2', 'u-display--flex@lg u-display--flex@xl u-display--none@xs u-display--none@sm u-display--none@md']
 ])
-@endbutton
+    @group(['direction' => 'horizontal', 'classList' => ['u-margin--auto']])
+        @field([
+            'id' => 'header-search-form--field',
+            'type' => 'text',
+            'attributeList' => [
+                'type' => 'search',
+                'name' => 's',
+                'required' => false,
+            ],
+            'label' => $lang->search,
+            'classList' => ['u-flex-grow--1'],
+            'size' => 'sm',
+            'radius' => 'sm',
+            'icon' => ['icon' => 'search']
+        ])
+        @endfield
+
+        @button([
+            'id' => 'search-form--submit',
+            'text' => $lang->search,
+            'color' => 'default',
+            'type' => 'basic',
+            'size' => 'sm',
+            'attributeList' => [
+                'id' => 'header-search-form--submit'
+            ], 
+        ])
+        @endbutton
+
+    @endgroup
+@endform
