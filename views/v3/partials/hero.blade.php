6267d2e79e9283d042626d48493e0621faaf0666
Sebastian Thulin <sebastian.thulin@helsingborg.se>
2020-12-02 09:56:04 +0100

    Toggle include of hero search, remove html

--- views/v3/partials/hero.blade.php
+++ views/v3/partials/hero.blade.php
@@ -1,13 +1,11 @@
 @section('hero')
     <div id="sidebar-slider-area--container" class="o-container o-container--fullwidth">
         @if (is_active_sidebar('slider-area') === true )
+            
             @includeIf('partials.sidebar', ['id' => 'slider-area', 'classes' => ['o-grid']])
 
-            @if (is_front_page() && is_array(get_field('search_display', 'option')) &&
-                in_array('hero', get_field('search_display', 'option')))
-                {{-- {{ get_search_form() }} --}}
-                @includeIf('partials.search.hero-search-form')
-            @endif
+            @includeWhen($showHeroSearch, 'partials.search.hero-search-form')
+
         @endif
     </div>
 @show
