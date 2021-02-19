902e798ac6a4261369a9a0b8cd162174996f71dd
Sebastian Thulin <sebastian.thulin@helsingborg.se>
2021-02-19 14:32:21 +0100

    Check if there are any tabMenuItems before rendering

--- views/v3/partials/header/business.blade.php
+++ views/v3/partials/header/business.blade.php
@@ -33,36 +33,38 @@
                 ])
                 @endbutton
 
-                <nav role="navigation" aria-label="{{$lang->relatedLinks}}"
-                     class="u-display--flex@lg u-display--flex@lx u-display--none@xs
-                            u-display--none@sm u-display--none@md u-print-display--none">
-                    @group([
-                        'classList' => [
-                            'u-justify-content--center@xs', 
-                            'u-justify-content--center@sm', 
-                            'u-justify-content--end', 
-                            'u-box-shadow--1',
-                            'u-rounded',
-                            'u-margin--auto'
-                        ]
-                    ])
-                        @foreach($tabMenuItems as $item)
-                            @button([
-                                'href'  => $item['href'], 
-                                'text'  => $item['label'],
-                                'size'  => 'sm',
-                                'style' => 'basic',
-                                'attributeList' => [
-                                    'role' => 'menuitem'
-                                ]
-                            ])
-                            @endbutton
-                        @endforeach
-                    @endgroup
-                </nav>
+                @if($tabMenuItems)
+                    <nav role="navigation" aria-label="{{$lang->relatedLinks}}"
+                        class="u-display--flex@lg u-display--flex@lx u-display--none@xs
+                                u-display--none@sm u-display--none@md u-print-display--none">
+                        @group([
+                            'classList' => [
+                                'u-justify-content--center@xs', 
+                                'u-justify-content--center@sm', 
+                                'u-justify-content--end', 
+                                'u-box-shadow--1',
+                                'u-rounded',
+                                'u-margin--auto'
+                            ]
+                        ])
+                            @foreach($tabMenuItems as $item)
+                                @button([
+                                    'href'  => $item['href'], 
+                                    'text'  => $item['label'],
+                                    'size'  => 'sm',
+                                    'style' => 'basic',
+                                    'attributeList' => [
+                                        'role' => 'menuitem'
+                                    ]
+                                ])
+                                @endbutton
+                            @endforeach
+                        @endgroup
+                    </nav>
+                @endif
 
-               {{-- Search form in header --}}
-               @includeWhen($showHeaderSearch, 'partials.search.header-search-form')
+                {{-- Search form in header --}}
+                @includeWhen($showHeaderSearch, 'partials.search.header-search-form')
                 
             </div>
 
