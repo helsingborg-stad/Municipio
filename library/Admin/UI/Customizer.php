962340958f7a06e39ae94a72f2fcccf8783ffca5
Sebastian Thulin <sebastian.thulin@helsingborg.se>
2021-02-26 15:43:30 +0100

    Dockblock

--- library/Admin/UI/Customizer.php
+++ library/Admin/UI/Customizer.php
@@ -13,6 +13,11 @@ class Customizer
     }); 
   }
 
+  /**
+   * Create a error message with installation instructions
+   *
+   * @return void
+   */
   public function thowNotice() {
     wp_die(
       __("<h1>Plugin install required </h1> <p>To use the customizer with municipio its required to use ACFCustomizer plugin. Please install this plugin by cloning <a href='https://www.awesomeacf.com/extension/customizer/'>ACF Customizer</a>.</p>"),
