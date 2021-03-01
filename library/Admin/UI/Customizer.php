f92cda6c51cd29fc0f1e28a8f99593bf52df5c67
Johan Silvergrund <johan.silvergrund@hiq.se>
2021-03-01 15:32:02 +0100

    method name typo

--- library/Admin/UI/Customizer.php
+++ library/Admin/UI/Customizer.php
@@ -8,7 +8,7 @@ class Customizer
   {
     add_action('init', function() {
       if(is_customize_preview() && !class_exists('ACFCustomizer\Core\Core')) {
-        $this->thowNotice();
+        $this->throwNotice();
       }
     }); 
   }
@@ -18,7 +18,7 @@ class Customizer
    *
    * @return void
    */
-  public function thowNotice() {
+  public function throwNotice() {
     wp_die(
       __("<h1>Plugin install required </h1> <p>To use the customizer with municipio its required to use ACFCustomizer plugin. Please install this plugin by cloning <a href='https://www.awesomeacf.com/extension/customizer/'>ACF Customizer</a>.</p>"),
       __("Plugin install required")
