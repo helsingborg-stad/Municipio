b6cf3cfeaf6359eb9be9aada9760d6c063558147
Alexander Boman Skoug <alexanderbomanskoug@gmail.com>
2021-03-08 10:48:30 +0100

    Improved format

--- views/v3/partials/search/header-search-form.blade.php
+++ views/v3/partials/search/header-search-form.blade.php
@@ -7,31 +7,36 @@
 ])
     @group(['direction' => 'horizontal', 'classList' => ['u-margin--auto']])
         @field([
-            'id' => 'header-search-form--field',
-            'type' => 'text',
-            'attributeList' => [
-                'type' => 'search',
-                'name' => 's',
-                'required' => false,
+            'id'            => 'header-search-form--field',
+            'type'          => 'text',
+            'placeholder'   => $lang->search,
+            'size'          => 'sm',
+            'radius'        => 'sm',
+            'icon'          => ['icon' => 'search'],
+            'classList'     => [
+                'u-flex-grow--1',
+                'u-box-shadow--1',
+                'u-rounded__left--8',
+                'u-overflow--hidden'
             ],
-            'placeholder' => $lang->search,
-            'classList' => ['u-flex-grow--1', 'u-box-shadow--1', 'u-rounded__left--8', 'u-overflow--hidden'],
-            'size' => 'sm',
-            'radius' => 'sm',
-            'icon' => ['icon' => 'search']
+            'attributeList' => [
+                'type'          => 'search',
+                'name'          => 's',
+                'required'      => false,
+            ]
         ])
         @endfield
 
         @button([
-            'id' => 'search-form--submit',
-            'text' => $lang->search,
-            'color' => 'default',
-            'type' => 'submit',
-            'size' => 'sm',
+            'id'            => 'search-form--submit',
+            'text'          => $lang->search,
+            'color'         => 'default',
+            'type'          => 'submit',
+            'size'          => 'sm',
             'attributeList' => [
-                'id' => 'header-search-form--submit'
+                'id'            => 'header-search-form--submit'
             ], 
-            'classList' => ['u-rounded__right--8']
+            'classList'     => ['u-rounded__right--8']
         ])
         @endbutton
 
