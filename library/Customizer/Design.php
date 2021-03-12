6a6efb797d42661758726d055a6807c9085065e7
Johan Silvergrund <johan.silvergrund@hiq.se>
2021-03-12 14:06:34 +0100

    POC - Add modifier filters with Customizer

--- library/Customizer/Design.php
+++ library/Customizer/Design.php
@@ -17,8 +17,16 @@ class Design
      * @var array|string[]
      */
     private $configurationFiles = [
-        'Colors' => MUNICIPIO_PATH . 'library/AcfFields/json/customizer-color.json',
-        'Radius' => MUNICIPIO_PATH . 'library/AcfFields/json/customizer-radius.json'
+        'vars' => [
+            'Colors' => MUNICIPIO_PATH . 'library/AcfFields/json/customizer-color.json',
+            'Radius' => MUNICIPIO_PATH . 'library/AcfFields/json/customizer-radius.json'
+        ],
+        'modifiers' => [
+            'Header' => MUNICIPIO_PATH . 'library/AcfFields/json/customizer-header.json',
+            'Footer' => MUNICIPIO_PATH . 'library/AcfFields/json/customizer-footer.json',
+            //'Card' => MUNICIPIO_PATH . 'library/AcfFields/json/customizer-card.json'
+        ]
+
     ];
 
     /**
@@ -30,6 +38,7 @@ class Design
         add_action('init', array($this, 'initPanels'));
         add_action('wp_head', array($this, 'getAcfCustomizerFields'), 5);
         add_action('wp_head', array($this, 'renderCssVariables'), 10);
+        add_action('wp_head', array($this, 'addFilters'), 10);
     }
 
     /**
@@ -38,10 +47,13 @@ class Design
      */
     public function initPanels()
     {
-        new \Municipio\Helper\Customizer(
-            __('Design', 'municipio'),
-            array_flip($this->configurationFiles)
-        );
+        foreach($this->configurationFiles as $index => $conf){
+            new \Municipio\Helper\Customizer(
+                __('Design', 'municipio'),
+                array_flip($this->configurationFiles[$index])
+            );
+        }
+
     }
 
     /**
@@ -51,41 +63,67 @@ class Design
     public function getAcfCustomizerFields()
     {
         if (is_array($this->configurationFiles) && !empty($this->configurationFiles)) {
+            foreach($this->configurationFiles as $typeIndex => $conf) {
+                foreach ($this->configurationFiles[$typeIndex] as $key => $config) {
 
-            foreach ($this->configurationFiles as $key => $config) {
+                    $data = file_get_contents($config);
+                    $themeMods = get_theme_mod(sanitize_title($key));
 
-                $data = file_get_contents($config);
-                $themeMods = get_theme_mod(sanitize_title($key));
+                    if (file_exists($config) && $data = json_decode($data)) {
 
-                if (file_exists($config) && $data = json_decode($data)) {
-
-                    if (count($data) != 1) {
-                        return new \WP_Error("Configuration file should not contain more than one group " . $config);
-                    }
-
-                    $data = array_pop($data);
-
-                    if (isset($data->fields) && !empty($data->fields)) {
-
-                        foreach ($data->fields as $index => $field) {
-                            $this->dataFieldStack[sanitize_title($data->title)][$index] = [
-                                $field->key => [
-                                    'group-id' => sanitize_title($data->title),
-                                    'name' => str_replace(['municipio_', '_'], ['', '-'], $field->name),
-                                    'default' => $field->default_value,
-                                    'value' => $themeMods[$field->key],
-                                    'prepend' => $field->prepend ?? null,
-                                    'append' => $field->append ?? null
-                                ]
-                            ];
+                        if (count($data) != 1) {
+                            return new \WP_Error("Configuration file should not contain more than one group " . $config);
                         }
-                    }
 
-                } else {
-                    return new \WP_Error("Could not read configuration file " . $config);
+                        $data = array_pop($data);
+
+                        if (isset($data->fields) && !empty($data->fields)) {
+
+                            foreach ($data->fields as $index => $field) {
+                                $this->dataFieldStack[$typeIndex][sanitize_title($data->title)][$index] = [
+                                    $field->key => [
+                                        'group-id' => sanitize_title($data->title),
+                                        'name' => str_replace(['municipio_', '_'], ['', '-'], $field->name),
+                                        'default' => $field->default_value,
+                                        'value' => $themeMods[$field->key],
+                                        'prepend' => $field->prepend ?? null,
+                                        'append' => $field->append ?? null
+                                    ]
+                                ];
+                            }
+                        }
+
+                    } else {
+                        return new \WP_Error("Could not read configuration file " . $config);
+                    }
                 }
             }
         }
+    }
+
+    /**
+     * Add Filters with CSS modifiers
+     * @return void
+     */
+    public function addFilters() {
+        $modifiers = [];
+        foreach($this->dataFieldStack['modifiers'] as $key => $stackItems) {
+            foreach ($stackItems as $index => $prop) {
+                $itemKey = key($stackItems[$index]);
+                $modifiers = (!empty($prop[$itemKey]['value']) ? $prop[$itemKey]['value'] : $prop[$itemKey]['default']);
+
+            }
+            add_filter('ComponentLibrary/Component/'.ucfirst($key) .'/Modifier', 'setModifiers', $modifiers);
+        }
+    }
+
+    /**
+     * Add filter callback
+     * @param $modifiers
+     * @return mixed
+     */
+    public function setModifiers($modifiers){
+        return implode(" ",$modifiers);
     }
 
     /**
@@ -95,7 +133,7 @@ class Design
     public function renderCssVariables()
     {
         $inlineStyle = null;
-        foreach ($this->dataFieldStack as $key => $stackItems) {
+        foreach ($this->dataFieldStack['vars'] as $key => $stackItems) {
 
             $inlineStyle .= PHP_EOL . '  /* Variables: ' . ucfirst($key) . ' */' . PHP_EOL;
 
