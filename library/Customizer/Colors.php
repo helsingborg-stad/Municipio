522276ecd0db84386d77bd8912c58323dc72fe83
Sebastian Thulin <sebastian.thulin@helsingborg.se>
2021-05-20 16:34:51 +0200

    Fixes warning array Color alpha

--- library/Customizer/Colors.php
+++ library/Customizer/Colors.php
@@ -15,22 +15,28 @@ class Colors
     public function prepareColor ($colorItem) {
     
         $colorItem['alpha'] = "1"; //Set default alpha value
-        if(is_array($colorItem['value'])) {
+
+        if(is_array($colorItem['value'])||(empty($colorItem['value']) && is_array($colorItem['default']))) {
 
             //Extra default values for group
             $defaultColor = $colorItem['default']['color'] ?? "";
             $defaultAlpha = $colorItem['default']['alpha'] . "%" ?? "1";
 
             //Collect set values for group
-            $setColor = array_values($colorItem['value'])[0];
-            $setAlpha = array_values($colorItem['value'])[1];
+            if (is_array($colorItem['value'])) {
+                $setColor = array_values($colorItem['value'])[0];
+                $setAlpha = array_values($colorItem['value'])[1];
+            } else {
+                $setColor = $colorItem['value'];
+                $setAlpha = $colorItem['alpha'];
+            }
 
             //Define set value else default values
             $colorItem['value'] = !empty($setColor) ? $setColor : $defaultColor;
             $colorItem['alpha'] = !empty($setAlpha) || $setAlpha == "0" ? $setAlpha .'%' : $defaultAlpha; //empty() returns true on "0"                        
 
         } 
-                                              
+
         return $this->convertHexToRgb($colorItem['value'], $colorItem['alpha'], $colorItem['default']);
     }
 
@@ -41,6 +47,6 @@ class Colors
     private function convertHexToRgb($value, $alpha, $default) {  
         $value = !empty($value) ? $value : $default;  
         $value = sscanf($value, "#%02x%02x%02x");
-        return "rgba({$value[0]},{$value[1]},{$value[2]}, $alpha)";
+        return "rgba({$value[0]}, {$value[1]}, {$value[2]}, $alpha)";
     }
 }
