f0a7512c6188f7442d123bf5aa968f2c340b2a60
Muckbuck <rosenborg.eric@gmail.com>
2021-06-04 09:07:32 +0200

    Feature/3.0/gutenberg theme option (#190)
    * Enque styleguide in admin
    
    * Add checkbox in theme options for activating gutenberg block editor
    
    Co-authored-by: Eric Rosenborg <eric.rosenborg@hiq.se>

--- functions.php
+++ functions.php
@@ -4,6 +4,13 @@ define('MUNICIPIO_PATH', get_template_directory() . '/');
 
 require_once MUNICIPIO_PATH . '/library/Bootstrap.php';
 
+add_filter("use_block_editor_for_post_type", "activate_gutenberg_editor");
+
+function activate_gutenberg_editor() {
+    return get_field('activate_gutenberg_editor', 'option');
+}
+
+
 add_action('after_setup_theme', function () {
     load_theme_textdomain('municipio', get_template_directory() . '/languages');
 });
