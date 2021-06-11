43bb7da595ff39594766ad9fd942f3c748e3b068
Sebastian Thulin <sebastian.thulin@helsingborg.se>
2021-06-11 11:04:38 +0200

    Add field for menu icon

---
+++ library/AcfFields/php/options-menu-icon.php
@@ -0,0 +1,46 @@
+<?php 
+
+if (function_exists('acf_add_local_field_group')) {
+    acf_add_local_field_group(array(
+    'key' => 'group_60c325749aeab',
+    'title' => __('Menu icon', 'municipio'),
+    'fields' => array(
+        0 => array(
+            'key' => 'field_60c3257965fd9',
+            'label' => '',
+            'name' => 'menu_item_icon',
+            'type' => 'text',
+            'instructions' => __('Icon', 'municipio'),
+            'required' => 0,
+            'conditional_logic' => 0,
+            'wrapper' => array(
+                'width' => '',
+                'class' => '',
+                'id' => '',
+            ),
+            'default_value' => '',
+            'placeholder' => '',
+            'prepend' => '',
+            'append' => '',
+            'maxlength' => '',
+        ),
+    ),
+    'location' => array(
+        0 => array(
+            0 => array(
+                'param' => 'nav_menu_item',
+                'operator' => '==',
+                'value' => 'all',
+            ),
+        ),
+    ),
+    'menu_order' => 0,
+    'position' => 'normal',
+    'style' => 'default',
+    'label_placement' => 'top',
+    'instruction_placement' => 'label',
+    'hide_on_screen' => '',
+    'active' => true,
+    'description' => '',
+));
+}
