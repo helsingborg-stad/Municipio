a71c5d8a1ce4608ac2efaf44bd08507bc6ad6bbf
Eric Rosenborg <eric.rosenborg@hiq.se>
2021-08-03 13:32:28 +0200

    Remove utility classes from fixed header

--- views/v3/partials/navigation/fixed.blade.php
+++ views/v3/partials/navigation/fixed.blade.php
@@ -4,10 +4,9 @@
         'classList' => [
             'site-header', isset($classList) ? is_array($classList) ? implode(' ', $classList) : $classList : '',
         ],
-        'context' => 'siteHeader'
+        
     ])
-    <div class="c-header__menu c-header__menu--secondary u-padding--05 u-display--none@xs
-                    u-display--none@sm u-display--none@md u-print-display--none">
+    <div class="c-header__menu c-header__menu--secondary u-padding--05 u-print-display--none">
         <div class="o-container">
             <nav role="navigation" aria-label="{{ $lang->primaryNavigation }}">
                 @nav([
