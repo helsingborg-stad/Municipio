933ad7f658c48acafbc654682f9520741b2f3cce
Sebastian Thulin <sebastian.thulin@helsingborg.se>
2021-08-18 10:29:36 +0200

    Only show emblem on frontpage

--- views/v3/partials/emblem.blade.php
+++ views/v3/partials/emblem.blade.php
@@ -1,7 +1,9 @@
-@logotype([
-    'src'=> $emblem,
-    'alt' => $lang->emblem,
-    'classList' => ['c-logotype--emblem', 'u-display--none@xs', 'u-display--none@sm'],
-    'hasRipple' => false
-])
-@endlogotype
+@if($isFrontPage)
+    @logotype([
+        'src'=> $emblem,
+        'alt' => $lang->emblem,
+        'classList' => ['c-logotype--emblem', 'u-display--none@xs', 'u-display--none@sm'],
+        'hasRipple' => false
+    ])
+    @endlogotype
+@endif
