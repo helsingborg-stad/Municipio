cf96c658e27b94899cd211c7bdc36b923413a299
Alexander Boman Skoug <alexanderbomanskoug@gmail.com>
2021-08-26 14:46:06 +0200

    - add new options to component usage

--- views/v3/partials/navigation/fixed.blade.php
+++ views/v3/partials/navigation/fixed.blade.php
@@ -1,9 +1,11 @@
 @if (!empty($quicklinksMenuItems) && $isFrontPage)
     @header([
-        'id' => 'sticky-header',
+        'id'                => 'sticky-header',
+        'textColor'         => $quicklinksOptions->textColor,
+        'backgroundColor'   => $quicklinksOptions->backgroundColor,
         'classList' => [
-            'site-header', isset($classList) ? is_array($classList) ? implode(' ', $classList) : $classList : '',
-        ]
+            'site-header'
+        ],
     ])
         <div class="c-header__menu c-header__menu--secondary u-padding--05 u-print-display--none">
             <div class="o-container">
