0b72b918cb8f40fee3caaafca1eeabea338079e7
Sebastian Thulin <sebastian.thulin@helsingborg.se>
2021-08-27 08:58:05 +0200

    Remove more templates, and change name of default.

--- library/Theme/CustomTemplates.php
+++ library/Theme/CustomTemplates.php
@@ -6,6 +6,25 @@ class CustomTemplates
 {
     public function __construct()
     {
+        add_action('default_page_template_title', array($this, 'renameDefaultTemplate'));
+        add_action('init', array($this, 'registerTemplates')); 
+    }
+
+    /**
+     * Change name on the default template
+     *
+     * @return string
+     */
+    public function renameDefaultTemplate () {
+        return __('Page (default template)', 'municipio');
+    }
+
+    /**
+     * Register templates
+     *
+     * @return void
+     */
+    public function registerTemplates () {
         \Municipio\Helper\Template::add(
             __('Full width', 'municipio'),
             \Municipio\Helper\Template::locateTemplate('full-width.blade.php'),
--- views/v3/one-page.blade.php
+++ views/v3/one-page.blade.php
@@ -1,1 +1,18 @@
 @extends('templates.master')
+@section('layout')
+    <div class="o-container">
+
+      {!! $hook->innerLoopStart !!}
+
+      @if($hasBlocks && $post)
+          {!! $post->postContentFiltered !!}
+      @endif
+
+      {!! $hook->innerLoopEnd !!}
+
+      @includeIf('partials.sidebar', ['id' => 'content-area-top', 'classes' => ['o-grid']])
+      @includeIf('partials.sidebar', ['id' => 'content-area', 'classes' => ['o-grid']])
+      @includeIf('partials.sidebar', ['id' => 'content-area-bottom', 'classes' => ['o-grid']])
+
+    </div>
+@stop
