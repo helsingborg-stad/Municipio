99bbc4fc116d131a2f09fa0a9e7bd8cbeb9ab741
Sebastian Thulin <sebastian.thulin@helsingborg.se>
2021-09-20 15:51:23 +0200

    Add functionality to set quicklinks to be displayed everywhere or only on frontpage.

--- views/v3/partials/navigation/fixed.blade.php
+++ views/v3/partials/navigation/fixed.blade.php
@@ -1,23 +1,25 @@
-@if (!empty($quicklinksMenuItems) && $isFrontPage)
-    @header([
-        'id'                => 'quicklinks-header',
-        'classList' => [
-            'site-header'
-        ],
-        'context' => ['site.quicklinks']
-    ])
-        <div class="c-header__menu c-header__menu--secondary u-padding--05 u-print-display--none">
-            <div class="o-container">
-                <nav role="navigation" aria-label="{{ $lang->primaryNavigation }}">
-                    @nav([
-                        'items' => $quicklinksMenuItems,
-                        'direction' => 'horizontal',
-                        'classList' => ['u-flex-wrap--no-wrap', 'u-justify-content--space-between'],
-                        'context' => ['site.quicklinks.nav']
-                    ])
-                    @endnav
-                </nav>
+@if (!empty($quicklinksMenuItems))
+    @if(($customize->quicklinks->quicklinksDisplayLocation == 'frontpage' && $isFrontPage) || ($customize->quicklinks->quicklinksDisplayLocation == 'everywhere'))
+        @header([
+            'id'                => 'quicklinks-header',
+            'classList' => [
+                'site-header'
+            ],
+            'context' => ['site.quicklinks']
+        ])
+            <div class="c-header__menu c-header__menu--secondary u-padding--05 u-print-display--none">
+                <div class="o-container">
+                    <nav role="navigation" aria-label="{{ $lang->primaryNavigation }}">
+                        @nav([
+                            'items' => $quicklinksMenuItems,
+                            'direction' => 'horizontal',
+                            'classList' => ['u-flex-wrap--no-wrap', 'u-justify-content--space-between'],
+                            'context' => ['site.quicklinks.nav']
+                        ])
+                        @endnav
+                    </nav>
+                </div>
             </div>
-        </div>
-    @endheader
+        @endheader
+    @endif
 @endif
