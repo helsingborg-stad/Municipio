8c83d0222fc3de9a096a0788d878c2dacaa88b7d
Alexander Boman Skoug <39676080+alexanderbomanskoug2@users.noreply.github.com>
2021-09-30 13:20:37 +0200

    - remove gutter from hero (#237)

--- views/v3/partials/hero.blade.php
+++ views/v3/partials/hero.blade.php
@@ -2,7 +2,7 @@
     <div id="sidebar-slider-area--container" class="o-container o-container--fullwidth u-print-display--none">
         @if (is_active_sidebar('slider-area') === true )
             
-            @includeIf('partials.sidebar', ['id' => 'slider-area', 'classes' => ['o-grid', 'o-grid--no-margin']])
+            @includeIf('partials.sidebar', ['id' => 'slider-area', 'classes' => ['o-grid', 'o-grid--no-margin', 'o-grid--no-gutter']])
 
             {{-- Search in hero --}}
             @includeWhen($showHeroSearch, 'partials.search.hero-search-form')
