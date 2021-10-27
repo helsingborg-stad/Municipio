db44a43dea6407f7cffd54c51a3b50b95628245f
nRamstedt <nikolas.ramstedt@gmail.com>
2021-10-27 09:38:52 +0200

    add kirki container

---
+++ library/Customizer.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Municipio;
+
+class Customizer
+{
+    public const KIRKI_CONFIG = "municipio_config";
+
+    public function __construct()
+    {
+        add_action('init', function () {
+            if (class_exists("Kirki")) {
+                return $this->init();
+            }
+
+            wp_die(
+                __("Kirki Customizer framework is required"),
+                __("Plugin install required"),
+                [
+                    'link_url' => "https://github.com/kirki-framework/kirki.git",
+                    'link_text' => __("Install plugin", 'municipio')
+                ]
+            );
+        });
+    }
+
+    public function init()
+    {
+        \Kirki::add_config(self::KIRKI_CONFIG, array(
+            'capability'    => 'edit_theme_options',
+            'option_type'   => 'theme_mod',
+        ));
+
+        new \Municipio\Customizer\Panels\Design();
+    }
+}
