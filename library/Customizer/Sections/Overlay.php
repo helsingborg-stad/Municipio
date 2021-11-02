41809dd7d4b4236c0998d6f1622e04b44eb76f4e
Sebastian Thulin <sebastian.thulin@helsingborg.se>
2021-11-02 10:16:06 +0100

    Set a default color, change description

--- library/Customizer/Sections/Overlay.php
+++ library/Customizer/Sections/Overlay.php
@@ -11,17 +11,17 @@ class Overlay
         \Kirki::add_section(self::SECTION_ID, array(
             'title'       => esc_html__('Overlay', 'municipio'),
             'description' => esc_html__('Set a overlay, the color will be overlayed on images to enhance readability.', 'municipio'),
-            'panel'          => $panelID,
-            'priority'       => 160,
+            'panel'       => $panelID,
+            'priority'    => 160,
         ));
  
         \Kirki::add_field(\Municipio\Customizer::KIRKI_CONFIG, [
             'type'        => 'color',
-            'settings'    => 'municipio_color_general_overlay',
-            'label'       => __('Color Control (with alpha channel)', 'kirki' ),
-            'description' => esc_html__( 'This is a color control - with alpha channel.', 'kirki' ),
+            'settings'    => 'color_general_overlay',
+            'label'       => __('General overlay color','kirki'),
+            'description' => esc_html__( 'Set a general overlay color.', 'kirki' ),
             'section'     => self::SECTION_ID,
-            'default'     => '#0088CC',
+            'default'     => 'rgba(0, 0, 0, .6);',
             'choices'     => [
                 'alpha' => true,
             ],
