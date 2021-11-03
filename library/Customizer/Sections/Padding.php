b735b4c896e06c3c50c101ea383f9315f030c040
Sebastian Thulin <sebastian.thulin@helsingborg.se>
2021-11-03 13:32:47 +0100

    Move main content padding to kirki

--- library/Customizer/Sections/Padding.php
+++ library/Customizer/Sections/Padding.php
@@ -14,5 +14,22 @@ class Padding
             'panel'          => $panelID,
             'priority'       => 160,
         ));
+
+        \Kirki::add_field(\Municipio\Customizer::KIRKI_CONFIG, [
+          'type'        => 'slider',
+          'settings'    => 'main_content_padding',
+          'label'       => esc_html__('Amount of padding around columns.', 'municipio'),
+          'description' => esc_html__('Padding will be applied in 8px increments.'),
+          'section'     => self::SECTION_ID,
+          'default'     => 0,
+          'choices'     => [
+              'min'  => 0,
+              'max'  => 12,
+              'step' => 2,
+          ],
+          'output' => [
+              'type' => 'controller'
+          ],
+      ]);
     }
 }
