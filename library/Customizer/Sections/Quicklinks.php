29e2ba331ba3e82d52bd59ebdedeb9b3ce1a3e8c
Sebastian Thulin <sebastian.thulin@helsingborg.se>
2021-11-03 15:10:27 +0100

    Implemented dropdowns for quicklinks

--- library/Customizer/Sections/Quicklinks.php
+++ library/Customizer/Sections/Quicklinks.php
@@ -14,5 +14,94 @@ class Quicklinks
             'panel'          => $panelID,
             'priority'       => 160,
         ));
+
+        \Kirki::add_field(\Municipio\Customizer::KIRKI_CONFIG, [
+          'type'        => 'select',
+          'settings'    => 'quicklinks_sticky',
+          'label'       => esc_html__('Sticky', 'municipio'),
+          'description' => esc_html__('Adjust how the quicklinks menu should behave when the user scrolls trough the page. This option should not be used in combination with a sticky header.', 'municipio'),
+          'section'     => self::SECTION_ID,
+          'default'     => '',
+          'priority'    => 10,
+          'choices'     => [
+              '' => esc_html__('Default', 'municipio'),
+              'sticky' => esc_html__('Stick to top', 'municipio'),
+          ],
+          'output' => [
+              'type' => 'modifier',
+              'context' => ['site.quicklinks'],
+          ],
+        ]);
+
+        \Kirki::add_field(\Municipio\Customizer::KIRKI_CONFIG, [
+          'type'        => 'select',
+          'settings'    => 'quicklinks_background',
+          'label'       => esc_html__('Background color', 'municipio'),
+          'description' => esc_html__('Choose a background color for the quicklinks section of the page.', 'municipio'),
+          'section'     => self::SECTION_ID,
+          'default'     => '',
+          'priority'    => 10,
+          'choices'     => [
+              '' => esc_html__('Default', 'municipio'),
+              'primary' => esc_html__('Primary', 'municipio'),
+              'secondary' => esc_html__('Secondary', 'municipio'),
+              'tertiary' => esc_html__('Tertiary', 'municipio'),
+          ],
+          'output' => [
+              'type' => 'modifier',
+              'context' => ['site.quicklinks'],
+          ],
+        ]);
+
+        \Kirki::add_field(\Municipio\Customizer::KIRKI_CONFIG, [
+          'type'        => 'select',
+          'settings'    => 'quicklinks_color',
+          'label'       => esc_html__('Text color', 'municipio'),
+          'description' => esc_html__('Select a font/text color to use.', 'municipio'),
+          'section'     => self::SECTION_ID,
+          'default'     => '',
+          'priority'    => 10,
+          'choices'     => [
+              '' => esc_html__('Default', 'municipio'),
+              'text-white' => esc_html__('White', 'municipio'),
+              'text-black' => esc_html__('Black', 'municipio'),
+              'text-primary' => esc_html__('Primary', 'municipio'),
+              'text-secondary' => esc_html__('Secondary', 'municipio'),
+              'text-tertiary' => esc_html__('Tertiary', 'municipio'),
+          ],
+          'output' => [
+              'type' => 'modifier',
+              'context' => ['site.quicklinks'],
+          ],
+        ]);
+
+        \Kirki::add_field(\Municipio\Customizer::KIRKI_CONFIG, [
+          'type'        => 'select',
+          'settings'    => 'quicklinks_location',
+          'label'       => esc_html__('Location', 'municipio'),
+          'description' => esc_html__('Quicklinks location.', 'municipio'),
+          'section'     => self::SECTION_ID,
+          'default'     => 'frontpage',
+          'priority'    => 10,
+          'choices'     => [
+              'frontpage' => esc_html__('Front page', 'municipio'),
+              'everywhere' => esc_html__('All pages', 'municipio'),
+          ],
+          'output' => [
+              'type' => 'controller',
+          ],
+        ]);
+
+        /*
+
+        $this->migrateThemeMod('quicklinks', 'quicklinks_background_type', 'field_61570dd479d9b');
+        $this->migrateThemeMod('quicklinks', 'quicklinks_custom_background', 'field_61570e6979d9c');
+        $this->migrateThemeMod('quicklinks', 'quicklinks_background', 'field_6123844e0f0bb');
+        $this->migrateThemeMod('quicklinks', 'quicklinks_color', 'field_6127571bcc76e');
+        $this->migrateThemeMod('quicklinks', 'quicklinks_sticky', 'field_61488b616937c');
+        $this->migrateThemeMod('quicklinks', 'quicklinks_location', 'field_61488c4f6b4fd');
+        
+        */
+
     }
 }
