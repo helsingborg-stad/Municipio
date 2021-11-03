e62a903b6837dedae0429d73f2190dd16077c1ec
Sebastian Thulin <sebastian.thulin@helsingborg.se>
2021-11-03 11:46:40 +0100

    Update misconfigured hero classname

--- library/Customizer/Sections/Hero.php
+++ library/Customizer/Sections/Hero.php
@@ -2,15 +2,15 @@
 
 namespace Municipio\Customizer\Sections;
 
-class Header
+class Hero
 {
     public const SECTION_ID = "municipio_customizer_section_hero";
 
     public function __construct($panelID)
     {
         \Kirki::add_section(self::SECTION_ID, array(
-            'title'       => esc_html__('Header', 'municipio'),
-            'description' => esc_html__('Header settings.', 'municipio'),
+            'title'       => esc_html__('Hero', 'municipio'),
+            'description' => esc_html__('Hero settings.', 'municipio'),
             'panel'          => $panelID,
             'priority'       => 160,
         ));
