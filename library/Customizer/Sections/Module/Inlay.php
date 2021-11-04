53e455904f57b82808578883fe6f172b0a6f4ae4
Sebastian Thulin <sebastian.thulin@helsingborg.se>
2021-11-04 15:57:16 +0100

    Add inlaylist modifier

--- library/Customizer/Sections/Module/Inlay.php
+++ library/Customizer/Sections/Module/Inlay.php
@@ -13,9 +13,28 @@ class Inlay
             'panel'          => $panelID,
             'priority'       => 160,
             'active_callback' => function() {
-              return post_type_exists('mod-inlay');
+              return post_type_exists('mod-inlaylist');
             }
         ));
+
+        \Kirki::add_field(\Municipio\Customizer::KIRKI_CONFIG, [
+            'type'        => 'select',
+            'settings'    => 'mod_inlay_list_modifier',
+            'label'       => esc_html__('List', 'municipio'),
+            'section'     => self::SECTION_ID,
+            'default'     => 'none',
+            'priority'    => 10,
+            'choices'     => [
+              'none' => esc_html__('None', 'municipio'),
+              'panel' => esc_html__('Panel', 'municipio'),
+              'accented' => esc_html__('Accented', 'municipio'),
+              'highlight' => esc_html__('Highlight', 'municipio'),
+            ],
+            'output' => [
+                'type' => 'modifier',
+                'context' => ['module.inlay.list'],
+            ],
+        ]);
 
     }
 }
