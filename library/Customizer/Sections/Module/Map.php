21232c42531e65afd5328a74e9acd47ed733ee11
Sebastian Thulin <sebastian.thulin@helsingborg.se>
2021-11-04 15:59:05 +0100

    Add map modifier

--- library/Customizer/Sections/Module/Map.php
+++ library/Customizer/Sections/Module/Map.php
@@ -17,5 +17,24 @@ class Map
             }
         ));
 
+        \Kirki::add_field(\Municipio\Customizer::KIRKI_CONFIG, [
+            'type'        => 'select',
+            'settings'    => 'mod_map_modifier',
+            'label'       => esc_html__('List', 'municipio'),
+            'section'     => self::SECTION_ID,
+            'default'     => 'none',
+            'priority'    => 10,
+            'choices'     => [
+              'none' => esc_html__('None', 'municipio'),
+              'panel' => esc_html__('Panel', 'municipio'),
+              'accented' => esc_html__('Accented', 'municipio'),
+              'highlight' => esc_html__('Highlight', 'municipio'),
+            ],
+            'output' => [
+                'type' => 'modifier',
+                'context' => ['module.map'],
+            ],
+        ]);
+
     }
 }
