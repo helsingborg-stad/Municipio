6c6bd406c0e4410febf0a5de29128bc63ad7556c
Sebastian Thulin <sebastian.thulin@helsingborg.se>
2021-11-04 16:33:47 +0100

    Add scriopt modifier

--- library/Customizer/Sections/Module/Script.php
+++ library/Customizer/Sections/Module/Script.php
@@ -17,5 +17,24 @@ class Script
             }
         ));
 
+        \Kirki::add_field(\Municipio\Customizer::KIRKI_CONFIG, [
+            'type'        => 'select',
+            'settings'    => 'mod_script_modifier',
+            'label'       => esc_html__('Script', 'municipio'),
+            'section'     => self::SECTION_ID,
+            'default'     => 'none',
+            'priority'    => 10,
+            'choices'     => [
+                'none' => esc_html__('None', 'municipio'),
+                'panel' => esc_html__('Panel', 'municipio'),
+                'accented' => esc_html__('Accented', 'municipio'),
+                'highlight' => esc_html__('Highlight', 'municipio'),
+            ],
+            'output' => [
+                'type' => 'modifier',
+                'context' => ['module.script'],
+            ],
+        ]);
+
     }
 }
