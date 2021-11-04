4d6eaf25c9c97ef23aee613a2a9943615b10a667
Sebastian Thulin <sebastian.thulin@helsingborg.se>
2021-11-04 16:29:23 +0100

    Add modifier settings for sections split

--- library/Customizer/Sections/Module/SectionsSplit.php
+++ library/Customizer/Sections/Module/SectionsSplit.php
@@ -17,5 +17,22 @@ class SectionsSplit
             }
         ));
 
+        \Kirki::add_field(\Municipio\Customizer::KIRKI_CONFIG, [
+            'type'        => 'select',
+            'settings'    => 'mod_section_split_modifier',
+            'label'       => esc_html__('Section Split', 'municipio'),
+            'section'     => self::SECTION_ID,
+            'default'     => 'none',
+            'priority'    => 10,
+            'choices'     => [
+                'none' => esc_html__('None', 'municipio'),
+                'highlight' => esc_html__('Highlight', 'municipio'),
+            ],
+            'output' => [
+                'type' => 'modifier',
+                'context' => ['sectionsSplit', 'module.sections.split'],
+            ],
+        ]);
+
     }
 }
