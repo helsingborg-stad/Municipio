a5e0b2f855e12b650a2ecaaa06e2dcd958b63ba8
Sebastian Thulin <sebastian.thulin@helsingborg.se>
2021-11-04 16:25:49 +0100

    Add local event modifier

--- library/Customizer/Sections/Module/LocalEvent.php
+++ library/Customizer/Sections/Module/LocalEvent.php
@@ -13,9 +13,27 @@ class LocalEvent
             'panel'          => $panelID,
             'priority'       => 160,
             'active_callback' => function() {
-              return post_type_exists('mod-localevent');
+              return post_type_exists('mod-local-events');
             }
         ));
+
+        \Kirki::add_field(\Municipio\Customizer::KIRKI_CONFIG, [
+            'type'        => 'select',
+            'settings'    => 'mod_localevent_modifier',
+            'label'       => esc_html__('Local Event', 'municipio'),
+            'section'     => self::SECTION_ID,
+            'default'     => 'none',
+            'priority'    => 10,
+            'choices'     => [
+                'none' => esc_html__('None', 'municipio'),
+                'panel' => esc_html__('Panel', 'municipio'),
+                'accented' => esc_html__('Accented', 'municipio'),
+            ],
+            'output' => [
+                'type' => 'modifier',
+                'context' => ['module.localevent.list'],
+            ],
+        ]);
 
     }
 }
