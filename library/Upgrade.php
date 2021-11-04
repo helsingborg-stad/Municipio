0a5e1a57dbed7d607643ec51c223c0794987505a
Sebastian Thulin <sebastian.thulin@helsingborg.se>
2021-11-04 16:46:21 +0100

    Map v13 db upgrade to each module type

--- library/Upgrade.php
+++ library/Upgrade.php
@@ -173,6 +173,35 @@ class Upgrade
     return true; 
   }
 
+  //Migrate modules stuff. TODO: TEST!
+  private function v_13($db) : bool {
+
+    //TODO: Must be granulary mapped to each to-field name
+
+    /*
+    $this->migrateThemeMod('modules', 'posts', 'field_6061d864c6873');
+    $this->migrateThemeMod('modules', 'posts', 'field_6062fd67a2eb4');
+    $this->migrateThemeMod('modules', 'posts', 'field_60631bb52591c');
+    $this->migrateThemeMod('modules', 'contacts', 'field_6063008d5068a');
+    $this->migrateThemeMod('modules', 'contacts', 'field_6090f318a40ef');
+    $this->migrateThemeMod('modules', 'inlay', 'field_606300da5068b');
+    $this->migrateThemeMod('modules', 'map', 'field_6063013a5068c');
+    $this->migrateThemeMod('modules', 'script', 'field_6063072c25917');
+    $this->migrateThemeMod('modules', 'text', 'field_60631b4025918');
+    $this->migrateThemeMod('modules', 'video', 'field_60631b5f25919');
+    $this->migrateThemeMod('modules', 'event', 'field_60641a8df5290');
+    $this->migrateThemeMod('modules', 'jsonrender', 'field_60643b600cf66');
+    $this->migrateThemeMod('modules', 'form', 'field_6064452900410');
+    $this->migrateThemeMod('modules', 'index', 'field_607843a6ba55e');
+    $this->migrateThemeMod('modules', 'localevent', 'field_607ff0d6b8426');
+    $this->migrateThemeMod('modules', 'sectionssplit', 'field_611f83757a727');
+    */ 
+
+    //$this->deleteThemeMod('modules');
+
+    return true; 
+  }
+
   /**
    * Move and clean out the old theme mod
    *
