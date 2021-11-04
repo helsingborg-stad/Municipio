cd44f3070040dcd00b4ce4a9131609d8b1a14169
Sebastian Thulin <sebastian.thulin@helsingborg.se>
2021-11-04 15:30:12 +0100

    Init module panels.

--- library/Customizer/Panels/Module.php
+++ library/Customizer/Panels/Module.php
@@ -14,5 +14,16 @@ class Module
             'description' => esc_html__('Manage design options on module level.', 'municipio'),
         ));
 
+        new \Municipio\Customizer\Sections\Module\Contacts(self::PANEL_ID);
+        new \Municipio\Customizer\Sections\Module\Index(self::PANEL_ID);
+        new \Municipio\Customizer\Sections\Module\Inlay(self::PANEL_ID);
+        new \Municipio\Customizer\Sections\Module\LocalEvent(self::PANEL_ID);
+        new \Municipio\Customizer\Sections\Module\Map(self::PANEL_ID);
+        new \Municipio\Customizer\Sections\Module\Posts(self::PANEL_ID);
+        new \Municipio\Customizer\Sections\Module\Script(self::PANEL_ID);
+        new \Municipio\Customizer\Sections\Module\SectionsSplit(self::PANEL_ID);
+        new \Municipio\Customizer\Sections\Module\Text(self::PANEL_ID);
+        new \Municipio\Customizer\Sections\Module\Video(self::PANEL_ID);
+
     }
 }
