504e536ea36d57f019a028216c0a39acc91db24c
Sebastian Thulin <sebastian.thulin@helsingborg.se>
2021-11-04 15:49:44 +0100

    Add contacts module modifiers

--- library/Customizer/Sections/Module/Contacts.php
+++ library/Customizer/Sections/Module/Contacts.php
@@ -17,5 +17,41 @@ class Contacts
             }
         ));
 
+        \Kirki::add_field(\Municipio\Customizer::KIRKI_CONFIG, [
+            'type'        => 'select',
+            'settings'    => 'mod_contacts_card_modifier',
+            'label'       => esc_html__('Card', 'municipio'),
+            'section'     => self::SECTION_ID,
+            'default'     => 'none',
+            'priority'    => 10,
+            'choices'     => [
+              'none' => esc_html__('None', 'municipio'),
+              'highlight' => esc_html__('Highlight', 'municipio'),
+            ],
+            'output' => [
+                'type' => 'modifier',
+                'context' => ['module.contacts.card'],
+            ],
+          ]);
+  
+          \Kirki::add_field(\Municipio\Customizer::KIRKI_CONFIG, [
+            'type'        => 'select',
+            'settings'    => 'mod_contacts_list_modifier',
+            'label'       => esc_html__('List', 'municipio'),
+            'section'     => self::SECTION_ID,
+            'default'     => 'none',
+            'priority'    => 10,
+            'choices'     => [
+              'none' => esc_html__('None', 'municipio'),
+              'panel' => esc_html__('Panel', 'municipio'),
+              'accented' => esc_html__('Accented', 'municipio'),
+              'highlight' => esc_html__('Highlight', 'municipio'),
+            ],
+            'output' => [
+                'type' => 'modifier',
+                'context' => ['module.contacts.list'],
+            ],
+          ]);
+
     }
 }
