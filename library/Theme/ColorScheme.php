46b39020f74521c62a0a14de2387b0dd6ac6101d
nRamstedt <nikolas.ramstedt@gmail.com>
2021-11-08 16:36:00 +0100

    fix: syntax err

--- library/Theme/ColorScheme.php
+++ library/Theme/ColorScheme.php
@@ -55,7 +55,7 @@ class ColorScheme
         //Fetch color scheme
         $colors = (array) apply_filters(
             'Municipio/Theme/ColorPickerPalette', 
-            [],//$this->getColorPalette()
+            []//,$this->getColorPalette()
         );
 
         //Add colors
