4ad9dad818734899746d90944ec5f106780749e6
Sebastian Thulin <sebastian.thulin@helsingborg.se>
2021-11-09 10:12:00 +0100

    Fetch colors, in helper.

--- library/Helper/Color.php
+++ library/Helper/Color.php
@@ -9,50 +9,36 @@ namespace Municipio\Helper;
 class Color
 {
     /**
-     * Prepare the color and alpha value
-     * 
-     * @param   array   $colorItem      [
-     *                                      ['value' => ['color', 'alpha'], 
-     *                                      ['default' => ['color' => '', 'alpha' => '']]
-     *                                  ]
-     * 
-     * @return  string                  Rgba css color value.
+     * Get color palette from theme options
+     *
+     * @return array
      */
-    public static function prepareColor($colorItem) {
-    
-        $colorItem['alpha'] = "1"; //Set default alpha value
+    public static function getPalette() {
 
-        if(is_array($colorItem['value'])||(empty($colorItem['value']) && is_array($colorItem['default']))) {
+        $colorTargetKeys = [
+            'color_palette_primary',
+            'color_palette_secondary'
+        ];
 
-            //Extra default values for group
-            $defaultColor = $colorItem['default']['color'] ?? "";
-            $defaultAlpha = $colorItem['default']['alpha'] . "%" ?? "1";
-
-            //Collect set values for group
-            if (is_array($colorItem['value'])) {
-                $setColor = array_values($colorItem['value'])[0];
-                $setAlpha = array_values($colorItem['value'])[1];
-            } else {
-                $setColor = $colorItem['value'];
-                $setAlpha = $colorItem['alpha'];
+        if(is_array($colorTargetKeys) && !empty($colorTargetKeys)) {
+            
+            $colors = []; $result = [];
+            
+            foreach ($colorTargetKeys as $key) {
+                $colors[$key] = \Kirki::get_option($key);
             }
 
-            //Define set value else default values
-            $colorItem['value'] = !empty($setColor) ? $setColor : $defaultColor;
-            $colorItem['alpha'] = !empty($setAlpha) || $setAlpha == "0" ? $setAlpha .'%' : $defaultAlpha; //empty() returns true on "0"                        
+            //Flatten
+            array_walk_recursive(
+                $colors,
+                function($v) use (&$result){ 
+                    $result[] = $v; 
+                }
+            ); 
 
-        } 
+            return array_unique(array_merge($result, ["#ffffff", "#000000"])); 
+        }
 
-        return self::convertHexToRgba($colorItem['value'], $colorItem['alpha'], $colorItem['default']);
-    }
-
-    /**
-     * Convert the hexadecimal value to rgba
-     * @return string
-     */
-    private static function convertHexToRgba($value, $alpha, $default) {  
-        $value = !empty($value) ? $value : $default;  
-        $value = sscanf($value, "#%02x%02x%02x");
-        return "rgba({$value[0]}, {$value[1]}, {$value[2]}, $alpha)";
+        return []; 
     }
 }
