bdb3166e63a2acb05e74c25e1346ac5fe253ffd1
Sebastian Thulin <sebastian.thulin@helsingborg.se>
2021-11-12 10:16:00 +0100

    Format class according to PSR12

--- library/Customizer/Applicators/ControllerVariables.php
+++ library/Customizer/Applicators/ControllerVariables.php
@@ -4,78 +4,78 @@ namespace Municipio\Customizer\Applicators;
 
 class ControllerVariables
 {
-  public function __construct() {
-    add_filter('Municipio/Controller/Customizer', array($this, 'get')); 
-  }
+    public function __construct()
+    {
+        add_filter('Municipio/Controller/Customizer', array($this, 'get'));
+    }
 
   /**
    * Get customizer controller variables
    *
    * @param array $stack  External stack objects
-   * 
+   *
    * @return object
    */
-  public function get($stack = []) {
+    public function get($stack = [])
+    {
 
-    //Get field definition
-    $fields = \Kirki::$fields; 
+        //Get field definition
+        $fields = \Kirki::$fields;
 
-    //Determine what's a controller var, fetch it
-    if(is_array($fields) && !empty($fields)) {
-        foreach($fields as $key => $field) {
-            if($this->isControllerSetting($field)) {
+        //Determine what's a controller var, fetch it
+        if (is_array($fields) && !empty($fields)) {
+            foreach ($fields as $key => $field) {
+                if ($this->isControllerSetting($field)) {
 
-              /**
-               * Implementation of output active_callback functionality for output. 
-               * Can handle multiple AND statements.  
-               */
-              if(is_string($field['args']['active_callback']) && $field['args']['active_callback'] === '__return_true') {
-                $stack[$key] = \Kirki::get_option($key);
-              } elseif(is_string($field['args']['active_callback']) && $field['args']['active_callback'] === '__return_false') {
-                $stack[$key] = null;
-              } elseif(is_array($field['args']['active_callback']) && !empty($field['args']['active_callback'])) {
+                    /**
+                     * Implementation of output active_callback functionality for output.
+                     * Can handle multiple AND statements.
+                     */
+                    if (is_string($field['args']['active_callback']) && $field['args']['active_callback'] === '__return_true') {
+                        $stack[$key] = \Kirki::get_option($key);
+                    } elseif (is_string($field['args']['active_callback']) && $field['args']['active_callback'] === '__return_false') {
+                        $stack[$key] = null;
+                    } elseif (is_array($field['args']['active_callback']) && !empty($field['args']['active_callback'])) {
+                        $shouldReturn = false;
 
-                $shouldReturn = false; 
+                        foreach ($field['args']['active_callback'] as $cb) {
+                            $cb = (object) $cb;
 
-                foreach($field['args']['active_callback'] as $cb) {
-                  $cb = (object) $cb;
+                            //Verify operator, before eval
+                            if ($this->isValidOperator($cb->operator) === false) {
+                                trigger_error("Provided operator in active callback for is not valid.", E_USER_ERROR);
+                            }
 
-                  //Verify operator, before eval
-                  if($this->isValidOperator($cb->operator) === false) {
-                    trigger_error("Provided operator in active callback for is not valid.", E_USER_ERROR);
-                  }
+                            //Verify value (sanity check)
+                            if (!preg_match('/^[a-z\d_-]+$/i', $cb->value)) {
+                                trigger_error("Provided value in active callback for is not valid, should be a string matching (a-z _ - digits).", E_USER_ERROR);
+                            }
 
-                  //Verify value (sanity check)
-                  if(!preg_match('/^[a-z\d_-]+$/i', $cb->value)) {
-                    trigger_error("Provided value in active callback for is not valid, should be a string matching (a-z _ - digits).", E_USER_ERROR);
-                  }
+                            //Create compare string
+                            if (is_string($cb->value)) {
+                                $cb->value = '"' . $cb->value . '"';
+                            }
 
-                  //Create compare string
-                  if(is_string($cb->value)) {
-                    $cb->value = '"'.$cb->value.'"'; 
-                  }
-      
-                  if(eval('return \Kirki::get_option("' . $cb->setting . '") ' . $cb->operator . ' ' . $cb->value . ';')) {
-                      $shouldReturn = true; 
-                  }
+                            if (eval('return \Kirki::get_option("' . $cb->setting . '") ' . $cb->operator . ' ' . $cb->value . ';')) {
+                                $shouldReturn = true;
+                            }
+                        }
+
+                        if ($shouldReturn === true) {
+                            $stack[$key] = \Kirki::get_option($key);
+                        } else {
+                            $stack[$key] = null;
+                        }
+                    }
                 }
-
-                if($shouldReturn === true) {
-                  $stack[$key] = \Kirki::get_option($key);
-                } else {
-                  $stack[$key] = null;
-                }
-              }
-              
             }
         }
-    }
 
-    //Camel case response keys, and return
-    return \Municipio\Helper\FormatObject::camelCase(
-        (object) $stack
-    ); 
-  }
+      //Camel case response keys, and return
+        return \Municipio\Helper\FormatObject::camelCase(
+            (object) $stack
+        );
+    }
 
   /**
    * Validate PHP operator
@@ -83,12 +83,13 @@ class ControllerVariables
    * @param string $operator
    * @return bool
    */
-  private function isValidOperator($operator) : bool {
-    if(in_array((string) $operator, ['==', '===', '!=', '<>', '!==', '>', '<', '>=', '<=', '<=>'])) {
-      return true; 
+    private function isValidOperator($operator) : bool
+    {
+        if (in_array((string) $operator, ['==', '===', '!=', '<>', '!==', '>', '<', '>=', '<=', '<=>'])) {
+            return true;
+        }
+        return false;
     }
-    return false; 
-  }
 
   /**
    * Determines if should be handled as a controller var.
@@ -96,17 +97,17 @@ class ControllerVariables
    * @param array $field
    * @return boolean
    */
-  private function isControllerSetting($field) {
+    private function isControllerSetting($field)
+    {
 
-    if(!isset($field['args']['output']['type'])) {
-      return false;
+        if (!isset($field['args']['output']['type'])) {
+            return false;
+        }
+
+        if ($field['args']['output']['type'] !== 'controller') {
+            return false;
+        }
+
+        return true;
     }
-
-    if($field['args']['output']['type'] !== 'controller') {
-      return false;
-    }
-
-    return true; 
-  }
-
 }
