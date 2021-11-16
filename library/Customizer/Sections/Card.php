773a7718b35432497e4460e776377fa4786dc7f9
Sebastian Thulin <sebastian.thulin@helsingborg.se>
2021-11-16 09:22:22 +0100

    Configure field for card border.

--- library/Customizer/Sections/Card.php
+++ library/Customizer/Sections/Card.php
@@ -46,6 +46,61 @@ class Card
             ]
         ], ['label' => esc_html__('Card background', 'municipio'), 'settings' => 'card_customization_active']);
 
+
+        KirkiCondidional::add_field(Customizer::KIRKI_CONFIG, [
+            [
+                'type'        => 'slider',
+                'settings'    => 'card_border_width',
+                'label'       => esc_html__('Border width', 'municipio'),
+                'section'     => self::SECTION_ID,
+                'default'     => 0,
+                'choices'     => [
+                    'min'  => 0,
+                    'max'  => 8,
+                    'step' => 1,
+                ],
+                'output'      => [
+                    'element'   => ':root',
+                    'property'  => '--c-card-border-width',
+                    'unit'      => 'px'
+                ],
+            ],
+            [
+                'type'        => 'color',
+                'settings'    => 'card_border_color',
+                'label'       => esc_html__('Border color', 'municipio'),
+                'section'     => self::SECTION_ID,
+                'default'     => '#000000',
+                'output'      => [
+                    'element'   => ':root',
+                    'property'  => '--c-card-border-color'
+                ]
+            ],
+            [
+                'type'        => 'select',
+                'settings'    => 'card_border_style',
+                'label'       => esc_html__('Border style', 'municipio'),
+                'section'     => self::SECTION_ID,
+                'default'     => 'solid',
+                'choices'     => [
+                    'dotted'    => esc_html__('Dotted', 'municipio'),
+                    'dashed'    => esc_html__('Dashed', 'municipio'),
+                    'solid'     => esc_html__('Solid', 'municipio'),
+                    'double'    => esc_html__('Double', 'municipio'),
+                    'groove'    => esc_html__('Groove', 'municipio'),
+                    'ridge'     => esc_html__('Ridge', 'municipio'),
+                    'inset'     => esc_html__('Inset', 'municipio'),
+                    'outset'    => esc_html__('Outset', 'municipio'),
+                    'none'      => esc_html__('None', 'municipio'),
+                    'hidden'    => esc_html__('Hidden', 'municipio')
+                ],
+                'output'      => [
+                    'element'   => ':root',
+                    'property'  => '--c-card-border-style'
+                ]
+            ]
+        ], ['label' => esc_html__('Card Border', 'municipio'), 'settings' => 'card_border_active']);
+
         KirkiCondidional::add_field(Customizer::KIRKI_CONFIG, [
             'type'        => 'color',
             'settings'    => 'card_color',
