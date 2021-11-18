0c86b795a1e87a7b9a19f083f6fb5e03ea719ec2
Sebastian Thulin <sebastian.thulin@helsingborg.se>
2021-11-18 14:01:48 +0100

    Add collection apperance settings

--- library/Customizer/Panels/Component.php
+++ library/Customizer/Panels/Component.php
@@ -18,6 +18,7 @@ class Component
         new \Municipio\Customizer\Sections\Quicklinks(self::PANEL_ID);
         new \Municipio\Customizer\Sections\Hero(self::PANEL_ID);
         new \Municipio\Customizer\Sections\Card(self::PANEL_ID);
+        new \Municipio\Customizer\Sections\Collection(self::PANEL_ID);
         new \Municipio\Customizer\Sections\Button(self::PANEL_ID);
     }
 }
