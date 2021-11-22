1e2497d20800b8d5195d8376498edfdddab021fd
Sebastian Thulin <sebastian.thulin@helsingborg.se>
2021-11-22 14:53:57 +0100

    Migrating overlay color to colors -> alpha

--- library/Upgrade.php
+++ library/Upgrade.php
@@ -9,7 +9,7 @@ namespace Municipio;
  */
 class Upgrade
 {
-    private $dbVersion = 15; //The db version we want to achive
+    private $dbVersion = 16; //The db version we want to achive
     private $dbVersionKey = 'municipio_db_version';
     private $db;
 
@@ -20,9 +20,9 @@ class Upgrade
     {
         //Development tools
         //WARNING: Do not use in PROD. This will destroy your db.
-        /*add_action('init', array($this, 'reset'), 1);
+        add_action('init', array($this, 'reset'), 1);
         add_action('init', array($this, 'debugPre'), 5);
-        add_action('init', array($this, 'debugAfter'), 20); */
+        add_action('init', array($this, 'debugAfter'), 20);
 
         //Production hook
         add_action('init', array($this, 'initUpgrade'), 10);
@@ -61,7 +61,12 @@ class Upgrade
      */
     public function reset()
     {
-        update_option('theme_mods_municipio', unserialize('a:8:{s:18:"custom_css_post_id";i:-1;s:16:"sidebars_widgets";a:2:{s:4:"time";i:1620390598;s:4:"data";a:13:{s:19:"wp_inactive_widgets";a:3:{i:0;s:6:"text-7";i:1;s:6:"text-8";i:2;s:6:"text-2";}s:15:"footer-area-top";a:0:{}s:11:"footer-area";a:3:{i:0;s:6:"text-6";i:1;s:6:"text-5";i:2;s:6:"text-4";}s:11:"slider-area";a:0:{}s:16:"content-area-top";a:0:{}s:12:"content-area";a:0:{}s:19:"content-area-bottom";a:0:{}s:13:"right-sidebar";a:0:{}s:12:"left-sidebar";a:0:{}s:19:"left-sidebar-bottom";a:0:{}s:11:"top-sidebar";a:0:{}s:14:"bottom-sidebar";a:0:{}s:9:"sidebar-1";a:0:{}}}s:18:"nav_menu_locations";a:2:{s:9:"main-menu";i:2;s:14:"secondary-menu";i:2;}s:6:"colors";a:10:{s:19:"field_60361bcb76325";s:7:"#0095db";s:19:"field_60364d06dc120";s:7:"#003359";s:19:"field_603fba043ab30";s:7:"#4989B6";s:19:"field_603fba3ffa851";s:7:"#5BA1D8";s:19:"field_603fbb7ad4ccf";s:7:"#1C73A6";s:19:"field_603fbbef1e2f8";s:7:"#90D2FF";s:19:"field_60911ccc38857";s:7:"#C2CED7";s:19:"field_6091280638858";s:7:"#DBE4E9";s:19:"field_6091282d38859";s:7:"#E4EBF0";s:19:"field_609128593885a";s:7:"#EEF3F6";}s:6:"radius";a:4:{s:19:"field_603662f7a16f8";s:1:"4";s:19:"field_6038fa31cfac6";s:1:"7";s:19:"field_6038fa400384b";s:1:"7";s:19:"field_6038fa52576ba";s:1:"7";}s:7:"modules";a:16:{s:19:"field_6061d864c6873";s:9:"highlight";s:19:"field_6062fd67a2eb4";s:8:"accented";s:19:"field_60631bb52591c";s:4:"none";s:19:"field_6063008d5068a";s:4:"none";s:19:"field_606300da5068b";s:4:"none";s:19:"field_6063013a5068c";s:4:"none";s:19:"field_6063072c25917";s:4:"none";s:19:"field_60631b4025918";s:4:"none";s:19:"field_60631b5f25919";s:4:"none";s:19:"field_60641a8df5290";s:4:"none";s:19:"field_60643b600cf66";s:4:"none";s:19:"field_6064452900410";s:4:"none";s:19:"field_607843a6ba55e";s:9:"highlight";s:19:"field_607843cdba55f";s:4:"none";s:19:"field_607ff0d6b8426";s:8:"accented";s:19:"field_6090f318a40ef";s:9:"highlight";}s:4:"site";a:1:{s:19:"field_6070186956c15";s:8:"accented";}s:6:"widths";a:4:{s:19:"field_609bdcc8348d6";s:4:"1124";s:19:"field_60928f237c070";s:4:"1220";s:19:"field_609bdcad348d5";s:4:"1220";s:19:"field_609298276e5b2";s:3:"720";}}'));
+        //Gy
+        update_option('theme_mods_municipio', unserialize('a:19:{s:18:"custom_css_post_id";i:3869;s:16:"sidebars_widgets";a:2:{s:4:"time";i:1494571854;s:4:"data";a:4:{s:19:"wp_inactive_widgets";a:0:{}s:9:"sidebar-1";a:6:{i:0;s:8:"search-2";i:1;s:14:"recent-posts-2";i:2;s:17:"recent-comments-2";i:3;s:10:"archives-2";i:4;s:12:"categories-2";i:5;s:6:"meta-2";}s:9:"sidebar-2";a:0:{}s:9:"sidebar-3";a:0:{}}}s:18:"nav_menu_locations";a:8:{s:13:"language-menu";i:115;s:15:"quicklinks-menu";i:116;s:9:"help-menu";i:0;s:16:"header-tabs-menu";i:0;s:9:"main-menu";i:0;s:14:"secondary-menu";i:0;s:19:"dropdown-links-menu";i:0;s:13:"mobile-drawer";i:116;}s:6:"colors";a:23:{s:19:"field_60361bcb76325";s:7:"#ec6701";s:19:"field_60364d06dc120";s:7:"#770000";s:19:"field_603fba043ab30";s:7:"#E84C31";s:19:"field_608c016efa73a";a:2:{s:19:"field_608c0259fa73b";s:7:"#e84c31";s:19:"field_608c02dcfa73c";s:2:"10";}s:19:"field_608c0375f70cd";a:2:{s:19:"field_608c0375f70ce";s:7:"#e84c31";s:19:"field_608c0375f70cf";s:2:"25";}s:19:"field_603fba3ffa851";s:7:"#EC6701";s:19:"field_603fbb7ad4ccf";s:7:"#B23700";s:19:"field_603fbbef1e2f8";s:7:"#FF983E";s:19:"field_608c038cf70d0";a:2:{s:19:"field_608c038cf70d1";s:7:"#ff983e";s:19:"field_608c038cf70d2";s:2:"10";}s:19:"field_608c03bcf70d3";a:2:{s:19:"field_608c03bcf70d4";s:7:"#ff983e";s:19:"field_608c03bcf70d5";s:2:"25";}s:19:"field_608c0e753ef05";s:7:"#ec6701";s:19:"field_608c0e813ef06";s:7:"#b23700";s:19:"field_608c0e8c3ef07";s:7:"#ff983e";s:19:"field_608c0ea33ef08";a:2:{s:19:"field_608c0ea33ef09";s:7:"#ae0b05";s:19:"field_608c0ea33ef0a";s:2:"75";}s:19:"field_608c0eae3ef0b";a:2:{s:19:"field_608c0eae3ef0c";s:7:"#ae0b05";s:19:"field_608c0eae3ef0d";s:2:"75";}s:19:"field_60868021879b6";s:7:"#770000";s:19:"field_608680ef879b7";s:7:"#e84c31";s:19:"field_60868147879b8";s:7:"#770000";s:19:"field_6086819f879b9";s:7:"#e84c31";s:19:"field_608681df879ba";s:7:"#AE0B05";s:19:"field_60911ccc38857";s:7:"#dec2c2";s:19:"field_6091280638858";s:7:"#F0DBD9";s:19:"field_6091282d38859";s:7:"#F5E4E3";}s:6:"radius";a:4:{s:19:"field_603662f7a16f8";s:1:"4";s:19:"field_6038fa31cfac6";s:1:"7";s:19:"field_6038fa400384b";s:1:"7";s:19:"field_6038fa52576ba";s:1:"7";}s:7:"modules";a:17:{s:19:"field_6061d864c6873";s:9:"highlight";s:19:"field_6062fd67a2eb4";s:8:"accented";s:19:"field_60631bb52591c";s:4:"none";s:19:"field_6063008d5068a";s:4:"none";s:19:"field_6090f318a40ef";s:9:"highlight";s:19:"field_606300da5068b";s:4:"none";s:19:"field_6063013a5068c";s:4:"none";s:19:"field_6063072c25917";s:4:"none";s:19:"field_60631b4025918";s:4:"none";s:19:"field_60631b5f25919";s:4:"none";s:19:"field_60641a8df5290";s:4:"none";s:19:"field_60643b600cf66";s:4:"none";s:19:"field_6064452900410";s:4:"none";s:19:"field_607843a6ba55e";s:9:"highlight";s:19:"field_607843cdba55f";s:4:"none";s:19:"field_607ff0d6b8426";s:8:"accented";s:19:"field_611f83757a727";s:9:"highlight";}s:4:"site";a:6:{s:19:"field_60d339b60049e";s:6:"normal";s:19:"field_60d3393d1231a";s:5:"large";s:19:"field_60cb4dd897cb8";s:5:"right";s:19:"field_61126702da36c";s:7:"duotone";s:19:"field_6070186956c15";s:8:"accented";s:19:"field_611e43ec4dfa5";s:1:"4";}s:6:"widths";a:6:{s:19:"field_609bdcc8348d6";s:4:"1284";s:19:"field_60928f237c070";s:4:"1284";s:19:"field_609bdcad348d5";s:4:"1284";s:19:"field_609298276e5b2";s:3:"688";s:19:"field_60d339b60049e";s:6:"normal";s:19:"field_60d3393d1231a";s:5:"large";}s:10:"quicklinks";a:5:{s:19:"field_61570dd479d9b";s:3:"hex";s:19:"field_61570e6979d9c";s:7:"#a04601";s:19:"field_6127571bcc76e";s:10:"text-white";s:19:"field_61488b616937c";s:0:"";s:19:"field_61488c4f6b4fd";s:10:"everywhere";}s:7:"padding";a:1:{s:19:"field_611e43ec4dfa5";s:1:"4";}s:6:"header";a:4:{s:19:"field_61434d3478ef7";s:6:"sticky";s:19:"field_61446365d1c7e";s:0:"";s:19:"field_614467575de00";s:0:"";s:19:"field_6070186956c15";s:8:"accented";}s:4:"hero";a:2:{s:19:"field_614c713ae73ea";a:2:{s:19:"field_614c7189e73eb";s:7:"#5f2900";s:19:"field_614c7197e73ec";s:2:"64";}s:19:"field_614c720fb65a4";a:2:{s:19:"field_614c720fb65a5";s:7:"#5f2900";s:19:"field_614c720fb65a6";s:2:"72";}}s:7:"overlay";a:1:{s:19:"field_615c1bc3772c6";a:2:{s:19:"field_615c1bc3780b0";s:7:"#5f2900";s:19:"field_615c1bc3780b6";s:2:"72";}}s:5:"posts";a:3:{s:19:"field_6061d864c6873";s:8:"accented";s:19:"field_6062fd67a2eb4";s:8:"accented";s:19:"field_60631bb52591c";s:4:"none";}s:8:"contacts";a:2:{s:19:"field_6063008d5068a";s:9:"highlight";s:19:"field_6090f318a40ef";s:9:"highlight";}s:5:"inlay";a:1:{s:19:"field_606300da5068b";s:8:"accented";}s:5:"index";a:1:{s:19:"field_607843a6ba55e";s:9:"highlight";}s:6:"script";a:1:{s:19:"field_6063072c25917";s:9:"highlight";}s:10:"localevent";a:1:{s:19:"field_607ff0d6b8426";s:8:"accented";}}'));
+
+        //Fsk
+        //update_option('theme_mods_municipio', unserialize('a:8:{s:18:"custom_css_post_id";i:-1;s:16:"sidebars_widgets";a:2:{s:4:"time";i:1620390598;s:4:"data";a:13:{s:19:"wp_inactive_widgets";a:3:{i:0;s:6:"text-7";i:1;s:6:"text-8";i:2;s:6:"text-2";}s:15:"footer-area-top";a:0:{}s:11:"footer-area";a:3:{i:0;s:6:"text-6";i:1;s:6:"text-5";i:2;s:6:"text-4";}s:11:"slider-area";a:0:{}s:16:"content-area-top";a:0:{}s:12:"content-area";a:0:{}s:19:"content-area-bottom";a:0:{}s:13:"right-sidebar";a:0:{}s:12:"left-sidebar";a:0:{}s:19:"left-sidebar-bottom";a:0:{}s:11:"top-sidebar";a:0:{}s:14:"bottom-sidebar";a:0:{}s:9:"sidebar-1";a:0:{}}}s:18:"nav_menu_locations";a:2:{s:9:"main-menu";i:2;s:14:"secondary-menu";i:2;}s:6:"colors";a:10:{s:19:"field_60361bcb76325";s:7:"#0095db";s:19:"field_60364d06dc120";s:7:"#003359";s:19:"field_603fba043ab30";s:7:"#4989B6";s:19:"field_603fba3ffa851";s:7:"#5BA1D8";s:19:"field_603fbb7ad4ccf";s:7:"#1C73A6";s:19:"field_603fbbef1e2f8";s:7:"#90D2FF";s:19:"field_60911ccc38857";s:7:"#C2CED7";s:19:"field_6091280638858";s:7:"#DBE4E9";s:19:"field_6091282d38859";s:7:"#E4EBF0";s:19:"field_609128593885a";s:7:"#EEF3F6";}s:6:"radius";a:4:{s:19:"field_603662f7a16f8";s:1:"4";s:19:"field_6038fa31cfac6";s:1:"7";s:19:"field_6038fa400384b";s:1:"7";s:19:"field_6038fa52576ba";s:1:"7";}s:7:"modules";a:16:{s:19:"field_6061d864c6873";s:9:"highlight";s:19:"field_6062fd67a2eb4";s:8:"accented";s:19:"field_60631bb52591c";s:4:"none";s:19:"field_6063008d5068a";s:4:"none";s:19:"field_606300da5068b";s:4:"none";s:19:"field_6063013a5068c";s:4:"none";s:19:"field_6063072c25917";s:4:"none";s:19:"field_60631b4025918";s:4:"none";s:19:"field_60631b5f25919";s:4:"none";s:19:"field_60641a8df5290";s:4:"none";s:19:"field_60643b600cf66";s:4:"none";s:19:"field_6064452900410";s:4:"none";s:19:"field_607843a6ba55e";s:9:"highlight";s:19:"field_607843cdba55f";s:4:"none";s:19:"field_607ff0d6b8426";s:8:"accented";s:19:"field_6090f318a40ef";s:9:"highlight";}s:4:"site";a:1:{s:19:"field_6070186956c15";s:8:"accented";}s:6:"widths";a:4:{s:19:"field_609bdcc8348d6";s:4:"1124";s:19:"field_60928f237c070";s:4:"1220";s:19:"field_609bdcad348d5";s:4:"1220";s:19:"field_609298276e5b2";s:3:"720";}}'));
+
         update_option($this->dbVersionKey, 1);
     }
 
@@ -186,8 +191,7 @@ class Upgrade
             set_theme_mod('hero_overlay_enable', 0);
         }
 
-        set_theme_mod('hero_overlay_neutral', $defaultOverlay);
-        set_theme_mod('hero_overlay_vibrant', $vibrantOverlay);
+         
 
         $this->deleteThemeMod('hero');
 
@@ -327,6 +331,29 @@ class Upgrade
         }
 
         delete_option('options_header_layout');
+        return true;
+    }
+
+    //Consolidate overlay
+    private function v_16($db): bool
+    {
+        $overlay = get_theme_mod(
+            'hero_overlay_vibrant',
+            get_theme_mod(
+                'hero_overlay_neutral',
+                get_theme_mod('overlay', "rgba(0,0,0,'0.55')")
+            )
+        );
+
+        if ($overlay) {
+            set_theme_mod('color_alpha', array('base' => $overlay));
+        }
+
+        $this->deleteThemeMod('hero_overlay_enable');
+        $this->deleteThemeMod('hero_overlay_vibrant');
+        $this->deleteThemeMod('hero_overlay_neutral');
+        $this->deleteThemeMod('overlay');
+
         return true;
     }
 
