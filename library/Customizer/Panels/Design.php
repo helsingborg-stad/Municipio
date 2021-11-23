52de1bf31b9e84eaf97bf61db01cd8121471b5e3
Sebastian Thulin <sebastian.thulin@helsingborg.se>
2021-11-23 08:36:21 +0100

    Remove setting for overlay, use alpha color -> base from colors panel

--- library/Customizer/Panels/Design.php
+++ library/Customizer/Panels/Design.php
@@ -19,7 +19,6 @@ class Design
         new \Municipio\Customizer\Sections\Typography(self::PANEL_ID);
         new \Municipio\Customizer\Sections\Width(self::PANEL_ID);
         new \Municipio\Customizer\Sections\Radius(self::PANEL_ID);
-        new \Municipio\Customizer\Sections\Overlay(self::PANEL_ID);
         new \Municipio\Customizer\Sections\Padding(self::PANEL_ID);
         new \Municipio\Customizer\Sections\Shadow(self::PANEL_ID);
     }
