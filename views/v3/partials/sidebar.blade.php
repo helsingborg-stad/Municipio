86a265e3f832ef775ec2add9e095f0ce0446e641
Sebastian Thulin <sebastian.thulin@helsingborg.se>
2021-12-09 13:20:20 +0100

    Add actions before & after sidebars running

--- views/v3/partials/sidebar.blade.php
+++ views/v3/partials/sidebar.blade.php
@@ -1,7 +1,9 @@
 @if(isset($id) && $id)
+    @php do_action('Municipio/sidebar/beforeSidebar'); @endphp
     @if (is_active_sidebar($id))
         <div id="sidebar-{{$id}}" class="sidebar-{{$id}} {{isset($classes) ? is_array($classes) ? implode(' ', $classes) : $classes : ''}}">
             @php dynamic_sidebar($id); @endphp {{-- TODO: Move functions to Controller --}}
         </div>
     @endif
+    @php do_action('Municipio/sidebar/afterSidebar'); @endphp
 @endif
