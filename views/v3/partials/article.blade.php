4898cd23ed77f01e0b2e4bc7db6147d6a8177657
Sebastian Thulin <sebastian.thulin@helsingborg.se>
2021-12-10 11:26:34 +0100

    Add more section to article blade

--- views/v3/partials/article.blade.php
+++ views/v3/partials/article.blade.php
@@ -1,8 +1,15 @@
 <article id="article" class="c-article c-article--readable-width s-article u-clearfix">
+    
+    @section('article-before-title')
+    @show
+    
     <!-- Title -->
     @typography(["element" => "h1"])
         {!! $postTitleFiltered !!}
     @endtypography
+
+    @section('article-after-title')
+    @show
 
     <!-- Blog style author signature -->
     @if(!$postTypeDetails->hierarchical && $isBlogStyle)
@@ -21,18 +28,32 @@
         @endsignature
     @endif
 
-    <!-- Feature Image -->
+    @section('article-before-featured-image')
+    @show
+
     @if (!empty($featuredImage->src))
-		@image([
-			'src'=> $featuredImage->src[0],
-			'alt' => $featuredImage->alt,
-			'classList' => ['c-article__feature-image']
-		])
-		@endimage
+        @image([
+            'src'=> $featuredImage->src[0],
+            'alt' => $featuredImage->alt,
+            'classList' => ['c-article__feature-image', 'u-box-shadow--1']
+        ])
+        @endimage
     @endif
+
+    @section('article-after-featured-image')
+    @show
+
+    @section('article-before-content')
+    @show
 
 	<!-- Content -->
 	{!! $postContentFiltered !!}
+
+    @section('article-after-content')
+    @show
+
+    @section('article-before-terms')
+    @show
 
     <!-- Terms -->
     @if(isset($terms))
@@ -40,7 +61,16 @@
         @endtags
     @endif
 
+    @section('article-after-terms')
+    @show
+
+    @section('article-before-comments')
+    @show
+
     <!-- Comments -->
 	@includeIf('partials.comments')
+
+    @section('article-after-comments')
+    @show
 	
 </article>
--- views/v3/partials/sidebar.blade.php
+++ views/v3/partials/sidebar.blade.php
@@ -1,9 +1,9 @@
 @if(isset($id) && $id)
-    @php do_action('Municipio/sidebar/beforeSidebar'); @endphp
+    @php do_action('Municipio/sidebar/beforeSidebar', $id); @endphp
     @if (is_active_sidebar($id))
         <div id="sidebar-{{$id}}" class="sidebar-{{$id}} {{isset($classes) ? is_array($classes) ? implode(' ', $classes) : $classes : ''}}">
             @php dynamic_sidebar($id); @endphp {{-- TODO: Move functions to Controller --}}
         </div>
     @endif
-    @php do_action('Municipio/sidebar/afterSidebar'); @endphp
+    @php do_action('Municipio/sidebar/afterSidebar', $id); @endphp
 @endif
