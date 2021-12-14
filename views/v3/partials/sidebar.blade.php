46c4b7b83e9e50426dfcf9c13f72ccb3ba84534a
Sebastian Thulin <sebastian.thulin@helsingborg.se>
2021-12-14 11:55:52 +0100

    Add before / after section to all sidebars

--- views/v3/partials/sidebar.blade.php
+++ views/v3/partials/sidebar.blade.php
@@ -1,9 +1,11 @@
 @if(isset($id) && $id)
     @php do_action('Municipio/sidebar/beforeSidebar', $id); @endphp
+    @section('sidebar.' . $id . '.before')@show
     @if (is_active_sidebar($id))
         <div id="sidebar-{{$id}}" class="sidebar-{{$id}} {{isset($classes) ? is_array($classes) ? implode(' ', $classes) : $classes : ''}}">
             @php dynamic_sidebar($id); @endphp {{-- TODO: Move functions to Controller --}}
         </div>
     @endif
+    @section('sidebar.' . $id . '.after')@show
     @php do_action('Municipio/sidebar/afterSidebar', $id); @endphp
 @endif
