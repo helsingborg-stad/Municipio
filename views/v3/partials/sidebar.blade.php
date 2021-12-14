7e3bab20957079d45f7c5e6891fb19217d0bf6f0
Sebastian Thulin <sebastian.thulin@helsingborg.se>
2021-12-14 12:00:57 +0100

    Fix typo

--- views/v3/partials/sidebar.blade.php
+++ views/v3/partials/sidebar.blade.php
@@ -3,7 +3,7 @@
     @if (is_active_sidebar($id))
         @section('sidebar.active.' . $id . '.before')@show
         <div id="sidebar-{{$id}}" class="sidebar-{{$id}} {{isset($classes) ? is_array($classes) ? implode(' ', $classes) : $classes : ''}}">
-            @php dynamic_sidebar($id); @endtypography
+            @php dynamic_sidebar($id); @endphp
         </div>
         @section('sidebar.active.' . $id . '.after')@show
     @endif
