45937675586e66d46a5e2726a0ed99d7aa4b6414
Sebastian Thulin <sebastian.thulin@helsingborg.se>
2022-01-10 13:31:19 +0100

    Fab menu now supports single link (removes popup)

--- views/v3/partials/navigation/floating.blade.php
+++ views/v3/partials/navigation/floating.blade.php
@@ -1,23 +1,39 @@
-@fab([
-    'position' => 'bottom-right',
-    'heading' => $floatingMenuLabels->heading,
-    'button' => [
-        'icon' => $floatingMenuLabels->buttonIcon,
+
+@if(count($floatingMenuItems) == 1)
+    <!-- Single link item -->
+    @button([
+        'classList' => ['u-fixed--bottom-right', 'u-margin--2'],
+        'icon' => !empty($floatingMenuItems[0]['icon']['icon']) ? $floatingMenuItems[0]['icon']['icon'] : $floatingMenuLabels->buttonIcon,
         'size' => 'md',
         'color' => 'primary',
-        'text' => $floatingMenuLabels->buttonLabel,
+        'text' => $floatingMenuItems[0]['label'] ? $floatingMenuItems[0]['label'] : $floatingMenuLabels->buttonLabel,
         'reversePositions' => true,
-    ],
-    'closeLabel' => $lang->close,
-    'closeIcon' =>'close',
-    'context' => ['site.navigation.floating']
-])
-    @nav([
-        'items' => $floatingMenuItems,
-        'direction' => 'vertical',
-        'includeToggle' => false,
-        'classList' => ['c-nav--tiles'],
-        'context' => ['site.navigation.floating.nav']
+        'href' => $floatingMenuItems[0]['href']
     ])
-    @endnav
-@endfab
+    @endbutton
+@else
+    <!-- Multiple link items -->
+    @fab([
+        'position' => 'bottom-right',
+        'heading' => $floatingMenuLabels->heading,
+        'button' => [
+            'icon' => $floatingMenuLabels->buttonIcon,
+            'size' => 'md',
+            'color' => 'primary',
+            'text' => $floatingMenuLabels->buttonLabel,
+            'reversePositions' => true,
+        ],
+        'closeLabel' => $lang->close,
+        'closeIcon' =>'close',
+        'context' => ['site.navigation.floating']
+    ])
+        @nav([
+            'items' => $floatingMenuItems,
+            'direction' => 'vertical',
+            'includeToggle' => false,
+            'classList' => ['c-nav--tiles'],
+            'context' => ['site.navigation.floating.nav']
+        ])
+        @endnav
+    @endfab
+@endif
