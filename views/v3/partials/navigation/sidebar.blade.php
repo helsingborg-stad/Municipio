e5d93549a15c75101243cb4352285f20b93a4f9c
Sebastian Thulin <sebastian.thulin@helsingborg.se>
2022-02-02 14:51:08 +0100

    Wrap nav with paper

--- views/v3/partials/navigation/sidebar.blade.php
+++ views/v3/partials/navigation/sidebar.blade.php
@@ -1,18 +1,20 @@
 @if (!empty($menuItems))
-    @nav([
-        'classList' => [
-            'c-nav--sidebar',            
-            'c-nav--bordered',            
-            'u-display--none@xs',
-            'u-display--none@sm',
-            'u-print-display--none'
-        ],
-        'items' => $menuItems,
-        'direction' => 'vertical',
-        'includeToggle' => true,
-        'depth' => $depth ?? 0,
-    ])
-    @endnav
+    @paper()
+        @nav([
+            'classList' => [
+                'c-nav--sidebar',            
+                'c-nav--bordered',            
+                'u-display--none@xs',
+                'u-display--none@sm',
+                'u-print-display--none'
+            ],
+            'items' => $menuItems,
+            'direction' => 'vertical',
+            'includeToggle' => true,
+            'depth' => $depth ?? 0,
+        ])
+        @endnav
+    @endpaper
 @else
     {{-- No menu items found --}}
 @endif
