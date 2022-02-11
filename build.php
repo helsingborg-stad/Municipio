7ffaae605d5ce30d40ca2269c6114891944b768c
Joel Bernerman <joel.bernerman@helsingborg.se>
2022-02-11 09:42:30 +0100

    point to correct branch, update github workflow to use node 16

--- build.php
+++ build.php
@@ -8,7 +8,7 @@ if (php_sapi_name() !== 'cli') {
 // Any command needed to run and build plugin assets when newly cheched out of repo.
 $buildCommands = [
     'npm ci --no-progress --no-audit',
-    'npm update @helsingborg-stad/styleguide@github:helsingborg-stad/styleguide#feat/cleanup-and-update-icons',
+    'npm update @helsingborg-stad/styleguide',
     'npm run build',
     'composer install --prefer-dist --no-progress'
 ];
