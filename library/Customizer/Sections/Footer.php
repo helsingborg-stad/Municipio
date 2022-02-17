8401ed4f52602f5bb5dff91b3817f4508d9d0b49
Erik Granström <erik.granstrom@helsingborg.se>
2022-02-17 14:53:38 +0100

    remove unused code

--- library/Customizer/Sections/Footer.php
+++ library/Customizer/Sections/Footer.php
@@ -34,43 +34,5 @@ class Footer
                 ]
             ],
         ]);
-
-        // \Kirki::add_field(\Municipio\Customizer::KIRKI_CONFIG, [
-        //     'type'        => 'background',
-        //     'settings'    => 'footer_background',
-        //     'label'       => esc_html__('Footer background', 'municipio'),
-        //     'description' => esc_html__('Set the background of the footer area.', 'municipio'),
-        //     'section'     => self::SECTION_ID,
-        //     'default'     => [
-        //         'background-color'      => 'rgba(20,20,20,.8)',
-        //         'background-image'      => '',
-        //         'background-repeat'     => 'repeat',
-        //         'background-position'   => 'center center',
-        //         'background-size'       => 'contain',
-        //         'background-attachment' => 'scroll',
-        //     ],
-        //     'transport'   => 'auto',
-        //     'css_vars'  => [
-        //            ['--c-footer-background-color', '$', 'background-color'],
-        //            ['--c-footer-background-image', '$', 'background-image'],
-        //            ['--c-footer-background-repeat', '$', 'background-repeat'],
-        //            ['--c-footer-background-position', '$', 'background-position'],
-        //            ['--c-footer-background-size', '$', 'background-size'],
-        //            ['--c-footer-background-attachment', '$', 'background-attachment']
-        //     ]
-        // ]);
-
-        // \Kirki::add_field(\Municipio\Customizer::KIRKI_CONFIG, [
-        //     'type'        => 'color',
-        //     'settings'    => 'overlay',
-        //     'label'       => esc_html__('Footer font color', 'municipio'),
-        //     'description' => esc_html__("Choose a font color contrasting the background.", 'municipio'),
-        //     'section'     => self::SECTION_ID,
-        //     'default'     => '#000000',
-        //     'output'      => [
-        //       'element'   => ':root',
-        //       'property'  => '--c-footer-color'
-        //     ]
-        //   ]);
     }
 }
