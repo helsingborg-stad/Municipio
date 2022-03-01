e1263209345b93b6c1c6a26a26a2ff893bb84fb8
Sebastian Thulin <sebastian.thulin@helsingborg.se>
2022-03-01 11:50:11 +0100

    Add view for grid
    Using card as placeholder, since it is similar.

---
+++ views/v3/partials/post/post-grid.blade.php
@@ -0,0 +1,19 @@
+@if ($posts)
+    <div class="o-grid">
+        @foreach($posts as $post)
+            <div class="o-grid-12 {{ $gridColumnClass }}">
+                @card([
+                    'link' => $post->permalink,
+                    'imageFirst' => true,
+                    'image' =>  $post->thumbnail,
+                    'heading' => $post->postTitle,
+                    'classList' => ['t-archive-card', 'u-height--100', 'u-height-100', 'u-flex-direction--column', 'u-display--flex'],
+                    'byline' => ['text' => $post->postDate, 'position' => 'body'],
+                    'content' => $post->excerptShort,
+                    'tags' => $post->termsunlinked
+                ])
+                @endcard
+            </div>
+        @endforeach
+    </div>
+@endif
