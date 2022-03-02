766917ea9eada41926b032786fe72171c536e101
Sebastian Thulin <sebastian.thulin@helsingborg.se>
2022-03-02 15:23:42 +0100

    Adds settings panels for all post types, fill with basic settings.
    Todo: Order by values

--- library/Customizer/Panels/Archive.php
+++ library/Customizer/Panels/Archive.php
@@ -14,6 +14,69 @@ class Archive
             'description' => esc_html__('Manage apperance options on archives.', 'municipio'),
         ));
 
-        //new \Municipio\Customizer\Sections\Header(self::PANEL_ID);
+        //Register panel for each archive
+        $archives = $this->getArchives();
+        if (is_array($archives) && !empty($archives)) {
+            foreach ($archives as $archive) {
+                new \Municipio\Customizer\Sections\Archive(
+                    self::PANEL_ID,
+                    $archive
+                );
+            }
+        }
+    }
+
+    /**
+     * Fetch archives
+     *
+     * @return array
+     */
+    private function getArchives(): array
+    {
+        $postTypes = array();
+
+        foreach ((array) get_post_types() as $key => $postType) {
+            $args = get_post_type_object($postType);
+
+            if (!$args->public || $args->name === 'page') {
+                continue;
+            }
+
+            $args->taxonomies = $this->getTaxonomies($postType);
+
+            $postTypes[$postType] = $args;
+        }
+
+        $postTypes['author'] = (object) array(
+            'name' => 'author',
+            'label' => __('Author'),
+            'has_archive' => true,
+            'is_author_archive' => true
+        );
+
+        return $postTypes;
+    }
+
+    /**
+     * Get taxonomies for post type
+     *
+     * @param string $postType
+     * @return array
+     */
+    private function getTaxonomies($postType): array
+    {
+        $taxonomies = get_object_taxonomies($postType, 'objects');
+
+        if (is_array($taxonomies) && !empty($taxonomies)) {
+            foreach ($taxonomies as $taxonomy) {
+                if ($taxonomy->public) {
+                    $stack[$taxonomy->name] = $taxonomy->label;
+                }
+            }
+
+            return $stack;
+        }
+
+        return [];
     }
 }
---
+++ library/Customizer/Sections/Archive.php
@@ -0,0 +1,134 @@
+<?php
+
+namespace Municipio\Customizer\Sections;
+
+class Archive
+{
+    public $sectionId;
+
+    public function __construct($panelID, $archive)
+    {
+        //Create section_id
+        $this->sectionId = $panelID . "_" . $archive->name;
+
+        //Panel
+        \Kirki::add_section($this->sectionId, array(
+            'title'         => $archive->label,
+            'panel'         => $panelID,
+            'priority'      => 160
+        ));
+
+        \Kirki::add_field(\Municipio\Customizer::KIRKI_CONFIG, [
+            'type'     => 'text',
+            'settings' => 'archive_' . $archive->name . '_heading',
+            'label'    => esc_html__('Archive heading', 'municipio'),
+            'section'  => $this->sectionId,
+            'default'  => ''
+        ]);
+
+        \Kirki::add_field(\Municipio\Customizer::KIRKI_CONFIG, [
+            'type'     => 'textarea',
+            'settings' => 'archive_' . $archive->name . '_body',
+            'label'    => esc_html__('Archive lead text', 'municipio'),
+            'section'  => $this->sectionId,
+            'default'  => ''
+        ]);
+
+        \Kirki::add_field(\Municipio\Customizer::KIRKI_CONFIG, [
+            'type'     => 'select',
+            'settings' => 'archive_' . $archive->name . '_style',
+            'label'    => esc_html__('Style', 'municipio'),
+            'section'  => $this->sectionId,
+            'default'  => 'cards',
+            'choices'  => [
+                'compressed' => esc_html__('Compressed', 'municipio'),
+                'cards' => esc_html__('Cards', 'municipio'),
+                'newsitem' => esc_html__('News', 'municipio'),
+                'list' => esc_html__('List', 'municipio'),
+                'grid' => esc_html__('Blocks', 'municipio')
+            ],
+        ]);
+
+        \Kirki::add_field(\Municipio\Customizer::KIRKI_CONFIG, [
+            'type'        => 'slider',
+            'settings'    => 'archive_' . $archive->name . '_post_count',
+            'label'       => esc_html__('Number of posts to display', 'municipio'),
+            'description' => esc_html__('How many posts that should be displayed on each page.', 'municipio'),
+            'section'     => $this->sectionId,
+            'transport'   => 'auto',
+            'default'     => 9,
+            'choices'     => [
+                'min'  => 4,
+                'max'  => 40,
+                'step' => 1,
+            ]
+        ]);
+
+        \Kirki::add_field(\Municipio\Customizer::KIRKI_CONFIG, [
+            'type'        => 'slider',
+            'settings'    => 'archive_' . $archive->name . '_number_of_columns',
+            'label'       => esc_html__('Number of columns to display', 'municipio'),
+            'description' => esc_html__('How many columns that the posts should be divided in.', 'municipio'),
+            'section'     => $this->sectionId,
+            'transport'   => 'auto',
+            'default'     => 4,
+            'choices'     => [
+                'min'  => 1,
+                'max'  => 4,
+                'step' => 1,
+            ]
+        ]);
+
+
+        \Kirki::add_field(\Municipio\Customizer::KIRKI_CONFIG, [
+            'type'        => 'slider',
+            'settings'    => 'archive_' . $archive->name . '_number_of_columns',
+            'label'       => esc_html__('Number of columns to display', 'municipio'),
+            'description' => esc_html__('How many columns that the posts should be divided in.', 'municipio'),
+            'section'     => $this->sectionId,
+            'transport'   => 'auto',
+            'default'     => 4,
+            'choices'     => [
+                'min'  => 1,
+                'max'  => 4,
+                'step' => 1,
+            ]
+        ]);
+
+        if (!empty($archive->taxonomies)) {
+            \Kirki::add_field(\Municipio\Customizer::KIRKI_CONFIG, [
+                'type'        => 'select',
+                'settings'    => 'archive_' . $archive->name . '_taxonomies_to_display',
+                'label'       => esc_html__('Taxonomy display', 'municipio'),
+                'description' => esc_html__('What taxonomies should be displayed?', 'municipio'),
+                'multiple'    => 4,
+                'section'     => $this->sectionId,
+                'choices'     => $archive->taxonomies
+            ]);
+        }
+
+        \Kirki::add_field(\Municipio\Customizer::KIRKI_CONFIG, [
+            'type'        => 'select',
+            'settings'    => 'archive_' . $archive->name . '_order_by',
+            'label'       => esc_html__('Order By', 'municipio'),
+            'description' => esc_html__('Select a key/value to order by.', 'municipio'),
+            'multiple'    => 2,
+            'section'     => $this->sectionId,
+            'choices'     => [
+            ]
+        ]);
+
+        \Kirki::add_field(\Municipio\Customizer::KIRKI_CONFIG, [
+            'type'        => 'select',
+            'settings'    => 'archive_' . $archive->name . '_order_direction',
+            'label'       => esc_html__('Order direction', 'municipio'),
+            'description' => esc_html__('Select a key/value to order by.', 'municipio'),
+            'multiple'    => 2,
+            'section'     => $this->sectionId,
+            'choices'     => [
+                'asc' => __("Ascending", 'municipio'),
+                'desc' => __("Decending", 'municipio')
+            ]
+        ]);
+    }
+}
