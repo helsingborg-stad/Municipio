8b3d4ed4305e7491dd307fbc7e765fda381c06f1
Sebastian Thulin <sebastian.thulin@helsingborg.se>
2022-03-09 16:20:09 +0100

    Mobile adaption

--- views/v3/partials/archive/archive-menu.blade.php
+++ views/v3/partials/archive/archive-menu.blade.php
@@ -1,39 +1,40 @@
 @if($archiveMenuItems)
   @paper()
-    <div class="o-grid">
-      <div class="o-grid-{{$hasQueryParameters ? '10' : '12'}}">
-        <nav role="navigation" aria-label="{{$lang->archiveNav}}" class="u-display--flex u-print-display--none u-margin__x--2">
-          @foreach($archiveMenuItems as $item)
-              @button([
-                  'href'  => $item['href'], 
-                  'text'  => $item['label'],
-                  'size'  => 'lg',
-                  'style' => 'basic',
-                  'icon' => $item['icon']['icon'] ?? null,
-                  'attributeList' => [
-                      'role' => 'menuitem'
-                  ],
-                  'reversePositions' => true,
-                  'classList' => [
-                    'u-padding__x--2'
-                  ]
-              ])
-              @endbutton
-          @endforeach
-        </nav>
-      </div>
-      @if($hasQueryParameters)
-        <div class="o-grid-2">
-          @button([
-              'href'  => $archiveBaseUrl, 
-              'text'  => $lang->resetFacetting,
-              'size'  => 'lg',
-              'style' => 'filled',
-              'icon' => 'close'
-          ])
-          @endbutton
+    <div class="o-grid u-print-display--none">
+      <div class="o-grid-12">
+        <div class="u-display--flex u-flex--gridgap u-align-content--center u-justify-content--space-between u-margin__x--2 u-margin__y--1">
+          <nav role="navigation" aria-label="{{$lang->archiveNav}}" class="u-display--flex u-flex--gridgap u-margin__left--0@xs u-margin__left--2@sm u-margin__left--2@md u-margin__left--2@lg u-flex-wrap" style="row-gap: 0;">
+            @foreach($archiveMenuItems as $item)
+                @button([
+                    'href'  => $item['href'], 
+                    'text'  => $item['label'],
+                    'size'  => 'md',
+                    'style' => 'basic',
+                    'icon' => $item['icon']['icon'] ?? null,
+                    'attributeList' => [
+                        'role' => 'menuitem'
+                    ],
+                    'classList' => [
+                        'u-margin--0'
+                    ],
+                    'reversePositions' => true
+                ])
+                @endbutton
+            @endforeach
+          </nav>
+          @if($hasQueryParameters)
+            @button([
+                'href'  => $archiveBaseUrl, 
+                'text'  => $lang->resetFacetting,
+                'size'  => 'sm',
+                'style' => 'filled',
+                'icon' => 'close',
+                'classList' => ['u-margin__y--auto', 'u-align-self--end'],
+            ])
+            @endbutton
+          @endif
         </div>
-      @endif
+      </div>
     </div>
   @endpaper
 @endif
