424e1d079b1e780b5162018a555274d745158d2b
Sebastian Thulin <sebastian.thulin@helsingborg.se>
2022-03-09 10:38:48 +0100

    Remove duplicate ratio param to component

--- views/v3/partials/post/post-grid.blade.php
+++ views/v3/partials/post/post-grid.blade.php
@@ -5,7 +5,7 @@
                 @block([
                     'link' => $post->permalink,
                     'heading' =>  $post->postTitle,
-                    'ratio' => '12:16',
+                    'ratio' => $archiveProps->format == 'tall' ? '12:16' : '4:3',
                     'meta' => $post->termsunlinked,
                     'filled' => true,
                     'image' => [
@@ -16,7 +16,6 @@
                     'date' => $post->archiveDate,
                     'classList' => ['t-archive-block'],
                     'context' => ['archive', 'archive.list', 'archive.list.block'],
-                    'ratio' => $archiveProps->format == 'tall' ? '12:16' : '4:3',
                 ])
                 @endblock
             </div>
