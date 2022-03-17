ab9daf8db93729323a13098b293cb6acb5063b70
Erik Granström <erik.granstrom@helsingborg.se>
2022-03-17 09:46:47 +0100

    Add option to hide from mobile

--- views/v3/partials/navigation/hamburgermenu.blade.php
+++ views/v3/partials/navigation/hamburgermenu.blade.php
@@ -3,10 +3,7 @@
         'menuItems' => $hamburgerMenuItems,
         'showSearch' => $showHamburgerMenuSearch,
         'parentButtons' => $customizer->hamburgerMenuParentButtons,
-        'classList' => [
-            'u-display--none',
-            !$customizer->hamburgerMenuMobile ? 'u-display--none@sm u-display--none@md' : '',
-        ]
+        'mobile' => $customizer->hamburgerMenuMobile,
     ])
     @endhamburgerMenu
 @endif
