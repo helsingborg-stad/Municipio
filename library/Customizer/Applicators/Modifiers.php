8dc517471f4c9061d913dae76be2e3e4a9a67eda
Freya <752642+faejr@users.noreply.github.com>
2022-03-18 13:38:08 +0100

    fix: move generic methods to abstractapplicator

--- library/Customizer/Applicators/Modifiers.php
+++ library/Customizer/Applicators/Modifiers.php
@@ -2,7 +2,7 @@
 
 namespace Municipio\Customizer\Applicators;
 
-class Modifiers
+class Modifiers extends AbstractApplicator
 {
     public function __construct()
     {
@@ -23,7 +23,7 @@ class Modifiers
         //Determine what's a controller var, fetch it
         if (is_array($fields) && !empty($fields)) {
             foreach ($fields as $key => $field) {
-                if (!$this->isModifierSetting($field)) {
+                if (!$this->isFieldType($field, 'modifier')) {
                     continue;
                 }
 
@@ -56,24 +56,7 @@ class Modifiers
                         }
                         // Check if contexts filter is multidimensional (new format)
                         if (count($filter['contexts']) !== count($filter['contexts'], COUNT_RECURSIVE)) {
-                            $applyModifier = [];
-                            foreach ($filter['contexts'] as $context) {
-                                //Verify operator, before eval
-                                if ($this->isValidOperator($context['operator']) === false) {
-                                    trigger_error("Provided operator in context for modifier is not valid.", E_USER_ERROR);
-                                }
-
-                                // Verify that context is a string and format it for eval
-                                if (is_string($context['context'])) {
-                                    $context['context'] = '"' . $context['context'] . '"';
-                                } else {
-                                    trigger_error("Provided context value in context for modifier is not a string.", E_USER_ERROR);
-                                }
-
-                                // Check if provided context exists in context array and compare using operator
-                                $applyModifier[] = (bool) eval('return in_array(' . $context['context'] . ', $contexts) ' . $context['operator'] . ' true;');
-                            }
-                            if ((bool) array_product($applyModifier)) {
+                            if ($this->hasFilterContexts($contexts, $filter['contexts'])) {
                                 $modifiers[] = $filter['value'];
                             }
                         }
@@ -83,37 +66,5 @@ class Modifiers
                 }, 10, 2);
             }
         }
-    }
-
-    /**
-     * Determines if should be handled as a modifier.
-     *
-     * @param array $field
-     * @return boolean
-     */
-    private function isModifierSetting($field, $lookForType = 'modifier')
-    {
-        if (isset($field['output']) && is_array($field['output']) && !empty($field['output'])) {
-            foreach ($field['output'] as $output) {
-                if (isset($output['type']) && $output['type'] === $lookForType) {
-                    return true;
-                }
-            }
-        }
-        return false;
-    }
-
-    /**
-     * Validate PHP operator
-     *
-     * @param string $operator
-     * @return bool
-     */
-    private function isValidOperator($operator): bool
-    {
-        if (in_array((string) $operator, ['==', '===', '!=', '<>', '!==', '>', '<', '>=', '<=', '<=>'])) {
-            return true;
-        }
-        return false;
     }
 }
