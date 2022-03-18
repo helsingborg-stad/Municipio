308a42ca70d80d8d1d0dc1b9e9bc0fe10a2bab4e
Erik Granström <erik.granstrom@helsingborg.se>
2022-03-18 16:00:08 +0100

    Remove example panel

--- library/Customizer/Panels/Menu.php
+++ library/Customizer/Panels/Menu.php
@@ -4,17 +4,10 @@ namespace Municipio\Customizer\Panels;
 
 class Menu
 {
-    public const PANEL_ID = "municipio_customizer_panel_menu";
+    public const PANEL_ID = "nav_menus";
 
     public function __construct()
     {
-        \Kirki::add_panel('nav_menus', array(
-            'priority'    => 1000,
-            'title'       => esc_html__('Example panel', 'municipio'),
-            'description' => esc_html__('A example panel', 'municipio'),
-            'section' => 'nav_menus'
-        ));
-
-        new \Municipio\Customizer\Sections\Menu('nav_menus');
+        new \Municipio\Customizer\Sections\Menu(self::PANEL_ID);
     }
 }
