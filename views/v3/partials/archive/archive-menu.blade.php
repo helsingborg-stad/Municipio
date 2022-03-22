a8c2376c526d8fa4cb0cc90168286ef3e9240fa0
Sebastian Thulin <sebastian.thulin@helsingborg.se>
2022-03-22 15:50:33 +0100

    Update archive nav to use nav

--- views/v3/partials/archive/archive-menu.blade.php
+++ views/v3/partials/archive/archive-menu.blade.php
@@ -3,45 +3,32 @@
   <div class="o-grid u-print-display--none">
     <div class="o-grid-12">
       @paper()
-        <div class="u-display--flex u-flex--gridgap-col u-flex-direction--column@xs u-flex-direction--column@sm u-align-content--center u-justify-content--space-between u-padding__x--2 u-padding__y--1">
-          <nav role="navigation" aria-label="{{$lang->archiveNav}}" class="u-display--flex u-flex--gridgap u-margin__left--0@xs u-margin__left--0@sm u-margin__left--2@md u-margin__left--2@lg u-flex-wrap" style="row-gap: 0;">
-            @foreach($archiveMenuItems as $item)
-                @button([
-                    'href'  => $item['href'], 
-                    'text'  => $item['label'],
-                    'size'  => 'md',
-                    'color' => $item['active'] ? 'primary' : 'default',
-                    'style' => $item['active'] ? 'basic' : 'basic',
-                    'icon' => $item['icon']['icon'] ?? null,
-                    'attributeList' => [
-                        'role' => 'menuitem'
-                    ],
-                    'classList' => [
-                        'u-margin--0'
-                    ],
-                    'reversePositions' => true
-                ])
-                @endbutton
-            @endforeach
+        <div class="u-display--flex u-flex--gridgap-col u-flex-direction--column@xs u-flex-direction--column@sm u-align-content--center u-justify-content--space-between">
+          <nav role="navigation" aria-label="{{$lang->archiveNav}}">
+            @nav([
+                'items' => $archiveMenuItems,
+                'direction' => 'horizontal',
+                'allowStyle' => false,
+                'classList' => ['u-flex-wrap'],
+                'context' => ['site.archive.nav']
+            ])
+            @endnav
           </nav>
           @if($hasQueryParameters)
-            @button([
-                'href'  => $archiveBaseUrl, 
-                'text'  => $lang->resetFacetting,
-                'size'  => 'sm',
-                'style' => 'filled',
-                'icon' => 'close',
-                'classList' => [
-                  'u-margin__y--1@xs',
-                  'u-margin__y--1@sm',
-                  'u-margin__y--auto@md',
-                  'u-margin__y--auto@lg',  
-                  'u-align-self--end', 
-                  'u-width--100@xs', 
-                  'u-width--100@sm'
-                ],
-            ])
-            @endbutton
+            <div class="u-padding--2 u-margin__y--auto@md u-margin__y--auto@lg u-align-self--end u-width--100@xs u-width--100@sm">
+              @button([
+                  'href'  => $archiveBaseUrl, 
+                  'text'  => $lang->resetFacetting,
+                  'size'  => 'sm',
+                  'style' => 'filled',
+                  'icon' => 'close',
+                  'classList' => [
+                    'u-width--100@xs',
+                    'u-width--100@sm'
+                  ],
+              ])
+              @endbutton
+            </div>
           @endif
         </div>
       @endpaper
