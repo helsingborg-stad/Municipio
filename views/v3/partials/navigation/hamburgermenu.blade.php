df3b5e7d6ffd3810e646ed8e3201419ed9df80f1
Sebastian Thulin <sebastian.thulin@helsingborg.se>
2022-04-12 13:55:58 +0200

    Insert slot in hamburger menu, with search func.

--- views/v3/partials/navigation/hamburgermenu.blade.php
+++ views/v3/partials/navigation/hamburgermenu.blade.php
@@ -6,6 +6,33 @@
             'parentStyle' => $customizer->hamburgerMenuParentStyle,
             'mobile' => $customizer->hamburgerMenuMobile,
         ])
+
+        <div class="o-grid-12">
+            @form(['method' => 'get', 'action' => '/'])
+                <label for="hamburger-menu-search" class="u-sr__only">{{ __('Search', 'component-library') }}</label>
+
+                @group([])
+                    @field([
+                        'id' => 'hamburger-menu-search',
+                        'type' => 'text',
+                        'attributeList' => [
+                            'type' => 'text',
+                            'name' => 's',
+                        ],
+                        'placeholder' => $lang->searchOn . " " . $siteName,
+                        'required' => true,
+                    ])
+                    @endfield
+                    @button([
+                        'color' => 'primary',
+                        'type' => 'submit',
+                        'text' => $lang->search
+                    ])
+                    @endbutton
+                @endgroup
+            @endform
+        </div>
+
         @endhamburgerMenu
     </div>
 @endif
