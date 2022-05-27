f35c0343ba9afe2bd1a7f282f8664bd03ce727eb
Sebastian Thulin <sebastian.thulin@helsingborg.se>
2022-05-27 16:11:20 +0200

    Update comments view. Move actions to actions section.

--- views/v3/partials/comments.blade.php
+++ views/v3/partials/comments.blade.php
@@ -10,105 +10,108 @@
 
                     <div class="parent" id="div-comment-{{$comment->comment_ID}}">
 
-                        @if (is_user_logged_in())
-                            <div class="comment--likes">
-                                @button([
-                                    'icon' => 'thumb_up',
-                                    'size' => 'sm',
-                                    'color' => '',
-                                    'style' => 'basic',
-                                    'type' => 'button',
-                                    'classList' => ['comment--likes-icon', \Municipio\Comment\Likes::likeButton($comment->comment_ID)['classList']],
-                                    'attributeList' => [
-                                        'data-commentid' => $comment->comment_ID,
-                                    ],
-                                ])
-                                @endbutton
-
-                                <span data-likes="{{\Municipio\Comment\Likes::likeButton($comment->comment_ID)['count']}}"
-                                      id="comment-likes-{{$comment->comment_ID}}">
-                                        {{\Municipio\Comment\Likes::likeButton($comment->comment_ID)['count']}}
-                                </span>
-                            </div>
-                        @endif
-
-                        <a name="comment-{{$comment->comment_ID}}"></a>
                         @comment([
+                            'id' => 'comment-' . $comment->comment_ID,
                             'author' => $comment->comment_author,
                             'author_image' => get_user_by('email', $comment->comment_author_email) ? get_avatar_url(get_user_by('email', $comment->comment_author_email)->data->ID ) : false,
                             'href' => '',
+                            'text' => get_comment_text($comment->comment_ID),
                             'icon' => 'face',
-                            'bubble_color' => 'dark',
+                            'bubble_color' => 'light',
                             'date_suffix' => __('ago', 'municipio'),
                             'date' => date('Y-m-d H:i', strtotime($comment->comment_date)),
                             'classList' => ['comment-'.$comment->comment_ID, 'comment-reply-link'],
                         ])
-                            {!! get_comment_text($comment->comment_ID) !!}
-                        @endcomment
-                    </div>
-
-                    <div class="reply comment--actions">
-                        @button([
-                            'icon' => 'reply',
-                            'reversePositions' => false,
-                            'style' => 'basic',
-                            'color' => 'secondary',
-                            'text' => __('Reply', 'municipio'),
-                            'componentElement' => 'div',
-                            'attributeList' => [
-                                'data-commentid' => $comment->comment_ID,
-                                'data-postid' => $post->id,
-                                'data-belowelement' => 'div-comment-'.$comment->comment_ID,
-                                'data-respondelement' => 'respond',
-                                'rev' => 'nofollow',
-                                'js-toggle-trigger' => 'hide-reply-'.$comment->comment_ID,
-                                'js-toggle-item' => 'hide-reply-'.$comment->comment_ID,
-                                'js-toggle-class' => 'u-display--none'
-                            ],
-                            'classList' => ['comment-reply-link']
-                        ])
-                        @endbutton
-                    </div>
-
-                    @if (!empty(get_comments(array('parent' => $comment->comment_ID, 'order' => 'asc'))))
-                        @foreach(get_comments(array('parent' => $comment->comment_ID, 'order' => 'asc'))  as $answer)
-
-                            <div class="child" id="div-comment-{{$answer->comment_ID}}">
+                            @slot('actions')
+                                
                                 @if (is_user_logged_in())
-                                    <div class="comment--likes">
+                                    <div class="comment--likes u-margin__right--2">
                                         @button([
                                             'icon' => 'thumb_up',
                                             'size' => 'sm',
                                             'color' => '',
                                             'style' => 'basic',
                                             'type' => 'button',
-                                            'classList' => ['comment--likes-icon', \Municipio\Comment\Likes::likeButton($answer->comment_ID)['classList']],
+                                            'classList' => ['comment--likes-icon', \Municipio\Comment\Likes::likeButton($comment->comment_ID)['classList']],
                                             'attributeList' => [
-                                                'data-commentid' => $answer->comment_ID,
+                                                'data-commentid' => $comment->comment_ID,
                                             ],
                                         ])
                                         @endbutton
 
-                                        <span data-likes="{{\Municipio\Comment\Likes::likeButton($answer->comment_ID)['count']}}"
-                                              id="comment-likes-{{$answer->comment_ID}}">
-                                            {{\Municipio\Comment\Likes::likeButton($answer->comment_ID)['count']}}
+                                        <span class="c-typography c-typography__variant--meta" data-likes="{{\Municipio\Comment\Likes::likeButton($comment->comment_ID)['count']}}"
+                                            id="comment-likes-{{$comment->comment_ID}}">
+                                                {{\Municipio\Comment\Likes::likeButton($comment->comment_ID)['count']}}
                                         </span>
                                     </div>
                                 @endif
 
-                                <a name="comment-{{$answer->comment_ID}}"></a>
+                                @button([
+                                    'size' => 'sm',
+                                    'icon' => 'reply',
+                                    'reversePositions' => false,
+                                    'style' => 'basic',
+                                    'color' => 'default',
+                                    'text' => __('Reply', 'municipio'),
+                                    'componentElement' => 'div',
+                                    'attributeList' => [
+                                        'data-commentid' => $comment->comment_ID,
+                                        'data-postid' => $post->id,
+                                        'data-belowelement' => 'div-comment-'.$comment->comment_ID,
+                                        'data-respondelement' => 'respond',
+                                        'rev' => 'nofollow',
+                                        'js-toggle-trigger' => 'hide-reply-'.$comment->comment_ID,
+                                        'js-toggle-item' => 'hide-reply-'.$comment->comment_ID,
+                                        'js-toggle-class' => 'u-display--none'
+                                    ],
+                                    'classList' => ['comment-reply-link']
+                                ])
+                                @endbutton
+
+                            @endslot
+                        @endcomment
+                    </div>
+
+                    @if (!empty(get_comments(array('parent' => $comment->comment_ID, 'order' => 'asc'))))
+                        @foreach(get_comments(array('parent' => $comment->comment_ID, 'order' => 'asc'))  as $answer)
+
+                            <div class="child" id="div-comment-{{$answer->comment_ID}}">
                                 @comment([
+                                    'id' => "comment-" . $answer->comment_ID,
                                     'author' => $answer->comment_author,
                                     'author_url' => 'mailto:'.$answer->comment_author_email,
                                     'author_image' => get_avatar_url(get_user_by('email', $answer->comment_author_email)->data->ID ),
                                     'href' => '',
+                                    'text' => get_comment_text($answer->comment_ID),
                                     'icon' => 'face',
-                                    'bubble_color' => 'dark',
+                                    'bubble_color' => 'light',
                                     'date_suffix' => __('ago', 'municipio'),
                                     'date' => date('Y-m-d H:i', strtotime($answer->comment_date)),
                                     'is_reply' => true
                                 ])
-                                    {!! get_comment_text($answer->comment_ID) !!}
+                                    @slot('actions')
+                                        @if (is_user_logged_in())
+                                            <div class="comment--likes">
+                                                @button([
+                                                    'icon' => 'thumb_up',
+                                                    'size' => 'sm',
+                                                    'color' => '',
+                                                    'style' => 'basic',
+                                                    'type' => 'button',
+                                                    'classList' => ['comment--likes-icon', \Municipio\Comment\Likes::likeButton($answer->comment_ID)['classList']],
+                                                    'attributeList' => [
+                                                        'data-commentid' => $answer->comment_ID,
+                                                    ],
+                                                ])
+                                                @endbutton
+
+                                                <span class="c-typography c-typography__variant--meta" data-likes="{{\Municipio\Comment\Likes::likeButton($answer->comment_ID)['count']}}"
+                                                    id="comment-likes-{{$answer->comment_ID}}">
+                                                    {{\Municipio\Comment\Likes::likeButton($answer->comment_ID)['count']}}
+                                                </span>
+                                            </div>
+                                        @endif
+                                    @endslot
                                 @endcomment
                             </div>
                         @endforeach
