b6e3919642a2a4cb9680011b859b7cb431fe7b90
Sebastian Thulin <sebastian.thulin@helsingborg.se>
2022-06-21 14:01:29 +0200

    fix: Remove google web font link. Not used
    This was a gdpr/schrems issue.

--- library/App.php
+++ library/App.php
@@ -37,7 +37,6 @@ class App
         new \Municipio\Theme\FileUploads();
         new \Municipio\Theme\Archive();
         new \Municipio\Theme\CustomTemplates();
-        new \Municipio\Theme\Font();
         new \Municipio\Theme\Navigation();
 
         new \Municipio\Search\General();
