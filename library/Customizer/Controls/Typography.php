ccee073d4239cec444f8f37caf13e3ebe570b821
nRamstedt <nikolas.ramstedt@gmail.com>
2022-07-01 09:56:53 +0200

    mock parse data

--- library/Customizer/Controls/Typography.php
+++ library/Customizer/Controls/Typography.php
@@ -9,9 +9,24 @@ class Typography
         add_filter('kirki_' . \Municipio\Customizer::KIRKI_CONFIG . '_dynamic_css', array($this, 'fixCssVariants'));
     }
 
-    public function fixCssVariants($css) 
+    public function fixCssVariants($css)
     {
         return $css;
     }
 
+    public function getFontVariantCss()
+    {
+        return [
+            ':root' => [
+                [
+                    'property' =>  '--font-weight-base',
+                    'value' => 400
+                ],
+                [
+                    'property' =>  '--font-weight-heading',
+                    'value' => 600
+                ]
+            ]
+        ];
+    }
 }
