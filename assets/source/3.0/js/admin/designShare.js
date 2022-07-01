bf9287df25cf941321aa5aee606eccdd4e549273
Freya <752642+faejr@users.noreply.github.com>
2022-07-01 14:45:54 +0200

    fix: design wont update if value has uppercase hex

--- assets/source/3.0/js/admin/designShare.js
+++ assets/source/3.0/js/admin/designShare.js
@@ -1,39 +1,64 @@
+const valueIsHexString = (value) => {
+  return typeof value === 'string' && value.indexOf('#') === 0;
+};
+
+const valueIsRgbaString = (value) => {
+  return typeof value === 'string' && value.indexOf('rgba(') === 0;
+};
+
+const scrubHexValue = (value) => {
+  if (value) {
+    if (typeof value === 'object') {
+      for (const [valueKey, valueValue] of Object.entries(value)) {
+        if (valueIsHexString(valueValue)) {
+          value[valueKey] = valueValue.toLowerCase();
+        }
+      }
+    } else if (valueIsHexString(value)) {
+      value = value.toLowerCase();
+    }
+  }
+
+  return value;
+};
+
 export default (() => {
   wp.customize.bind('ready', function() {
     let customize = this;
     customize('load_design', function(selectedValue) {
       selectedValue.bind(function(value) {
-
-        let incompabileKeyStack = []; 
+        let incompatibleKeyStack = [];
 
         if (value.length != 32) {
-          throw 'The selected theme id is not valid'; 
+          throw 'The selected theme id is not valid';
         } else {
           fetch('https://customizer.helsingborg.io/id/' + value)
-          .then(response => response.json())
-          .then((data) => {
-            if(Object.keys(data.mods).length > 0) {
-              
-              for (const [key, value] of Object.entries(data.mods)) {
-                if(typeof customize.control(key) !== 'undefined') {
-                  customize.control(key).setting.set(value);
-                } else {
-                  incompabileKeyStack.push(key); 
+            .then((response) => response.json())
+            .then((data) => {
+              if (Object.keys(data.mods).length > 0) {
+                for (const [key, value] of Object.entries(data.mods)) {
+                  if (typeof customize.control(key) !== 'undefined') {
+                    const scrubbedValue = scrubHexValue(value);
+                    customize.control(key).setting.set(scrubbedValue);
+                  } else {
+                    incompatibleKeyStack.push(key);
+                  }
                 }
-              }
 
-              if(incompabileKeyStack.length != 0) {
-                throw 'The selected theme may be incompatible with this version of the theme customizer. Some settings ('  + incompabileKeyStack.join(", ") + ') may be missing.';
+                if (incompatibleKeyStack.length != 0) {
+                  throw 'The selected theme may be incompatible with this version of the theme customizer. Some settings (' +
+                    incompatibleKeyStack.join(', ') +
+                    ') may be missing.';
+                }
+              } else {
+                throw 'This theme seems to be empty, please select another one.';
               }
-
-            } else {
-              throw 'This theme seems to be empty, please select another one.';
-            }
-          }).catch(error => {
-            alert(error); 
-          });
-        } 
-      }); 
+            })
+            .catch((error) => {
+              alert(error);
+            });
+        }
+      });
     });
   });
 })();
