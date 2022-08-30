52ca5a4f7fcfec757a56756940af92829b55857b
NiclasNorin <niclas.norin@frontwalker.se>
2022-08-30 10:26:28 +0200

    moved drawer to component library

--- views/v3/partials/navigation/drawer.blade.php
+++ views/v3/partials/navigation/drawer.blade.php
@@ -1,49 +1,33 @@
 @if (!empty($mobileMenuItems))
-    <div class="c-drawer c-drawer--right c-drawer--primary js-drawer u-display--none@lg {{'c-drawer--' . (!empty($mobileMenuItems)&&!empty($mobileMenuSecondaryItems) ? 'duotone' : 'monotone') }}" js-toggle-class="is-open" js-toggle-item="js-drawer">
-        <div class="c-drawer__header">
+@drawer([
+    'classList' => ['c-drawer--' . (!empty($mobileMenuItems)&&!empty($mobileMenuSecondaryItems) ? 'duotone' : 'monotone')]
+])
 
-            @button([
-                'id' => 'mobile-menu-trigger-close',
-                'style' => 'basic',
-                'icon' => 'close',
-                'attributeList' => [
-                    'aria-controls' => 'navigation',
-                    'js-toggle-trigger' => 'js-drawer'
-                ],
-                'classList' => [
-                    'c-drawer__close',
-                    'u-display--none@lg'
-                ],
-                'size' => 'md',
-                'text' => $lang->close
-            ])
-            @endbutton
-
-            @includeWhen(
+    @slot('search')
+        @includeWhen(
                 $showMobileSearchDrawer,
                 'partials.search.mobile-search-form',
                 ['classList' => ['u-margin__top--2', 'u-width--100']]
             )
+    @endslot
 
-        </div>
-        <div class="c-drawer__body">
-            @if (!empty($mobileMenuItems)||!empty($mobileMenuSecondaryItems)) 
-                
-                {{-- Placed in another file, due to ajax loading --}}
-                @includeIf(
-                    'partials.navigation.mobile', 
-                    [
-                        'menuItems' => $mobileMenuItems, 
-                        'classList' => [
-                            'c-nav--drawer',
-                            'c-nav--dark',
-                            'site-nav-mobile__primary',
-                        ]
+    @if (!empty($mobileMenuItems)||!empty($mobileMenuSecondaryItems)) 
+    @slot('menu')
+        @includeIf(
+            'partials.navigation.mobile', 
+                [
+                    'menuItems' => $mobileMenuItems, 
+                    'classList' => [
+                        'c-nav--drawer',
+                        'c-nav--dark',
+                        'site-nav-mobile__primary',
                     ]
-                )
+                ]
+            )  
 
                 {{-- No ajax in wp-menus, thus not in its own file --}}
-                @nav([
+
+            @nav([
                     'classList' => [
                         'c-nav--drawer',
                         'c-nav--dark',
@@ -53,12 +37,11 @@
                     'direction' => 'vertical',
                     'includeToggle' => true
                 ])
-                @endnav
+            @endnav  
+    @endslot
+      @else
+      {{-- No menu items found --}}
+      @endif
 
-            @else
-                {{-- No menu items found --}}
-            @endif
-        </div>
-    </div>
-    <div class="drawer-overlay js-close-drawer u-display--none@lg" js-toggle-trigger="js-drawer"></div>
+@enddrawer  
 @endif
