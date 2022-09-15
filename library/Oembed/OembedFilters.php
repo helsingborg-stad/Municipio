f6af4b2be2beebe47e7bb0dcce643ae6a638f135
Anna <anna@thehappyhipster.se>
2022-09-15 14:11:56 +0200

    Only replace the iframe dom element, not the entire string

--- library/Oembed/OembedFilters.php
+++ library/Oembed/OembedFilters.php
@@ -6,32 +6,27 @@ class OembedFilters
 {
     public function __construct()
     {
-
-		add_filter('oembed_result', '\Municipio\Oembed\OembedFilters::oembed_result', 99, 2);
-
+        add_filter('oembed_result', '\Municipio\Oembed\OembedFilters::oembed_result', 99, 2);
     }
 
-	static public function oembed_result( $data, $url ) {	
+    public static function oembed_result($data, $url)
+    {
+        if (str_contains($data, '<iframe')) {
+            $dom = new \DOMDocument();
+            $dom->loadHTML($data, LIBXML_HTML_NOIMPLIED | LIBXML_HTML_NODEFDTD);
+            $xpath = new \DOMXPath($dom);
+            $iframe = $xpath->query("//iframe");
 
-		if ( str_contains( $data, '<iframe' ) ) {
+            if ((bool) $iframe) {
+                foreach ($iframe as $item) {
+                    $src         = $item->getAttribute('src');
+                    $item->setAttribute('src', 'about:blank');
+                    $item->setAttribute('data-src', $src);
+                }
+                $data = $dom->saveXML();
+            }
+        }
 
-			$dom = new \DOMDocument();
-			$dom->loadHTML($data, LIBXML_HTML_NOIMPLIED | LIBXML_HTML_NODEFDTD);
-			$xpath = new \DOMXPath($dom);
-
-			if( (bool) $xpath->query("//iframe") ) {
-
-				$src         = $xpath->query("//iframe")[0]->getAttribute('src');
-				$width       = $xpath->query("//iframe")[0]->getAttribute('width');
-				$height      = $xpath->query("//iframe")[0]->getAttribute('height');
-				$frameborder = $xpath->query("//iframe")[0]->getAttribute('frameborder');
-
-				$data = "<iframe src='about:blank' data-src='{$src}' width='{$width}' height='{$height}' frameborder='{$frameborder}'></iframe>";
-
-			}
-
-		}
-
-		return $data;
-	}
+        return $data;
+    }
 }
