72674005c90cdfeba9e9bf8c764d3b82a2545ff9
NiclasNorin <niclas.norin@frontwalker.se>
2022-09-28 11:01:12 +0200

    Added options to embed

--- library/Oembed/OembedFilters.php
+++ library/Oembed/OembedFilters.php
@@ -11,6 +11,20 @@ class OembedFilters
 
     public static function oembed_dataparse($output, $data, $url)
     {
+        $data->lang = (object) [
+            'knownLabels' => [
+                'title' => __('We need your consent to continue', 'modularity'),
+                'info' => sprintf(__('This part of the website shows content from %s. By continuing, <a href="%s"> you are accepting GDPR and privacy policy</a>.', 'modularity'), '{SUPPLIER_WEBSITE}', '{SUPPLIER_POLICY}'),
+                'button' => __('I understand, continue.', 'modularity'),
+            ],
+
+            'unknownLabels' => [
+                'title' => __('We need your consent to continue', 'modularity'),
+                'info' => __('This part of the website shows content from another website. By continuing, you are accepting GDPR and privacy policy.', 'modularity'),
+                'button' => __('I understand, continue.', 'modularity'),
+            ],
+        ];
+        
         $output = render_blade_view('partials.iframe', ['data' => $data, 'src' => $url]);
 
         return $output;
--- views/v3/partials/iframe.blade.php
+++ views/v3/partials/iframe.blade.php
@@ -4,5 +4,6 @@
 	'width' => $data->width,
 	'title' =>  $data->title,
     'classList' => ['js-suppressed-iframe'],
+	'options' =>json_encode($data->lang),
 ])
 @endiframe
