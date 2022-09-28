17d65c4f6879750e4242f4ec142adb3c4b070420
Anna Johansson <anna@thehappyhipster.se>
2022-09-28 07:20:34 +0200

    Add .js-suppressed-iframe

--- library/Oembed/OembedFilters.php
+++ library/Oembed/OembedFilters.php
@@ -9,22 +9,29 @@ class OembedFilters
         add_filter('oembed_result', '\Municipio\Oembed\OembedFilters::oembed_result', 99, 2);
     }
 
-    public static function oembed_result($data, $url)
+    public static function oembed_result($html, $data)
     {
-        if (str_contains($data, '<iframe')) {
+        $output = '';
+
+        if (str_contains($html, '<iframe')) {
             $doc = new \DOMDocument();
-            $doc->loadHTML($data);
+            $doc->loadHTML($html);
 
             $iframes = $doc->getElementsByTagName('iframe');
 
             foreach ($iframes as $iframe) {
                 $src = $iframe->getAttribute('src');
+                $iframe->setAttribute('src', 'about:blank');
                 $iframe->setAttribute('data-src', $src);
+
+                $classes = $iframe->getAttribute('class');
+                $classes .= ' js-suppressed-iframe';
+                $iframe->setAttribute('class', $classes);
             }
 
-            $data = $doc->saveHTML();
+            $output = $doc->saveHTML();
         }
 
-        return $data;
+        return $output;
     }
 }
