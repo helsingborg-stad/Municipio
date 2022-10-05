85ce7f14b38d47fb47c89a9caebba6d6bd751cc8
Sebastian Thulin <sebastian.thulin@helsingborg.se>
2022-10-05 11:43:58 +0200

    Fixes HTML5 theme support

--- library/Theme/Support.php
+++ library/Theme/Support.php
@@ -114,7 +114,16 @@ class Support
         add_theme_support('editor-styles');
         add_theme_support('menus');
         add_theme_support('post-thumbnails');
-        add_theme_support('html5');
+        add_theme_support('html5', array(
+            'comment-list',
+            'comment-form',
+            'search-form',
+            'gallery',
+            'caption',
+            'style',
+            'script'
+        ));
+
         add_theme_support(
             'post-formats',
             array(
