d9f566698c056c111b7922dd3c2484255feb00cc
Nikolas Ramstedt <nikolas.ramstedt@helsingborg.se>
2022-10-06 12:40:15 +0200

    fix: check $signature

--- views/v3/partials/signature.blade.php
+++ views/v3/partials/signature.blade.php
@@ -1,24 +1,26 @@
 <!-- Signature -->
-@if($postTypeDetails->hierarchical)
-  @signature([
-      'author' => $signature->name, 
-      'published' => $signature->published,
-      'updated' => $signature->updated,
-      'avatar_size' => 'sm',
-      'avatar' => $signature->avatar,
-      'authorRole' => $signature->role,
-      'link' => $signature->link,
-      'updatedLabel' => $publishTranslations->updated,
-      'publishedLabel' => $publishTranslations->publish,
-      'classList' => $classList
-  ])
-  @endsignature
-@elseif(!$postTypeDetails->hierarchical && $postType == 'post')
-  @signature([
-      'published' => $signature->publish,
-      'updated' => $signature->updated,
-      'updatedLabel' => $publishTranslations->updated,
-      'publishedLabel' => $publishTranslations->publish,
-  ])
-  @endsignature
+@if (!empty($signature))
+    @if ($postTypeDetails->hierarchical)
+        @signature([
+            'author' => $signature->name,
+            'published' => $signature->published,
+            'updated' => $signature->updated,
+            'avatar_size' => 'sm',
+            'avatar' => $signature->avatar,
+            'authorRole' => $signature->role,
+            'link' => $signature->link,
+            'updatedLabel' => $publishTranslations->updated,
+            'publishedLabel' => $publishTranslations->publish,
+            'classList' => $classList
+        ])
+        @endsignature
+    @elseif(!$postTypeDetails->hierarchical && $postType == 'post')
+        @signature([
+            'published' => $signature->publish,
+            'updated' => $signature->updated,
+            'updatedLabel' => $publishTranslations->updated,
+            'publishedLabel' => $publishTranslations->publish
+        ])
+        @endsignature
+    @endif
 @endif
