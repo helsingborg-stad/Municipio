f5b4fe103fa5cfe9ae2f2f5e3ccafd8aac4f4bf3
NiclasNorin <niclas.norin@frontwalker.se>
2022-10-20 17:14:20 +0200

    Removed class
    This damn line created so many bugs :D

--- views/v3/partials/iframe.blade.php
+++ views/v3/partials/iframe.blade.php
@@ -3,7 +3,6 @@
 	'height' => $data->height,
 	'width' => $data->width,
 	'title' =>  $data->title,
-    'classList' => ['js-suppressed-iframe'],
 	'options' =>json_encode($data->lang),
 	'modifier' => '--video'
 ])
