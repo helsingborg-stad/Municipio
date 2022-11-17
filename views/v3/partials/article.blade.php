3a5ddcc9fb3d8f7aec3adbac5e000d3d32c9fa4f
Anna Johansson <anna@thehappyhipster.se>
2022-11-17 10:42:17 +0100

    Added post_language to the extended $post variable and print it on the <article> element if it exists

--- views/v3/partials/article.blade.php
+++ views/v3/partials/article.blade.php
@@ -1,4 +1,8 @@
-<article id="article" class="c-article c-article--readable-width s-article u-clearfix">
+<article id="article" class="c-article c-article--readable-width s-article u-clearfix" 
+@if($postLanguage)
+	lang="{{ $postLanguage }}"
+@endif
+>
     
     <!-- Title -->
     @section('article.title.before')@show
