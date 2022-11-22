bbf9f3f310ed245649bb0468f75bcb3ff1129713
Anna Johansson <anna@thehappyhipster.se>
2022-11-22 16:35:07 +0100

    partial blade view for serving image component via render_blade_view

---
+++ views/v3/partials/image.blade.php
@@ -0,0 +1,11 @@
+@image([
+    'src'=> $src,
+    'alt' => $alt,
+    'caption' => $caption,
+    'openModal' => $openModal,
+    'srcset' => $srcset,
+    'width' => $width,
+    'height' => $height,
+    'imageClass' => $imageClass
+])
+@endimage
