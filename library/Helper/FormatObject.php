83edab98ef6cb12adc49ad4d509751a2f219fe99
Anna Johansson <anna@thehappyhipster.se>
2022-12-01 09:56:29 +0100

    Set utf-8 encoding

--- library/Helper/FormatObject.php
+++ library/Helper/FormatObject.php
@@ -6,12 +6,11 @@ use DOMDocument;
 
 class FormatObject
 {
-
     /**
-     * Camelcase snake_case object or string 
-     * 
+     * Camelcase snake_case object or string
+     *
      * @param   object|array|string   $object The item, snake case
-     * 
+     *
      * @return  object|array|string   $object The item, camel case
      */
     public static function camelCase($item)
@@ -19,35 +18,35 @@ class FormatObject
         switch ($item) {
             case is_array($item):
             case is_object($item):
-                return self::camelCaseObject($item); 
+                return self::camelCaseObject($item);
                 break;
             case is_string($item):
-                return self::camelCaseString($item); 
+                return self::camelCaseString($item);
                 break;
             default:
-               throw new \Exception("Input is not a array, object or string. Cannot camelCase value.");
+                throw new \Exception("Input is not a array, object or string. Cannot camelCase value.");
         }
     }
 
     /**
      * Camelcase snake_case object or array
-     * 
+     *
      * @param   object|array   $object The object, snake case
-     * 
+     *
      * @return  object|array   $object The object, camel case
      */
     public static function camelCaseObject($object)
     {
-        return (object) self::mapArrayKeys(function($string) {
+        return (object) self::mapArrayKeys(function ($string) {
             return lcfirst(implode('', array_map('ucfirst', explode('_', str_replace('-', '_', strtolower($string))))));
         }, (array) $object);
     }
 
     /**
-     * Camelcase snake_case string 
-     * 
+     * Camelcase snake_case string
+     *
      * @param   string   $string The string, snake case
-     * 
+     *
      * @return  string   $string The string, camel case
      */
     public static function camelCaseString($string)
@@ -57,16 +56,17 @@ class FormatObject
 
     /**
      * Replaces old keys with new (recursivley)
-     * 
+     *
      * @param   function    $func    Function for transformation of key
      * @param   array       $array   The array to filter
-     * 
+     *
      * @return  array       $return  The array with renamed keys
      */
-    public static function mapArrayKeys(callable $func, array $array) {
+    public static function mapArrayKeys(callable $func, array $array)
+    {
         $return = array();
         foreach ($array as $key => $value) {
-          $return[$func($key)] = is_array($value) ? self::mapArrayKeys($func, $value) : $value;
+            $return[$func($key)] = is_array($value) ? self::mapArrayKeys($func, $value) : $value;
         }
         return $return;
     }
@@ -74,16 +74,16 @@ class FormatObject
     /**
      * It takes a string of HTML, creates a new DOMDocument, loads the HTML into the DOMDocument, and
      * then imports the root node of the DOMDocument into the DOMDocument that was passed in
-     * 
+     *
      * @param DOMDocument doc The DOMDocument object that you want to add the node to.
      * @param string str The string to be converted to a DOMNode
-     * 
+     *
      * @return A DOMNode object.
      */
-    public static function createNodeFromString( DOMDocument $doc, string $str)
+    public static function createNodeFromString(DOMDocument $doc, string $str)
     {
         $d = new \DOMDocument();
-        $d->loadHTML($str);
+        $d->loadHTML('<?xml encoding="utf-8" ?>' . $str);
         return $doc->importNode($d->documentElement, true);
     }
 }
