2739ca5d03f2f852559401f0c0dc497c52dc472b
Anna Johansson <anna@thehappyhipster.se>
2022-12-21 15:27:05 +0100

    Controllers and views for purpose "project"

---
+++ templates/project/ArchiveProject.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Municipio\Controller;
+
+/**
+ * Class ArchiveProject
+ *
+ * @package Municipio\Controller
+ */
+class ArchiveProject extends \Municipio\Controller\Archive
+{
+}
---
+++ templates/project/SingularProject.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Municipio\Controller;
+
+/**
+ * Class SingularProject
+ * @package Municipio\Controller
+ */
+class SingularProject extends \Municipio\Controller\Singular
+{
+    public function __construct() {
+        parent::init();
+    }
+}
