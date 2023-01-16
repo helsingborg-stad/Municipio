e5e134f9f1fffd30f4cd5b0245b16e2d8f4a0755
Thor Brink <thor.brink@helsingborg.se>
2023-01-16 07:17:00 +0100

    Refactor Customizer Logo field

--- library/Customizer/Sections/Logo.php
+++ library/Customizer/Sections/Logo.php
@@ -2,6 +2,7 @@
 
 namespace Municipio\Customizer\Sections;
 use Kirki\Compatibility\Kirki;
+use Kirki\Field\Image as ImageField;
 
 class Logo
 {
@@ -9,33 +10,36 @@ class Logo
 
     public function __construct($panelID)
     {
-        Kirki::add_section(self::SECTION_ID, array(
-            'title'       => esc_html__('Logo', 'municipio'),
-            'description' => esc_html__('Logo settings.', 'municipio'),
-            'panel'          => $panelID,
-            'priority'       => 160,
-        ));
 
-        $primaryLogoField = new \Kirki\Field\Image([
-          'settings' => 'primary_logo',
-          'label'       => esc_html__('Primary logo', 'municipio'),
-          'section'     => self::SECTION_ID,
-        ]);
-
-        $secondaryLogoField = new \Kirki\Field\Image([
-          'settings' => 'secondary_logo',
-          'label'       => esc_html__('Secondary logo', 'municipio'),
-          'section'     => self::SECTION_ID,
-        ]);
-
-        $emblemField = new \Kirki\Field\Image([
-          'settings' => 'emblem',
-          'label'       => esc_html__('Emblem', 'municipio'),
-          'section'     => self::SECTION_ID,
-        ]);
-
+        $primaryLogoField = $this->getImageField(self::SECTION_ID, 'logotype', esc_html__('Primary logo', 'municipio'), esc_html__('Only accepts .svg-files (Scalable Vector Graphics).', 'municipio'));
+        $secondaryLogoField = $this->getImageField(self::SECTION_ID, 'logotype_negative', esc_html__('Secondary logo', 'municipio'), esc_html__('Upload your secondary logotype in .svg format (Scalable Vector Graphics). The secondary logotype is usually 100% white and can be used on dark or colored backgrounds.', 'municipio'));
+        $emblemField = $this->getImageField(self::SECTION_ID, 'logotype_emblem', esc_html__('Emblem', 'municipio'), esc_html__('Upload an emblem in .svg format (Scalable Vector Graphics). The emblem will be used to strengthen the website brand, when a sub brand is used.', 'municipio'), 'url');
+      
+        $this->addSection($panelID);
         Kirki::add_field($primaryLogoField);
         Kirki::add_field($secondaryLogoField);
         Kirki::add_field($emblemField);
+    }
+
+    private function addSection(string $panelID) {
+      Kirki::add_section(self::SECTION_ID, array(
+        'title'         => esc_html__('Logo', 'municipio'),
+        'description'   => esc_html__('Logo settings.', 'municipio'),
+        'panel'         => $panelID,
+        'priority'      => 160,
+      ));
+    }
+
+    private function getImageField(string $sectionID, string $setting, string $label, string $description, string $saveAs = 'array'):ImageField {
+      $sanitizedSaveAs = $saveAs === 'url' ?? 'array';
+      return new ImageField([
+          'settings'      => $setting,
+          'label'         => $label,
+          'description'   => $description,
+          'section'       => $sectionID,
+          'choices'       => [
+            'save_as' => $sanitizedSaveAs,
+          ],
+      ]);
     }
 }
