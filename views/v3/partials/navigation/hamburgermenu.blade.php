47fc01901469ac27e60822ebb5bca576ea3759e5
Thor Brink <thor.brink@helsingborg.se>
2023-01-18 12:51:47 +0000

    Fix: Hamburger menu setting not applied

--- views/v3/partials/navigation/hamburgermenu.blade.php
+++ views/v3/partials/navigation/hamburgermenu.blade.php
@@ -10,30 +10,32 @@
             ]
         ])
             <div class="o-grid-12">
-                @form(['method' => 'get', 'action' => '/'])
-                    <label for="hamburger-menu-search" class="u-sr__only">
-                        {{ $lang->searchOn . " " . $siteName }}
-                    </label>
-                    @group([])
-                        @field([
-                            'id' => 'hamburger-menu-search',
-                            'type' => 'search',
-                            'name' => 's',
-                            'placeholder' => $lang->searchOn . " " . $siteName,
-                            'required' => true,
-                            'attributeList' => [
-                                'aria-label' => $lang->searchOn . " " . $siteName
-                            ],
-                        ])
-                        @endfield
-                        @button([
-                            'color' => 'primary',
-                            'type' => 'submit',
-                            'text' => $lang->search
-                        ])
-                        @endbutton
-                    @endgroup
-                @endform
+                @if($showHamburgerMenuSearch)
+                    @form(['method' => 'get', 'action' => '/'])
+                        <label for="hamburger-menu-search" class="u-sr__only">
+                            {{ $lang->searchOn . " " . $siteName }}
+                        </label>
+                        @group([])
+                            @field([
+                                'id' => 'hamburger-menu-search',
+                                'type' => 'search',
+                                'name' => 's',
+                                'placeholder' => $lang->searchOn . " " . $siteName,
+                                'required' => true,
+                                'attributeList' => [
+                                    'aria-label' => $lang->searchOn . " " . $siteName
+                                ],
+                            ])
+                            @endfield
+                            @button([
+                                'color' => 'primary',
+                                'type' => 'submit',
+                                'text' => $lang->search
+                            ])
+                            @endbutton
+                        @endgroup
+                    @endform
+                @endif
             </div>
         @endhamburgerMenu
 @endif
