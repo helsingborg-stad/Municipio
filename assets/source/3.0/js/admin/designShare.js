3cd167ba6dfcbd5ad8912d73e5271ce0671c88c1
Anna Johansson <anna@thehappyhipster.se>
2023-01-19 16:22:34 +0100

    Update custom css on design sync

--- assets/source/3.0/js/admin/designShare.js
+++ assets/source/3.0/js/admin/designShare.js
@@ -1,63 +1,70 @@
 const valueIsHexString = value => {
-  return typeof value === 'string' && value.indexOf('#') === 0;
+    return typeof value === 'string' && value.indexOf('#') === 0;
 };
 
 const scrubHexValue = value => {
-  if (value) {
-    if (typeof value === 'object') {
-      for (const [valueKey, valueValue] of Object.entries(value)) {
-        if (valueIsHexString(valueValue)) {
-          value[valueKey] = valueValue.toLowerCase();
+    if (value) {
+        if (typeof value === 'object') {
+            for (const [valueKey, valueValue] of Object.entries(value)) {
+                if (valueIsHexString(valueValue)) {
+                    value[valueKey] = valueValue.toLowerCase();
+                }
+            }
+        } else if (valueIsHexString(value)) {
+            value = value.toLowerCase();
         }
-      }
-    } else if (valueIsHexString(value)) {
-      value = value.toLowerCase();
     }
-  }
 
-  return value;
+    return value;
 };
 
 export default (() => {
-  wp.customize.bind('ready', function() {
-    let customize = this;
-    customize('load_design', function(selectedValue) {
-      selectedValue.bind(function(value) {
-        let incompatibleKeyStack = [];
+    wp.customize.bind('ready', function () {
+        let customize = this;
+        customize('load_design', function (selectedValue) {
+            selectedValue.bind(function (value) {
+                let incompatibleKeyStack = [];
 
-        if (value.length != 32) {
-          throw 'The selected theme id is not valid';
-        } else {
-          fetch('https://customizer.helsingborg.io/id/' + value)
-            .then(response => response.json())
-            .then(data => {
-              if (Object.keys(data.mods).length > 0) {
-                for (const [key, value] of Object.entries(data.mods)) {
-                  const control = customize.control(key);
-                  if (typeof control !== 'undefined') {
-                    const scrubbedValue = scrubHexValue(value);
-                    control.setting.set(scrubbedValue);
-                  } else {
-                    if(!key.startsWith('archive_')) {
-                      incompatibleKeyStack.push(key);
-                    }
-                  }
-                }
+                if (value.length != 32) {
+                    throw 'The selected theme id is not valid';
+                } else {
+                    fetch('https://customizer.helsingborg.io/id/' + value)
+                        .then(response => response.json())
+                        .then(data => {
 
-                if (incompatibleKeyStack.length > 0) {
-                  throw 'The selected theme may be incompatible with this version of the theme customizer. Some settings (' +
-                    incompatibleKeyStack.join(', ') +
-                    ') may be missing.';
+                            const css_control = customize.control('custom_css');
+                            const css = data.css ? data.css : '';
+
+                            css_control.setting.set(css);
+
+                            if (Object.keys(data.mods).length > 0) {
+                                for (const [key, value] of Object.entries(data.mods)) {
+                                    const control = customize.control(key);
+
+                                    if (typeof control !== 'undefined') {
+                                        const scrubbedValue = scrubHexValue(value);
+                                        control.setting.set(scrubbedValue);
+                                    } else {
+                                        if (!key.startsWith('archive_')) {
+                                            incompatibleKeyStack.push(key);
+                                        }
+                                    }
+                                }
+
+                                if (incompatibleKeyStack.length > 0) {
+                                    // throw 'The selected theme may be incompatible with this version of the theme customizer. Some settings (' +
+                                    // incompatibleKeyStack.join(', ') +
+                                    // ') may be missing.';
+                                }
+                            } else {
+                                throw 'This theme seems to be empty, please select another one.';
+                            }
+                        })
+                        .catch(error => {
+                            alert(error);
+                        });
                 }
-              } else {
-                throw 'This theme seems to be empty, please select another one.';
-              }
-            })
-            .catch(error => {
-              alert(error);
             });
-        }
-      });
+        });
     });
-  });
 })();
