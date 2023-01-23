512e429ac3825c882b422d1b57fd6e62330fdaa5
Anna <anna@thehappyhipster.se>
2023-01-23 13:49:33 +0100

    Feature/custom typsnitt fljer inte med i design share filen kopieras inte ver cu 865bg8cmq (#431)
    * Fixed a typo
    
    * If $mod contains font-family, add a reference to it in the stack under custom_fonts
    
    * Removed print_r
    
    * If font file exists, add the url to it
    to custom_fonts
    
    * Removed unnecessary comment
    
    * Changed nesting
    
    * Shortened row length
    Removed whitespaces
    
    * Added     private function getUploadedFontUrl()
    Changed   get_row to get_col
    Added     LIMIT 1 in sql
    
    * See https://sonarcloud.io/project/issues?open=AYXQJrXxeyp3DVxqKynV&id=helsingborg-stad_Municipio
    
    * Removed whitespace

---
+++ library/Helper/File.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Municipio\Helper;
+
+class File
+{
+    /**
+     * It takes a filename and returns the URL of the file if it exists in the media library
+     *
+     * @param string filename The name of the file you want to get the URL for.
+     *
+     * @return ?string The URL of the file.
+     */
+    public static function getFileUrl(string $filename): ?string
+    {
+        global $wpdb;
+        $meta = $wpdb->get_col(
+            $wpdb->prepare(
+                "SELECT post_id FROM `$wpdb->postmeta`
+                WHERE `meta_key` LIKE '_wp_attached_file'
+                AND `meta_value` LIKE '%$filename%'
+                LIMIT 1"
+            )
+        );
+        if (!empty($meta)) {
+            return \wp_get_attachment_url($meta[0]);
+        }
+        return null;
+    }
+}
