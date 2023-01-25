a11ff9b7948ca3012bd15691c851509201921875
Thor Brink <thor.brink@helsingborg.se>
2023-01-25 10:24:19 +0000

    Refactor for loop

--- assets/source/3.0/js/utils/replaceRemoteFilesWithLocalInString.ts
+++ assets/source/3.0/js/utils/replaceRemoteFilesWithLocalInString.ts
@@ -4,17 +4,18 @@ import { findUrlsInString } from "./findUrlsInString";
 export async function replaceRemoteFilesWithLocalInString(css: string, origin?: string): Promise<string> {
     const urls = findUrlsInString(css, origin);
 
-    if (urls.length < 1)
+    if (urls.length < 1) {
         return css;
+    }
 
-    for (let index = 0; index < urls.length; index++) {
-        const url = urls[index];
+    for (const url of urls) {
         const migratedUrl = await migrateRemoteMediaFile(url);
 
-        if (migratedUrl === null)
+        if (migratedUrl === null) {
             continue;
+        }
 
-        css = css.replace(url, migratedUrl);
+        css = css.replace(url, migratedUrl);   
     }
 
     return css;
