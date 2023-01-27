51e170e6c068b1fb3e89f225a3ec85dc23c58d46
Anna Johansson <anna@thehappyhipster.se>
2023-01-27 13:00:27 +0100

    Only attempt sideload if response is a 200

--- assets/source/3.0/js/restApi/newEndpoint.ts
+++ assets/source/3.0/js/restApi/newEndpoint.ts
@@ -1,36 +1,39 @@
 export enum NameSpace {
-    MUNICIPIO_V1 = 'municipio/v1'
+    MUNICIPIO_V1 = 'municipio/v1',
 }
 
-type RequestMethod = 'GET'|'POST'|'PUT'|'PATCH'|'DELETE'
+type RequestMethod = 'GET' | 'POST' | 'PUT' | 'PATCH' | 'DELETE';
 
 interface EndpointOptions {
-    nameSpace:NameSpace,
-    route:string, 
-    method:RequestMethod
+    nameSpace: NameSpace;
+    route: string;
+    method: RequestMethod;
 }
 
-export const newEndpoint = <T, A>(options:EndpointOptions) => ({
-    call: (callArgs?:A):Promise<T> => {
-        return fetch(
-            `/wp-json/${options.nameSpace}/${options.route}`,
-            {
-                method: options.method,
-                body: JSON.stringify(callArgs),
-                headers: {
-                    'Content-Type': 'application/json',
-                    'X-WP-Nonce': wpApiSettings.nonce ?? ''
-                },
-            }
-            ).then(response => {
-                const contentType = response.headers.get("content-type");
-                const contentTypeIsJson = (contentType && contentType.indexOf("application/json") !== -1)
-                
-                if(contentTypeIsJson) {
-                    return response.json()
+export const newEndpoint = <T, A>(options: EndpointOptions) => ({
+    call: (callArgs?: A): Promise<T> => {
+        return fetch(`/wp-json/${options.nameSpace}/${options.route}`, {
+            method: options.method,
+            body: JSON.stringify(callArgs),
+            headers: {
+                'Content-Type': 'application/json',
+                'X-WP-Nonce': wpApiSettings.nonce ?? '',
+            },
+        }).then((response) => {
+            if (response.ok) {
+                return response.json();
+            } else if (200 == response.status) {
+                const contentType = response.headers.get('content-type');
+                const contentTypeIsJson = contentType && contentType.indexOf('application/json') !== -1;
+
+                if (contentTypeIsJson) {
+                    return response.json();
                 }
 
-                return response.text()
-            })
-    }
-})
+                return response.text();
+            } else {
+                return response.statusText;
+            }
+        });
+    },
+});
