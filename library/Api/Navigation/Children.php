c25d7b971a3c2a57b0a81c6121a59bdb9cfd0a54
Thor Brink <thor.brink@helsingborg.se>
2023-01-27 08:34:23 +0100

    Fix hamburger submenu items not expanding (#447)

--- library/Api/Navigation/Children.php
+++ library/Api/Navigation/Children.php
@@ -13,7 +13,6 @@ class Children extends RestApiEndpoint
 
     public function handleRegisterRestRoute(): bool
     {
-        add_filter('Municipio/Navigation/Item', array($this, 'appendFetchUrl'), 10, 2);
         return register_rest_route(self::NAMESPACE, self::ROUTE, array(
             'methods' => 'GET',
             'callback' => array($this, 'handleRequest'),
@@ -35,61 +34,5 @@ class Children extends RestApiEndpoint
         }
 
         return rest_ensure_response([]);
-    }
-
-    public function appendFetchUrl($item, $identifier)
-    {
-        $targetMenuIdentifiers = ['mobile', 'sidebar'];
-
-        if (!in_array($identifier, $targetMenuIdentifiers)) {
-            return $item;
-        }
-
-        if (isset($item['id']) && is_numeric($item['id'])) {
-            $depth = $this->getPageDepth($item['id']) + 1;
-        } else {
-            $depth = 0;
-        }
-
-        $dataFetchUrl = apply_filters(
-            'Municipio/homeUrl',
-            esc_url(get_home_url())
-        )   . '/wp-json/municipio/v1/navigation/children/render'
-            . '?' . 'pageId=' .  $item['id'] . '&viewPath=' . 'partials.navigation.'
-            . $identifier . '&identifier=' . $identifier . '&depth=' . $depth;
-
-        $item['attributeList'] = array(
-            'data-fetch-url' => $dataFetchUrl
-        );
-
-        return $item;
-    }
-
-    /**
-     * Get depth of page
-     *
-     * @param int $postId
-
-     * @return int The depth of the page
-     */
-    private function getPageDepth($postId, $depth = 0)
-    {
-        $object = get_post($postId);
-
-        //Not found, fake 0
-        if (!is_a($object, 'WP_Post')) {
-            return 0;
-        }
-
-        //Set post parent
-        $parentId = $object->post_parent;
-
-        //Get depth
-        while ($parentId > 0) {
-            $page = get_post($parentId);
-            $parentId = $page->post_parent;
-            $depth++;
-        }
-        return $depth;
     }
 }
--- library/Theme/Navigation.php
+++ library/Theme/Navigation.php
@@ -15,6 +15,7 @@ class Navigation
     public function __construct()
     {
         add_action('init', array($this, 'registerNavigationMenus'), 15, 2);
+        add_filter('Municipio/Navigation/Item', array($this, 'appendFetchUrl'), 10, 2);
     }
 
     public static function getMenuLocations() {
@@ -74,5 +75,61 @@ class Navigation
         }
 
         return $archiveMenu;
+    }
+
+    public function appendFetchUrl($item, $identifier)
+    {
+        $targetMenuIdentifiers = ['mobile', 'sidebar'];
+
+        if (!in_array($identifier, $targetMenuIdentifiers)) {
+            return $item;
+        }
+
+        if (isset($item['id']) && is_numeric($item['id'])) {
+            $depth = $this->getPageDepth($item['id']) + 1;
+        } else {
+            $depth = 0;
+        }
+
+        $dataFetchUrl = apply_filters(
+            'Municipio/homeUrl',
+            esc_url(get_home_url())
+        )   . '/wp-json/municipio/v1/navigation/children/render'
+            . '?' . 'pageId=' .  $item['id'] . '&viewPath=' . 'partials.navigation.'
+            . $identifier . '&identifier=' . $identifier . '&depth=' . $depth;
+
+        $item['attributeList'] = array(
+            'data-fetch-url' => $dataFetchUrl
+        );
+
+        return $item;
+    }
+
+    /**
+     * Get depth of page
+     *
+     * @param int $postId
+
+     * @return int The depth of the page
+     */
+    private function getPageDepth($postId, $depth = 0)
+    {
+        $object = get_post($postId);
+
+        //Not found, fake 0
+        if (!is_a($object, 'WP_Post')) {
+            return 0;
+        }
+
+        //Set post parent
+        $parentId = $object->post_parent;
+
+        //Get depth
+        while ($parentId > 0) {
+            $page = get_post($parentId);
+            $parentId = $page->post_parent;
+            $depth++;
+        }
+        return $depth;
     }
 }
