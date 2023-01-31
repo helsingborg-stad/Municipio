52101e204b306029f193e9190f1aaa54066697e2
Anna Johansson <anna@thehappyhipster.se>
2023-01-31 11:23:00 +0100

    WIP Temp fix to Purpose::getPurposes

--- library/Helper/Purpose.php
+++ library/Helper/Purpose.php
@@ -5,30 +5,33 @@ namespace Municipio\Helper;
 class Purpose
 {
     /**
-     * Return an array containing key and label of all the purpose classes available in the templates folder.
+     * ! WIP
+     * ! This method will need to be rebuilt when purposes are moved to library/Controller/Purpose
+     * Return an array containing key and label of all the purpose classes available in the registered controller directories.
      *
-     * @return array An array of all the classes in the templates folder.
+     * @return array An array of all the classes .
      */
     public static function getPurposes(): array
     {
-
         $purposes    = [];
+        foreach (\Municipio\Helper\Controller::getControllerPaths() as $path) {
+            if (is_dir($purposePath = $path . DIRECTORY_SEPARATOR . 'purpose')) {
+                $recurse = new \RecursiveIteratorIterator(new \RecursiveDirectoryIterator($purposePath));
+                $recurse->setMaxDepth(1);
 
-        $purposePath = MUNICIPIO_PATH . 'templates';
-        $recurse     = new \RecursiveIteratorIterator(new \RecursiveDirectoryIterator($purposePath));
-        $recurse->setMaxDepth(1);
+                foreach ($recurse as $item) {
+                    if (1 !== $recurse->getDepth() || $recurse->isDot() || !$item->isFile()) {
+                        continue;
+                    }
 
-        foreach ($recurse as $item) {
-            if (1 !== $recurse->getDepth() || $recurse->isDot() || !$item->isFile()) {
-                continue;
-            }
-
-            $className = '\Municipio\Controller\\' . pathinfo($item->getFilename(), PATHINFO_FILENAME);
-            if (!class_exists($className)) {
-                require_once $item->getPathname();
-            }
-            if ('singular' === $className::getType()) {
-                $purposes[$className::getKey()] = $className::getLabel();
+                    $className = '\Municipio\Controller\\' . pathinfo($item->getFilename(), PATHINFO_FILENAME);
+                    if (!class_exists($className)) {
+                        require_once $item->getPathname();
+                    }
+                    if ('singular' === $className::getType()) {
+                        $purposes[$className::getKey()] = $className::getLabel();
+                    }
+                }
             }
         }
         return $purposes;
