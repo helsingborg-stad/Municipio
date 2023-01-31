c5c866c2b796756b15908b4e9114d5b5a6a13aa5
Thor Brink <thor.brink@helsingborg.se>
2023-01-31 13:01:22 +0000

    Add documentation to Municipio\Api\View

--- library/Api/View/Render.php
+++ library/Api/View/Render.php
@@ -7,14 +7,21 @@ use stdClass;
 use WP_Error;
 use WP_Http;
 use WP_REST_Request;
-use WP_REST_Response;
 use WP_REST_Server;
 
+/**
+ * Registers and handles REST route view/render/{view}.
+ * Allows rendering blade templates via the REST api.
+ */
 class Render extends RestApiEndpoint
 {
     private const NAMESPACE = 'municipio/v1';
     private const ROUTE = 'view/render/(?P<view>[a-z|.|\/]+)'; // Letters, dots and frontslashes are allowed.
 
+    /**
+     * Register the REST route
+     * @return bool
+     */
     public function handleRegisterRestRoute(): bool
     {
         return register_rest_route(self::NAMESPACE, self::ROUTE, array(
@@ -36,26 +43,30 @@ class Render extends RestApiEndpoint
         ));
     }
 
+    /**
+     * Handle the REST request.
+     * Returns the html for the requested blade view on success, WP_Error on fail.
+     * @param WP_REST_Request $request
+     * @return WP_REST_Response|WP_Error
+     */
     public function handleRequest(WP_REST_Request $request)
     {
         $params = $request->get_params('GET');
 
         try {
-            $markup = $this->render($params['view'], (array)$params['data'] ?? []);
+            $markup = render_blade_view($params['view'], (array)$params['data'] ?? [], false, false);
+            return rest_ensure_response($markup);
         } catch (\Throwable $th) {
             $error = new WP_Error();
             $error->add(null, $th->getMessage(), ['status' => WP_Http::BAD_REQUEST]);
             return rest_ensure_response($error);
         }
-
-        return rest_ensure_response($markup);
     }
 
-    public function render($view, $data)
-    {
-        return render_blade_view($view, $data, false, false);
-    }
-
+    /**
+     * Callback for checking permissions for the REST route
+     * @return bool
+     */
     public function permissionCallback(): bool
     {
         return true;
