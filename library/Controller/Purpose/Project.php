04750bbc520b74f4d5ec1dc706c8eef4f02d3b78
Anna Johansson <anna@thehappyhipster.se>
2023-02-06 14:35:41 +0100

    Appends the structured data array for purpose "Project"

--- library/Controller/Purpose/Project.php
+++ library/Controller/Purpose/Project.php
@@ -15,6 +15,9 @@ class Project extends PurposeFactory
     {
         $this->label = __('Project', 'municipio');
         $this->key = 'project';
+
+        // Append structured data
+        add_filter('Municipio/StructuredData', array($this, 'appendStructuredData'), 10, 3);
     }
     public function getLabel(): string
     {
@@ -23,5 +26,91 @@ class Project extends PurposeFactory
     public function getKey(): string
     {
         return $this->key;
+    }
+    /**
+     * Appends the structured data array (used for schema.org markup) with additional data
+     *
+     * @param array structuredData The structured data array that we're going to append to.
+     * @param string postType The post type of the current page.
+     * @param int postId The ID of the post you want to add structured data to.
+     *
+     * @return array The modified structured data array.
+     */
+    public function appendStructuredData(array $structuredData, string $postType, int $postId): array
+    {
+
+        if (empty($postId)) {
+            return $structuredData;
+        }
+
+        $additionalData = [];
+
+        if ('project' === $postType) {
+            $founder     = get_the_terms($postId, 'organisation');
+            $brands      = get_the_terms($postId, 'participants');
+            $departments = get_the_terms($postId, 'operation');
+            $sponsors    = get_the_terms($postId, 'partner');
+            $location    = (array) get_post_meta($postId, 'map', true);
+        } else {
+            $founder     = (array) get_post_meta($postId, 'founder');
+            $brands      = (array) get_post_meta($postId, 'brands');
+            $departments = (array) get_post_meta($postId, 'departments');
+            $sponsors    = (array) get_post_meta($postId, 'sponsors');
+            $location    = (array) get_post_meta($postId, 'map', true);
+        }
+
+        if (is_iterable($founder) && !is_wp_error($founder)) {
+            foreach ($founder as $founder) {
+                $additionalData['founder'][] = [
+                    '@type' => 'Organization',
+                    'name'  => $founder
+                ];
+            }
+        }
+        if (is_iterable($brands) && !is_wp_error($brands)) {
+            foreach ($brands as $brand) {
+                $additionalData['brand'][] = [
+                    '@type' => 'Organization',
+                    'name'  => $brand
+                ];
+            }
+        }
+        if (is_iterable($sponsors) && !is_wp_error($sponsors)) {
+            foreach ($sponsors as $sponsor) {
+                $additionalData['sponsor'][] = [
+                    '@type' => 'Organization',
+                    'name'  => $sponsor
+                ];
+            }
+        }
+        if (is_iterable($departments) && !is_wp_error($departments)) {
+            foreach ($departments as $department) {
+                $additionalData['department'][] = [
+                    '@type' => 'Organization',
+                    'name' => $department
+                ];
+            }
+        }
+        if (!empty($location['address'])) {
+            $additionalData['location'][] = [
+                '@type'   => 'Place',
+                'address' => $location['address'],
+            ];
+        }
+        if (!empty($location['lat']) && !empty($location['lng'])) {
+            $additionalData['location'][] = [
+                '@type'     => 'GeoCoordinates',
+                'latitude'  => $location['lat'],
+                'longitude' => $location['lng'],
+            ];
+        }
+        if (!empty($location['country'])) {
+            $additionalData['location'][] = [
+                '@type'     => 'PostalAddess',
+                'country'  => $location['country'],
+            ];
+        }
+
+        return array_merge($structuredData, $additionalData);
     }
 }
