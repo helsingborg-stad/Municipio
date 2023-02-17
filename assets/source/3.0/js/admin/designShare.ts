c73888822e24c46ddfcbef07cb35d0e5be14d020
Thor Brink <thor.brink@helsingborg.se>
2023-02-17 11:04:41 +0000

    Fix design import not awaiting settings before itteration

--- assets/source/3.0/js/admin/designShare.ts
+++ assets/source/3.0/js/admin/designShare.ts
@@ -19,7 +19,7 @@ async function handleLoadSettingChange(id:any) {
     
     const settings = getSettings()
     const excludedSettings = getExcludedSettingIds();
-    const formattedMods = getFormattedMods(apiResponse.mods)
+    const formattedMods = await getFormattedMods(apiResponse.mods)
     resetSettingsToDefault(settings)
     importSettings(formattedMods, excludedSettings)
 }
