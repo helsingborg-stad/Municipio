83f5650e52d370e8c67f5ecd2f5513754cf0c5af
Anna Johansson <anna@thehappyhipster.se>
2023-02-27 13:50:10 +0100

    Return array containing type and src or type and key for term icon with the helper getTermIcon()

--- functions.php
+++ functions.php
@@ -7,3 +7,12 @@ require_once MUNICIPIO_PATH . '/library/Bootstrap.php';
 add_action('after_setup_theme', function () {
     load_theme_textdomain('municipio', get_template_directory() . '/languages');
 });
+
+add_action('init', function () {
+    $term = get_term(49042, 'activity');
+    // $term = get_queried_object();
+    // echo '<pre>' . print_r($term, true) . '</pre>';
+    $termIcon = \Municipio\Helper\Term::getTermIcon($term);
+    echo '<pre>' . var_export($termIcon, true) . '</pre>';
+    // die;
+});
