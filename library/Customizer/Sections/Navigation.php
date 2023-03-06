8fa3ff644b97297108f5be4f59d11f3f29640cc3
Sebastian Thulin <sebastian.thulin@helsingborg.se>
2023-03-06 17:08:18 +0100

    Add section for component navigation

--- library/Customizer/Sections/Navigation.php
+++ library/Customizer/Sections/Navigation.php
@@ -8,16 +8,26 @@ class Navigation
     {
         return [
             'sidebar' => (object) [
-                'label' => esc_html__('Sidebar', 'municipio'),
-                'scopeClass' => 's-sidebar'
+                'label' => esc_html__('Sidebar navigation', 'municipio'),
+                'scopeClass' => '.s-nav-sidebar',
+                'types' => [
+                    'vertical'
+                ]
             ],
             'drawer' => (object) [
-                'label' => esc_html__('Sidebar', 'municipio'),
-                'scopeClass' => 's-drawer'
+                'label' => esc_html__('Drawer navigation', 'municipio'),
+                'scopeClass' => '.s-nav-drawer',
+                'types' => [
+                    'vertical'
+                ]
             ],
             'primary' => (object) [
-                'label' => esc_html__('Primary', 'municipio'),
-                'scopeClass' => 's-primary'
+                'label' => esc_html__('Primary navigation', 'municipio'),
+                'scopeClass' => '.s-nav-primary',
+                'types' => [
+                    'vertical',
+                    'horizontal'
+                ]
             ]
         ];
     }
@@ -30,38 +40,54 @@ class Navigation
         if(is_countable($scopes)) {
             foreach($scopes as $key => $scope) {
 
-                \Kirki::add_field(
-                    \Municipio\Customizer::KIRKI_CONFIG,
-                    [
-                        'type'        => 'multicolor',
-                        'settings'    => 'nav_color_' . $key,
-                        'label'       => $scope->label . " " . esc_html__('colors', 'municipio'),
-                        'section'     => $sectionID,
-                        'priority'    => 10,
-                            'transport' => 'auto',
-                        'choices'     => [
-                            'divider'    => esc_html__('Divider', 'municipio'),
-                            'outline'    => esc_html__('Outline', 'municipio'),
-                        ],
-                        'default'     => [
-                            'divider'    => 'rgba(0,0,0,0)',
-                            'outline'    => 'rgba(0,0,0,0)',
-                        ],
-                        'output' => [
-                            [
-                                'choice'    => 'divider',
-                                'element'   => $scope->scopeClass,
-                                'property'  => '--color-border-divider',
-                            ],
-                            [
-                                'choice'    => 'outline',
-                                'element'   => $scope->scopeClass,
-                                'property'  => '--color-border-outline',
-                            ],
-                        ]
-                    ]
-                );
+                if(in_array('vertical', $scope->types)) {
+                    $this->addVerticalColorConfiguration($scope, $sectionID); 
+                }
+
+                if(in_array('horizontal', $scope->types)) {
+                    $this->addHorizontalColorConfiguration($scope, $sectionID); 
+                }
+
             }
         }
+    }
+
+
+    private function addVerticalColorConfiguration($scope, $sectionID) {
+        \Kirki::add_field(
+            \Municipio\Customizer::KIRKI_CONFIG,
+            [
+                'type'        => 'multicolor',
+                'settings'    => 'nav_color_' . $key,
+                'label'       => $scope->label . " " . esc_html__('colors', 'municipio'),
+                'section'     => $sectionID,
+                'priority'    => 10,
+                'transport'   => 'auto',
+                'choices'     => [
+                    'divider'    => esc_html__('Divider', 'municipio'),
+                    'outline'    => esc_html__('Outline', 'municipio'),
+                ],
+                'default'     => [
+                    'divider'    => 'rgba(0,0,0,0)',
+                    'outline'    => 'rgba(0,0,0,0)',
+                ],
+                'output' => [
+                    [
+                        'choice'    => 'divider',
+                        'element'   => $scope->scopeClass,
+                        'property'  => '--color-border-divider',
+                    ],
+                    [
+                        'choice'    => 'outline',
+                        'element'   => $scope->scopeClass,
+                        'property'  => '--color-border-outline',
+                    ],
+                ]
+            ]
+        );
+    }
+
+    private function addHorizontalColorConfiguration($scope, $sectionID) {
+
     }
 }
