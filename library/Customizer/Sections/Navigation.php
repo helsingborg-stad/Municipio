96035dce1d313c0839b1bbabd56fe6fa850f5996
Sebastian Thulin <sebastian.thulin@helsingborg.se>
2023-03-07 12:53:23 +0100

    Add the ability to change colors on main nav elements.

--- library/Customizer/Sections/Navigation.php
+++ library/Customizer/Sections/Navigation.php
@@ -2,6 +2,9 @@
 
 namespace Municipio\Customizer\Sections;
 
+use Municipio\Helper\KirkiCondidional as KirkiCondidional;
+use Municipio\Customizer as Customizer;
+
 class Navigation
 {
     private function getScopes(): array
@@ -25,11 +28,28 @@ class Navigation
                 'label' => esc_html__('Primary navigation', 'municipio'),
                 'scopeClass' => '.s-nav-primary',
                 'types' => [
-                    'vertical',
-                    'horizontal'
+                    'horizontal',
+                    'dropdown'
                 ]
             ]
         ];
+    }
+
+    private function getOrientationLabel(string $orientation): string
+    {
+        if($orientation == 'horizontal') {
+            return " (" . __('Horizontal','municipio') . ")";
+        }
+
+        if($orientation == 'vertical') {
+            return " (" . __('Vertical','municipio') . ")";
+        }
+
+        if($orientation == 'dropdown') {
+            return " (" . __('Dropdown','municipio') . ")";
+        }
+
+        return ""; 
     }
 
     public function __construct(string $sectionID)
@@ -41,11 +61,30 @@ class Navigation
             foreach($scopes as $key => $scope) {
 
                 if(in_array('vertical', $scope->types)) {
-                    $this->addVerticalColorConfiguration($key, $scope, $sectionID); 
+                    $this->addVerticalColorConfiguration(
+                        $key,
+                        $scope,
+                        $sectionID,
+                        $this->getOrientationLabel('vertical')
+                    ); 
+                }
+
+                if(in_array('dropdown', $scope->types)) {
+                    $this->addVerticalColorConfiguration(
+                        $key,
+                        $scope,
+                        $sectionID,
+                        $this->getOrientationLabel('dropdown')
+                    ); 
                 }
 
                 if(in_array('horizontal', $scope->types)) {
-                    $this->addHorizontalColorConfiguration($key, $scope, $sectionID); 
+                    $this->addHorizontalColorConfiguration(
+                        $key, 
+                        $scope, 
+                        $sectionID,
+                        $this->getOrientationLabel('horizontal')
+                    ); 
                 }
 
             }
@@ -53,41 +92,121 @@ class Navigation
     }
 
 
-    private function addVerticalColorConfiguration($key, $scope, $sectionID) {
-        \Kirki::add_field(
-            \Municipio\Customizer::KIRKI_CONFIG,
-            [
+    private function addVerticalColorConfiguration($key, $scope, $sectionID, $orientationLabel) {
+        KirkiCondidional::add_field(
+            Customizer::KIRKI_CONFIG, [
                 'type'        => 'multicolor',
-                'settings'    => 'nav_color_' . $key,
-                'label'       => $scope->label . " " . esc_html__('colors', 'municipio'),
+                'settings'    => 'nav_v_color_' . $key,
+                'label'       => $scope->label . " " . esc_html__('colors', 'municipio') . $orientationLabel,
                 'section'     => $sectionID,
                 'priority'    => 10,
                 'transport'   => 'auto',
+                'alpha'       => true,
                 'choices'     => [
-                    'divider'    => esc_html__('Divider', 'municipio'),
-                    'outline'    => esc_html__('Outline', 'municipio'),
+                    'contrasting' => esc_html__('Default Contrast', 'municipio'),
+                    'background_active' => esc_html__('Background (Active)', 'municipio'),
+                    'contrasting_active' => esc_html__('Contrasting (Active)', 'municipio'),
+                    'background_expanded' => esc_html__('Background (Expanded)', 'municipio'),
+                    'divider_color' => esc_html__('Divider', 'municipio'),
+                    'font_size' => esc_html__('Font size', 'municipio'),
                 ],
                 'default'     => [
-                    'divider'    => 'rgba(0,0,0,0)',
-                    'outline'    => 'rgba(0,0,0,0)',
+                    'contrasting'           => '#000',
+                    'background_active'     => '#fff',
+                    'contrasting_active'    => '#000',
+                    'background_expanded'   => 'rgba(0,0,0,.04)',
+                    'divider_color'         => '#eee',
                 ],
                 'output' => [
                     [
-                        'choice'    => 'divider',
+                        'choice'    => 'contrasting',
                         'element'   => $scope->scopeClass,
-                        'property'  => '--color-border-divider',
+                        'property'  => '--c-nav-v-color-contrasting',
                     ],
                     [
-                        'choice'    => 'outline',
+                        'choice'    => 'background_active',
                         'element'   => $scope->scopeClass,
-                        'property'  => '--color-border-outline',
+                        'property'  => '--c-nav-v-background-active',
                     ],
+                    [
+                        'choice'    => 'contrasting_active',
+                        'element'   => $scope->scopeClass,
+                        'property'  => '--c-nav-v-color-contrasting-active',
+                    ],
+                    [
+                        'choice'    => 'background_expanded',
+                        'element'   => $scope->scopeClass,
+                        'property'  => '--c-nav-v-background-expanded',
+                    ],
+                    [
+                        'choice'    => 'divider_color',
+                        'element'   => $scope->scopeClass,
+                        'property'  => '--c-nav-v-divider-color',
+                    ],
+                    [
+                        'choice'    => 'font_size',
+                        'element'   => $scope->scopeClass,
+                        'property'  => '--c-nav-v-font-size',
+                    ]
                 ]
+            ], 
+            [
+                'label' => esc_html__('Tailor:', 'municipio') . $scope->label . " " . esc_html__('colors', 'municipio') . $orientationLabel, 
+                'description' => esc_html__('Enable to customize this navigation location. If disabled, a general colorscheme will be inherited.', 'municipio'),
+                'settings' => 'nav_v_color_' . $key . '_customized'
             ]
         );
     }
 
-    private function addHorizontalColorConfiguration($key, $scope, $sectionID) {
-
+    private function addHorizontalColorConfiguration($key, $scope, $sectionID, $orientationLabel) {
+        KirkiCondidional::add_field(
+            Customizer::KIRKI_CONFIG, [
+                'type'        => 'multicolor',
+                'settings'    => 'nav_h_color_' . $key,
+                'label'       => $scope->label . " " . esc_html__('colors', 'municipio') . $orientationLabel,
+                'description' => esc_html__('Enable to customize this navigation location. If disabled, a general colorscheme will be inherited.', 'municipio'),
+                'section'     => $sectionID,
+                'priority'    => 10,
+                'transport'   => 'auto',
+                'alpha'       => true,
+                'choices'     => [
+                    'contrasting' => esc_html__('Default Contrast', 'municipio'),
+                    'background_active' => esc_html__('Background (Active)', 'municipio'),
+                    'contrasting_active' => esc_html__('Contrasting (Active)', 'municipio'),
+                    'background_expanded' => esc_html__('Background (Expanded)', 'municipio'),
+                    'divider_color' => esc_html__('Divider', 'municipio'),
+                    'font_size' => esc_html__('Font size', 'municipio'),
+                ],
+                'default'     => [
+                    'contrasting'           => '#000',
+                    'background_active'     => '#fff',
+                    'contrasting_active'    => '#000',
+                    'background_expanded'   => 'rgba(0,0,0,.04)',
+                    'divider_color'         => '#eee',
+                ],
+                'output' => [
+                    [
+                        'choice'    => 'contrasting',
+                        'element'   => $scope->scopeClass,
+                        'property'  => '--c-nav-h-color-contrasting',
+                    ],
+                    [
+                        'choice'    => 'background_active',
+                        'element'   => $scope->scopeClass,
+                        'property'  => '--c-nav-h-background-active',
+                    ],
+                    [
+                        'choice'    => 'contrasting_active',
+                        'element'   => $scope->scopeClass,
+                        'property'  => '--c-nav-h-color-contrasting-active',
+                    ]
+                ]
+            ], 
+            [
+                'label' => esc_html__('Tailor:', 'municipio') . $scope->label . " " . esc_html__('colors', 'municipio') . $orientationLabel, 
+                'settings' => 'nav_h_color_' . $key . '_customized'
+            ],
+            
+        );
     }
 }
