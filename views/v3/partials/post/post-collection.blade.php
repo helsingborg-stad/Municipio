80991e7646e27b3d061f10a1d484ef9fc773200d
NiclasNorin <niclas.norin@frontwalker.se>
2023-03-29 10:43:58 +0200

    Collection post updated look archive

--- views/v3/partials/post/post-collection.blade.php
+++ views/v3/partials/post/post-collection.blade.php
@@ -1,70 +1,52 @@
 @if ($posts)
-
     @collection([
         'unbox' => true,
-        'classList' => ['o-grid']
+        'classList' => ['c-collection--posts', 'o-grid']
     ])
         @foreach ($posts as $post)
+        @php var_dump($post) @endphp
             @collection__item([
                 'link' => $post->permalink,
-                'classList' => [$gridColumnClass]
+                'classList' => [$gridColumnClass, 'c-collection__item--post'],
+                'containerAware' => true,
             ])
-                @group(['direction' => 'horizontal', 'classList' => ['o-grid', 'u-padding--0']])
-                @if ($displayFeaturedImage)
-                    @image([
-                        'src' => $post->thumbnail['src'],
-                        'alt' => $post->thumbnail['alt'],
-                        'placeholderIconSize' => 'sm',
-                        'placeholderIcon' => 'image',
-                        'placeholderText' => '',
-                        'classList' => ['o-grid-4@sm', 'o-grid-12', 'u-height--100']
-                    ])
-                    @endimage
-                    @group(['direction' => 'vertical', 'classList' => ['o-grid-8@sm', 'o-grid-12']])
-                    @else
-                        @group(['direction' => 'vertical', 'classList' => ['o-grid-12']])
-                        @endif
-                        @typography([
-                            'element' => 'h3',
-                            'classList' => ['c-collection__content__title']
+               @slot('before')
+                    @if($displayFeaturedImage)
+                        @image([
+                            'src' => $post->thumbnail['src'],
+                            'alt' => $post->thumbnail['alt'],
+                            'placeholderIconSize' => 'sm',
+                            'placeholderIcon' => 'image',
+                            'placeholderText' => '',
                         ])
-                            {{ $post->postTitle }}
-                        @endtypography
-                        @if ($post->termsUnlinked)
-                            @typography([
-                                'element' => 'p',
-                                'variant' => 'meta',
-                                'classList' => ['c-collection__content__terms']
-                            ])
-                                @foreach ($post->termsUnlinked as $term)
-                                    @typography([
-                                        'element' => 'span',
-                                        'variant' => 'meta',
-                                        'classList' => ['c-collection__content__term', 'u-margin__right--1']
-                                    ])
-                                        {{ $term['label'] }}
-                                    @endtypography
-                                @endforeach
-                            @endtypography
-                        @endif
-                        @if ($displayReadingTime && $post->readingTime)
-                            @typography([
-                                'element' => 'p',
-                                'variant' => 'meta',
-                                'classList' => ['c-collection__content__reading-time']
-                            ])
-                                {{ $post->readingTime }}
-                            @endtypography
-                        @endif
-                        @typography([
-                            'element' => 'p',
-                            'classList' => ['c-collection__content__excerpt']
-                        ])
-                            {{ $post->excerptShort }}
-                        @endtypography
-                    @endgroup
-                @endgroup
-            @endcollection__item
+                        @endimage
+                    @endif
+                @endslot
+
+                 @group([
+                    'direction' => 'vertical'
+                ])
+            @group([
+                'justifyContent' => 'space-between'
+            ])
+                @typography([
+                    'element' => 'h2',
+                    'variant' => 'h3',
+                ])
+                     {{ $post->postTitle }}
+                @endtypography
+                {{-- TODO: Add icon --}}
+            @endgroup
+                @tags([
+                    'tags' => $post->termsUnlinked,
+                    'classList' => ['u-padding__y--2']
+                ])
+                @endtags
+                @typography([])
+                    {{ $post->excerptShort }}
+                @endtypography
+            @endgroup
+        @endcollection__item
         @endforeach
     @endcollection
 @endif
