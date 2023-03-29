3c1f887cd11ef257517e0d126dc7b5f7f9802f3d
Anna Johansson <anna@thehappyhipster.se>
2023-03-29 13:01:49 +0200

    Allow for displaying helper-nav below page content via usage of .u-order--9

--- views/v3/templates/header.blade.php
+++ views/v3/templates/header.blade.php
@@ -1,15 +1,11 @@
 @header([
     'id' => 'site-header',
     'classList' => array_merge(
-        [
-            'site-header', 
-            $customizer->hamburgerMenuMobile ? 'hamburger-menu-mobile' : '',
-        ],
+        ['site-header', $customizer->hamburgerMenuMobile ? 'hamburger-menu-mobile' : ''],
         (array) isset($classList) ? $classList : []
     ),
     'context' => 'site.header'
 ])
-
     @include('partials.header.skip-to-main-content')
     @includeWhen($hasMainMenu, 'partials.header.skip-to-main-menu')
     @includeWhen($hasSideMenu, 'partials.header.skip-to-side-menu')
@@ -18,19 +14,18 @@
     @section('search-form')
         @includeWhen($showNavigationSearch, 'partials.search.search-modal')
     @show
-    
+
     {{-- Navbars --}}
     @section('navigation')
-        
+
         {{-- Top Navigation --}}
         @yield('top-navigation')
-        
+
         {{-- Primary Navigation --}}
         @yield('primary-navigation')
-        
+
         {{-- Secondary Navigation --}}
         @yield('secondary-navigation')
-    
-    @show
 
+    @show
 @endheader
--- views/v3/templates/master.blade.php
+++ views/v3/templates/master.blade.php
@@ -1,135 +1,139 @@
 <!DOCTYPE html>
 <html {!! $languageAttributes !!}>
-    <head>
 
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+<head>
 
-        <title>{!! $pageTitle !!}</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-        <meta name="pubdate" content="{{ $pagePublished }}">
-        <meta name="moddate" content="{{ $pageModified }}">
+    <title>{!! $pageTitle !!}</title>
 
-        <meta name="apple-mobile-web-app-capable" content="yes">
-        <meta name="format-detection" content="telephone=yes">
-        <meta name="HandheldFriendly" content="true">
+    <meta name="pubdate" content="{{ $pagePublished }}">
+    <meta name="moddate" content="{{ $pageModified }}">
 
-        <script>
-            var ajaxurl = '{!! $ajaxUrl !!}';
-        </script>
+    <meta name="apple-mobile-web-app-capable" content="yes">
+    <meta name="format-detection" content="telephone=yes">
+    <meta name="HandheldFriendly" content="true">
 
-        @if($structuredData)
-            <script type="application/ld+json">
+    <script>
+        var ajaxurl = '{!! $ajaxUrl !!}';
+    </script>
+
+    @if ($structuredData)
+        <script type="application/ld+json">
                 {!! $structuredData !!}
             </script>
-        @endif
+    @endif
 
-        {{-- Wordpress required call to wp_header() --}}
-        {!! $wpHeader !!}
+    {{-- Wordpress required call to wp_header() --}}
+    {!! $wpHeader !!}
 
-    </head>
+</head>
 
-    <body
-        class="{{ $bodyClass }}"
-        js-page-id="{{$pageID}}"
-        @if($customizer->headerSticky === 'sticky')
-            data-js-toggle-item="hamburger-menu"
-            data-js-toggle-class="hamburger-menu-open"
-        @endif
-    >
-        <div class="site-wrapper">
-            
-            {{-- Site header --}}
-            @section('site-header')
-                @if (!empty($customizer->headerApperance))
-                    @includeIf('partials.header.' . $customizer->headerApperance)
-                @endif
-            @show
+<body class="{{ $bodyClass }}" js-page-id="{{ $pageID }}"
+    @if ($customizer->headerSticky === 'sticky') data-js-toggle-item="hamburger-menu"
+            data-js-toggle-class="hamburger-menu-open" @endif>
+    <div class="site-wrapper u-display--grid">
 
-            {{-- Hero area and top sidebar --}}
-            @hasSection('hero-top-sidebar')
-                @yield('hero-top-sidebar')
+        {{-- Site header --}}
+        @section('site-header')
+            @if (!empty($customizer->headerApperance))
+                @includeIf('partials.header.' . $customizer->headerApperance)
             @endif
-
-            {{-- Notices Notice::add() --}}
-            @if($notice) 
-                @foreach ($notice as $noticeItem)
-                    @notice($noticeItem)
-                    @endnotice
-                @endforeach
-            @endif
-
-            {{-- Before page layout --}}
-            @yield('before-layout')
-
-            {{-- Page layout --}}
-            <main id="main-content">
-                @section('layout')
-                    <div class="o-container">
-
-                        @hasSection('helper-navigation')
-                            <div class="o-grid o-grid--no-margin u-print-display--none">
-                                <div class="o-grid-12">
-                                    @yield('helper-navigation')
-                                </div>
-                            </div>
-                        @endif
-
-                        @hasSection('above')
-                            <div class="o-grid u-print-display--none">
-                                <div class="o-grid-12">
-                                    @yield('above')
-                                </div>
-                            </div>
-                        @endif
-
-                        <!--  Main content padder -->
-                        <div class="u-padding__x--{{ $mainContentPadding['md'] }}@lg u-padding__x--{{ $mainContentPadding['lg'] }}@lg u-margin__bottom--12">
-                            <div class="o-grid o-grid--nowrap@lg">
-                                
-                                @hasSection('sidebar-left')
-                                    <div class="o-grid-12 o-grid-{{$leftColumnSize}}@lg o-order-2 o-order-1@lg u-print-display--none">
-                                        @yield('sidebar-left')
-                                    </div>
-                                @endif
-                        
-                                <div class="o-grid-12 o-grid-auto@lg o-order-1 o-order-2@lg u-display--flex u-flex--gridgap  u-flex-direction--column">
-                                    @yield('content')
-                                </div>
-
-                                @hasSection('sidebar-right')
-                                    <div class="o-grid-12 o-grid-{{$rightColumnSize}}@lg o-order-3 o-order-3@lg u-print-display--none">
-                                        @yield('sidebar-right')
-                                    </div>
-                                @endif
-                            </div>
-                        </div>
-
-                        @hasSection('below')
-                            <div class="o-grid">
-                                <div class="o-grid-12">
-                                    @yield('below')
-                                </div>
-                            </div>
-                        @endif
-                    </div>
-                @show
-            </main>
-
-            {{-- After page layout --}}
-            @yield('after-layout')
-
-        </div>
-
-        @section('footer')
-            @includeIf('partials.footer')
         @show
 
-        {{-- Floating menu --}}
-        @includeWhen($floatingMenuItems, 'partials.navigation.floating')
+        @includeWhen(!$helperNavBeforeContent, 'partials.navigation.helper', [
+            'classList' => ['u-order--9'],
+        ])
+        {{-- Hero area and top sidebar --}}
+        @hasSection('hero-top-sidebar')
+            @yield('hero-top-sidebar')
+        @endif
 
-        {{-- Wordpress required call to wp_footer() --}}
-        {!! $wpFooter !!}
+        {{-- Notices Notice::add() --}}
+        @if ($notice)
+            @foreach ($notice as $noticeItem)
+                @notice($noticeItem)
+                @endnotice
+            @endforeach
+        @endif
 
-    </body>
+        {{-- Before page layout --}}
+        @yield('before-layout')
+
+        {{-- Page layout --}}
+        <main id="main-content">
+            @section('layout')
+                <div class="o-container">
+
+                    @hasSection('helper-navigation')
+                        <div class="o-grid o-grid--no-margin u-print-display--none">
+                            <div class="o-grid-12">
+                                @yield('helper-navigation')
+                            </div>
+                        </div>
+                    @endif
+
+                    @hasSection('above')
+                        <div class="o-grid u-print-display--none">
+                            <div class="o-grid-12">
+                                @yield('above')
+                            </div>
+                        </div>
+                    @endif
+
+                    <!--  Main content padder -->
+                    <div
+                        class="u-padding__x--{{ $mainContentPadding['md'] }}@lg u-padding__x--{{ $mainContentPadding['lg'] }}@lg u-margin__bottom--12">
+                        <div class="o-grid o-grid--nowrap@lg">
+
+                            @hasSection('sidebar-left')
+                                <div
+                                    class="o-grid-12 o-grid-{{ $leftColumnSize }}@lg o-order-2 o-order-1@lg u-print-display--none">
+                                    @yield('sidebar-left')
+                                </div>
+                            @endif
+
+                            <div
+                                class="o-grid-12 o-grid-auto@lg o-order-1 o-order-2@lg u-display--flex u-flex--gridgap  u-flex-direction--column">
+                                @yield('content')
+                            </div>
+
+                            @hasSection('sidebar-right')
+                                <div
+                                    class="o-grid-12 o-grid-{{ $rightColumnSize }}@lg o-order-3 o-order-3@lg u-print-display--none">
+                                    @yield('sidebar-right')
+                                </div>
+                            @endif
+                        </div>
+                    </div>
+
+                    @hasSection('below')
+                        <div class="o-grid">
+                            <div class="o-grid-12">
+                                @yield('below')
+                            </div>
+                        </div>
+                    @endif
+                </div>
+            @show
+        </main>
+
+        {{-- After page layout --}}
+        @yield('after-layout')
+
+    </div>
+
+    @section('footer')
+        @includeIf('partials.footer')
+    @show
+
+    {{-- Floating menu --}}
+    @includeWhen($floatingMenuItems, 'partials.navigation.floating')
+
+    {{-- Wordpress required call to wp_footer() --}}
+    {!! $wpFooter !!}
+
+</body>
+
 </html>
