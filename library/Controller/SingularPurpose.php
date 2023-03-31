e137470ddea4a3533f4135a2a1d9ec9708e5393e
Anna Johansson <anna@thehappyhipster.se>
2023-03-31 16:07:50 +0200

    SingularPurpose

--- library/Controller/SingularPurpose.php
+++ library/Controller/SingularPurpose.php
@@ -47,16 +47,16 @@ class SingularPurpose extends \Municipio\Controller\Singular
         parent::init();
         $fields = get_fields($this->getPageID());
 
-        $this->data['list'][] = $this->createListItem($fields['location']['street_name'] . ' ' . $fields['location']['street_number'], 'location_on', $this->buildMapsLink($fields['location']));
-        $this->data['list'][] = $this->createListItem($fields['phone'], 'call');
-        $this->data['list'][] = $this->createListItem(__('Visit website', 'municipio'), 'language', $fields['website']);
+        $this->data['list'][] = !empty($fields['location']) ? $this->createListItem($fields['location']['street_name'] . ' ' . $fields['location']['street_number'], 'location_on', $this->buildMapsLink($fields['location'])) : false;
+        $this->data['list'][] = !empty($fields['phone']) ? $this->createListItem($fields['phone'], 'call') : false;
+        $this->data['list'][] = !empty($fields['website']) ? $this->createListItem(__('Visit website', 'municipio'), 'language', $fields['website']) : false;
 
-        $other = $this->getTermNames($fields['other']);
+        $other = !empty($fields['other']) ? $this->getTermNames($fields['other']) : false;
         if (!empty($other)) {
             foreach ($other as $item) {
                 $this->data['list'][] = $this->createListItem($item->name, $item->icon['src']);
             }
-        }        
+        }
 
         $this->data['labels'] = [
             'related' => __('Related', 'municipio'),
@@ -64,19 +64,21 @@ class SingularPurpose extends \Municipio\Controller\Singular
         ];
 
         $postId = $this->data['post']->id;
-        $this->data['relatedPosts'] = $this->getRelatedPosts($postId); 
+        $this->data['relatedPosts'] = $this->getRelatedPosts($postId);
 
         return $this->data;
     }
 
-    private function buildMapsLink($locations) {
+    private function buildMapsLink($locations)
+    {
         if (empty($locations) || empty($locations['lng']) || empty($locations['lat'])) {
             return false;
         }
         return 'https://www.google.com/maps/dir/?api=1&destination=' . $locations['lat'] . ',' . $locations['lng'] . '&travelmode=transit';
     }
 
-    private function getRelatedPosts($postId) {
+    private function getRelatedPosts($postId)
+    {
         $taxonomies = get_post_taxonomies($postId);
         $postTypes = get_post_types(array('public' => true, '_builtin' => false), 'objects');
 
@@ -85,7 +87,7 @@ class SingularPurpose extends \Municipio\Controller\Singular
             $terms = get_the_terms($postId, $taxonomy);
             if (!empty($terms)) {
                 foreach ($terms as $term) {
-                    $arr[$taxonomy][] = $term->term_id;                 
+                    $arr[$taxonomy][] = $term->term_id;
                 }
             }
         }
@@ -93,7 +95,7 @@ class SingularPurpose extends \Municipio\Controller\Singular
         if (empty($arr)) {
             return false;
         }
-        
+
         $posts = [];
         foreach ($postTypes as $postType) {
             $args = array(
@@ -115,7 +117,7 @@ class SingularPurpose extends \Municipio\Controller\Singular
             }
 
             $result = get_posts($args);
-            
+
             if (!empty($result)) {
                 foreach ($result as &$post) {
                     $post = \Municipio\Helper\Post::preparePostObject($post);
@@ -127,15 +129,17 @@ class SingularPurpose extends \Municipio\Controller\Singular
         return $posts;
     }
 
-    private function createListItem ($label, $icon, $href = false) {
+    private function createListItem($label, $icon, $href = false)
+    {
         if (!empty($label) && $label != " ") {
             return ['label' => $label, 'icon' => ['icon' => $icon, 'size' => 'md'], 'href' => $href];
         }
-        
+
         return false;
     }
 
-    private function getTermNames ($termIds) {
+    private function getTermNames($termIds)
+    {
         if (empty($termIds)) {
             return false;
         }
@@ -143,8 +147,8 @@ class SingularPurpose extends \Municipio\Controller\Singular
         $terms = array();
         foreach ($termIds as $termId) {
             $term = get_term($termId);
-            if(!$term || is_wp_error($term)) { 
-                continue; 
+            if (!$term || is_wp_error($term)) {
+                continue;
             }
             $term->icon = TermHelper::getTermIcon($term);
             $terms[] = $term;
