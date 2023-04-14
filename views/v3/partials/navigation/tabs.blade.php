2475d065bd8940a1b4b0cdb73d30f3cfbe4dc179
Sebastian Thulin <sebastianthulin@users.noreply.github.com>
2023-04-14 11:17:31 +0200

    Change tab menu to be a real menu (#545)
    This also displays the tab menu as traditional buttons.
    
    Co-authored-by: Sebastian Thulin <sebastian.thulin@helsingborg.se>

--- views/v3/partials/navigation/tabs.blade.php
+++ views/v3/partials/navigation/tabs.blade.php
@@ -1,35 +1,15 @@
 @if($tabMenuItems)
-    <nav aria-label="{{$lang->relatedLinks}}"
-        class="u-display--flex@lg u-display--flex@lx u-display--none@xs
-                u-display--none@sm u-display--none@md u-print-display--none">
-        @group([
-            'classList' => [
-                'u-justify-content--center@xs', 
-                'u-justify-content--center@sm', 
-                'u-justify-content--end', 
-                'u-box-shadow--1',
-                'u-rounded',
-                'u-margin--auto',
-                'u-color__bg--lightest'
-            ]
-        ])
-            @foreach($tabMenuItems as $item)
-                @button([
-                    'href'  => $item['href'], 
-                    'text'  => $item['label'],
-                    'size'  => 'sm',
-                    'style' => 'basic',
-                    'icon' => $item['icon']['icon'] ?? null,
-                    'attributeList' => [
-                        'role' => 'menuitem'
-                    ],
-                    'reversePositions' => true,
-                    'classList' => [
-                        'u-padding__x--2'
-                    ]
-                ])
-                @endbutton
-            @endforeach
-        @endgroup
-    </nav>
+    @nav([
+        'id' => 'tabs',
+        'items' => $tabMenuItems,
+        'direction' => 'horizontal',
+        'includeToggle' => false,
+        'allowStyle' => true,
+        'buttonColor' => 'default',
+        'height' => 'sm',
+        'classList' => [
+            'u-width--auto'
+        ]
+    ])
+    @endnav
 @endif
