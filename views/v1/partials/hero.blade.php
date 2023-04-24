678ce2fdbf4d01d64984f85f5f52a2b4e00065d4
Anna Johansson <anna@thehappyhipster.se>
2023-04-24 17:18:25 +0200

    Don't print markup if sidebar is empty

--- views/v1/partials/hero.blade.php
+++ views/v1/partials/hero.blade.php
@@ -1,4 +1,4 @@
-@if (is_active_sidebar('slider-area') === true )
+@if (is_active_sidebar('slider-area'))
     <div class="hero has-stripe sidebar-slider-area">
         <div class="grid">
             <?php dynamic_sidebar('slider-area'); ?>
@@ -6,7 +6,9 @@
 
         @include('partials.stripe')
 
-        @if (rtrim($_SERVER['REQUEST_URI'], "/") == "" && is_array(get_field('search_display', 'option')) && in_array('hero', get_field('search_display', 'option')))
+        @if (rtrim($_SERVER['REQUEST_URI'], '/') == '' &&
+                is_array(get_field('search_display', 'option')) &&
+                in_array('hero', get_field('search_display', 'option')))
             {{ get_search_form() }}
         @endif
     </div>
