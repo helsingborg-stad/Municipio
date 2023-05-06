4bf769fb3e60e5aeafafcbc558527bb83c1b82e5
Anna Johansson <anna@thehappyhipster.se>
2023-05-06 17:20:53 +0200

    - feat(Location): create Location class for location-related functionality - refactor(ArchivePurpose): use Location class for handling locations - refactor(ArchivePurpose): move addLocationToArchivePosts to Location class - refactor(Location): add setupPins and setupPostsWithLocation methods - docs(Location): add DocBlocks for Location class methods

--- library/Controller/ArchivePurpose.php
+++ library/Controller/ArchivePurpose.php
@@ -2,96 +2,28 @@
 
 namespace Municipio\Controller;
 
+use Municipio\Helper\Location;
+use Municipio\Helper\Purpose;
+
 class ArchivePurpose extends \Municipio\Controller\Archive
 {
-    /**
-     * Initializes the archive purpose controller.
-     *
-     * This method sets a flag to display the OpenStreetMap on archive pages that have the purpose "place"
-     * or a secondary purpose of "place". It also registers a filter to add location data to the archive posts.
-     */
     public function init()
     {
-
-        add_filter('Municipio/Controller/Archive/getArchivePosts', [$this, 'addLocationToArchivePosts'], 10, 1);
+        add_filter('Municipio/Controller/Archive/getArchivePosts', [Location::class, 'addLocationDataToPosts'], 10, 1);
 
         parent::init();
 
         $this->data['displayArchiveLoop'] = (bool) ($this->data['archiveProps']->displayArchiveLoop ?? true);
 
-        if (\Municipio\Helper\Purpose::hasPurpose('place', get_post_type(), true)) {
+        if (Purpose::hasPurpose('place', get_post_type(), true)) {
             $this->data['displayOpenstreetmap'] = (bool) ($this->data['archiveProps']->displayOpenstreetmap ?? false);
 
             if ($this->data['displayOpenstreetmap']) {
                 $this->data['displayGoogleMapsLink'] = (bool) ($this->data['archiveProps']->displayGoogleMapsLink
                 ?? true);
-                $this->data['pins'] = $this->setupPins($this->data['posts']);
-                $this->data['postsWithLocation'] = $this->setupPostsWithLocation($this->data['posts']);
+                $this->data['pins'] = Location::createPinDataForPosts($this->data['posts'], $this->data['displayGoogleMapsLink']);
+                $this->data['postsWithLocation'] = Location::filterPostsWithLocationData($this->data['posts']);
             }
         }
-    }
-    private function setupPins(array $posts = []): array
-    {
-        $pins = [];
-
-        if (!empty($posts)) {
-            foreach ($posts as $post) {
-                if (!empty($post->location['lat']) && !empty($post->location['lng'])) {
-                    $pin = [
-                    'lat' => $post->location['lat'],
-                    'lng' => $post->location['lng'],
-                    'tooltip' => [
-                        'title' => $post->postTitle ?? '',
-                        'content' => $post->postExcerpt ?? '',
-                        ],
-                    ];
-
-                    if ($this->data['displayGoogleMapsLink']) {
-                    // TODO Change "direction" to "directions" once the component has been updated
-                        $pin['tooltip']['direction'] = [
-                            'url' => '#',
-                            'label' => __('Get directions on Google Maps', 'municipio'),
-                        ];
-                    }
-
-                    $pins[] = $pin;
-                }
-            }
-        }
-
-        return $pins;
-    }
-    private function setupPostsWithLocation(array $posts = []): array
-    {
-        $postsWithLocation = [];
-
-        if (!empty($posts)) {
-            foreach ($posts as $post) {
-                if (!empty($post->location['lat']) && !empty($post->location['lng'])) {
-                    $postsWithLocation[] = $post;
-                }
-            }
-        }
-
-        return $postsWithLocation;
-    }
-
-
-    /**
-     * Adds location data to the archive posts.
-     *
-     * @param array $posts An array of post objects to add location data to.
-     *
-     * @return array The modified array of post objects.
-     */
-    public function addLocationToArchivePosts($posts)
-    {
-        foreach ($posts as &$_post) {
-            $location = get_field("location", $_post->id);
-            if (!empty($location) && !empty($location["lat"]) && !empty($location["lng"])) {
-                $_post->location = $location;
-            }
-        }
-        return $posts;
     }
 }
