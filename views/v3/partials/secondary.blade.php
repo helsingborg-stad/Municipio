b1a0da1218b4c57ab5f2dcb6cb39f1629bd3791f
Anna Johansson <anna@thehappyhipster.se>
2023-05-08 08:26:23 +0200

    Added default values

--- views/v3/partials/secondary.blade.php
+++ views/v3/partials/secondary.blade.php
@@ -12,7 +12,11 @@
         @if ($displaySecondaryMap && !empty($secondaryQuery->pins))
             @openStreetMap([
                 'pins' => $secondaryQuery->pins,
-                'classList' => ['u-margin__top--4']
+                'classList' => ['u-margin__top--4'],
+                'startPosition' => ['lat' => '56.046029', 'lng' => '12.693904', 'zoom' => 14],
+                'height' => '100vh',
+                'containerAware' => true,
+                'mapStyle' => 'default'
             ])
             @endopenStreetMap
         @else
