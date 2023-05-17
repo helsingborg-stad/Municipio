1d5a87f5052024cd6733bb099dea2a99bb8ae20e
Anna <anna.johansson4@helsingborg.se>
2023-05-17 14:59:51 +0200

    feature/Sida fr term ska ha modulomrde ovanfr kartans lista med inlgg CU 865c9peut (#599)
    * Removed hardcoded values for map on secondaryQuery
    
    * feat(views): add secondary map and right sidebar to secondary.blade.php

--- views/v3/partials/secondary.blade.php
+++ views/v3/partials/secondary.blade.php
@@ -9,16 +9,20 @@
     @if (!empty($secondaryQuery->posts))
 
         @if ($displaySecondaryMap && !empty($secondaryQuery->pins))
+
             @openStreetMap([
                 'pins' => $secondaryQuery->pins,
                 'classList' => ['u-margin__top--4'],
-                'startPosition' => ['lat' => '56.046029', 'lng' => '12.693904', 'zoom' => 14],
                 'height' => '100vh',
                 'containerAware' => true,
-                'mapStyle' => 'default',
                 'fullWidth' => true
             ])
                 @slot('sidebarContent')
+                    @includeIf('partials.sidebar', [
+                        'id' => 'right-sidebar',
+                        'classes' => ['o-grid', 'openstreetmap-right-sidebar'],
+                    ])
+
                     @collection([
                         'classList' => ['o-grid', 'o-grid--horizontal'],
                         'attributeList' => [
