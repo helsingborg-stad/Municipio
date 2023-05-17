1cb805897ded652b5c745a0b1c2a034ec124488c
Anna Johansson <anna@thehappyhipster.se>
2023-05-17 14:53:39 +0200

    feat: Add 'page' import to main.scss refactor: Remove extra space in CustomTemplates.php feat: Add 'Page (centered)' template

---
+++ views/v3/page-centered.blade.php
@@ -0,0 +1,40 @@
+@extends('templates.single')
+
+@section('content')
+<div class="content u-margin__left--auto u-margin__right--auto">
+    {!! $hook->loopStart !!}
+
+    @includeIf('partials.sidebar', ['id' => 'content-area-top', 'classes' => ['o-grid']])
+
+    @section('loop')
+    @includeIf('partials.loop')
+    @show
+
+    @includeIf('partials.sidebar', ['id' => 'content-area', 'classes' => ['o-grid']])
+
+    @includeWhen($displayQuicklinksAfterContent, 'partials.navigation.fixed')
+
+    @includeWhen($displaySecondaryQuery, 'partials.secondary', [
+    'posts' => $secondaryQuery->posts ?? [],
+    'postType' => $secondaryPostType ?? null,
+    ])
+    {!! $hook->loopEnd !!}
+</div>
+@stop
+@section('sidebar-right')
+@stop
+
+@section('below')
+<div class="content u-margin__left--auto u-margin__right--auto">
+    @includeIf('partials.sidebar', ['id' => 'content-area-bottom', 'classes' => ['o-grid']])
+    @includeWhen(empty($isBlogStyle), 'partials.signature', [
+    'classList' => ['u-margin__y--2'],
+    ])
+</div>
+<div class="content u-margin__left--auto u-margin__right--auto">
+    <!-- Comments -->
+    @section('article.comments.before')@show
+    @includeIf('partials.comments')
+    @section('article.comments.after')@show
+</div>
+@stop
