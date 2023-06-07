68b2ddde9d238366915be21b753ffb744ffbe7d7
NiclasNorin <niclas.norin@frontwalker.se>
2023-06-07 13:32:54 +0200

    reset perPage

--- views/v3/partials/secondary.blade.php
+++ views/v3/partials/secondary.blade.php
@@ -160,7 +160,7 @@
                         'classList' => ['u-padding__top--8', 'u-padding__bottom--6', 'u-justify-content--center'],
                         'useJS' => true,
                         'current' => 1,
-                        'perPage' => 4,
+                        'perPage' => 20,
                         'pagesToShow' => 4,
                         'keepDOM' => true,
                     ])
