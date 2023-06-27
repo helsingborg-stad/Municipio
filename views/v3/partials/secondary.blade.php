c1542e07e9bc81d5f7f5cb6205b26d0fce26e6a8
NiclasNorin <niclas.norin@frontwalker.se>
2023-06-27 15:27:50 +0200

    Fix: removed unnecessary pagination attribute

--- views/v3/partials/secondary.blade.php
+++ views/v3/partials/secondary.blade.php
@@ -153,7 +153,6 @@
                     @endcollection
 
                     @pagination([
-                        'list' => [['href' => '?pagination=1', 'label' => 'Page 1']],
                         'classList' => ['u-padding__top--8', 'u-padding__bottom--6', 'u-justify-content--center'],
                         'useJS' => true,
                         'current' => 1,
