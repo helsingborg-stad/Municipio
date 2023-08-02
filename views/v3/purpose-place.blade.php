9036cb5d2df6892c0edbce33aa1b6f2f20c45051
NiclasNorin <niclas.norin@frontwalker.se>
2023-08-02 09:14:21 +0200

    Feat: Single place osm

--- views/v3/purpose-place.blade.php
+++ views/v3/purpose-place.blade.php
@@ -1,5 +1,143 @@
 @extends('templates.single')
+
 @section('hero-top-sidebar')
+
+@openStreetMap([
+        'classList' => ['u-margin__top--4'],
+        'height' => '100vh',
+        'containerAware' => true,
+        'fullWidth' => true,
+        'expanded' => true,
+        'startPosition' => array('lat' => $post->location['lat'], 'lng' => $post->location['lng'], 'zoom' => 17)
+    ])
+@slot('sidebarContent')
+@if (!empty($featuredImage['src']))
+        @hero([
+            'image' => $featuredImage['src'],
+        ])
+        @endhero
+    @endif
+
+    <div class="o-container">
+        {{-- @php echo '<pre>' . print_r( $post->location, true ) . '</pre>';@endphp --}}
+        @paper([
+            'attributeList' => [
+                'style' => !empty($featuredImage['src']) ? 'transform:translateY(calc(max(-50%, -50px)))' : 'margin-top: 32px',
+                'data-js-map-location' => json_encode($post->location['pin']),
+
+            ],
+            'classList' => ['u-padding--6']
+        ])
+            @group([
+                'justifyContent'=> 'space-between',
+            ])
+                @typography([
+                    'element' => 'h1',
+                    'variant' => 'h1'
+                ])
+                    {{ $post->postTitleFiltered }}
+                @endtypography
+                @if ($post->callToActionItems['floating'])
+                    @icon($post->callToActionItems['floating'])
+                    @endicon
+                @endif
+            @endgroup
+            <div class="o-grid">
+                <div class="o-grid-12@sm o-grid-9@md o-grid-9@lg">
+                    @typography([])
+                        {!! $post->postContentFiltered !!}
+                    @endtypography
+                </div>
+                <div class="o-grid-12@sm o-grid-3@md o-grid-3@lg">
+
+                    @if (!empty($post->placeInfo))
+                        @listing([
+                            'list' => $post->placeInfo,
+                            'icon' => false,
+                            'classList' => [
+                                'unlist',
+                                'u-padding__top--2@xs',
+                                'u-padding__top--2@sm',
+                                'u-padding__bottom--3',
+                                'u-margin__top--2'
+                            ],
+                            'padding' => 4
+                        ])
+                        @endlisting
+                    @endif
+
+                    @if (!empty($post->bookingLink))
+                        @button([
+                            'text' => $lang->bookHere,
+                            'color' => 'primary',
+                            'style' => 'filled',
+                            'href' => $post->bookingLink,
+                            'classList' => ['u-width--100'],
+                        ])
+                        @endbutton
+                    @endif
+
+                </div>
+            </div>
+        @endpaper
+        @foreach ($relatedPosts as $postType => $posts)
+                @group([
+                    'justifyContent' => 'space-between'
+                ])
+                    @typography([
+                        'element' => 'h2'
+                    ])
+                        {{ $lang->related }} {{ $postType }}
+                    @endtypography
+                    @if (!empty(get_post_type_archive_link($postType)))
+                        @link([
+                            'href' => get_post_type_archive_link($postType)
+                        ])
+                            {{ $lang->showAll }}
+                        @endlink
+                    @endif
+                @endgroup
+                @group([
+                    'fluidGrid' => true,
+                    'classList' => [
+                        'o-grid'
+                    ]
+                ])
+                @foreach ($posts as $post)
+                        @segment([
+                            'layout' => 'card',
+                            'title' => $post->postTitleFiltered,
+                            'content' => $post->excerptShorter,
+                            'image' => $post->thumbnail['src'],
+                            'buttons' => [['text' => $labels['readMore'], 'href' => $post->permalink]],
+                            'tags' => $post->termsUnlinked,
+                            'meta' => $post->readingTime,
+                            'icon' => $post->termIcon['icon'] ? $post->termIcon : false,
+                            'attributeList' => [
+                                'data-js-map-location' => json_encode($post->location['pin']),
+                            ]
+                        ])
+                        @if ($post->callToActionItems['floating'])
+                            @slot('floating')
+                                @icon($post->callToActionItems['floating'])
+                                @endicon
+                            @endslot
+                        @endif
+                        @endsegment
+                @endforeach
+                @endgroup
+        @endforeach
+    </div>
+    @endslot
+@endopenStreetMap
+@stop
+
+@section('helper-navigation')
+@stop
+
+@section('content')
+@stop
+{{-- @section('hero-top-sidebar')
     @if (!empty($featuredImage['src']))
         @hero([
             'image' => $featuredImage['src'],
@@ -121,3 +259,6 @@
         </div>
     @endforeach
 @stop
+ --}}
+
+ 
