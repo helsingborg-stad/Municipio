19349036f06ea064393abe2943b439f671b90b87
Sebastian Thulin <sebastian.thulin@helsingborg.se>
2023-08-18 11:38:19 +0200

    Mitigate errors on archive date formats. Set defaults.

--- library/Controller/Archive.php
+++ library/Controller/Archive.php
@@ -561,6 +561,17 @@ class Archive extends \Municipio\Controller\BaseController
     {
         $preparedPosts = [];
 
+        //Set defaults
+        if (is_array($posts) && !empty($posts)) {
+            foreach ($posts as $post) {
+                if (!is_object($post)) {
+                    continue;
+                }
+                $post->archiveDateFormat = $archiveProps->dateFormat ?? 'default';
+                $post->archiveDate = false;
+            }
+        }
+
         if (!isset($archiveProps->dateField) || is_null($archiveProps->dateField) || $archiveProps->dateField === 'none') {
             return $posts;
         }
@@ -578,6 +589,10 @@ class Archive extends \Municipio\Controller\BaseController
                 if (!is_object($post)) {
                     continue;
                 }
+
+                //Defaults 
+                $post->archiveDateFormat = $archiveProps->dateFormat ?? 'default';
+                $post->archiveDate = false;
 
                 if (!is_null($archiveProps->dateField)) {
                     if ($isMetaKey === true) {
