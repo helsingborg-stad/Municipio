968694096678b194898816bd89166ab19b9a53db
Sebastian Thulin <sebastian.thulin@helsingborg.se>
2023-09-01 09:56:40 +0200

    Update build.php

--- build.php
+++ build.php
@@ -5,6 +5,12 @@ if (php_sapi_name() !== 'cli') {
     exit(0);
 }
 
+/* Parameters: 
+ --no-composer      Does not install vendors. Just create the autoloader.
+ --cleanup          Remove removeables. 
+ --allow-gulp       Allow gulp to be used. 
+*/
+
 // Any command needed to run and build plugin assets when newly cheched out of repo.
 $buildCommands = [];
 
@@ -26,9 +32,11 @@ if(file_exists('package.json') && file_exists('package-lock.json')) {
 }
 
 //Run build if package-lock.json is found
-if(file_exists('package-lock.json')) {
+if(file_exists('package-lock.json') && !file_exists('gulp.js')) {
     $buildCommands[] = 'npx --yes browserslist@latest --update-db';
     $buildCommands[] = 'npm run build';
+} elseif(file_exists('package-lock.json') && file_exists('gulp.js') && is_array($argv) && in_array('--allow-gulp', $argv)) {
+    $buildCommands[] = 'gulp';
 }
 
 // Files and directories not suitable for prod to be removed.
@@ -36,6 +44,7 @@ $removables = [
     '.git',
     '.gitignore',
     '.github',
+    '.gitattributes',
     'build.php',
     '.npmrc',
     'composer.json',
@@ -45,7 +54,14 @@ $removables = [
     'package-lock.json',
     'package.json',
     'phpunit.xml.dist',
-    'README.md'
+    'README.md',
+    'gulpfile.js',
+    './node_modules/',
+    './source/sass/',
+    './source/js/',
+    'LICENSE',
+    'babel.config.js',
+    'yarn.lock'
 ];
 
 $dirName = basename(dirname(__FILE__));
@@ -65,7 +81,7 @@ foreach ($buildCommands as $buildCommand) {
 }
 
 // Remove files and directories if '--cleanup' argument is supplied to save local developers from disasters.
-if (isset($argv[1]) && $argv[1] === '--cleanup') {
+if(is_array($argv) && in_array('--cleanup', $argv)) {
     foreach ($removables as $removable) {
         if (file_exists($removable)) {
             print "Removing $removable from $dirName\n";
