d46dbc35bf5ba89f0c09506dec28c43fe7b87cfc
Niclas Norin <niclas.norin@helsingborg.se>
2023-09-14 12:30:21 +0200

    Feat: Adding acf fields

---
+++ library/AcfFields/php/options-menu-mega.php
@@ -0,0 +1,77 @@
+<?php 
+
+if (function_exists('acf_add_local_field_group')) {
+    acf_add_local_field_group(array(
+    'key' => 'group_6502be085ee3b',
+    'title' => __('Mega menu', 'municipio'),
+    'fields' => array(
+        0 => array(
+            'key' => 'field_6502be0a752a8',
+            'label' => __('Mega menu button label', 'municipio'),
+            'name' => 'mega_menu_button_label',
+            'type' => 'text',
+            'instructions' => '',
+            'required' => 0,
+            'conditional_logic' => 0,
+            'wrapper' => array(
+                'width' => '',
+                'class' => '',
+                'id' => '',
+            ),
+            'default_value' => '',
+            'maxlength' => '',
+            'placeholder' => '',
+            'prepend' => '',
+            'append' => '',
+        ),
+        1 => array(
+            'key' => 'field_6502be35752a9',
+            'label' => __('Mega menu button icon', 'municipio'),
+            'name' => 'mega_menu_button_icon',
+            'type' => 'select',
+            'instructions' => '',
+            'required' => 0,
+            'conditional_logic' => 0,
+            'wrapper' => array(
+                'width' => '',
+                'class' => '',
+                'id' => '',
+            ),
+            'choices' => array(
+            ),
+            'default_value' => false,
+            'return_format' => 'value',
+            'multiple' => 0,
+            'allow_null' => 0,
+            'ui' => 0,
+            'ajax' => 0,
+            'placeholder' => '',
+        ),
+    ),
+    'location' => array(
+        0 => array(
+            0 => array(
+                'param' => 'nav_menu',
+                'operator' => '==',
+                'value' => 'location/hamburger-menu',
+            ),
+        ),
+    ),
+    'menu_order' => 0,
+    'position' => 'normal',
+    'style' => 'default',
+    'label_placement' => 'left',
+    'instruction_placement' => 'label',
+    'hide_on_screen' => '',
+    'active' => true,
+    'description' => '',
+    'show_in_rest' => 0,
+    'acfe_display_title' => '',
+    'acfe_autosync' => array(
+        0 => 'json',
+    ),
+    'acfe_form' => 0,
+    'acfe_meta' => '',
+    'acfe_note' => '',
+));
+}
