88f79a42ffaae09b822f2b3b35178cfe8fb9bf8f
Anna Johansson <anna@thehappyhipster.se>
2023-09-27 16:42:10 +0000

    fix: Leaving appendStructuredData() in place for now, but deactivating it for the time being. It is not working properly and needs to be fixed. See commit #1

--- library/Controller/ContentType/Place.php
+++ library/Controller/ContentType/Place.php
@@ -51,5 +51,62 @@ class Place extends ContentTypeFactory
         }
         return 'https://www.google.com/maps/dir/?api=1&destination=' . $location['lat'] . ',' . $location['lng'] . '&travelmode=transit';
     }
-    
+    /**
+    * Appends the structured data array (used for schema.org markup) with additional data
+    *
+    * @param array $structuredData The structured data to append location data to.
+    * @param string $postType The post type of the post.
+    * @param int $postId The ID of the post to retrieve location data for.
+    *
+    * @return array The updated structured data.
+    *
+    */
+    public function appendStructuredData(array $structuredData, string $postType, int $postId): array
+    {
+        if (empty($postId)) {
+            return $structuredData;
+        }
+
+        $locationMetaKeys = ['map', 'location']; // Post meta keys we'l check for location data.
+        $additionalData = ['location' => []];
+
+        foreach ($locationMetaKeys as $key) {
+            $location = get_post_meta($postId, $key, true);
+            if (empty($location)) {
+                continue;
+            }
+
+            // General address
+            if (!empty($location['formatted_address'])) {
+                $additionalData['location'][] = [
+                    '@type'   => 'Place',
+                    'address' => $location['formatted_address'],
+                ];
+            } elseif (!empty($location['address'])) {
+                $additionalData['location'][] = [
+                    '@type'   => 'Place',
+                    'address' => $location['address'],
+                ];
+            }
+
+            // Coordinates
+            if (!empty($location['lat']) && !empty($location['lng'])) {
+                $additionalData['location'][] = [
+                    '@type'     => 'GeoCoordinates',
+                    'latitude'  => $location['lat'],
+                    'longitude' => $location['lng'],
+                ];
+            }
+
+            // Country
+            if (!empty($location['country'])) {
+                $additionalData['location'][] = [
+                    '@type'          => 'PostalAddress',
+                    'addressCountry' => $location['country'],
+                ];
+            }
+        }
+
+        return array_merge($structuredData, $additionalData);
+    }
 }
