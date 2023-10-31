6258c15930094c6c43ff7b6a4da3aeca8e6509cf
NiclasNorin <niclasn@outlook.com>
2023-10-31 12:12:13 +0100

    feat: clear oembed cache

--- library/Content/Cache.php
+++ library/Content/Cache.php
@@ -17,5 +17,15 @@ class Cache
             $post = get_post($postId);
             \Municipio\Helper\Cache::clearCache($postId, $post);
         });
+
+        $this->clearWPOembedCache();
+    }
+
+    public function clearWPOembedCache() {
+        global $wpdb;
+        if (empty(get_option('cleared_wp_oembed_cache'))) {
+            $wpdb->query("DELETE FROM {$wpdb->postmeta} WHERE meta_key LIKE '_oembed_%' AND meta_value LIKE '<iframe%'");
+            update_option('cleared_wp_oembed_cache', true);
+        }
     }
 }
