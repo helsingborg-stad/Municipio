d9bbdee5eaf07a51c70271575f7237bb3cc2917b
Thor Brink <thor.brink@helsingborg.se>
2023-11-09 09:41:03 +0000

    fix: notice when eventsTitle not set

--- library/Controller/ContentType/School/SchoolDataPreparer.php
+++ library/Controller/ContentType/School/SchoolDataPreparer.php
@@ -548,6 +548,7 @@ class SchoolDataPreparer implements DataPrepearerInterface
     private function appendEventData()
     {
         $this->data['events'] = null;
+        $this->data['eventsTitle'] = null;
         
         if( !post_type_exists(self::EVENT_POST_TYPE) || !taxonomy_exists(self::EVENT_TAXONOMY) ) {    
             return;
