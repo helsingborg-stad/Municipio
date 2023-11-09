25b639e4a7bcf90ae42b5c586ed35398c588d7f2
Niclas Norin <niclas.norin@helsingborg.se>
2023-11-09 11:00:03 +0100

    chore: adding hidden validation field

---
+++ library/AcfFields/php/hidden-validation.php
@@ -0,0 +1,50 @@
+<?php 
+
+if (function_exists('acf_add_local_field_group')) {
+    acf_add_local_field_group(array(
+    'key' => 'group_654a2a57e6897',
+    'title' => __('Hidden validation', 'municipio'),
+    'fields' => array(
+        0 => array(
+            'key' => 'field_654a2a58ca4e9',
+            'label' => __('Hidden validation', 'municipio'),
+            'name' => 'hidden_validation',
+            'type' => 'acfe_hidden',
+            'instructions' => '',
+            'required' => 0,
+            'conditional_logic' => 0,
+            'wrapper' => array(
+                'width' => '',
+                'class' => '',
+                'id' => '',
+            ),
+            'default_value' => '',
+        ),
+    ),
+    'location' => array(
+        0 => array(
+            0 => array(
+                'param' => 'post_type',
+                'operator' => '==',
+                'value' => 'all',
+            ),
+        ),
+    ),
+    'menu_order' => 0,
+    'position' => 'normal',
+    'style' => 'seamless',
+    'label_placement' => 'left',
+    'instruction_placement' => 'label',
+    'hide_on_screen' => '',
+    'active' => true,
+    'description' => '',
+    'show_in_rest' => 0,
+    'acfe_display_title' => '',
+    'acfe_autosync' => array(
+        0 => 'json',
+    ),
+    'acfe_form' => 0,
+    'acfe_meta' => '',
+    'acfe_note' => '',
+));
+}
