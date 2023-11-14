cc2176431372a900fc65e5333d28e03231a41fd5
Niclas Norin <niclas.norin@helsingborg.se>
2023-11-14 15:59:03 +0100

    fix: removing font-weights since its crashing if there is no font

--- views/v3/partials/content/pdf/style.blade.php
+++ views/v3/partials/content/pdf/style.blade.php
@@ -1,14 +1,17 @@
 <style>
-    @font-face {
-        font-family: 'helsingborg-sans-medium';
-        font-display:swap;
-        font-style: normal;
-        src: url('https://media.helsingborg.se/uploads/networks/1/2023/04/helsingborg-sans-medium.woff') format("woff");
-        font-weight: 600;
-        unicode-range: U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6, U+02DA, U+02DC, U+0304, U+0308, U+0329, U+2000-206F, U+2074, U+20AC, U+2122, U+2191, U+2193, U+2212, U+2215, U+FEFF, U+FFFD;
+
+    /* Typography */
+    article,
+    section:not(.pdf-toc),
+    p {
+        font-family: 'Roboto', sans-serif;
+        font-weight: 400;
     }
-    
-    /* Headings */
+
+    .lead {
+        font-weight: bold;
+    }
+
     h1,
     h2,
     h3,
@@ -16,46 +19,39 @@
     h5,
     h6 {
         margin-top: 0;
-        font-weight: 600;
-        font-family: 'helsingborg-sans-medium', sans-serif;
+        font-weight: 700;
+        font-family: 'Roboto', sans-serif;
     }
 
     h1,
     .pdf-heading--h1 {
-        font-family: 'helsingborg-sans-medium', sans-serif;
         font-size: {{ !empty($styles['typography_h1']['font-size']) ? $styles['typography_h1']['font-size'] : '48px' }};
-        line-height: {{ !empty($styles['typography_h1']['line-height']) ? $styles['typography_h1']['line-height'] : '1.25' }};
-        font-weight: {{ !empty($styles['typography_h1']['variant']) ? $styles['typography_h1']['variant'] : 'bold'}}
+        line-height: 1.1;
     }
 
     h2 {
         font-size: {{ !empty($styles['typography_h2']['font-size']) ? $styles['typography_h2']['font-size'] : '32px' }};
         line-height: {{ !empty($styles['typography_h2']['line-height']) ? $styles['typography_h2']['line-height'] : '1.25' }};
-        font-weight: {{ !empty($styles['typography_h2']['variant']) ? $styles['typography_h2']['variant'] : 'bold'}}
     }
 
     h3 {
         font-size: {{ !empty($styles['typography_h3']['font-size']) ? $styles['typography_h3']['font-size'] : '24px' }};
         line-height: {{ !empty($styles['typography_h3']['line-height']) ? $styles['typography_h3']['line-height'] : '1.25' }};
-        font-weight: {{ !empty($styles['typography_h3']['variant']) ? $styles['typography_h3']['variant'] : 'bold'}}   
     }
 
     h4 {
         font-size: {{ !empty($styles['typography_h4']['font-size']) ? $styles['typography_h4']['font-size'] : '20px' }};
         line-height: {{ !empty($styles['typography_h4']['line-height']) ? $styles['typography_h4']['line-height'] : '1.25' }};
-        font-weight: {{ !empty($styles['typography_h4']['variant']) ? $styles['typography_h4']['variant'] : 'bold'}}
     }
 
     h5 {
         font-size: {{ !empty($styles['typography_h5']['font-size']) ? $styles['typography_h5']['font-size'] : '18px' }};
         line-height: {{ !empty($styles['typography_h5']['line-height']) ? $styles['typography_h5']['line-height'] : '1.25' }};
-        font-weight: {{ !empty($styles['typography_h5']['variant']) ? $styles['typography_h5']['variant'] : 'bold'}}
     }
 
-    h5 {
+    h6 {
         font-size: {{ !empty($styles['typography_h6']['font-size']) ? $styles['typography_h6']['font-size'] : '16px' }};
         line-height: {{ !empty($styles['typography_h6']['line-height']) ? $styles['typography_h6']['line-height'] : '1.25' }};
-        font-weight: {{ !empty($styles['typography_h6']['variant']) ? $styles['typography_h6']['variant'] : 'bold'}}
     }
 
     h2.pdf-post__heading {
@@ -116,7 +112,7 @@
     }
 
     body {
-        font-size: 1rem;
+        font-size: 16px;
     }  
 
     .pdf-container {
