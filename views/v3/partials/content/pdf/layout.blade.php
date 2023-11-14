ac57ad737f1752f12a83056cb28de8b188e87799
Niclas Norin <niclas.norin@helsingborg.se>
2023-11-14 17:00:12 +0100

    chore: remove page code

--- views/v3/partials/content/pdf/layout.blade.php
+++ views/v3/partials/content/pdf/layout.blade.php
@@ -48,17 +48,6 @@
 	');
 </script>
     
-{{-- <script type="text/php">
-    if (isset($pdf)) {
-        $text = "page {PAGE_NUM} / {PAGE_COUNT}";
-        $size = 10;
-        $font = $fontMetrics->getFont("Verdana");
-        $width = $fontMetrics->get_text_width($text, $font, $size) / 2;
-        $x = ($pdf->get_width() - $width) / 2;
-        $y = $pdf->get_height() - 35;
-        $pdf->page_text($x, $y, $text, $font, $size);
-    }
-</script> --}}
 </body>
 
 <!-- /Article pages -->
