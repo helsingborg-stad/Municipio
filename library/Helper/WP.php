07fe80069e7922e36a1e25b907aa3088dfb0dc95
Thor Brink <thor.brink@helsingborg.se>
2023-11-15 14:05:59 +0000

    docs: WP::getPost

--- library/Helper/WP.php
+++ library/Helper/WP.php
@@ -131,6 +131,20 @@ class WP
         );
     }
 
+    /**
+     * Retrieves post data given a post ID or post object.
+     *
+     * @param int|WP_Post|null $post Optional. Post ID or post object. `null`, `false`, `0` and other PHP falsey values
+     * return the current global post inside the loop. A numerically valid post ID that
+     * points to a non-existent post returns `null`. Defaults to global $post.
+     * @param string $output Optional. The required return type. One of OBJECT, ARRAY_A, or ARRAY_N, which
+     * correspond to a WP_Post object, an associative array, or a numeric array,
+     * respectively. Default OBJECT.
+     * @param string $filter Optional. Type of filter to apply. Accepts 'raw', 'edit', 'db',
+     * or 'display'. Default 'raw'.
+     * @return WP_Post|array|null Type corresponding to $output on success or null on failure.
+     * When $output is OBJECT, a `WP_Post` instance is returned.
+     */
     public static function getPost($post = null, $output = OBJECT, $filter = 'raw') {
 
         if (self::isRemotePostID($post)) {
