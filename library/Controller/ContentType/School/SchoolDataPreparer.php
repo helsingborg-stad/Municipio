07f23f290e3ea168a13e8af5d090f8e965835709
Anna Johansson <anna@thehappyhipster.se>
2023-11-16 06:02:33 +0000

    feat: Fallback to true if no value is set for displayOnWebsite

--- library/Controller/ContentType/School/SchoolDataPreparer.php
+++ library/Controller/ContentType/School/SchoolDataPreparer.php
@@ -278,7 +278,7 @@ class SchoolDataPreparer implements DataPrepearerInterface
     {
         $this->data['application'] = [];
 
-        $this->data['application']['displayOnWebsite'] = (bool) $this->postMeta->ctaApplication->display_on_website;
+        $this->data['application']['displayOnWebsite'] = (bool) $this->postMeta->ctaApplication->display_on_website ?: true;
 
         $this->data['application']['title'] = $this->postMeta->ctaApplication->title ?: sprintf(__('Do you want to apply to %s?', 'municipio'), get_queried_object()->post_title);
         $this->data['application']['description'] = $this->postMeta->ctaApplication->description ?: '';
