b235a98b4e88280d6b2b90f09bad39f767b3aec3
NiclasNorin <niclasn@outlook.com>
2023-11-16 11:29:00 +0100

    chore: structuring

--- library/Api/Pdf/CreatePdf.php
+++ library/Api/Pdf/CreatePdf.php
@@ -16,6 +16,25 @@ class CreatePdf
     }
 
     public function renderView($ids) {
+
+        [$posts, $postTypes] = $this->getPosts($ids);
+        $cover = $this->getCover($postTypes);
+        $styles = $this->getThemeMods();
+        $fonts = $this->getFonts($styles);
+
+        if (!empty($posts)) {
+            $html = render_blade_view('partials.content.pdf.layout', [
+                'posts'     => $posts,
+                'styles'    => $styles,
+                'cover'     => $cover,
+                'fonts'     => $fonts
+            ]);
+
+            $this->renderPdf($html);
+        }
+    }
+
+    private function getPosts($ids) {
         $posts = [];
         $postTypes = [];
         if (!empty($ids) && is_array($ids)) {
@@ -30,20 +49,7 @@ class CreatePdf
                 }
             }
         }
-        
-        $cover = $this->getCover($postTypes);
-        $styles = $this->getThemeMods();
-        $fonts = $this->getFonts($styles);
-
-        if (!empty($posts)) {
-            $html = render_blade_view('partials.content.pdf.layout', [
-                'posts'     => $posts,
-                'styles'    => $styles,
-                'cover'     => $cover,
-            ]);
-
-            $this->renderPdf($html);
-        }
+        return [$posts, $postTypes];
     }
 
     private function getCover($postTypes) {
@@ -69,41 +75,52 @@ class CreatePdf
             'heading'       => $heading,
             'introduction'  => $introduction,
             'cover'         => $cover,
-            'emblem'        => $emblem
+            'emblem'        => $emblem,
         ];
     }
 
     private function getFonts($styles) {
-        // $args = array(
-        //     'post_type'      => 'attachment',
-        //     'posts_per_page' => -1,
-        //     'post_status'    => 'inherit',
-        //     'post_mime_type' => 'application/font-woff'
-        // );
-
-        // $customFonts = new \WP_Query($args);
-
-        // $heading = $styles['typography_heading'];
-        // $base = $styles['typography_base'];
-    
-        // if (!empty($customFonts->posts) && (!empty($heading['font-family']) || $base['font-family']) && is_array($customFonts->posts)) {
-        //     foreach ($customFonts->posts as $font) {
-        //         if (!empty($font->post_name)) {
-        //             if ($font->post_name == $heading['font-family']) {
-        //                 $heading['src'] = !empty($font->guid) ? $font->guid : '';
-        //             }
+        $args = array(
+            'post_type'      => 'attachment',
+            'posts_per_page' => -1,
+            'post_status'    => 'inherit',
+            'post_mime_type' => 'font/ttf'
+        );
+        
+        $customFonts = new \WP_Query($args);
+        $heading = $styles['typography_heading'];
+        $base = $styles['typography_base'];
+        
+        if (!empty($customFonts->posts) && (!empty($heading['font-family']) || $base['font-family']) && is_array($customFonts->posts)) {
+            foreach ($customFonts->posts as $font) {
+                if (!empty($font->post_title)) {
+                    if ($font->post_title == $heading['font-family']) {
+                        $heading['src'] = !empty($font->ID) ? wp_get_attachment_url( $font->ID ) : '';
+                    }
                     
-        //             if ($font->post_name == $base['font-family']) {
-        //                 $base['src'] = !empty($font->guid) ? $font->guid : '';
-        //             }
-        //         }
-        //     }
-        // }
+                    if ($font->post_title == $base['font-family']) {
+                        $base['src'] = !empty($font->ID) ? wp_get_attachment_url( $font->ID ) : '';
+                    } 
+                }
+            }
+        }
 
-        // return [
-        //     'base' => $base,
-        //     'heading' => $heading
-        // ];
+        if (empty($base['src']) && !empty($base['font-family'])) {
+            $base['google-font'] = $this->createGoogleFontImport($base['font-family']);
+        }
+
+        if (empty($heading['src']) && !empty($heading['font-family'])) {
+            $heading['google-font'] = $this->createGoogleFontImport($heading['font-family']);
+        }
+
+        return [
+            'base' => $base,
+            'heading' => $heading
+        ];
+    }
+
+    private function createGoogleFontImport($fontFamily) {
+        return 'https://fonts.googleapis.com/css2?family=' . urlencode($fontFamily) . ':wght@100;300;400;500;600;700;800;900&display=swap';
     }
 
     private function getThemeMods() {
