5d44aed69ae85eb63d64f22a42cc4514db0d730b
NiclasNorin <niclasn@outlook.com>
2023-11-21 17:03:03 +0100

    fix: archive endpoint

--- library/Api/Pdf/PdfArchiveEndpoint.php
+++ library/Api/Pdf/PdfArchiveEndpoint.php
@@ -5,12 +5,12 @@ namespace Municipio\Api\Pdf;
 use Municipio\Api\RestApiEndpoint;
 use WP_REST_Request;
 use WP_REST_Response;
+use Municipio\Api\Pdf\PdfHelper;
 
 class PdfArchiveEndpoint extends RestApiEndpoint
 {
     private const NAMESPACE = 'pdf/v2';
-    // private const ROUTE = '/id=(?P<id>\d+(?:,\d+)*)';
-    private const ROUTE = '/(?P<postType>[a-zA-Z]+)/(?P<query>.*)';
+    private const ROUTE = '/(?P<postType>[a-zA-Z]+)';
     
     public function handleRegisterRestRoute(): bool
     {
@@ -22,8 +22,43 @@ class PdfArchiveEndpoint extends RestApiEndpoint
 
     public function handleRequest(WP_REST_Request $request): WP_REST_Response
     {
-        $query = $request->get_param('query');
+        $pdfHelper = new PdfHelper();
         $postType = $request->get_param('postType');
-        echo '<pre>' . print_r( $query, true ) . '</pre>';die;
+        $queryParams = $request->get_query_params();
+
+        $posts = $this->getArchivePosts($postType, $queryParams);
+        
+        if (!empty($posts)) {
+            $cover = $pdfHelper->getCoverFieldsForPostType($postType);
+            $pdf = new \Municipio\Api\Pdf\CreatePdf();
+            return $pdf->renderView($posts, $cover);
+        }
+    }
+
+    private function getArchivePosts($postType = false, $queryParams = false) {
+        $posts = [];
+        $args = [
+            'post_type' => $postType,
+            'tax_query' => [],
+            'posts_per_page' => -1
+        ];
+        foreach ($queryParams as $key => $values) {
+            $args['tax_query'][] = [
+                'taxonomy' => $key,
+                'field' => 'slug',
+                'terms' => $values
+            ];
+        }
+        
+        $query = new \WP_Query($args);
+        
+        if (!empty($query->posts)) {
+            foreach($query->posts as &$post) {
+                $post = \Municipio\Helper\Post::preparePostObject($post);
+                array_push($posts, $post);
+            }
+        }
+        
+        return $posts;
     }
 }
