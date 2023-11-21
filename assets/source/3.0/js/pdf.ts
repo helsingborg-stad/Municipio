a2e2a4d1a275fc19e09467607ec449d6085513f7
NiclasNorin <niclasn@outlook.com>
2023-11-21 17:01:39 +0100

    feat: ts render with correct url

--- assets/source/3.0/js/pdf.ts
+++ assets/source/3.0/js/pdf.ts
@@ -1,39 +1,64 @@
 // await fetch(ajaxurl + "/?action=generate_pdf", {
     class PdfGen {
         button: Element;
-        isSingle: boolean;
         attr: string;
+        isSingle: boolean;
+        pageId: string | null;
+        postType: string | null;
 
         constructor(button: Element, attr: string) {
             this.button = button;
             this.attr = attr;
-            this.isSingle = this.checkIfArchiveOrSingle();
+            this.isSingle = attr === 'single' ? true : false;
+            this.pageId = document.body.getAttribute('data-js-page-id');
+            this.postType = document.body.getAttribute('data-js-post-type');
 
-            this.addListeners();
-        }
-
-        checkIfArchiveOrSingle() {
-            return /^[\d,]+$/.test(this.attr)
-        }
-    
-        addListeners() {
-            this.button.addEventListener('click', (e) => {
+            button.addEventListener('click', (e) => {
                 e.preventDefault();
+                console.log("click");
                 this.isSingle ? this.fetchPdfForSingle() : this.fetchPdfForArchive();
             });
         }
     
         async fetchPdfForArchive() {
-            const path = window.location.pathname;
             const queryString = window.location.search;
+            const searchParams = new URLSearchParams(queryString);
+            
+            if (this.postType) {
+                searchParams.delete('paged');
+                window.location.href = '/wp-json/pdf/v2/' + this.postType + '/' + (searchParams.toString() ? '?' + searchParams.toString() : '');
+                /* try {
+                    const response = await fetch( '/wp-json/pdf/v2/' + this.postType + '/' + (queryString ? queryString : ''), {
+                        method: 'GET',
+                    });
 
-            const urlPart = path + queryString;
+                    if (!response.ok) {
+                        throw new Error(`HTTP error! Status: ${response.status}`);
+                    }
+                    const blob = await response.blob();
+                    const url = window.URL.createObjectURL(blob);
+                    const a = document.createElement('a');
+                    a.style.display = 'none';
+                    a.href = url;
+                    // a.download = "pdf";
+                    document.body.appendChild(a);
+                    a.click();
+                    window.URL.revokeObjectURL(url);
+                } catch (error) {
+                    console.log('error with response: ' + error);
+                } */
 
-            console.log(urlPart);
+                
+                // window.location.href = '/wp-json/pdf/v2/' + this.postType + '/' + (queryString ? queryString : '');
+            }
         }
 
         async fetchPdfForSingle() {
-            try {
+            if (this.pageId) {
+                window.location.href = '/wp-json/pdf/v2/id=' + this.pageId;
+            }
+        
+            /* try {
                 const response = await fetch("/wp-json/pdf/v2/id=" + this.attr, {
                     method: 'GET',
                 });
@@ -54,7 +79,7 @@
                 window.URL.revokeObjectURL(url);
             } catch (error) {
                 console.error('Error fetching PDF:', error);
-            }
+            } */
         }
         
     }
