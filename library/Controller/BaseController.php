059569b342beb7b72830f7e736595c37d2664b54
NiclasNorin <103985736+NiclasNorin@users.noreply.github.com>
2023-11-23 10:54:49 +0100

    feat: original link filter (#771)
    * feat: original link filter
    
    * feat: use general filter
    
    * fix: correct array item

--- library/Controller/BaseController.php
+++ library/Controller/BaseController.php
@@ -304,6 +304,20 @@ class BaseController
             return (object) array_merge((array) $obj, $lang);
         }, 10, 1);
         
+        if (!empty($_SERVER['HTTP_HOST'])) {
+            add_filter("ComponentLibrary/Component/Attribute", function ($attributes) {
+                if (!empty($attributes['href'])) {
+                    $parsedUrl = parse_url($attributes['href']);
+
+                    if (!empty($parsedUrl['host']) && $parsedUrl['host'] !== $_SERVER['HTTP_HOST']) {
+                        $attributes['data-js-original-link'] = $attributes['href'];
+                    }
+                }
+
+                return $attributes;
+            }, 10, 1);
+        }
+        
 
         //Wordpress hooks
         $this->data['hook'] = (object) array(
