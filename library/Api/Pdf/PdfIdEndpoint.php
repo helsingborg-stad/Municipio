a2be1af4a6f4d863c404d0baebc004010f722caa
Sebastian Thulin <sebastian.thulin@helsingborg.se>
2023-12-08 13:32:45 +0100

    fix: trim post id:s

--- library/Api/Pdf/PdfIdEndpoint.php
+++ library/Api/Pdf/PdfIdEndpoint.php
@@ -79,7 +79,7 @@ class PdfIdEndpoint extends RestApiEndpoint
         $postTypes = [];
         if (!empty($ids) && is_array($ids)) {
             foreach ($ids as $id) {
-                $post = get_post($id);
+                $post = get_post(trim($id));
                 if (!empty($post->post_status) && $post->post_status == 'publish') {
                     $post = \Municipio\Helper\Post::preparePostObject($post);
                     if (!empty($post->postType)) {
