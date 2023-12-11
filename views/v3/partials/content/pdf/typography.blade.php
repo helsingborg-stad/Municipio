cf41a78867f99c4d6a4b898cc99cf51f0548e56f
Sebastian Thulin <sebastian.thulin@helsingborg.se>
2023-12-11 13:49:57 +0100

    fix: add cdata tags. Remove escaping.

--- views/v3/partials/content/pdf/typography.blade.php
+++ views/v3/partials/content/pdf/typography.blade.php
@@ -1,20 +1,21 @@
-<style>
+<style type="text/css">
+    /*<![CDATA[*/
     @if (!empty($fonts['localGoogleFonts']))
-        {{ $fonts['localGoogleFonts'] }}
+        {!! $fonts['localGoogleFonts'] !!}
     @endif
     @if(!empty($fonts['heading']['src']))
             @font-face {
-                font-family: {{ $fonts['heading']['font-family'] }};
-                font-weight: {{ $fonts['heading']['variant']}};
-                src: url({{$fonts['heading']['src']}}) format('truetype');
+                font-family: {!! $fonts['heading']['font-family'] !!};
+                font-weight: {!! $fonts['heading']['variant']!!};
+                src: url({!!$fonts['heading']['src']!!}) format('truetype');
             } 
     @endif
 
     @if(!empty($fonts['base']['src']))
             @font-face {
-                font-family: {{ $fonts['base']['font-family'] }};
-                font-weight: {{ $fonts['base']['variant']}};
-                src: url({{$fonts['heading']['src']}}) format('truetype');
+                font-family: {!! $fonts['base']['font-family'] !!};
+                font-weight: {!! $fonts['base']['variant']!!};
+                src: url({!!$fonts['heading']['src']!!}) format('truetype');
             }
     @endif
 
@@ -24,8 +25,8 @@
     h4,
     h5,
     h6 {
-        font-family: {{$fonts['heading']['font-family']}}, sans-serif;
-        font-weight: {{ $fonts['heading']['variant']}} !important;
+        font-family: {!!$fonts['heading']['font-family']!!}, sans-serif;
+        font-weight: {!! $fonts['heading']['variant']!!} !important;
         margin-top: 0;
     }
 
@@ -33,8 +34,8 @@
     section:not(.pdf-toc),
     p,
     .pdf-frontpage__introduction {
-        font-family: {{$fonts['base']['font-family']}};
-        font-weight: {{ $fonts['base']['variant']}};
+        font-family: {!!$fonts['base']['font-family']!!};
+        font-weight: {!! $fonts['base']['variant']!!};
     }
 
     .lead {
@@ -43,27 +44,28 @@
 
     h1,
     .pdf-heading--h1 {
-        font-size: {{ !empty($styles['typography_h1']['font-size']) ? $styles['typography_h1']['font-size'] : '48px' }};
+        font-size: {!! !empty($styles['typography_h1']['font-size']) ? $styles['typography_h1']['font-size'] : '48px' !!};
         line-height: 0.9;
     }
 
     h2 {
-        font-size: {{ !empty($styles['typography_h2']['font-size']) ? $styles['typography_h2']['font-size'] : '32px' }};
+        font-size: {!! !empty($styles['typography_h2']['font-size']) ? $styles['typography_h2']['font-size'] : '32px' !!};
     }
 
     h3 {
-        font-size: {{ !empty($styles['typography_h3']['font-size']) ? $styles['typography_h3']['font-size'] : '24px' }};
+        font-size: {!! !empty($styles['typography_h3']['font-size']) ? $styles['typography_h3']['font-size'] : '24px' !!};
     }
 
     h4 {
-        font-size: {{ !empty($styles['typography_h4']['font-size']) ? $styles['typography_h4']['font-size'] : '20px' }};
+        font-size: {!! !empty($styles['typography_h4']['font-size']) ? $styles['typography_h4']['font-size'] : '20px' !!};
     }
 
     h5 {
-        font-size: {{ !empty($styles['typography_h5']['font-size']) ? $styles['typography_h5']['font-size'] : '18px' }};
+        font-size: {!! !empty($styles['typography_h5']['font-size']) ? $styles['typography_h5']['font-size'] : '18px' !!};
     }
 
     h6 {
-        font-size: {{ !empty($styles['typography_h6']['font-size']) ? $styles['typography_h6']['font-size'] : '16px' }};
+        font-size: {!! !empty($styles['typography_h6']['font-size']) ? $styles['typography_h6']['font-size'] : '16px' !!};
     }
+    /*]]>*/
 </style>
