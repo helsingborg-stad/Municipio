2a96347687c558a0c482989d08ed0f127984ba93
Thor Brink <thor.brink@helsingborg.se>
2023-12-12 10:42:25 +0000

    test: Municipio\Helper\Data::isJson

---
+++ library/Helper/Data.isJson.Test.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace Municipio\Helper;
+
+use PHPUnit\Framework\TestCase;
+
+class DataIsJsonTest extends TestCase {
+
+    /**
+     * @testdox isJson returns true when valid json
+     * @covers \Municipio\Helper\Data::isJson
+     */
+    public function testIsJsonReturnsTrueWhenValidJson() {
+        // Given
+        $json = '{"foo": "bar"}';
+
+        // When
+        $result = Data::isJson($json);
+
+        // Then
+        $this->assertTrue($result);
+    }
+
+    /**
+     * @testdox isJson returns false when invalid json
+     * @dataProvider invalidJsonDataProvider
+     * @covers \Municipio\Helper\Data::isJson
+     */
+    public function testIsJsonReturnsFalseWhenInvalidJson($json) {
+        // When
+        $result = Data::isJson($json);
+
+        // Then
+        $this->assertFalse($result);
+    }
+
+    public function invalidJsonDataProvider() {
+        return [ ['{"foo": "bar"'], [null], [false], [array()], ['foo'], ];
+    }
+
+}
