c16509c6d1dfdeda4942b5a7a83d3f23df7262b7
Niclas Norin <niclas.norin@helsingborg.se>
2023-12-13 15:42:25 +0100

    fix: alt text

--- views/v3/partials/content/pdf/post.blade.php
+++ views/v3/partials/content/pdf/post.blade.php
@@ -6,7 +6,7 @@
         <h2 class="pdf-post__heading pdf-heading--h1" id="post-{{$index}}">{{ $post->postTitleFiltered }}</h2>
     @endif
     @if (!empty($post->images['featuredImage']['src']))
-        <img class="pdf-post__featured-image" src="{{$post->featuredImage['src']}}">
+        <img class="pdf-post__featured-image" src="{{$post->images['featuredImage']['src']}}" alt="{{$post->images['featuredImage']['alt'] ?? ''}}">
     @endif
     @if (!empty($post->postContentFiltered))
         {!! $post->postContentFiltered !!}
