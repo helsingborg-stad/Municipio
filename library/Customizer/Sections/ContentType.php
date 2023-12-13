dee65bd6a1bfb72df125405ad763ff87a5eda574
Anna Johansson <anna@thehappyhipster.se>
2023-12-13 13:51:10 +0000

    feat: Setting up section for content type settings in the customizer

---
+++ library/Customizer/Sections/ContentType.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace Municipio\Customizer\Sections;
+
+class ContentType
+{
+    public $sectionId;
+
+    public function __construct(string $sectionID, object $postType)
+    {
+
+        $contentTypes = self::getContentTypes();
+        $nullChoice = [
+            '' => esc_html__( 'Select content type', 'municipio' ),
+        ];
+        
+        \Kirki::add_field(\Municipio\Customizer::KIRKI_CONFIG, [
+            'type'     => 'select',
+            'settings' => 'posttype_' . $postType->name . '_contenttype',
+            'label'    => esc_html__('Content Type', 'municipio'),
+            'description' => esc_html__('Select the content type to use for this post type.', 'municipio'),
+            'section'  => $sectionID,
+            'default'  => '',
+            'placeholder' => esc_html__( 'Select content type', 'municipio' ),
+            'choices'  => array_merge($nullChoice, $contentTypes),
+            'output' => [
+                [
+                    'type' => 'controller',
+                    'as_object' => true,
+                ]
+            ],
+        ]);
+
+
+        \Kirki::add_field(\Municipio\Customizer::KIRKI_CONFIG, [
+            'type'     => 'switch',
+            'settings' => 'posttype_' . $postType->name . '_hide_map',
+            'label'    => esc_html__('Show/hide map', 'municipio'),
+            'description' => esc_html__('Whether to show a map on single posts of this posttype.', 'municipio'),
+            'section'  => $sectionID,
+            'default'     => 0,
+            'choices' => [
+                1  => esc_html__('Show map', 'municipio'),
+                0 => esc_html__('Hide map', 'municipio'),
+            ],
+            'output' => [
+                [
+                    'type' => 'controller',
+                    'as_object' => true,
+                ]
+            ],
+            'active_callback' => [
+                [
+                    'setting'  => 'posttype_' . $postType->name . '_contenttype',
+                    'operator' => '==',
+                    'value'    => 'place',
+                ]
+            ],
+        ]);
+       
+    }
+    private static function getContentTypes(bool $includeExtras = false) {
+        return \Municipio\Helper\ContentType::getRegisteredContentTypes($includeExtras);
+    }
+}
