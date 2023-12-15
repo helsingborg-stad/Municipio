f43ad519c8ba12a072563d1aa92b331bcf50ebd6
Anna <anna.johansson4@helsingborg.se>
2023-12-15 11:14:39 +0100

    Removing console.log

--- assets/source/3.0/js/admin/optionsReading.ts
+++ assets/source/3.0/js/admin/optionsReading.ts
@@ -1,8 +1,6 @@
 document.addEventListener('DOMContentLoaded', () => {
     const postsPerPageInput: HTMLInputElement | null = document.querySelector('#posts_per_page');
 
-    console.log("postsPerPageInput: ", postsPerPageInput);
-
     if (postsPerPageInput) {
         let parentElement: HTMLElement | null = postsPerPageInput.parentElement;
 
