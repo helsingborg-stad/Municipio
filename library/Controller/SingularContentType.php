9148832d6f2f77835d9717276cb356b4e97db353
Anna <anna.johansson4@helsingborg.se>
2023-12-19 09:59:47 +0100

    Removed commented method

--- library/Controller/SingularContentType.php
+++ library/Controller/SingularContentType.php
@@ -62,75 +62,10 @@ class SingularContentType extends \Municipio\Controller\Singular
      *
      * @return array The data to send to the view.
      */
-    // public function init()
-    // {
-    //     parent::init();
+    public function init()
+    {
+        parent::init();
+        return $this->data;
+    }
 
-    //     // TODO Should related posts really be set here? They aren't technically dependant on the post having a content type. Figure out a better place to place this.
-    //     // $this->data['relatedPosts'] = $this->getRelatedPosts($this->data['post']->id);
-
-    //     return $this->data;
-    // }
-
-    
-    /**
-     * Get related posts based on the taxonomies of the current post.
-     *
-     * @param int $postId The ID of the current post.
-     *
-     * @return array|bool An array of related posts or false if no related posts are found.
-     */
-    // private function getRelatedPosts($postId)
-    // {
-    //     $taxonomies = get_post_taxonomies($postId);
-    //     $postTypes = get_post_types(array('public' => true, '_builtin' => false), 'objects');
-
-    //     $arr = [];
-    //     foreach ($taxonomies as $taxonomy) {
-    //         $terms = get_the_terms($postId, $taxonomy);
-    //         if (!empty($terms)) {
-    //             foreach ($terms as $term) {
-    //                 if( $term instanceof WP_Term ) {
-    //                     $arr[$taxonomy][] = $term->term_id;
-    //                 }
-    //             }
-    //         }
-    //     }
-
-    //     if (empty($arr)) {
-    //         return false;
-    //     }
-
-    //     $posts = [];
-    //     foreach ($postTypes as $postType) {
-    //         $args = array(
-    //         'numberposts' => 3,
-    //         'post_type' => $postType->name,
-    //         'post__not_in' => array($postId),
-    //         'tax_query' => array(
-    //             'relation' => 'OR',
-    //         ),
-    //         );
-
-    //         foreach ($arr as $tax => $ids) {
-    //             $args['tax_query'][] = array(
-    //             'taxonomy' => $tax,
-    //             'field' => 'term_id',
-    //             'terms' => $ids,
-    //             'operator' => 'IN',
-    //             );
-    //         }
-
-    //         $result = get_posts($args);
-
-    //         if (!empty($result)) {
-    //             foreach ($result as &$post) {
-    //                 $post = \Municipio\Helper\Post::preparePostObject($post);
-    //                 $posts[$postType->label] = $result;
-    //             }
-    //         }
-    //     }
-
-    //     return $posts;
-    // }
 }
