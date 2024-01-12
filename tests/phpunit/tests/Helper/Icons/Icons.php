ca3379407a8252a04c64694fc4ba11fcb420810f
Niclas <niclasn@outlook.com>
2024-01-12 14:40:37 +0100

    test: helper icons

---
+++ tests/phpunit/tests/Helper/Icons/Icons.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace Municipio\Tests\Helper;
+
+use WP_Mock\Tools\TestCase;
+use Municipio\Helper\Icons;
+use WP_Mock;
+use Mockery;
+use tad\FunctionMocker\FunctionMocker;
+
+/**
+ * Class ListingTest
+ */
+class IconsTest extends TestCase
+{
+    /**
+     * @testdox getIcons returns false if missing file
+    */
+    public function testGetIconsReturnsFalse()
+    {
+        // Given
+        $this->mockedData();
+
+        // When
+        $result = Icons::getIcons();
+
+        // Then
+        $this->assertFalse($result);
+    }
+
+    /**
+     * @testdox getIcons returns empty array when get_theme_mod function doesn't exist
+    */
+    public function testGetIcons()
+    {
+        // Given
+        $this->mockedData();
+        $icons          = file_get_contents(__DIR__ . '/icons.json');
+        $fileExistsMock = FunctionMocker::replace('file_exists', true);
+        $fileExistsMock = FunctionMocker::replace('file_get_contents', $icons);
+
+        // When
+        $result = Icons::getIcons();
+
+        // Then
+        $this->assertEquals('10k', $result[0]);
+    }
+
+    /**
+     * Mocked data
+    */
+    private function mockedData()
+    {
+        define('MUNICIPIO_PATH', '/');
+    }
+}
