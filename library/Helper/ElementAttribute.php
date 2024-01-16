4b38718d96352b28e56247cf86a6739c03c5c8f6
Niclas <niclasn@outlook.com>
2024-01-16 11:41:01 +0100

    test: helper ElementAttribute

--- library/Helper/ElementAttribute.php
+++ library/Helper/ElementAttribute.php
@@ -2,11 +2,21 @@
 
 namespace Municipio\Helper;
 
+/**
+ * Class ElementAttribute
+ * Helper class for managing HTML element attributes and classes.
+ */
 class ElementAttribute
 {
     public $attributes = array();
-    public $classes = array();
+    public $classes    = array();
 
+     /**
+     * Add one or more classes to the element.
+     *
+     * @param string|array $classes The class or classes to add.
+     * @return bool Returns true on success, false on failure.
+     */
     public function addClass($classes)
     {
         if (!is_string($classes) && !is_array($classes) || empty($classes)) {
@@ -17,8 +27,7 @@ class ElementAttribute
             $classes = array($classes);
         }
 
-        foreach ($classes as $class)
-        {
+        foreach ($classes as $class) {
             if (empty($class)) {
                 continue;
             }
@@ -27,6 +36,12 @@ class ElementAttribute
         }
     }
 
+    /**
+     * Remove one or more classes from the element.
+     *
+     * @param string|array $classes The class or classes to remove.
+     * @return bool Returns true on success, false on failure.
+     */
     public function removeClass($classes)
     {
         if (!is_string($classes) && !is_array($classes) || empty($classes)) {
@@ -44,32 +59,62 @@ class ElementAttribute
         }
     }
 
-    public function addAttribute($attribute, $values = '')
+
+    /**
+     * Add one or more attributes to the element.
+     *
+     * @param string|array $attributes The attribute or attributes to add.
+     * @param string|array $values The value or values for the attribute.
+     * @return bool Returns true on success, false on failure.
+     */
+    public function addAttribute($attributes, $values = '')
     {
-        if (
-            !is_string($attribute)
-            && !is_array($attribute)
-            || !is_string($values)
-            && !is_array($values)
-            && !is_array($attribute)
-            || empty($attribute)
-            || is_string($attribute)
-            && empty($values)
-        ) {
+        if (!$this->checkValidAttributeValues($attributes, $values)) {
             return false;
         }
 
-        if (!is_array($attribute)) {
-            $attribute = array($attribute => $values);
+        if (!is_array($attributes)) {
+            $attributes = array($attributes => $values);
         }
 
-        foreach ($attribute as $attributeKey => $attributeValues) {
+        foreach ($attributes as $attributeKey => $attributeValues) {
             $this->newAttribute($attributeKey, $attributeValues);
         }
     }
 
-    public function newAttribute($attribute, $values)
+    /**
+     * Check that attributes and values are valid
+     *
+     * @param mixed $attributes The attribute or attributes to add.
+     * @param mixed $values The value or values for the attribute.
+     * @return bool Returns true on success, false on failure.
+     */
+    private function checkValidAttributeValues($attributes, $values): bool
     {
+        if (!is_string($attributes) && !is_array($attributes) || empty($attributes)) {
+            return false;
+        }
+
+        if (!is_string($values) && !is_array($values) || is_string($attributes) && empty($values)) {
+            return false;
+        }
+
+        // If both conditions are met, return true
+        return true;
+    }
+
+     /**
+     * Add a new attribute with values to the element.
+     *
+     * @param string $attribute The attribute to add.
+     * @param string|array $values The value or values for the attribute.
+     */
+    public function newAttribute($attribute = '', $values = '')
+    {
+        if (!is_string($values) && !is_array($values) || empty($attribute)) {
+            return false;
+        }
+
         if (!isset($this->attributes[$attribute])) {
             $this->attributes[$attribute] = array();
         }
@@ -83,19 +128,11 @@ class ElementAttribute
         }
     }
 
-    public function replaceClasses($classes)
-    {
-        if (!is_array($classes) || !is_string($classes) || empty($classes)) {
-            return false;
-        }
-
-        if (is_string($classes)) {
-            $classes = array($classes);
-        }
-
-        $this->classes = $classes;
-    }
-
+    /**
+     * Get the classes currently set for the element.
+     *
+     * @return array|null Returns an array of classes or null if none are set.
+     */
     public function getClasses()
     {
         if (isset($this->classes) && is_array($this->classes) && !empty($this->classes)) {
@@ -103,6 +140,12 @@ class ElementAttribute
         }
     }
 
+    /**
+     * Get the attributes for the element.
+     *
+     * @param bool $mergeClasses Whether to merge classes into the attributes.
+     * @return array|null Returns an array of attributes or null if none are set.
+     */
     public function getAttributes($mergeClasses = true)
     {
         if ($mergeClasses && isset($this->classes) && is_array($this->classes) && !empty($this->classes)) {
@@ -117,19 +160,15 @@ class ElementAttribute
         }
     }
 
-    public function replaceAttributes($attributes)
-    {
-        if (!is_array($classes) || empty($classes)) {
-            return false;
-        }
-
-        $this->attributes = $attributes;
-    }
-
+    /**
+     * Output the attributes as a string.
+     *
+     * @param bool $mergeClasses Whether to merge classes into the attributes.
+     * @return string|bool Returns the string representation of attributes or false if none are set.
+     */
     public function outputAttributes($mergeClasses = true)
     {
         if ($mergeClasses && isset($this->classes) && is_array($this->classes) && !empty($this->classes)) {
-
             if (!isset($this->attributes['class'])) {
                 $this->attributes = array_merge(array('class' => array()), $this->attributes);
             }
@@ -145,8 +184,9 @@ class ElementAttribute
 
     /**
      * Turn an array of HTML attributes into a string
-     * @param string $content String to get HTML attributes from
-     * @return array       Attributes
+     *
+     * @param array $attributesArray Array to get HTML attributes from
+     * @return string       Attributes
      */
     public static function attributesToString($attributesArray)
     {
@@ -157,11 +197,11 @@ class ElementAttribute
         $attributes = array();
 
         foreach ($attributesArray as $attribute => $value) {
-            if (!is_array($value) && !is_string($value) || !$value || empty($value)) {
+            if (!is_array($value) && !is_string($value) || empty($value) || empty($attribute)) {
                 continue;
             }
 
-            $values = (is_array($value)) ? implode(' ', array_unique($value)) : $value;
+            $values       = (is_array($value)) ? implode(' ', array_unique($value)) : $value;
             $attributes[] = $attribute . '="' . $values . '"';
         }
 
