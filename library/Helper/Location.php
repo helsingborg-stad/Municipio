2fbdab78815309ad7fd4e72098cc788273897844
NiclasNorin <103985736+NiclasNorin@users.noreply.github.com>
2024-01-17 14:57:56 +0100

    test: library/Helper/Location.php (#818)

--- library/Helper/Location.php
+++ library/Helper/Location.php
@@ -2,23 +2,25 @@
 
 namespace Municipio\Helper;
 
+/**
+ * Class Location
+ */
 class Location
 {
     /**
      * Add location data to the given posts.
      *
      * @param array $posts An array of post objects to add location data to.
-     *
      * @return array The modified array of post objects.
      */
     public static function addLocationDataToPosts(array $posts): array
     {
         foreach ($posts as &$_post) {
-            if(!isset($_post->id) || empty($_post->id)) {
+            if (empty($_post->id)) {
                 continue;
             }
             $location = get_field("location", $_post->id);
-            if (!empty($location) && !empty($location["lat"]) && !empty($location["lng"])) {
+            if (!empty($location["lat"]) && !empty($location["lng"])) {
                 $_post->location = $location;
             }
         }
@@ -28,16 +30,16 @@ class Location
      * Create pins from the given posts .
      *
      * @param array $posts An array of post objects to create pins from .
-     * @param boolean $displayGoogleMapsLink Whether to include a Google Maps link in the tooltip .
+     *
      * @return array $pins An array of pins .
      */
-    public static function createPins(array $posts, bool $displayGoogleMapsLink): array
+    public static function createMapMarkers(array $posts): array
     {
         $pins = [];
 
         if (!empty($posts)) {
             foreach ($posts as $post) {
-                $pin = self::createPin($post, $displayGoogleMapsLink);
+                $pin = self::createMapMarker($post);
                 if (!empty($pin)) {
                     $pins[] = $pin;
                 }
@@ -51,37 +53,30 @@ class Location
      * Create a pin from the given post.
      *
      * @param WP_Post $post The post to create pin data from.
-     * @param boolean $displayGoogleMapsLink Whether to include a Google Maps link in the tooltip.
+     *
      * @return array $pin The pin data.
      */
-    public static function createMapMarker($post): array
+    public static function createMapMarker(\WP_Post $post): array
     {
         $pin = [];
         if (!empty($post->location['lat']) && !empty($post->location['lng'])) {
             $permalink = get_permalink($post->id);
-            $directions = "";
 
             $pin = [
-                'lat' => $post->location['lat'],
-                'lng' => $post->location['lng'],
+                'lat'     => $post->location['lat'],
+                'lng'     => $post->location['lng'],
                 'tooltip' => [
-                    'title' => $post->post_title ?? '',
-                    'excerpt' => $post->post_excerpt ?? '',
-                    'url' => $permalink,
+                    'title'      => $post->post_title ?? '',
+                    'excerpt'    => $post->post_excerpt ?? '',
+                    'url'        => $permalink,
+                    'directions' => [
+                        'url'   => 'https://www.google.com/maps/dir/?api=1&destination=' .
+                        $post->location['lat'] . ',' . $post->location['lng'] . '&travelmode=transit',
+                        'label' => __('Get directions on Google Maps', 'municipio'),
+                    ]
                 ],
-                /* Maybe not needed? */
-                'url' => $permalink,
             ];
 
-            // Add directions link to tooltip
-            $directions = 'https://www.google.com/maps/dir/?api=1&destination=' . $post->location['lat'] . ',' . $post->location['lng'] . '&travelmode=transit';
-            
-            if ($directions) {
-                $pin['tooltip']['directions'] = [
-                'url' => $directions,
-                'label' => __('Get directions on Google Maps', 'municipio'),
-                ];
-            }
             // Add icon to pin
             if (!empty($post->termIcon)) {
                 $pin['icon'] = $post->termIcon;
