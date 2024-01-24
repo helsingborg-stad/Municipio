2f945d1ce9b61194ed7f3addcc93b0056fe606da
Thor Brink <thor.brink@helsingborg.se>
2024-01-24 10:05:35 +0000

    refactor: apply coding standards

--- tests/phpunit/tests/Content/ResourceFromApi/ResourceRegistry/SortByParentPostType.php
+++ tests/phpunit/tests/Content/ResourceFromApi/ResourceRegistry/SortByParentPostType.php
@@ -1,19 +1,30 @@
 <?php
 
+namespace Municipio\Tests\Content\ResourceFromApi\ResourceRegistry;
+
+use Mockery;
 use Municipio\Content\ResourceFromApi\ResourceInterface;
 use WP_Mock\Tools\TestCase;
 
-class SortByParentPostTypeTest extends TestCase {
-    public function testResourcesWithParentPostTypesSortedToLastInArray() {
-        
+/**
+ * Class SortByParentPostTypeTest
+ */
+class SortByParentPostTypeTest extends TestCase
+{
+    /**
+     * @testdox Resources with parent post types are sorted to last in array.
+     */
+    public function testResourcesWithParentPostTypesSortedToLastInArray()
+    {
+
         $withoutParentPostType = Mockery::mock(ResourceInterface::class);
         $withoutParentPostType->shouldReceive('getName')->andReturn('without-parent');
         $withoutParentPostType->shouldReceive('getArguments')->andReturn([]);
 
-        $withParentPostType = Mockery::mock(ResourceInterface::class);  
+        $withParentPostType = Mockery::mock(ResourceInterface::class);
         $withParentPostType->shouldReceive('getName')->andReturn('with-parent');
         $withParentPostType->shouldReceive('getArguments')->andReturn([ 'parent_post_types' => ['foo']]);
-        
+
         $resources = [
             $withParentPostType,
             $withoutParentPostType,
@@ -21,7 +32,7 @@ class SortByParentPostTypeTest extends TestCase {
 
         $sorter = new \Municipio\Content\ResourceFromApi\ResourceRegistry\SortByParentPostType();
         $sorted = $sorter->sortByParentPostType($resources);
-        
+
         $this->assertEquals('without-parent', array_shift($sorted)->getName());
         $this->assertEquals('with-parent', array_shift($sorted)->getName());
     }
