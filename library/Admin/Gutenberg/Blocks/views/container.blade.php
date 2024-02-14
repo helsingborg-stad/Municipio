bd055fd171eee79f3236fb34e0d1147dcd6014c4
Anna <anna.johansson4@helsingborg.se>
2024-02-14 16:02:55 +0100

    Feature/hantera block registrerade i municipio cu 8693u4vha (#854)
    * refactor: PSR-12 formatting applied
    
    * refactor: Support anchor links when registering Municipio blocks
    refactor: Populate and apply anchors on Municipio container block
    
    * refactor: Populate and apply anchors on Municipio button block
    
    * refactor: Populate and print block anchors as id
    
    * refactor: Added documentation for the BlockManager class

--- library/Admin/Gutenberg/Blocks/views/container.blade.php
+++ library/Admin/Gutenberg/Blocks/views/container.blade.php
@@ -1,5 +1,5 @@
-<div class="{{ $classList }} u-padding__y--{{ $amount }}" style="{{ $style }}">
-    <div class="{{ $contentClassList }}">
+<div class="{{ $classList }} u-padding__y--{{ $amount }}" style="{{ $style }}" id="{{ $anchor }}>
+    <div class="{{ $contentClassList }}"">
         {!! '<InnerBlocks />' !!}
     </div>
 </div>
--- library/Admin/Gutenberg/Blocks/views/margin.blade.php
+++ library/Admin/Gutenberg/Blocks/views/margin.blade.php
@@ -1,4 +1,4 @@
-<div class="{{ $classList }} c-margin u-padding--{{ $amount }}">
+<div class="{{ $classList }} c-margin u-padding--{{ $amount }}" id="{{ $anchor }}">
   <div class="c-margin__inner">
     {!! '<InnerBlocks />' !!}
   </div>
