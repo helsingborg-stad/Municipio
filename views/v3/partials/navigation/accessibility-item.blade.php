9e7a383cebfb54d817b2e1c4df513dad39607118
NiclasNorin <niclasn@outlook.com>
2024-02-14 11:33:49 +0100

    feat: buttons for accessibility items

--- views/v3/partials/navigation/accessibility-item.blade.php
+++ views/v3/partials/navigation/accessibility-item.blade.php
@@ -1,18 +1,15 @@
 <li role="menuitem">
-    @link([
-        'href' => $item['href'] ?? null,
+    @button([
+        'text' => $item['text'],
+        'style' => 'outlined',
+        'color' => 'primary',
+        'href' => $item['href'] ?? false,
+        'icon' => $item['icon'],
+        'size' => 'sm',
         'attributeList' => array_merge($item['attributeList'] ?? [], [
             'onClick' => $item['script'] ?? '',
             'aria-label' => $item['label'] ?? '',
-        ]),
+        ])
     ])
-    @icon([
-        'icon' => $item['icon'],
-        'size' => $item['size'] ?? 'sm',
-        'filled' => $item['filled'] ?? true,
-        'classList' => $item['classList'] ?? []
-    ])
-    @endicon
-    {{ $item['text'] }}
-    @endlink
+    @endbutton
 </li>
