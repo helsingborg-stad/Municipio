15ca626f16a333be9250d50ba60c2084c5a7209c
Anna Johansson <anna.johansson4@helsingborg.com>
2024-03-07 13:14:56 +0000

    chore: Added documentation refactor: Removed superfluous ACF field for content type Place

--- library/AcfFields/php/content-type-fields.php
+++ library/AcfFields/php/content-type-fields.php
@@ -1,4 +1,16 @@
 <?php
+/**
+ * Initializes and configures custom ACF field groups for content types.
+ *
+ * This file utilizes the ContentTypeMetaFieldManager class to register custom ACF
+ * field groups tailored to specific content types within Municipio.
+ * It ensures that each content type has the necessary custom fields available
+ * in the WordPress admin for content management.
+ *
+ * @package Municipio\Admin\Acf
+ * @uses ContentTypeMetaFieldManager To register and manage custom ACF field groups.
+ * @global $contentTypeMetaFieldManager Instance of ContentTypeMetaFieldManager for field registration.
+ */
 
 use Municipio\Admin\Acf\ContentTypeMetaFieldManager;
 
