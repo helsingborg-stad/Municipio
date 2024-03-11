356fa6e10de09ae547f0ee34b61f7fa0acd74994
NiclasNorin <niclasn@outlook.com>
2024-03-11 14:18:29 +0100

    feat: archive endpoint categorized posts

--- views/v3/partials/content/pdf/post.blade.php
+++ views/v3/partials/content/pdf/post.blade.php
@@ -1,9 +1,9 @@
 <article class="pdf-post {{ !empty($cover) || $hasMoreThanOnePost ? 'pdf-page-break' : '' }}">
     <script class="pdf-script" type="text/php">
-        $GLOBALS['chapters']['{{$index + 1}}'] = $pdf->get_page_number();
+        $GLOBALS['chapters']['{{$postIndex + 1}}'] = $pdf->get_page_number();
     </script>
     @if (!empty($post->postTitleFiltered))
-        <h2 class="pdf-post__heading pdf-heading--h1" id="post-{{$index}}">{{ $post->postTitleFiltered }}</h2>
+        <h2 class="pdf-post__heading pdf-heading--h1" id="post-{{$postIndex}}">{{ $post->postTitleFiltered }}</h2>
     @endif
     @if (!empty($post->images['featuredImage']['src']))
         <img class="pdf-post__featured-image" src="{{$post->images['featuredImage']['src']}}" alt="{{$post->images['featuredImage']['alt'] ?? ''}}">
