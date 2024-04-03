ad6ba9f93fc4ea3e14c62a8c0320eadfeacf7f5a
NiclasNorin <niclasn@outlook.com>
2024-04-03 11:58:26 +0200

    feat: using schema data for place posts

--- library/Helper/Post.php
+++ library/Helper/Post.php
@@ -4,6 +4,7 @@ namespace Municipio\Helper;
 
 use Municipio\Helper\Navigation;
 use Municipio\Helper\Image;
+use Municipio\Helper\PostSchemaData;
 use WP_Error;
 use WP_Post;
 
@@ -261,17 +262,19 @@ class Post
                 $postObject
             );
         }
-
-        /* Get location data */
-        $postObject->location = get_field('location', $postObject->ID);
-        if (!empty($postObject->location['pin'])) {
-            $postObject->location['pin'] = \Municipio\Helper\Location::createMapMarker($postObject);
-        }
-
+        
+        
         if (!empty($postObject->post_type)) {
             $postObject->contentType = \Modularity\Module\Posts\Helper\ContentType::getContentType(
                 $postObject->post_type
             );
+        }
+
+        /* Get location data */
+        $schemaData = get_field('schema', $postObject->ID);
+        if (!empty($schemaData)) {
+            $schemaDataHandler = new PostSchemaData($postObject, $schemaData);
+            $postObject = $schemaDataHandler->addSchemaDataToPost();
         }
 
         return apply_filters('Municipio/Helper/Post/postObject', $postObject);
