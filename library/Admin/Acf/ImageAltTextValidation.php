c97dfdd6a6f8b0a8e4f8bcde104c3df939fab8a7
Sebastian Thulin <sebastian.thulin@helsingborg.se>
2024-05-03 15:08:01 +0200

    chore: refacor the alt text detector
    A little easier to read.

--- library/Admin/Acf/ImageAltTextValidation.php
+++ library/Admin/Acf/ImageAltTextValidation.php
@@ -9,43 +9,170 @@ class ImageAltTextValidation
         add_action( 'acf/validate_value', array($this, 'checkAttachedImagesAltTexts'), 10, 4);
     }
 
-    public function checkAttachedImagesAltTexts ($valid, $value, $field, $name) {
-        global $_POST;
+    /**
+     * Retrieves the language strings.
+     *
+     * @return object The language strings.
+     */
+    private function lang(): object {
+        return (object) [
+            'altTextFeaturedImage'  => __("Please add an alt text to the featured image.", 'municipio'),
+            'altTextSingular'       => __("Please add an alt text to the image.", 'municipio')
+        ];
+    }
 
-        $altIsEmpty = false;
-        if ($this->postHasFeaturedImage($field, $_POST)) {
-            $altIsEmpty = empty($this->getAltText($_POST['_thumbnail_id'])) ? "Please add an alt text to the featured image." : false;
+    /**
+     * Retrieves the post data.
+     *
+     * @return array|null The post data, or null if it is empty.
+     */
+    private function getPostData(): ?string {
+        if (empty($_POST) || !is_array($_POST)) {
+            return null;
+        }
+        return $_POST;
+    }
+
+    /**
+     * Checks if the alt text is set for the featured image and the image field.
+     *
+     * @param bool $valid The current validation status.
+     * @param mixed $value The value of the field.
+     * @param array $field The field array.
+     * @param string $name The field name.
+     * @return bool Returns true if the alt text is set for the featured image and the image field, false otherwise.
+     */
+    public function checkAttachedImagesAltTexts($valid, $value, $field, $name) {
+    
+        //Declare vars.
+        $validationErrorMessage = false;
+        $postData               = $this->getPostData();
+
+        //Not a post action
+        if (empty($postData) || !is_array($postData)) {
+            return $valid;
+        }
+
+        //Featured image
+        if ($this->postHasFeaturedImage($field, $postData)) {
+            $thumbnailID            = $postData['_thumbnail_id'];
+            $thumbnailImageAltText  = $this->getAltText($thumbnailID);
+
+            if(is_null($thumbnailImageAltText)) {
+                $validationErrorMessage = $this->lang()->altTextFeaturedImage;
+            }
         }
         
+        //Image field
         if ($this->imageFieldHasValue($field, $value)) {
-            $altIsEmpty = empty($this->getAltText($value)) ? "Please add alt texts to the images." : false;
+            $fieldImageAltText = $this->getAltText($value);
+
+            if(is_null($fieldImageAltText)) {
+                $validationErrorMessage = $this->lang()->altTextSingular;
+            }
         }
 
+        //Focus field
         if ($this->focusFieldHasValue($field, $value)) {
-            $altIsEmpty = empty($this->getAltText($value['id'])) ? "Please add an alt text to the image." : false;
+            $fieldImageAltText = $this->getAltText($value['id']);
+
+            if(is_null($fieldImageAltText)) {
+                $validationErrorMessage = $this->lang()->altTextSingular;
+            }
         }
 
-        if ($altIsEmpty) {
-            acf_add_validation_error($_POST['acf']['field_654a2a58ca4e9'], $altIsEmpty );
-            return __('Please add an alt text to the image', 'municipio');
+        if ($validationErrorMessage !== false) {
+            acf_add_validation_error(
+                $_POST['acf']['field_654a2a58ca4e9'], 
+                $validationErrorMessage
+            );
+        }
+    }
+
+    /**
+     * Checks if a post has a featured image.
+     *
+     * @param array $field The field array.
+     * @param array $post The post array.
+     * @return bool Returns true if the post has a featured image, false otherwise.
+     */
+    private function postHasFeaturedImage($field, $post)
+    {
+        if(!isset($field['key'])) {
+            return false;
         }
 
-        return $valid;
+        if($field['key'] !== 'field_654a2a58ca4e9') {
+            return false;
+        }
+
+        if(empty($post) || empty($post['_thumbnail_id'])) {
+            return false;
+        }
+
+        if($post['_thumbnail_id'] === '-1') {
+            return false;
+        }
+
+        return true;
     }
 
-    private function postHasFeaturedImage($field, $post) {
-        return !empty($field['key']) && $field['key'] == 'field_654a2a58ca4e9' && !empty($post) && !empty($post['_thumbnail_id']) && $post['_thumbnail_id'] !== '-1';
+    /**
+     * Checks if an image field has a valid value.
+     *
+     * @param array $field The image field array.
+     * @param mixed $value The value of the image field.
+     * @return bool Returns true if the image field has a valid value, false otherwise.
+     */
+    private function imageFieldHasValue($field, $value): bool
+    {
+        if($field['type'] !== 'image') {
+            return false;
+        }
+
+        if(empty($value) || !is_numeric($value)) {
+            return false;
+        }
+
+        return true;
     }
 
-    private function imageFieldHasValue($field, $value) {
-        return !empty($field['type']) && $field['type'] == 'image' && !empty($value) && is_numeric($value);
+    /**
+     * Checks if the focus field has a valid value.
+     *
+     * @param array $field The field array.
+     * @param mixed $value The value of the field.
+     * @return bool Returns true if the focus field has a valid value, false otherwise.
+     */
+    private function focusFieldHasValue($field, $value): bool
+    {
+        if($field['type'] !== 'focuspoint') {
+            return false;
+        }
+
+        if(empty($value) || !is_array($value)) {
+            return false;
+        }
+
+        if(empty($value['id']) || !is_numeric($value['id'])) {
+            return false;
+        }
+
+        return true;
     }
 
-    private function focusFieldHasValue($field, $value) {
-        return !empty($field['type']) && $field['type'] == 'focuspoint' && !empty($value) && is_array($value);
-    }
-
-    private function getAltText($id) {
-        return get_post_meta($id, '_wp_attachment_image_alt', true);
+    /**
+     * Retrieves the alt text for an image attachment.
+     *
+     * @param int $id The ID of the image attachment.
+     * @return string|null The alt text of the image attachment, or null if it is empty.
+     */
+    private function getAltText($id): ?string
+    {
+        $altText = get_post_meta($id, '_wp_attachment_image_alt', true);
+        if (!empty($altText)) {
+            return $altText;
+        }
+        return null;
     }
 }
