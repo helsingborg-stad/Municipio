9ed5b074cedbf9d06f862542c8911788451f6ea0
NiclasNorin <niclasn@outlook.com>
2024-05-24 12:27:01 +0200

    feat: updating design if new design set

--- library/PostTypeDesign/SaveDesigns.php
+++ library/PostTypeDesign/SaveDesigns.php
@@ -8,7 +8,8 @@ use Municipio\PostTypeDesign\ConfigSanitizer;
 class SaveDesigns {
     public function __construct(private string $optionName) 
     {
-        add_action('customize_save_after', array($this, 'storeDesign'));
+        add_action('wp', array($this, 'storeDesign'));
+        // add_action('customize_save_after', array($this, 'storeDesign'));
     }
 
     public function storeDesign($customizerManager = null) 
@@ -19,12 +20,21 @@ class SaveDesigns {
             return;
         }
         
-        delete_option('post_type_design');
+        // delete_option('post_type_design');
         $designOption   = get_option('post_type_design');
+
         foreach ($postTypes as $postType) {
             $design = get_theme_mod($postType . '_load_design');
 
-            if (empty($design) || isset($designOption[$postType])) {
+            if (
+                empty($design) || 
+                (isset($designOption[$postType]) && $designOption[$postType]['designId'] === $design)
+            ) {
+                if (empty($design) && isset($designOption[$postType])) {
+                    unset($designOption[$postType]);
+                    update_option('post_type_design', $designOption);
+                }
+                
                 continue;
             }
 
@@ -37,7 +47,12 @@ class SaveDesigns {
             $designConfig = $configTransformerInstance->transform();
 
             if (!empty($designConfig)) {
-                $designOption[$postType] = ['design' => $designConfig, 'css' => $css];
+                $designOption[$postType] = [
+                    'design' => $designConfig, 
+                    'css' => $css, 
+                    'designId' => $design
+                ];
+
                 update_option('post_type_design', $designOption);
             }
         }
