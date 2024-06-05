e8404243a99520b2b60d4438c9677565e1e4cf76
NiclasNorin <niclasn@outlook.com>
2024-06-05 09:41:19 +0200

    style: code styling

--- library/Admin/MunicipioMenuItems/Separator.php
+++ library/Admin/MunicipioMenuItems/Separator.php
@@ -2,52 +2,53 @@
 
 namespace Municipio\Admin\MunicipioMenuItems;
 
-class Separator {
+class Separator
+{
     public function __construct()
     {
         add_filter('Municipio/Navigation/Nested', array($this, 'separateMenus'), 10, 3);
     }
 
-    public function separateMenus($items, $identifier, $pageId) {
-            if (!empty($items)) {
-                $menus = [];
-                $i = 0;
-            
-                foreach ($items as &$item) {
-                    if ($item['post_type'] === 'separator') {
-                        $i++;
-                        $menus[$i]['title'] = $item['label'];
-                        $fields = get_fields($item['id']);
-                        echo '<pre>' . print_r( $fields, true ) . '</pre>';
-                        
-                        $menus[$i]['attributeList']['style'] = $this->setNavigationColorVariables($fields);
-                    } else {
-                        $menus[$i]['items'][] = $item;
-                    }
-                }
-            
-                if ($identifier === 'mobile') {
-                    // echo '<pre>' . print_r((object) $menus, true) . '</pre>';
+    public function separateMenus($items, $identifier, $pageId)
+    {
+        if (!empty($items)) {
+            $menus = [];
+            $i     = 0;
+
+            foreach ($items as &$item) {
+                if ($item['post_type'] === 'separator') {
+                    $i++;
+                    $menus[$i]['title'] = $item['label'];
+                    $fields             = get_fields($item['id']);
+
+                    $menus[$i]['attributeList']['style'] = $this->setNavigationColorVariables($fields);
+                } else {
+                    $menus[$i]['items'][] = $item;
                 }
             }
-            
+
+            if ($identifier === 'mobile') {
+            }
+        }
+
             return !empty($menus) && count($menus) > 1 ? (object) $menus : $items;
     }
 
-    private function setNavigationColorVariables($fields) {
+    private function setNavigationColorVariables($fields)
+    {
         $style = '';
         if (!empty($fields['text_color'])) {
             $style .= '--c-nav-v-color-contrasting: ' . $fields['text_color'] . ';';
         }
-        
+
         if (!empty($fields['expanded_background_color'])) {
             $style .= '--c-nav-v-background-expanded: ' . $fields['expanded_background_color'] . ';';
         }
-        
+
         if (!empty($fields['background_color'])) {
             $style .= 'background-color: ' . $fields['background_color'] . ';';
         }
-        
+
         if (!empty($fields['active_background_color'])) {
             $style .= '--c-nav-v-background-active: ' . $fields['active_background_color'] . ';';
         }
