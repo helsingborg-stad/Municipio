451ce458211c0837f9f75d26f60fc50aa8fed892
Sebastian Thulin <sebastian.thulin@helsingborg.se>
2024-08-07 13:27:24 +0200

    fix: further improve swatch fetching

--- library/Helper/KirkiSwatches.php
+++ library/Helper/KirkiSwatches.php
@@ -28,16 +28,21 @@ class KirkiSwatches
 
         // Compute colors if not cached
         if (function_exists('get_theme_mod')) {
+
+            $colorPalettePrimary    = get_theme_mod('color_palette_primary');
+            $colorPaletterSecondary = get_theme_mod('color_palette_secondary');
+            $colorBackground        = get_theme_mod('color_background')['background'];
+
             self::$cachedColors = [
-                get_theme_mod('color_palette_primary')['base']          ?? '#ae0b05',
-                get_theme_mod('color_palette_primary')['dark']          ?? '#770000',
-                get_theme_mod('color_palette_primary')['light']         ?? '#e84c31',
-                get_theme_mod('color_palette_primary')['contrasting']   ?? '#ffffff',
-                get_theme_mod('color_palette_secondary')['base']        ?? '#ec6701',
-                get_theme_mod('color_palette_secondary')['dark']        ?? '#b23700',
-                get_theme_mod('color_palette_secondary')['light']       ?? '#ff983e',
-                get_theme_mod('color_palette_secondary')['contrasting'] ?? '#ffffff',
-                get_theme_mod('color_background')['background']         ?? '#f5f5f5',
+                $colorPalettePrimary['base']           ?? '#ae0b05',
+                $colorPalettePrimary['dark']           ?? '#770000',
+                $colorPalettePrimary['light']          ?? '#e84c31',
+                $colorPalettePrimary['contrasting']    ?? '#ffffff',
+                $colorPaletterSecondary['base']        ?? '#ec6701',
+                $colorPaletterSecondary['dark']        ?? '#b23700',
+                $colorPaletterSecondary['light']       ?? '#ff983e',
+                $colorPaletterSecondary['contrasting'] ?? '#ffffff',
+                $colorBackground                       ?? '#f5f5f5',
             ];
         } else {
             self::$cachedColors = [];
