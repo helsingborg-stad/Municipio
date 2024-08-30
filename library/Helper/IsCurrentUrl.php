19c594abc28fe8646316dcc0dd90633adcfaa769
Sebastian Thulin <sebastian.thulin@helsingborg.se>
2024-08-30 16:16:29 +0200

    fix: add a typesafe varaint of parse url.

--- library/Helper/IsCurrentUrl.php
+++ library/Helper/IsCurrentUrl.php
@@ -15,9 +15,9 @@ class IsCurrentUrl
         $currentUrl = self::sanitizePath($_SERVER['REQUEST_URI']);
 
         //Check if urls match
-        if (parse_url($url, PHP_URL_PATH) !== null) {
-            $checkUrl = self::sanitizePath(parse_url($url, PHP_URL_PATH));
-            if ($urlQuery = parse_url($url, PHP_URL_QUERY)) {
+        if (self::parseUrlSafe($url, PHP_URL_PATH) !== null) {
+            $checkUrl = self::sanitizePath(self::parseUrlSafe($url, PHP_URL_PATH));
+            if ($urlQuery = self::parseUrlSafe($url, PHP_URL_QUERY)) {
                 $checkUrl .=  '?' . $urlQuery;
             }
 
@@ -27,8 +27,8 @@ class IsCurrentUrl
         }
 
         //Check if querystrings match, path is empty
-        if (parse_url($url, PHP_URL_PATH) == null && !empty(parse_url($url, PHP_URL_QUERY))) {
-            if (parse_url($url, PHP_URL_QUERY) == trim(strstr($currentUrl, "?"), "?")) {
+        if (self::parseUrlSafe($url, PHP_URL_PATH) == null && !empty(self::parseUrlSafe($url, PHP_URL_QUERY))) {
+            if (self::parseUrlSafe($url, PHP_URL_QUERY) == trim(strstr($currentUrl, "?"), "?")) {
                 return true;
             }
         }
@@ -39,7 +39,7 @@ class IsCurrentUrl
     public static function isCurrentOrAncestorUrl(string $url): bool
     {
         $currentUrl = self::sanitizePath(!empty($_SERVER['REQUEST_URI']) ? $_SERVER['REQUEST_URI'] : "");
-        $url        = self::sanitizePath(parse_url($url, PHP_URL_PATH));
+        $url        = self::sanitizePath(self::parseUrlSafe($url, PHP_URL_PATH));
 
         if (empty($url) || !is_string($currentUrl)) {
             return false;
@@ -65,5 +65,64 @@ class IsCurrentUrl
     public static function sanitizePath(string $path): string
     {
         return rtrim(trim($path, '/'), '/');
+    }
+
+    /**
+     * Get the current request uri, if it exists.
+     *
+     * @return string
+     */
+    public static function getRequestUri(): string 
+    {
+        return isset($_SERVER['REQUEST_URI']) && is_string($_SERVER['REQUEST_URI']) 
+        ? $_SERVER['REQUEST_URI'] 
+        : '';
+    }
+
+    /**
+     * Get the current request uri query, if it exists.
+     *
+     * @return string
+     */
+    public static function getRequestUriQuery(): string
+    {
+        $requestUri = self::getRequestUri();
+        if(!empty($requestUri) && strpos($requestUri, '?') !== false) {
+            return self::parseUrlSafe($requestUri, PHP_URL_QUERY);
+        }
+        return '';
+    }
+
+    /**
+     * Get the current request uri path, if it exists.
+     * 
+     * @return string
+     */
+    public function getRequestUriPath(): string
+    {
+        $requestUri = self::getRequestUri();
+        if(!empty($requestUri)) {
+            return self::parseUrlSafe($requestUri, PHP_URL_PATH);
+        }
+        return '';
+    }
+
+    /**
+     * Get the current request uri path, if it exists.
+     * 
+     * @return string
+     */
+    private static function parseUrlSafe(string $url, int|string $method): string
+    {
+        // Validate the URL
+        if (!filter_var($url, FILTER_VALIDATE_URL)) {
+            return '';
+        }
+
+        // Parse the URL and get the desired component
+        $parsedUrl = parse_url($url, is_int($method) ? $method : constant('PHP_URL_' . strtoupper($method)));
+
+        // Ensure that the result is a string or return an empty string if it's not set
+        return is_string($parsedUrl) ? $parsedUrl : '';
     }
 }
