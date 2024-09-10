8d67dd3302c7c8af295b7a31bf1e1895f347bd94
Niclas <niclasn@outlook.com>
2024-09-10 15:32:15 +0200

    feat: brand text flexible

--- views/v3/partials/header/components/logotype.blade.php
+++ views/v3/partials/header/components/logotype.blade.php
@@ -1,20 +1,9 @@
-@link(['href' => $homeUrl, 'classList' => ['u-margin__right--auto', 'u-display--flex', 'u-no-decoration']])
-    @if($headerBrandEnabled)
-        @brand([
-            'logotype' => [
-                'src'=> $logotype,
-                'alt' => $lang->goToHomepage
-            ],
-            'text' => $brandText,
-        ])
-        @endbrand
-    @else
-        @logotype([
-            'src'=> $logotype,
-            'alt' => $lang->goToHomepage,
-            'classList' => ['c-nav__logo', 'c-header__logotype'],
-            'context' => ['site.header.logo', 'site.header.casual.logo']
-        ])
-        @endlogotype
-    @endif
+@link(['href' => $homeUrl, 'classList' => ['c-header__logotype-container', 'u-margin__right--auto', 'u-display--flex', 'u-no-decoration']])
+    @logotype([
+        'src'=> $logotype,
+        'alt' => $lang->goToHomepage,
+        'classList' => ['c-nav__logo', 'c-header__logotype'],
+        'context' => ['site.header.logo', 'site.header.casual.logo']
+    ])
+    @endlogotype
 @endlink
