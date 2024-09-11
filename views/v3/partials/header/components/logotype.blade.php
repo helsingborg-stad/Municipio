cf0817a8d4b22353909d159d4503a55fbf76cecb
Niclas <niclasn@outlook.com>
2024-09-11 14:44:50 +0200

    feat: show only logo if custom placed brand text

--- views/v3/partials/header/components/logotype.blade.php
+++ views/v3/partials/header/components/logotype.blade.php
@@ -1,9 +1,20 @@
-@link(['href' => $homeUrl, 'classList' => ['c-header__logotype-container', 'u-margin__right--auto', 'u-display--flex', 'u-no-decoration']])
-    @logotype([
-        'src'=> $logotype,
-        'alt' => $lang->goToHomepage,
-        'classList' => ['c-nav__logo', 'c-header__logotype'],
-        'context' => ['site.header.logo', 'site.header.casual.logo']
-    ])
-    @endlogotype
+@link(['href' => $homeUrl, 'classList' => ['u-margin__right--auto', 'u-display--flex', 'u-no-decoration']])
+    @if($headerBrandEnabled && !$headerData['hasSeparateBrandText'])
+        @brand([
+            'logotype' => [
+                'src'=> $logotype,
+                'alt' => $lang->goToHomepage
+            ],
+            'text' => $brandText,
+        ])
+        @endbrand
+    @else
+        @logotype([
+            'src'=> $logotype,
+            'alt' => $lang->goToHomepage,
+            'classList' => ['c-nav__logo', 'c-header__logotype'],
+            'context' => ['site.header.logo', 'site.header.casual.logo']
+        ])
+        @endlogotype
+    @endif
 @endlink
