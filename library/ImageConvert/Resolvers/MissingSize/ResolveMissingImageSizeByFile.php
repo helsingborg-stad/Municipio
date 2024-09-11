acd668d1068d01d0da013342909c65bee6e53af8
Sebastian Thulin <sebastian.thulin@helsingborg.se>
2024-09-11 10:58:13 +0200

    fix: remove common. not needed anymore. moved to relevant class.

--- library/ImageConvert/Resolvers/MissingSize/ResolveMissingImageSizeByFile.php
+++ library/ImageConvert/Resolvers/MissingSize/ResolveMissingImageSizeByFile.php
@@ -2,8 +2,6 @@
 
 namespace Municipio\ImageConvert\Resolvers\MissingSize;
 
-use Municipio\ImageConvert\Common\IsConsideredImage;
-use Municipio\ImageConvert\Common\IsSizeSufficient;
 use Municipio\ImageConvert\Contract\ImageContract;
 
 class ResolveMissingImageSizeByFile implements ResolveMissingImageSizeInterface
@@ -15,20 +13,48 @@ class ResolveMissingImageSizeByFile implements ResolveMissingImageSizeInterface
 
     public function getAttachmentDimensions(ImageContract $image): ?array
     {
-        $file = get_attached_file($image->getId()); //$this->wpService->getAttachmedFile($id);
-        if ($file && IsConsideredImage::isConsideredImage($file)) {
+        // Get the file path from the ImageContract
+        $file = $image->getPath();
+
+        if ($file && $this->isConsideredImage($file)) {
             $fetchedImage = file_exists($file) ? getimagesize($file) : false;
+
             if ($fetchedImage !== false && isset($fetchedImage[0], $fetchedImage[1])) {
                 $size = ['width' => $fetchedImage[0], 'height' => $fetchedImage[1]];
 
-                if (IsSizeSufficient::isSizeSufficient($size)) {
+                if ($this->isSizeSufficient($size)) {
                     wp_update_attachment_metadata($image->getId(), $size);
                     return $size;
                 }
             }
         }
 
-        // Delegate to the inner resolver
+        // Delegate to the inner resolver if size could not be determined
         return $this->inner->getAttachmentDimensions($image);
+    }
+
+    /**
+     * Check if the file is considered an image by verifying the MIME type.
+     *
+     * @param string $file
+     * @return bool
+     */
+    private function isConsideredImage(string $file): bool
+    {
+        $mimeType = mime_content_type($file);
+
+        // Check if the MIME type starts with 'image/'
+        return strpos($mimeType, 'image/') === 0;
+    }
+
+    /**
+     * Check if the width and height of the image are set.
+     *
+     * @param array $size
+     * @return bool
+     */
+    private function isSizeSufficient(array $size): bool
+    {
+        return isset($size['width'], $size['height']);
     }
 }
