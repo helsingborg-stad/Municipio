96f9171b9a6a94ef8ae2939b2bd9745d49558a81
Niclas <niclasn@outlook.com>
2024-09-11 16:41:34 +0200

    feat: more basic solution

--- library/MunicipioMenuItems/Separator.php
+++ library/MunicipioMenuItems/Separator.php
@@ -9,52 +9,37 @@ class Separator
 {
     public function __construct(private AddFilter $wpService, private GetFields $acfService)
     {
-        $this->wpService->addFilter('Municipio/Navigation/Nested', array($this, 'separateMenus'), 1, 3);
+        $this->wpService->addFilter('Municipio/Navigation/Nested', array($this, 'separateMenus'), 999, 3);
     }
 
     public function separateMenus($items, $identifier, $pageId)
     {
         if (empty($items)) {
-            return $items;
+            return [];
         }
 
-        $structuredNavMenu = (object) [];
-        foreach ($items as $index => $item) {
+        foreach ($items as $index => &$item) {
+            if ($item['post_type'] === 'separator') {
+                $fields = $this->acfService->getFields($item['id']);
+                echo '<pre>' . print_r($fields, true) . '</pre>';
+                $item['attributeList']['style'] = $this->setNavigationColorVariables($fields);
+            }
         }
-        // if (!empty($items)) {
-        //     foreach ($items as $index => &$item) {
-        //         if ($item['post_type'] === 'separator') {
-        //             $fields                         = $this->acfService->getFields($item['id']);
-        //             $item['attributeList']['style'] = $this->setNavigationColorVariables($fields);
-        //             if (!empty($item['children'])) {
-        //                 $children = $item['children'];
-        //                 foreach ($children as &$child) {
-        //                     $child['attributeList']['style'] = $this->setNavigationColorVariables($fields);
-        //                 }
-        //                 array_splice($items, $index + 1, 0, $children);
-        //                 $item['children'] = null;
-        //             }
-        //         }
-        //     }
-        // }
 
-        if ($identifier === 'mobile') {
-            echo '<pre>' . print_r($items, true) . '</pre>';
-        }
             return $items;
     }
 
     private function setNavigationColorVariables($fields)
     {
-        $style = '';
-        if (!empty($fields['text_color'])) {
-            $style .= '--c-nav-v-color-contrasting: ' . $fields['text_color'] . ';';
-            $style .= '--c-nav-v-color-contrasting-active: ' . $fields['text_color'] . ';';
-        }
+        $style           = '';
+        $backgroundColor = $fields['background_color'] ?? 'inherit';
+        $textColor       = $fields['text_color'] ?? 'inherit';
 
-        if (!empty($fields['background_color'])) {
-            $style .= '--c-nav-v-item-background: ' . $fields['background_color'] . ';';
-            $style .= '--c-nav-v-background-active: ' . $fields['background_color'] . ';';
+        foreach (['v', 'h'] as $direction) {
+            $style .= '--c-nav-' . $direction . '-item-background: ' . $backgroundColor . ';';
+            $style .= '--c-nav-' . $direction . '-color-contrasting: ' . $textColor . ';';
+            $style .= '--c-nav-' . $direction . '-color-contrasting-active: ' . $textColor . ';';
+            $style .= '--c-nav-' . $direction . '-background-active: ' . $fields['background_color'] . ';';
         }
 
         return $style;
