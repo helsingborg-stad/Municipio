a6dc40c10b8443c52655efd0c9d0650435a8f7b2
Sebastian Thulin <sebastian.thulin@helsingborg.se>
2024-09-16 11:04:19 +0200

    hotfix: avoid crash when metadata is non existent, avoid crash in default image handler.

--- library/ImageConvert/Resolvers/MissingSize/ResolveMissingImageSizeByMeta.php
+++ library/ImageConvert/Resolvers/MissingSize/ResolveMissingImageSizeByMeta.php
@@ -21,19 +21,21 @@ class ResolveMissingImageSizeByMeta implements ResolveMissingImageSizeInterface
             $image->getId()
         );
 
-        // Filter the metadata to ensure 'width' and 'height' exist and are numeric
-        $metaDataDimensions = array_filter(
-            $metaData,
-            fn($value, $key) => in_array($key, ['width', 'height']) && is_numeric($value),
-            ARRAY_FILTER_USE_BOTH
-        );
+        if (is_array($metaData)) {
+            // Filter the metadata to ensure 'width' and 'height' exist and are numeric
+            $metaDataDimensions = array_filter(
+                $metaData,
+                fn($value, $key) => in_array($key, ['width', 'height']) && is_numeric($value),
+                ARRAY_FILTER_USE_BOTH
+            );
 
-        // Check if both 'width' and 'height' were found and are numeric
-        if (count($metaDataDimensions) === 2) {
-            return [
-            'width'  => (int) $metaData['width'],
-            'height' => (int) $metaData['height']
-            ];
+            // Check if both 'width' and 'height' were found and are numeric
+            if (count($metaDataDimensions) === 2) {
+                return [
+                'width'  => (int) $metaData['width'],
+                'height' => (int) $metaData['height']
+                ];
+            }
         }
 
         // Delegate to the inner resolver if needed
