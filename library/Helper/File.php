7795d368d1cac9ba68259556d70a22c07b579937
Niclas <niclasn@outlook.com>
2024-09-17 11:14:38 +0200

    fix: namings

--- library/Helper/File.php
+++ library/Helper/File.php
@@ -28,7 +28,7 @@ class File
         return null;
     }
 
-    public static function fileIsAvailable(string $filePath): bool
+    public static function uploadedFileIsAvailable(string $filePath): bool
     {
         $uploadsDir = wp_upload_dir();
 
