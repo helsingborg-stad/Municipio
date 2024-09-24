b75a7af1b63676be4406c676c9012bae476e975f
Thor Brink <thor.brink@helsingborg.se>
2024-09-24 07:55:17 +0000

    refactor: apply code standards

--- library/ExternalContent/WpPostArgsFromSchemaObject/SchemaDataDecorator.php
+++ library/ExternalContent/WpPostArgsFromSchemaObject/SchemaDataDecorator.php
@@ -6,12 +6,25 @@ use Municipio\ExternalContent\Sources\SourceInterface;
 use Municipio\ExternalContent\WpPostArgsFromSchemaObject\WpPostArgsFromSchemaObjectInterface;
 use Spatie\SchemaOrg\BaseType;
 
+/**
+ * Class SchemaDataDecorator
+ *
+ * This class decorates the schema data by adding it to the post meta input.
+ */
 class SchemaDataDecorator implements WpPostArgsFromSchemaObjectInterface
 {
+    /**
+     * SchemaDataDecorator constructor.
+     *
+     * @param WpPostArgsFromSchemaObjectInterface $inner
+     */
     public function __construct(private WpPostArgsFromSchemaObjectInterface $inner)
     {
     }
 
+    /**
+     * @inheritDoc
+     */
     public function create(BaseType $schemaObject, SourceInterface $source): array
     {
         $post                             = $this->inner->create($schemaObject, $source);
