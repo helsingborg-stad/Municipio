0c0c43fdd4c9072f807d8f26ed01c56467767615
Thor Brink <thor.brink@helsingborg.se>
2024-09-24 08:47:26 +0000

    feat: populate project template with data

--- library/Controller/SingularProject.php
+++ library/Controller/SingularProject.php
@@ -16,5 +16,13 @@ class SingularProject extends \Municipio\Controller\Singular
     public function init()
     {
         parent::init();
+
+        $this->data['category']   = wp_get_post_terms($this->data['post']->id, '@meta.category')[0]->name ?? null;
+        $this->data['technology'] = wp_get_post_terms($this->data['post']->id, '@meta.technology')[0]->name ?? null;
+        $this->data['status']     = wp_get_post_terms($this->data['post']->id, '@meta.status')[0]->name ?? null;
+        $this->data['department'] = wp_get_post_terms($this->data['post']->id, 'department')[0]->name ?? null;
+        $this->data['progress']   = get_post_meta($this->data['post']->id, 'progress', true) ?? null;
+        // echo '<pre>' . print_r($this->data, true) . '</pre>';
+        // die();
     }
 }
--- views/v3/single-schema-project.blade.php
+++ views/v3/single-schema-project.blade.php
@@ -1,1 +1,32 @@
 @extends('templates.single')
+
+@section('article.content')
+<pre>
+Category: {!! $category !!}
+Title: {!! $post->schemaObject['name'] !!}
+Status: {!! $status !!}
+Progress: {!! $progress !!}
+</pre>
+<pre>
+Content: {{$post->postContent}}
+</pre>
+@stop
+
+@section('sidebar.right-sidebar.before')
+<pre>
+Drivs av
+{!! $department !!}
+</pre>
+<pre>
+Kategori
+{!! $category !!}
+</pre>
+<pre>
+Teknologier
+{!! $technology !!}
+</pre>
+<pre>
+Kontakt
+{!! $post->schemaObject['employee']['alternateName'] !!}
+</pre>
+@stop
