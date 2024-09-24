5c2bba6fca65aa48e91d32f9602c06baca60764e
Thor Brink <thor.brink@helsingborg.se>
2024-09-24 05:04:52 +0000

    fix: external content sync triggered multiple times

--- library/ExternalContent/Sync/Triggers/TriggerSyncFromGetParams.php
+++ library/ExternalContent/Sync/Triggers/TriggerSyncFromGetParams.php
@@ -8,26 +8,44 @@ use WpService\Contracts\DoAction;
 use WpService\Contracts\VerifyNonce;
 use WpService\Contracts\SafeRedirect;
 
+/**
+ * Class TriggerSyncFromGetParams
+ *
+ * This class triggers synchronization of external content based on GET parameters.
+ */
 class TriggerSyncFromGetParams extends TriggerSync implements Hookable
 {
     public const GET_PARAM_TRIGGER   = 'sync_external_content';
     public const GET_PARAM_POST_TYPE = 'post_type';
     public const GET_PARAM_POST_ID   = 'sync_post_id';
 
+    /**
+     * Constructor for TriggerSyncFromGetParams.
+     *
+     * @param AddAction&DoAction&VerifyNonce&SafeRedirect $wpService The WordPress service.
+     */
     public function __construct(
         private AddAction&DoAction&VerifyNonce&SafeRedirect $wpService
     ) {
         parent::__construct($wpService);
     }
 
+    /**
+     * @inheritDoc
+     */
     public function addHooks(): void
     {
         $this->wpService->addAction('admin_init', array($this, 'tryToTriggerSync'));
     }
 
+    /**
+     * Tries to trigger the synchronization process based on GET parameters.
+     *
+     * @return void
+     */
     public function tryToTriggerSync(): void
     {
-        if (!$this->shouldTrigger()) {
+        if (!$this->shouldTrigger() || !isset($_GET[self::GET_PARAM_POST_TYPE])) {
             return;
         }
 
@@ -36,14 +54,24 @@ class TriggerSyncFromGetParams extends TriggerSync implements Hookable
 
         $this->trigger($postType, $postId);
 
+        if (empty($_SERVER['HTTP_REFERER'])) {
+            return;
+        }
+
         $this->wpService->safeRedirect($_SERVER['HTTP_REFERER']);
         exit;
     }
 
+    /**
+     * Determines if the synchronization should be triggered based on GET parameters.
+     *
+     * @return bool True if the synchronization should be triggered, false otherwise.
+     */
     private function shouldTrigger(): bool
     {
         return
             isset($_GET[self::GET_PARAM_TRIGGER]) &&
+            !empty($_GET['_wpnonce']) &&
             $this->wpService->verifyNonce($_GET['_wpnonce']);
     }
 }
--- library/ExternalContent/UI/PageRowActionsSyncButton.php
+++ library/ExternalContent/UI/PageRowActionsSyncButton.php
@@ -7,6 +7,11 @@ use WP_Post;
 use WpService\Contracts\AddFilter;
 use WpService\Contracts\NonceUrl;
 
+/**
+ * Class PageRowActionsSyncButton
+ *
+ * This class adds a sync button to the page and post row actions in the WordPress admin.
+ */
 class PageRowActionsSyncButton implements Hookable
 {
     /**
@@ -19,15 +24,28 @@ class PageRowActionsSyncButton implements Hookable
     {
     }
 
+    /**
+     * @inheritDoc
+     */
     public function addHooks(): void
     {
         $this->wpService->addFilter('page_row_actions', array($this, 'addSyncButton'), 10, 2);
         $this->wpService->addFilter('post_row_actions', array($this, 'addSyncButton'), 10, 2);
     }
 
+    /**
+     * Adds a sync button to the row actions for pages and posts.
+     *
+     * @param array $actions The existing row actions.
+     * @param WP_Post $post The current post object.
+     * @return array The modified row actions.
+     */
     public function addSyncButton(array $actions, WP_Post $post): array
     {
-        $postTypeHasExternalContentSource = array_filter($this->sourceConfigs, fn($config) => $config->getPostType() === $post->post_type);
+        $postTypeHasExternalContentSource = array_filter(
+            $this->sourceConfigs,
+            fn($config) => $config->getPostType() === $post->post_type
+        );
 
         if (empty($postTypeHasExternalContentSource)) {
             return $actions;
