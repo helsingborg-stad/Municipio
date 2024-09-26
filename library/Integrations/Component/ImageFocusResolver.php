718c9e13f1abeb923fcf11b13a75489ab1e98c2e
Sebastian Thulin <sebastian.thulin@helsingborg.se>
2024-09-26 10:14:48 +0200

    feat: add integrations for ImageResolver & ImageFocusResolver

---
+++ library/Integrations/Component/ImageFocusResolver.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Municipio\Integrations\Component;
+
+use \ComponentLibrary\Integrations\Image\ImageFocusResolverInterface;
+
+class ImageFocusResolver implements ImageFocusResolverInterface {
+
+  /**
+   * Constructor
+   * 
+   * @param array $data The data array to resolve from
+   */
+  public function __construct(private $data){}
+
+  /**
+   * Get focus point
+   * 
+   * @return array
+   */
+  public function getFocusPoint(): array {
+    $data = $this->data;
+    if($data && isset($data['left'], $data['top'])) {
+      return [
+        'left' => $data['left'] ?? 50,
+        'top' => $data['top'] ?? 50
+      ];
+    }
+    return ['left' => 50, 'top' => 50];
+  } 
+}
---
+++ library/Integrations/Component/ImageResolver.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace Municipio\Integrations\Component;
+
+use \ComponentLibrary\Integrations\Image\ImageResolverInterface;
+
+class ImageResolver implements ImageResolverInterface {
+  
+  /**
+   * Get image url
+   * 
+   * @param int $id
+   * @param array $size
+   * @return string|null
+   */
+  public function getImageUrl(int $id, array $size): ?string {
+
+    //Make 0 values in array false 
+    $size = array_map(function($value) {
+      return $value === 0 ? false : $value; 
+    }, $size);
+
+    $image = wp_get_attachment_image_src($id, $size); 
+    if($image !== false && isset($image[0]) && filter_var($image[0], FILTER_VALIDATE_URL)) {
+      return $image[0]; 
+    }
+    return null; 
+  }
+
+  /**
+   * Get image alt
+   * 
+   * @param int $id
+   * @return string|null
+   */
+  public function getImageAltText(int $id): ?string {
+    $alt = get_post_meta($id, '_wp_attachment_image_alt', true); 
+    if($alt) {
+      return $alt; 
+    }
+    return null;
+  }
+}
