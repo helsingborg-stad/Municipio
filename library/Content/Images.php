c046ee1bf0f47e101d6063efadf25f8dcc10bf67
Sebastian Thulin <sebastian.thulin@helsingborg.se>
2024-09-26 21:00:02 +0200

    fix: linting

--- library/Content/Images.php
+++ library/Content/Images.php
@@ -4,6 +4,7 @@ namespace Municipio\Content;
 
 use Municipio\Integrations\Component\ImageResolver;
 use ComponentLibrary\Integrations\Image\Image as ImageComponentContract;
+
 class Images
 {
     public function __construct()
@@ -14,9 +15,9 @@ class Images
 
     /**
      * Normalize images
-     * 
+     *
      * @param string $content
-     * 
+     *
      * @return string
      */
     public static function normalizeImages($content)
@@ -27,7 +28,7 @@ class Images
             $dom = new \DOMDocument();
             $dom->loadHTML($encoding . $content, LIBXML_NOERROR);
 
-            $links = $dom->getElementsByTagName('a');
+            $links  = $dom->getElementsByTagName('a');
             $images = $dom->getElementsByTagName('img');
 
             self::processLinks($dom, $links);
@@ -43,10 +44,10 @@ class Images
 
     /**
      * Process links
-     * 
+     *
      * @param \DOMDocument $dom
      * @param \DOMNodeList $links
-     * 
+     *
      * @return void
      */
     private static function processLinks($dom, $links)
@@ -62,7 +63,7 @@ class Images
             $linkedImage = $link->firstChild;
             if (self::isSelfLinked($link, $linkedImage)) {
                 $captionText = self::extractCaption($link->parentNode);
-                $altText = $linkedImage->getAttribute('alt') ?: $captionText;
+                $altText     = $linkedImage->getAttribute('alt') ?: $captionText;
                 self::replaceWithBladeTemplate($dom, $link, $linkedImage, $altText, $captionText);
             }
         }
@@ -70,10 +71,10 @@ class Images
 
     /**
      * Process images
-     * 
+     *
      * @param \DOMDocument $dom
      * @param \DOMNodeList $images
-     * 
+     *
      * @return void
      */
     private static function processImages($dom, $images)
@@ -81,14 +82,13 @@ class Images
         //Check that we actually have images
         if (!is_object($images) || empty($images)) {
             return;
-        } 
+        }
 
         foreach ($images as $image) {
-
             //Only process images that are not already normalized
             if (apply_filters('Municipio/Content/ImageNormalized', false, $image)) {
                 continue;
-            } 
+            }
 
             //Get caption and alt text
             $captionText = self::extractCaption($image->parentNode);
@@ -101,23 +101,23 @@ class Images
 
     /**
      * Check if the link is self linked
-     * 
+     *
      * @param \DOMElement $link
-     * 
+     *
      * @return bool
      */
     private static function isSelfLinked($link, $image)
     {
-        $imgDir = pathinfo($image->getAttribute('src'), PATHINFO_DIRNAME);
+        $imgDir  = pathinfo($image->getAttribute('src'), PATHINFO_DIRNAME);
         $linkDir = pathinfo($link->getAttribute('href'), PATHINFO_DIRNAME);
         return $linkDir === $imgDir;
     }
 
     /**
      * Extract the caption from the parent node
-     * 
+     *
      * @param \DOMElement $parentNode
-     * 
+     *
      * @return string     The caption text
      */
     private static function extractCaption($parentNode)
@@ -134,23 +134,23 @@ class Images
 
     /**
      * Replace the image with a blade template
-     * 
+     *
      * @param \DOMDocument $dom
      * @param \DOMElement $element
      * @param \DOMElement $image
      * @param string $altText
      * @param string $captionText
-     * 
+     *
      * @return void
      */
     private static function replaceWithBladeTemplate($dom, $element, $image, $altText, $captionText)
     {
-        $url            = self::sanitizeRequestUrl($image->getAttribute('src'));
-        $attachmentId   = attachment_url_to_postid($url);
+        $url          = self::sanitizeRequestUrl($image->getAttribute('src'));
+        $attachmentId = attachment_url_to_postid($url);
 
         //Handle as know local image (in wp database)
-        if(is_numeric($attachmentId)) {
-            //Get image contract 
+        if (is_numeric($attachmentId)) {
+            //Get image contract
             $imageComponentContract = ImageComponentContract::factory(
                 (int) $attachmentId,
                 [768, false],
@@ -168,7 +168,7 @@ class Images
         }
 
         //Handle as unknown or external image (not in wp database)
-        if(!is_numeric($attachmentId)) {
+        if (!is_numeric($attachmentId)) {
             $html = render_blade_view('partials.content.image', [
                 'src'              => $url,
                 'alt'              => $altText,
@@ -183,7 +183,7 @@ class Images
             ]);
         }
 
-        if(is_string($html) && !empty($html)) {
+        if (is_string($html) && !empty($html)) {
             $newNode = \Municipio\Helper\FormatObject::createNodeFromString($dom, $html);
             if ($newNode) {
                 $element->parentNode->replaceChild($newNode, $element);
@@ -193,9 +193,9 @@ class Images
 
     /**
      * Check if image has been normalized
-     * 
+     *
      * @param bool $normalized
-     * 
+     *
      * @return bool
      */
     public static function imageHasBeenNormalized($normalized, $image): bool
@@ -208,24 +208,24 @@ class Images
 
     /**
      * Sanitize the request URL
-     * 
+     *
      * @param string $url
-     * 
+     *
      * @return string
      */
     private static function sanitizeRequestUrl($url): string
     {
         $parsedUrl = parse_url($url);
 
-        if(!$parsedUrl) {
+        if (!$parsedUrl) {
             return $url;
         }
-        
+
         // Reconstruct the URL without the query part
-        $sanitizedUrl = isset($parsedUrl['scheme']) ? $parsedUrl['scheme'] . '://' : '';
+        $sanitizedUrl  = isset($parsedUrl['scheme']) ? $parsedUrl['scheme'] . '://' : '';
         $sanitizedUrl .= isset($parsedUrl['host']) ? $parsedUrl['host'] : '';
         $sanitizedUrl .= isset($parsedUrl['path']) ? $parsedUrl['path'] : '';
-        
+
         return $sanitizedUrl;
     }
 }
