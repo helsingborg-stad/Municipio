c8adf1bcd8cb493c7e84e28324d482a33540adf7
Sebastian Thulin <sebastian.thulin@helsingborg.se>
2024-09-26 17:57:22 +0200

    fix: add docs. remove function that we dosent need.

--- library/Content/Images.php
+++ library/Content/Images.php
@@ -12,6 +12,13 @@ class Images
         add_filter('Municipio/Content/ImageNormalized', array($this, 'imageHasBeenNormalized'), 10, 2);
     }
 
+    /**
+     * Normalize images
+     * 
+     * @param string $content
+     * 
+     * @return string
+     */
     public static function normalizeImages($content)
     {
         $encoding = '<?xml encoding="utf-8" ?>';
@@ -34,6 +41,14 @@ class Images
         return $content;
     }
 
+    /**
+     * Process links
+     * 
+     * @param \DOMDocument $dom
+     * @param \DOMNodeList $links
+     * 
+     * @return void
+     */
     private static function processLinks($dom, $links)
     {
         if (!is_object($links) || empty($links)) return;
@@ -51,6 +66,14 @@ class Images
         }
     }
 
+    /**
+     * Process images
+     * 
+     * @param \DOMDocument $dom
+     * @param \DOMNodeList $images
+     * 
+     * @return void
+     */
     private static function processImages($dom, $images)
     {
         if (!is_object($images) || empty($images)) return;
@@ -66,6 +89,13 @@ class Images
         }
     }
 
+    /**
+     * Check if the link is self linked
+     * 
+     * @param \DOMElement $link
+     * 
+     * @return bool
+     */
     private static function isSelfLinked($link, $image)
     {
         $imgDir = pathinfo($image->getAttribute('src'), PATHINFO_DIRNAME);
@@ -73,6 +103,13 @@ class Images
         return $linkDir === $imgDir;
     }
 
+    /**
+     * Extract the caption from the parent node
+     * 
+     * @param \DOMElement $parentNode
+     * 
+     * @return string     The caption text
+     */
     private static function extractCaption($parentNode)
     {
         $captionText = '';
@@ -85,13 +122,24 @@ class Images
         return $captionText;
     }
 
+    /**
+     * Replace the image with a blade template
+     * 
+     * @param \DOMDocument $dom
+     * @param \DOMElement $element
+     * @param \DOMElement $image
+     * @param string $altText
+     * @param string $captionText
+     * 
+     * @return void
+     */
     private static function replaceWithBladeTemplate($dom, $element, $image, $altText, $captionText)
     {
         $url            = self::sanitizeRequestUrl($image->getAttribute('src'));
         $attachmentId   = attachment_url_to_postid($url);
 
         if(is_numeric($attachmentId)) {
-
+            echo 'Attachment ID found for image: ' . $url . '<br>';
             //Get image contract 
             $imageComponentContract = ImageComponentContract::factory(
                 (int) $attachmentId,
@@ -107,8 +155,8 @@ class Images
                     'parsed' => true,
                 ],
             ]);
-
         } else {
+            echo 'No attachment ID found for image: ' . $url . '<br>';
             $html = render_blade_view('partials.content.image', [
                 'src'              => $url,
                 'alt'              => $altText,
@@ -122,13 +170,21 @@ class Images
                 ],
             ]);
         }
-
-        $newNode = \Municipio\Helper\FormatObject::createNodeFromString($dom, $html);
-        if ($newNode) {
-            $element->parentNode->replaceChild($newNode, $element);
+        if(is_string($html) && !empty($html)) {
+            $newNode = \Municipio\Helper\FormatObject::createNodeFromString($dom, $html);
+            if ($newNode) {
+                $element->parentNode->replaceChild($newNode, $element);
+            }
         }
     }
 
+    /**
+     * Check if image has been normalized
+     * 
+     * @param bool $normalized
+     * 
+     * @return bool
+     */
     public static function imageHasBeenNormalized($normalized, $image): bool
     {
         if ($image->getAttribute('parsed')) {
@@ -142,14 +198,15 @@ class Images
         return $normalized;
     }
 
-    private static function getAttachmentId($url)
+    /**
+     * Sanitize the request URL
+     * 
+     * @param string $url
+     * 
+     * @return string
+     */
+    private static function sanitizeRequestUrl($url): string
     {
-        global $wpdb;
-        $attachment = $wpdb->get_col($wpdb->prepare("SELECT ID FROM $wpdb->posts WHERE guid='%s';", $url));
-        return $attachment[0];
-    }
-
-    private static function sanitizeRequestUrl($url) {
         $parsedUrl = parse_url($url);
 
         if(!$parsedUrl) {
