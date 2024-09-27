ad35e3c5050fe0569645b089e25e2e9af59a8113
Niclas <niclasn@outlook.com>
2024-09-27 10:04:32 +0200

    chore: docs

--- library/Controller/Navigation/Decorators/Default/AppendIsAncestorDecorator.php
+++ library/Controller/Navigation/Decorators/Default/AppendIsAncestorDecorator.php
@@ -7,6 +7,14 @@ use Municipio\Controller\Navigation\Helper\GetAncestorIds;
 
 class AppendIsAncestorDecorator implements DefaultMenuItemDecoratorInterface
 {
+    /**
+     * Decorates a menu item with the "ancestor" property based on its ID and the list of ancestors.
+     *
+     * @param array|object $menuItem The menu item to decorate.
+     * @param MenuConfigInterface $menuConfig The menu configuration.
+     * @param array $ancestors The list of ancestor menu item IDs.
+     * @return array The decorated menu item.
+     */
     public function decorate(array|object $menuItem, MenuConfigInterface $menuConfig, array $ancestors): array
     {
         if (!isset($menuItem['id']) || empty($ancestors)) {
