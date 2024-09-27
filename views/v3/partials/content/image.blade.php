cd6ead3f9235f8b33b089fb92d187e3193b924f0
Sebastian Thulin <sebastianthulin@users.noreply.github.com>
2024-09-27 10:28:25 +0200

    feat: rendering images in content correctly with ImageComponentContract (#1025)
    * fix: remove attributes not supported anymore.
    
    * fix: refactor image handling to smaller functions. Implement imagecomponentcontract.
    
    * fix: add docs. remove function that we dosent need.
    
    * fix: removes doc, set return type.
    
    * fix: various improvements in handling and syntax.
    
    * fix: linting
    
    * fix: update and build styles.
    
    * fix: get size of image dynamicly.
    
    * fix: lint
    
    ---------
    
    Co-authored-by: Sebastian Thulin <sebastian.thulin@helsingborg.se>

--- views/v3/partials/content/image.blade.php
+++ views/v3/partials/content/image.blade.php
@@ -1,11 +1,9 @@
 @image([
     'src'=> !empty($src) ? $src : false,
     'alt' => !empty($alt) ? $alt : false,
-    'heading' => !empty($heading) ? $heading : false,
-    'imgAttributeList' => !empty($imgAttributeList) ? $imgAttributeList : [],
-    'openModal' => !empty($openModal) ? $openModal : false,
-    'isPanel' => !empty($isPanel) ? $isPanel : false,
+    'imgAttributeList' => !empty($imgAttributeList) && is_array($imgAttributeList) ? $imgAttributeList : [],
+    'attributeList' => !empty($attributeList) && is_array($attributeList) ? $attributeList : [],
     'caption'   => !empty($caption) ? $caption : false,
-    'classList' => !empty($classList) ? $classList : [],
+    'classList' => !empty($classList) ? $classList : []
 ])
 @endimage
