c4b39af1b3c8b0eac59f66271be7544df240b925
Thor Brink <thor.brink@helsingborg.se>
2024-09-30 05:38:07 +0000

    refactor: apply code standards

--- library/ExternalContent/Sync/Triggers/TriggerSyncIfNotInProgress.php
+++ library/ExternalContent/Sync/Triggers/TriggerSyncIfNotInProgress.php
@@ -4,14 +4,23 @@ namespace Municipio\ExternalContent\Sync\Triggers;
 
 use Municipio\ExternalContent\Sync\SyncInPorgress\PostTypeSyncInProgressInterface;
 
+/**
+ * Class TriggerSyncIfNotInProgress
+ */
 class TriggerSyncIfNotInProgress implements TriggerSyncInterface
 {
+    /**
+     * Constructor.
+     */
     public function __construct(
         private PostTypeSyncInProgressInterface $inProgress,
         private TriggerSyncInterface $inner
     ) {
     }
 
+    /**
+     * @inheritDoc
+     */
     public function trigger(string $postType, ?int $postId = null): void
     {
         if ($this->inProgress->isInProgress($postType)) {
--- library/ExternalContent/Sync/Triggers/TriggerSyncInterface.php
+++ library/ExternalContent/Sync/Triggers/TriggerSyncInterface.php
@@ -4,5 +4,12 @@ namespace Municipio\ExternalContent\Sync\Triggers;
 
 interface TriggerSyncInterface
 {
+    /**
+     * Triggers the synchronization of external content.
+     *
+     * @param string $postType The post type to sync.
+     * @param int|null $postId The post id to sync.
+     * @return void
+     */
     public function trigger(string $postType, ?int $postId = null): void;
 }
