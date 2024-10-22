02a92c552f3d60b9f055f669f209311526d5f708
Thor Brink <thor.brink@helsingborg.se>
2024-10-22 08:32:29 +0000

    wip: add settings field

--- library/AcfFields/php/options-comment-settings.php
+++ library/AcfFields/php/options-comment-settings.php
@@ -6,6 +6,26 @@
     'title' => __('Comment Settings', 'municipio'),
     'fields' => array(
         0 => array(
+            'key' => 'field_67175e844ab4a',
+            'label' => __('Disable discussion feature', 'municipio'),
+            'name' => 'disable_discussion_feature',
+            'aria-label' => '',
+            'type' => 'true_false',
+            'instructions' => __('Disable discussion settings page, comment form and posted comments for all post types.', 'municipio'),
+            'required' => 0,
+            'conditional_logic' => 0,
+            'wrapper' => array(
+                'width' => '',
+                'class' => '',
+                'id' => '',
+            ),
+            'message' => '',
+            'default_value' => 0,
+            'ui_on_text' => __('Disabled', 'municipio'),
+            'ui_off_text' => '',
+            'ui' => 1,
+        ),
+        1 => array(
             'key' => 'field_67173bdc8a308',
             'label' => __('Hide discussion for logged out users', 'municipio'),
             'name' => 'hide_discussion_for_logged_out_users',
@@ -13,7 +33,15 @@
             'type' => 'true_false',
             'instructions' => '',
             'required' => 0,
-            'conditional_logic' => 0,
+            'conditional_logic' => array(
+                0 => array(
+                    0 => array(
+                        'field' => 'field_67175e844ab4a',
+                        'operator' => '!=',
+                        'value' => '1',
+                    ),
+                ),
+            ),
             'wrapper' => array(
                 'width' => '',
                 'class' => '',
