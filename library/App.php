a63b46d9a5046e5883a95cb867a9bf0f2e9f4ff2
Thor Brink <thor.brink@helsingborg.se>
2024-10-22 11:21:51 +0000

    feat: add option to disable discussions feature completely

--- library/App.php
+++ library/App.php
@@ -10,6 +10,7 @@ use Municipio\Api\RestApiEndpointsRegistry;
 use Municipio\BrandedEmails\ApplyMailHtmlTemplate;
 use Municipio\BrandedEmails\HtmlTemplate\Config\HtmlTemplateConfigService;
 use Municipio\BrandedEmails\HtmlTemplate\DefaultHtmlTemplate;
+use Municipio\Comment\OptionalDisableDiscussionFeature;
 use Municipio\Comment\OptionalHideDiscussionWhenLoggedOut;
 use Municipio\Config\Features\SchemaData\SchemaDataConfigInterface;
 use Municipio\Content\ResourceFromApi\Api\ResourceFromApiRestController;
@@ -218,6 +219,7 @@ class App
         new \Municipio\Comment\Likes();
         new \Municipio\Comment\Filters();
         new \Municipio\Comment\Form();
+        $this->hooksRegistrar->register(new OptionalDisableDiscussionFeature($this->wpService, $this->acfService));
         $this->hooksRegistrar->register(new OptionalHideDiscussionWhenLoggedOut($this->wpService, $this->acfService));
 
         /**
