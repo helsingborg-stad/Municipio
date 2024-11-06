539e4dae3df813f5ab94272bc884eccd82e8665f
Niclas <niclasn@outlook.com>
2024-11-06 14:47:49 +0100

    chore: docblocks

--- library/Admin/Roles/General.php
+++ library/Admin/Roles/General.php
@@ -16,8 +16,18 @@ use WpService\Contracts\UpdateOption;
 use WpService\Contracts\WpDoingAjax;
 use WpService\Contracts\WpRedirect;
 
+/**
+ * Class General
+ *
+ * This class represents the General role in the Admin section of the Municipio theme.
+ * It contains methods and properties related to the General role.
+ */
 class General
 {
+    
+    /**
+     * Constructor for the General class.
+     */
     public function __construct(private AddFilter&AddAction&CurrentUserCan&ShowAdminBar&IsAdmin&WpDoingAjax&WpRedirect&HomeUrl&GetRole&AddRole&DeleteOption&UpdateOption&RemoveRole $wpService)
     {
         $this->wpService->addAction('admin_init', array($this, 'removeUnusedRoles'));
@@ -28,13 +38,22 @@ class General
         }
     }
 
+    /**
+     * Handles users without the edit_posts capability.
+     *
+     * This method is responsible for handling users who do not have the edit_posts capability. It performs the following actions:
+     * - Hides the admin bar for the user.
+     * - Adds an action to the 'admin_init' hook to redirect the user to the home URL if they are an admin and not making an AJAX request.
+     *
+     * @return void
+     */
     public function handleUsersWithoutEditPostsCapability()
     {
         $this->wpService->showAdminBar(false);
 
         $this->wpService->addAction('admin_init', function () {
             if ($this->wpService->isAdmin() && !$this->wpService->wpDoingAjax()) {
-                $this->wpService->wpRedirect($this->wpService->homeUrl()); // Redirect to homepage or another URL
+                $this->wpService->wpRedirect($this->wpService->homeUrl());
                 exit;
             }
         });
