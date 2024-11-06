55c6863e927670307e7abb86f593200db955e851
Niclas Norin <103985736+NiclasNorin@users.noreply.github.com>
2024-11-06 11:38:33 +0100

    feat: standard menu or page tree fallback menu (#1109)

--- library/Controller/Navigation/Decorators/Menu/PageTreeAppendMenuItemsFetchUrl.php
+++ library/Controller/Navigation/Decorators/Menu/PageTreeAppendMenuItemsFetchUrl.php
@@ -31,7 +31,6 @@ class PageTreeAppendMenuItemsFetchUrl implements MenuInterface
 
         $targetMenuIdentifiers = [
             'mobile'          => 'mobile',
-            'primary'         => 'mobile',
             'additional-menu' => 'mobile',
             'sidebar'         => 'sidebar',
         ];
