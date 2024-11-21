836bfc6a600d5569f2b5b0556946d427da57eefb
Niclas <niclasn@outlook.com>
2024-11-21 13:45:49 +0100

    feat: added styling for duotone second area

--- library/Customizer/Sections/Menu/Drawer.php
+++ library/Customizer/Sections/Menu/Drawer.php
@@ -32,6 +32,29 @@ class Drawer
                   ],
                 ]
             ],
-          ]);
+        ]);
+
+        KirkiField::addField([
+            'type'        => 'select',
+            'settings'    => 'drawer_color_scheme_secondary_area',
+            'label'       => esc_html__('Drawer secondary area', 'municipio'),
+            'description' => esc_html__('If using both the areas in the drawer menu this will decide the bottom area', 'municipio'),
+            'section'     => $sectionID,
+            'default'     => 'duotone-primary',
+            'priority'    => 10,
+            'choices'     => [
+                ''  => esc_html__('Basic', 'municipio'),
+                'duotone-primary' => esc_html__('Primary', 'municipio'),
+                'duotone-secondary' => esc_html__('Secondary', 'municipio'),
+            ],
+            'output'          => [
+                [
+                    'type'    => 'modifier',
+                    'context' => [
+                      'site.header.drawer'
+                  ],
+                ]
+            ],
+        ]);
     }
 }
