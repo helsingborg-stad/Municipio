09fd392c5c446f8946eacc6755beb941ebdd4482
Sebastian Thulin <sebastian.thulin@helsingborg.se>
2024-11-22 12:04:47 +0100

    fix: tests

---
+++ library/Customizer.test.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Municipio;
+
+use PHPUnit\Framework\TestCase;
+use WpService\Implementations\FakeWpService;
+use Municipio\TestUtils\WpMockFactory;
+
+/**
+ * @group wp_mock
+ */
+class CustomizerTest extends TestCase
+{
+    /**
+     * @testdox sanitizeKirkiDefaultArrayValue converts empty string value to array if default is array
+     */
+    public function testSanitizeKirkiDefaultArrayValueConvertsEmptyStringValueToArrayIfDefaultIsArray()
+    {
+        $wpService  = new FakeWpService([
+          'addFilter' => true,
+          'addAction' => true
+        ]);
+        $wpdb      = WpMockFactory::createWpdb([]);
+      
+        $value      = '';
+        $default    = ['foo' => 'bar'];
+        $customizer = new \Municipio\Customizer(
+          $wpService,
+          $wpdb
+        );
+
+        $sanitizedValue = $customizer->sanitizeKirkiDefaultArrayValue($value, $default);
+
+        $this->assertEquals(['foo' => 'bar'], $sanitizedValue);
+    }
+}
