52ecb271829539eb5b2f17b8b99351337121eea9
viktor7ltz <viktor.schultz@consid.se>
2024-12-06 15:06:39 +0000

    tinymce table added

---
+++ library/Admin/TinyMce/Table/Table.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Municipio\Admin\TinyMce\Table;
+
+class Table extends \Municipio\Admin\TinyMce\PluginClass
+{
+    public function init()
+    {
+        $this->pluginSlug = 'table';
+    }
+}
