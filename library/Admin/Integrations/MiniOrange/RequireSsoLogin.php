a6d26861f5be675d34330dc897fcd64619a712d5
Sebastian Thulin <sebastian.thulin@helsingborg.se>
2024-12-11 10:38:55 +0100

    feat: adds ability to require sso login.

--- library/Admin/Integrations/MiniOrange/RequireSsoLogin.php
+++ library/Admin/Integrations/MiniOrange/RequireSsoLogin.php
@@ -11,7 +11,24 @@ class RequireSsoLogin implements Hookable
 
   public function addHooks(): void
   {
+    if (!$this->isEnabled()) {
+      return;
+    }
+
     $this->wpService->addAction('init', array($this, 'redirectToSsoProvider'));
+  }
+
+  /**
+   * Check if the SSO login required is enabled
+   *
+   * @return bool
+   */
+  private function isEnabled(): bool
+  {
+    if(!defined('MO_SAML_PLUGIN_DIR')) {
+      return false;
+    }
+    return false;
   }
 
   /**
@@ -21,24 +38,46 @@ class RequireSsoLogin implements Hookable
    */
   public function redirectToSsoProvider(): void
   {
-    if (strpos($_SERVER['REQUEST_URI'], 'wp-login.php') !== false) {
-      if(($_GET['option']??null) != 'saml_user_login'){
-        wp_redirect($this->getSsoUrl());
+      if (strpos($_SERVER['REQUEST_URI'], 'wp-login.php') === false) {
+        return;
       }
-    }
+
+      if (($_GET['option'] ?? null) === 'saml_user_login') {
+        return;
+      }
+
+      if (in_array(($_GET['action'] ?? null), ['logout', 'log-out'])) {
+        return;
+      }
+
+      // Redirect to the SSO URL
+      wp_redirect($this->getSsoUrl());
+      exit;
   }
 
   /**
-   * Get the SSO URL from the settings
+   * Get the SSO URL with redirect parameter
    *
    * @return string|null
    */
   private function getSsoUrl(): ?string
   {
-    return add_query_arg(
+    $url = $_SERVER['REQUEST_URI'] ?? home_url('/wp-login.php');
+
+    $url = add_query_arg(
       'option',
       'saml_user_login',
-      $_SERVER['REQUEST_URI'] ?? home_url('/wp-login.php')
+      $url
     );
+
+    if(isset($_GET['redirect_to'])) {
+      $url = add_query_arg(
+        'redirect_to',
+        $_GET['redirect_to'],
+        $url
+      );
+    }
+
+    return $url;
   }
 }
