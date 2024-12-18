97d69e1647efcec9c105b746208c08a698f82341
Thor Brink <thor.brink@helsingborg.se>
2024-12-18 12:05:41 +0000

    feat: add getTermIcon method to PostObject and decorators for term icon retrieval

--- library/PostObject/Decorators/PostObjectWithTermIcons.test.php
+++ library/PostObject/Decorators/PostObjectWithTermIcons.test.php
@@ -54,4 +54,74 @@ class PostObjectWithTermIconsTest extends TestCase
         $this->assertIsArray($decorator->getTermIcons());
         $this->assertContainsOnlyInstancesOf(TermIconInterface::class, $decorator->getTermIcons());
     }
+
+    /**
+     * @testdox getTermIcon() returns null if the post object has no terms.
+     */
+    public function testGetTermIconReturnsNullIfPostObjectHasNoTerms()
+    {
+        $wpService  = new FakeWpService(['getObjectTaxonomies' => ['category'], 'getTheTerms' => []]);
+        $postObject = $this->createMock(PostObjectInterface::class);
+        $postObject->method('getId')->willReturn(1);
+        $postObject->method('getPostType')->willReturn('post');
+
+        $tryGetTermIcon = $this->createMock(TryGetTermIconInterface::class);
+
+        $decorator = new PostObjectWithTermIcons($postObject, $wpService, $tryGetTermIcon);
+
+        $this->assertNull($decorator->getTermIcon());
+    }
+
+    /**
+     * @testdox getTermIcon() returns a single term icon if the post object has terms.
+     */
+    public function testGetTermIconReturnsSingleTermIconIfPostObjectHasTerms()
+    {
+        $wpService = new FakeWpService([
+            'getObjectTaxonomies' => ['category'],
+            'getTheTerms'         => [
+                (object) ['term_id' => 1],
+                (object) ['term_id' => 2],
+            ],
+        ]);
+
+        $postObject = $this->createMock(PostObjectInterface::class);
+        $postObject->method('getId')->willReturn(1);
+        $postObject->method('getPostType')->willReturn('post');
+
+        $tryGetTermIcon = $this->createMock(TryGetTermIconInterface::class);
+        $tryGetTermIcon->method('tryGetTermIcon')->willReturn($this->createMock(TermIconInterface::class));
+
+        $decorator = new PostObjectWithTermIcons($postObject, $wpService, $tryGetTermIcon);
+
+        $this->assertInstanceOf(TermIconInterface::class, $decorator->getTermIcon());
+    }
+
+    /**
+     * @testdox getTermIcon() returns a term icon for a specific taxonomy if the post object has terms.
+     */
+    public function testGetTermIconReturnsTermIconForSpecificTaxonomyIfPostObjectHasTerms()
+    {
+        $term           = $this->getMockBuilder(\stdClass::class)->setMockClassName('WP_Term')->getMock();
+        $term->taxonomy = 'category';
+        $wpService      = new FakeWpService([
+            'getObjectTaxonomies' => ['category'],
+            'getTheTerms'         => [
+                (object) ['term_id' => 1],
+                (object) ['term_id' => 2],
+            ],
+            'getTerm'             => $term
+        ]);
+
+        $postObject = $this->createMock(PostObjectInterface::class);
+        $postObject->method('getId')->willReturn(1);
+        $postObject->method('getPostType')->willReturn('post');
+
+        $tryGetTermIcon = $this->createMock(TryGetTermIconInterface::class);
+        $tryGetTermIcon->method('tryGetTermIcon')->willReturn($this->createMock(TermIconInterface::class));
+
+        $decorator = new PostObjectWithTermIcons($postObject, $wpService, $tryGetTermIcon);
+
+        $this->assertInstanceOf(TermIconInterface::class, $decorator->getTermIcon('category'));
+    }
 }
