0baf85c2c877646474dbf7ce8b37c38c787fdf6a
Thor Brink <thor.brink@helsingborg.se>
2025-01-10 10:35:14 +0100

    feat: cache TermIconResolver results (#1236)

---
+++ library/PostObject/Icon/Resolvers/CachedIconResolver.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace Municipio\PostObject\Icon\Resolvers;
+
+use Municipio\PostObject\Icon\IconInterface;
+use Municipio\PostObject\PostObjectInterface;
+use PHPUnit\Framework\TestCase;
+
+/**
+ * Class CachedIconResolver
+ *
+ * This class is responsible for resolving icons with caching mechanism.
+ */
+class CachedIconResolver implements IconResolverInterface
+{
+    private static $cache = [];
+
+    /**
+     * Constructor.
+     *
+     * @param PostObjectInterface $postObject
+     * @param IconResolverInterface $innerResolver
+     */
+    public function __construct(private PostObjectInterface $postObject, private IconResolverInterface $innerResolver)
+    {
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function resolve(): ?IconInterface
+    {
+        $cacheKey = (string)$this->postObject->getId();
+
+        if (array_key_exists($cacheKey, self::$cache)) {
+            return self::$cache[$cacheKey];
+        }
+
+        return self::$cache[$cacheKey] = $this->innerResolver->resolve();
+    }
+}
