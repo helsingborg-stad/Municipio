31503c507f4d993b46f28337c65be95ac46a6b6f
Niclas <niclasn@outlook.com>
2025-01-10 10:02:23 +0100

    chore: doc

--- library/Admin/Private/UserGroupRestriction.php
+++ library/Admin/Private/UserGroupRestriction.php
@@ -13,11 +13,24 @@ class UserGroupRestriction
     private string $userGroupMetaKey  = 'user-group-visibility';
     private string $userGroupTaxonomy = 'user_group';
 
+    /**
+     * UserGroupRestriction class constructor.
+     *
+     * @param AddAction $wpService An instance of the AddAction class.
+     * @param WpGetCurrentUser $wpService An instance of the WpGetCurrentUser class.
+     * @param WpGetPostTerms $wpService An instance of the WpGetPostTerms class.
+     * @param IsWpError $wpService An instance of the IsWpError class.
+     */
     public function __construct(private AddAction&WpGetCurrentUser&WpGetPostTerms&IsWpError $wpService)
     {
         $this->wpService->addAction('pre_get_posts', array($this, 'restrictPosts'));
     }
 
+    /**
+     * Retrieves the current user.
+     *
+     * @return WP_User The current user object.
+     */
     private function getUser(): WP_User
     {
         static $user;
@@ -29,6 +42,12 @@ class UserGroupRestriction
         return $user;
     }
 
+    /**
+     * Retrieves the user groups for a given user.
+     *
+     * @param WP_User $user The user object.
+     * @return string|null The user groups as a string or null if no user groups are found.
+     */
     private function getUserGroups(WP_User $user): ?string
     {
         static $userGroups;
@@ -37,7 +56,7 @@ class UserGroupRestriction
             $userGroups = $this->wpService->wpGetPostTerms($user->ID, $this->userGroupTaxonomy);
 
             if ($this->wpService->isWpError($userGroups) || empty($userGroups)) {
-                return ""; // Return an empty string if no terms are found or there's an error
+                return null;
             }
 
             $userGroups = array_map(function ($term) {
@@ -45,10 +64,15 @@ class UserGroupRestriction
             }, $userGroups);
         }
 
-        // Return the first value, or an empty string if $userGroups is empty
         return $userGroups[0] ?? null;
     }
 
+    /**
+     * Restricts the posts in a query based on user group restrictions.
+     *
+     * @param WP_Query $query The WP_Query object representing the current query.
+     * @return void
+     */
     public function restrictPosts($query)
     {
         $user = $this->getUser();
@@ -86,6 +110,12 @@ class UserGroupRestriction
         $query->set('meta_query', $metaQuery);
     }
 
+    /**
+     * Determines if a post can have private status.
+     *
+     * @param string|array $postStatus The status of the post(s).
+     * @return bool Returns true if the post(s) can have private status, false otherwise.
+     */
     private function canHavePrivatePosts(string|array $postStatus): bool
     {
         $postStatus = is_array($postStatus) ? $postStatus : [$postStatus];
