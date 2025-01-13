4e59b586f001ffd377b14708e2e5453d54b3d0bd
Sebastian Thulin <sebastian.thulin@helsingborg.se>
2025-01-13 12:56:10 +0100

    fix: remove protected methods from interface

--- library/Helper/User/FieldResolver/UserGroupUrlInterface.php
+++ library/Helper/User/FieldResolver/UserGroupUrlInterface.php
@@ -10,28 +10,4 @@ interface UserGroupUrlInterface
      * @return string|null
      */
     public function get(): ?string;
-
-    /**
-     * Resolve the arbitrary URL.
-     *
-     * @param string $termId
-     * @return string|null
-     */
-    public function resolveArbitraryUrl(string $termId): ?string;
-
-    /**
-     * Resolve the post type URL.
-     *
-     * @param string $termId
-     * @return string|null
-     */
-    public function resolvePostTypeUrl(string $termId): ?string;
-
-    /**
-     * Resolve the blog ID URL.
-     *
-     * @param string $termId
-     * @return string|null
-     */
-    public function resolveBlogIdUrl(string $termId): ?string;
 }
