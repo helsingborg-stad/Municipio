adbf586baf6be9d7a30e39d9b66311f2bf5394ce
Sebastian Thulin <sebastian.thulin@helsingborg.se>
2025-01-15 16:14:16 +0100

    fix: clarify the claass for auto sso

--- library/Integrations/MiniOrange/RequireSsoLogin.php
+++ library/Integrations/MiniOrange/RequireSsoLogin.php
@@ -5,79 +5,97 @@ namespace Municipio\Integrations\MiniOrange;
 use Municipio\Integrations\MiniOrange\Config\MiniOrangeConfig;
 use Municipio\HooksRegistrar\Hookable;
 use WpService\WpService;
-
+/**
+ * Require SSO login for all users.
+ * 
+ * This will automaticly redirect the user to the selected SSO provider when visiting the login page.
+ * This is done simply by adding the `option=saml_user_login` parameter to the URL and reload the page.
+ * 
+ */
 class RequireSsoLogin implements Hookable
 {
+    private const ACTIONS_TO_EXCLUDE    = ['logout', 'log-out'];
+    private const ALLOWED_PROTOCOLS     = ['http', 'https'];
+
     public function __construct(private WpService $wpService, private MiniOrangeConfig $config)
     {
     }
 
+    /**
+     * Add hooks
+     */
     public function addHooks(): void
     {
-        if (!$this->isEnabled()) {
-            return;
-        }
-
-        $this->wpService->addAction('init', array($this, 'redirectToSsoProvider'));
+        $this->wpService->addAction('init', function () {
+            if ($this->isEnabled()) {
+                $this->redirectToSsoProvider();
+            }
+        });
     }
 
-  /**
-   * Check if the SSO login required is enabled
-   *
-   * @return bool
-   */
+    /**
+     * Check if the automatic SSO login is enabled.
+     *
+     * @return bool
+     */
     private function isEnabled(): bool
     {
         return $this->config->requireSsoLogin();
     }
 
-  /**
-   * Redirect to the SSO provider if the user tries to access the login page
-   *
-   * @return void
-   */
-    public function redirectToSsoProvider(): void
+    /**
+     * Redirect to the SSO provider if the current request should be redirected.
+     */
+    private function redirectToSsoProvider(): void
     {
-        if (strpos($_SERVER['REQUEST_URI'], 'wp-login.php') === false) {
+        if (!$this->shouldRedirectToSso()) {
             return;
         }
 
-        if (($_GET['option'] ?? null) === 'saml_user_login') {
-            return;
-        }
-
-        if (in_array(($_GET['action'] ?? null), ['logout', 'log-out'])) {
-            return;
-        }
-
-        // Redirect to the SSO URL
-        $this->wpService->wpRedirect($this->getSsoUrl());
-        exit;
-    }
-
-  /**
-   * Get the SSO URL with redirect parameter
-   *
-   * @return string|null
-   */
-    private function getSsoUrl(): ?string
-    {
-        $url = $_SERVER['REQUEST_URI'] ?? $this->wpService->homeUrl('/wp-login.php');
-
-        $url = add_query_arg(
-            'option',
-            'saml_user_login',
-            $url
+        $redirectUrl = $this->getSsoUrlWithAllParams();
+        $redirectUrl = $this->wpService->escUrlRaw(
+            $redirectUrl, 
+            self::ALLOWED_PROTOCOLS
         );
 
-        if (isset($_GET['redirect_to'])) {
-            $url = add_query_arg(
-                'redirect_to',
-                $_GET['redirect_to'],
-                $url
-            );
+        if ($redirectUrl) {
+            $this->wpService->wpSafeRedirect($redirectUrl);
+            exit;
         }
+    }
 
-        return $url;
+    /*
+    * Check if the current request should be redirected to the SSO provider.
+    *
+    * @return bool
+    */
+    private function shouldRedirectToSso(): bool
+    {
+        $requestUri = $_SERVER['REQUEST_URI'] ?? null;
+        $option = $_GET['option'] ?? null;
+        $action = $_GET['action'] ?? null;
+
+        return $requestUri && strpos($requestUri, 'wp-login.php') !== false
+            && $option !== 'saml_user_login'
+            && !in_array($action, self::ACTIONS_TO_EXCLUDE, true);
+    }
+
+    /*
+    * Get the URL to the SSO provider with all existing GET parameters
+    * and the `option` parameter set to `saml_user_login`.
+    * 
+    * When the login page is accessed with the `option` parameter set to `
+    * saml_user_login`, it will trigger the SSO login in miniOrange plugin.
+    *
+    * @return string|null
+    */
+    private function getSsoUrlWithAllParams(): ?string
+    {
+        $base   = $_SERVER['REQUEST_URI'] ?? $this->wpService->homeUrl('/wp-login.php');
+        $params = $_GET ?? [];
+
+        $params['option'] = 'saml_user_login';
+
+        return $this->wpService->addQueryArg($params, $base);
     }
 }
