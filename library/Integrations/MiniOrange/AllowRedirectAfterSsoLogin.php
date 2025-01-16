8c3590aa9286dc5b94554a2febaa6a8b478fc2ab
Thor Brink <thor.brink@helsingborg.se>
2025-01-16 10:26:55 +0000

    docs: add missing docblocks

--- library/Integrations/MiniOrange/AllowRedirectAfterSsoLogin.php
+++ library/Integrations/MiniOrange/AllowRedirectAfterSsoLogin.php
@@ -7,6 +7,9 @@ use WpService\Contracts\AddAction;
 use WpService\Contracts\ApplyFilters;
 use WpService\Contracts\WpSafeRedirect;
 
+/**
+ * Allow redirect after SSO login.
+ */
 class AllowRedirectAfterSsoLogin implements Hookable
 {
     public const APPLIED_FLAG             = 'customMiniOrgangeLoginRedirectApplied';
@@ -27,6 +30,11 @@ class AllowRedirectAfterSsoLogin implements Hookable
         $this->wpService->addAction('set_logged_in_cookie', [$this, 'allowRedirectAfterSsoLogin']);
     }
 
+    /**
+     * Allow redirect after SSO login.
+     *
+     * @return void
+     */
     public function allowRedirectAfterSsoLogin(): void
     {
         if (!$this->doingMiniOrgangeLogin() || $this->isCustomMiniOrgangeLoginRedirectApplied()) {
@@ -39,16 +47,29 @@ class AllowRedirectAfterSsoLogin implements Hookable
         }
     }
 
+    /**
+     * Check if doing MiniOrgange login.
+     *
+     * @return bool
+     */
     private function doingMiniOrgangeLogin(): bool
     {
         return isset($_POST['SAMLResponse']) && isset($_POST['RelayState']);
     }
 
+    /**
+     * Set applied flag.
+     */
     private function setAppliedFlag(): void
     {
         $_POST[self::APPLIED_FLAG] = true;
     }
 
+    /**
+     * Check if custom MiniOrgange login redirect is applied.
+     *
+     * @return bool
+     */
     private function isCustomMiniOrgangeLoginRedirectApplied(): bool
     {
         return isset($_POST[self::APPLIED_FLAG]);
