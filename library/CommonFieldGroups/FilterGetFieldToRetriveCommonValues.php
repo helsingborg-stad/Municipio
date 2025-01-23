052dad1b51ee24332277801839f2f6023e5ca334
Sebastian Thulin <sebastian.thulin@helsingborg.se>
2025-01-23 19:29:45 +0100

    fix: also filter keys in options
    This will make get field and get option to retrive identical data.

--- library/CommonFieldGroups/FilterGetFieldToRetriveCommonValues.php
+++ library/CommonFieldGroups/FilterGetFieldToRetriveCommonValues.php
@@ -32,117 +32,80 @@ class FilterGetFieldToRetriveCommonValues implements Hookable
     }
 
     /**
-     * Populates the $fieldsToFilter array dynamically from ACF field groups provided in the config.
-     *
-     * @return void
-     */
-    public function populateFieldsToFilter(): void
-    {
-        $acfGroupKeys = $this->config->getAcfFieldGroupsToFilter();
- 
-        foreach ($acfGroupKeys as $groupData) {
-            foreach ($groupData as $groupId) {
-                $this->fieldsToFilter = array_merge(
-                    $this->fieldsToFilter,
-                    $this->getFieldKeysForGroup($groupId) ?: []
-                );
+ * Populates the $fieldsToFilter array dynamically from ACF field groups provided in the config.
+ *
+ * @return void
+ */
+public function populateFieldsToFilter(): void
+{
+    $acfGroupKeys = $this->config->getAcfFieldGroupsToFilter();
+
+    foreach ($acfGroupKeys as $groupData) {
+        foreach ($groupData as $groupId) {
+            $fields = $this->getFieldKeysForGroup($groupId);
+            foreach ($fields as $field) {
+                $this->fieldsToFilter[] = $field; // Append fields (with both name and key)
             }
         }
+    }
 
-        //TODO: Handle resolving of fields with repeaters. 
-        //TODO: Also, filter all _options_* prefixes, to return the field id. 
-        $this->fieldsToFilter[] = "broken_links_local_domains_0_domain"; 
-        $this->fieldsToFilter[] = "broken_links_local_domains_1_domain"; 
-        
-        //Make unique array
-        $this->fieldsToFilter = $fieldsToFilter = array_unique($this->fieldsToFilter);
+    // Add additional custom fields to filter
+    $this->fieldsToFilter[] = ['name' => 'broken_links_local_domains', 'key' => 'field_6718e7d0b54f7'];
 
-        //Get all values from the keys provided
-        $this->siteSwitcher->runInSite(
-            $this->wpService->getMainSiteId(),
-            function() {
-                foreach($this->fieldsToFilter as $fieldToFilter) {
-                    $fieldToFilterWithPrefix = "options_" . $fieldToFilter; 
-                    $this->fieldsKeyValueStore[$fieldToFilter] = get_option($fieldToFilterWithPrefix, false); 
-                }
+    // Ensure unique fields by filtering out duplicates based on `name`
+    $this->fieldsToFilter = array_unique($this->fieldsToFilter, SORT_REGULAR);
+
+    // Retrieve the values for each field from the main site
+    $this->siteSwitcher->runInSite(
+        $this->wpService->getMainSiteId(),
+        function () {
+            foreach ($this->fieldsToFilter as $field) {
+                $optionKey = "options_" . $field['name'];           // Key for actual option value
+                $metaKey = "_options_" . $field['name'];           // Key for field key (meta reference)
+
+                // Fetch both the value and the field key reference
+                $this->fieldsKeyValueStore[$optionKey] = get_option($optionKey, false); // Main value
+                $this->fieldsKeyValueStore[$metaKey] = get_option($metaKey, false);    // Field key reference
             }
+        }
+    );
+
+    // Add the stored values to this site's filters
+    foreach ($this->fieldsKeyValueStore as $fieldKey => $fieldValue) {
+        $this->wpService->addFilter(
+            'pre_option_' . $fieldKey,
+            fn($localValue) => $fieldValue
         );
-
-        //Filter the stored values into to this site
-        foreach($this->fieldsKeyValueStore as $optionKey => $optionValue) {
-            
-            //Allow get_option to find this
-            $this->wpService->addFilter(
-                'pre_option_' . $optionKey, 
-                function($localValue) use ($optionValue) {  
-                    return $optionValue;
-                }
-            );
-
-            //Allow get_field to find this
-            $this->wpService->addFilter(
-                'acf/load_value/name=' . $optionKey,
-                function($localValue) use ($optionValue) {  
-                    return $optionValue;
-                }
-            );
-        }
-
-        foreach($this->fieldsKeyValueStore as $optionKey => $optionValue) {
-            if(isset($_GET['debug'])) {
-                var_dump([
-                    'key' => $optionKey,
-                    'get-field' => get_field($optionKey, 'option'),
-                    'get-option' => get_option($optionKey)
-                ]);
-            }
-        }
     }
 
-    /**
-     * Retrieves the field keys for a specific ACF field group by its ID.
-     *
-     * @param string $groupId The ACF field group ID.
-     * @return array The field keys in the group.
-     */
-    public function getFieldKeysForGroup(string $groupId): array
-    {
-        //TODO: Remove when acf function is delcared in service
-        $func = $this->acfService->acfGetFields ?? 'acf_get_fields';
-
-        // Call the function and return the field keys
-        return array_map(fn($field) => $field['name'], $func($groupId) ?: []);
+    if (isset($_GET['debug'])) {
+        foreach ($this->fieldsKeyValueStore as $fieldKey => $fieldValue) {
+            var_dump([
+                'key' => $fieldKey,
+                'value' => $fieldValue,
+                'get-option' => get_option($fieldKey),
+            ]);
+        }
     }
+}
 
-    /**
-     * Filters the ACF field value based on predefined conditions.
-     *
-     * @param mixed  $localValue   The current value of the field.
-     * @param int    $postId  The ID of the post being edited.
-     * @param string $field   The field object being loaded.
-     * @return mixed The filtered value for the field.
-     */
-    public function filterFieldValue(mixed $localValue, null|string|int $id, array $field)
-    {
-        
-        if(!in_array($id, ['option', 'options'])) {
-            return $localValue;
-        }
+/**
+ * Retrieves the field keys for a specific ACF field group by its ID.
+ *
+ * @param string $groupId The ACF field group ID.
+ * @return array An array of fields with both 'name' and 'key'.
+ */
+public function getFieldKeysForGroup(string $groupId): array
+{
+    // TODO: Remove when acf function is declared in service
+    $func = $this->acfService->acfGetFields ?? 'acf_get_fields';
 
-        if($this->wpService->isMainSite()) {
-            return $localValue;
-        }
-
-        
-
-        if (in_array($field['name'], $this->fieldsToFilter)) {
-
-            
-            return $this->getFieldValueFromMainBlog($field['name']);
-        }
-        
-        return $localValue;
-    }
+    // Return both 'name' and 'key' for each field
+    return array_map(
+        fn($field) => ['name' => $field['name'], 'key' => $field['key']],
+        $func($groupId) ?: []
+    );
+}
 
     /**
      * Fetches the field value from the main blog using the SiteSwitcher.
