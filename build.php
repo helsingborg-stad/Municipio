88e2ce892ffd8fa603956e98fedf4392ab01469b
Mackan <Sven65@users.noreply.github.com>
2025-01-29 14:02:41 +0100

    build: add option --install-npm to build.php to install from NPM directly. (#1021)
    * Add option --install-npm to build.php to install from NPM directly.
    
    * refactor: apply coding standards
    
    ---------
    
    Co-authored-by: Thor Brink <thorbrink@gmail.com>
    Co-authored-by: Thor Brink <thor.brink@helsingborg.se>

--- build.php
+++ build.php
@@ -5,34 +5,44 @@ if (php_sapi_name() !== 'cli') {
     exit(0);
 }
 
-/* Parameters: 
+/* Parameters:
  --no-composer      Does not install vendors. Just create the autoloader.
- --cleanup          Remove removeables. 
- --allow-gulp       Allow gulp to be used. 
+ --cleanup          Remove removeables.
+ --install-npm      Install NPM package instead
 */
 
 // Any command needed to run and build plugin assets when newly cheched out of repo.
 $buildCommands = [];
 
 //Add composer build, if flag --no-composer is undefined.
-//Dump autloader. 
+//Dump autloader.
 //Only if composer.json exists.
-if(file_exists('composer.json')) {
-    if(is_array($argv) && !in_array('--no-composer', $argv)) {
-        $buildCommands[] = 'composer install --prefer-dist --no-progress --no-dev'; 
+if (file_exists('composer.json')) {
+    if (is_array($argv) && !in_array('--no-composer', $argv)) {
+        $buildCommands[] = 'composer install --prefer-dist --no-progress --no-dev';
     }
     $buildCommands[] = 'composer dump-autoload';
 }
 
 //Run npm if package.json is found
-$buildCommands[] = 'npm update --no-progress --no-audit';
-
-//Run build if package-lock.json is found
-if(file_exists('package-lock.json') && !file_exists('gulp.js')) {
-    $buildCommands[] = 'npx --yes browserslist@latest --update-db';
-    $buildCommands[] = 'npm run build';
-} elseif(file_exists('package-lock.json') && file_exists('gulp.js') && is_array($argv) && in_array('--allow-gulp', $argv)) {
-    $buildCommands[] = 'gulp';
+if (file_exists('package.json') && file_exists('package-lock.json')) {
+    if (is_array($argv) && !in_array('--install-npm', $argv)) {
+        $buildCommands[] = 'npm ci --no-progress --no-audit';
+    } else {
+        $npmPackage      = json_decode(file_get_contents('package.json'));
+        $buildCommands[] = "npm install $npmPackage->name";
+        $buildCommands[] = "rm -rf ./dist";
+        $buildCommands[] = "mv node_modules/$npmPackage->name/dist ./";
+    }
+} elseif (file_exists('package.json') && !file_exists('package-lock.json')) {
+    if (is_array($argv) && !in_array('--install-npm', $argv)) {
+        $buildCommands[] = 'npm install --no-progress --no-audit';
+    } else {
+        $npmPackage      = json_decode(file_get_contents('package.json'));
+        $buildCommands[] = "npm install $npmPackage->name";
+        $buildCommands[] = "rm -rf ./dist";
+        $buildCommands[] = "mv node_modules/$npmPackage->name/dist ./";
+    }
 }
 
 // Files and directories not suitable for prod to be removed.
@@ -51,7 +61,6 @@ $removables = [
     'package.json',
     'phpunit.xml.dist',
     'README.md',
-    'gulpfile.js',
     './node_modules/',
     './source/sass/',
     './source/js/',
@@ -63,12 +72,12 @@ $removables = [
 $dirName = basename(dirname(__FILE__));
 
 // Run all build commands.
-$output = '';
+$output   = '';
 $exitCode = 0;
 foreach ($buildCommands as $buildCommand) {
     print "---- Running build command '$buildCommand' for $dirName. ----\n";
     $timeStart = microtime(true);
-    $exitCode = executeCommand($buildCommand);
+    $exitCode  = executeCommand($buildCommand);
     $buildTime = round(microtime(true) - $timeStart);
     print "---- Done build command '$buildCommand' for $dirName.  Build time: $buildTime seconds. ----\n";
     if ($exitCode > 0) {
@@ -77,7 +86,7 @@ foreach ($buildCommands as $buildCommand) {
 }
 
 // Remove files and directories if '--cleanup' argument is supplied to save local developers from disasters.
-if(is_array($argv) && in_array('--cleanup', $argv)) {
+if (is_array($argv) && in_array('--cleanup', $argv)) {
     foreach ($removables as $removable) {
         if (file_exists($removable)) {
             print "Removing $removable from $dirName\n";
