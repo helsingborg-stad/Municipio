d16134abbac2522e4bccffc2c358ec5b40d938f0
Niclas <niclasn@outlook.com>
2025-02-05 10:15:05 +0100

    chore: missing comment

--- library/PostObject/Date/TimestampResolverInterface.php
+++ library/PostObject/Date/TimestampResolverInterface.php
@@ -4,5 +4,10 @@ namespace Municipio\PostObject\Date;
 
 interface TimestampResolverInterface
 {
+    /**
+     * Resolve the timestamp.
+     *
+     * @return int
+     */
     public function resolve(): int;
 }
