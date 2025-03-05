05a241dcdc06bee9480f073b1e26e99baa11ed99
Thor Brink <thor.brink@helsingborg.se>
2025-03-05 13:54:10 +0000

    refactor: remove unused

--- library/SchemaData/Settings/SchemaDataSettingsInterface.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace Municipio\SchemaData;
-
-interface SchemaDataSettingsInterface
-{
-    public function getPostTypesWithEnabledSchemaType(): array;
-    public function getSchemaTypeByPostType(string $postType): string;
-}
