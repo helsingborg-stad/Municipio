c26fec293a10c1f3237e22d3dba1184854e85da2
Thor Brink <thorbrink@gmail.com>
2025-03-26 12:39:35 +0100

    fix: replace span with div for html validation (#1353)

--- views/v3/partials/post/schema/event/sidebar-header.blade.php
+++ views/v3/partials/post/schema/event/sidebar-header.blade.php
@@ -1,4 +1,4 @@
-@element(['componentElement' => 'span', 'classList' => ['u-display--flex', 'u-align-items--center', 'u-gap-1', 'u-margin__bottom--1']])
+@element(['classList' => ['u-display--flex', 'u-align-items--center', 'u-gap-1', 'u-margin__bottom--1']])
     @icon(['icon' => $icon, 'size' => 'md'])@endicon
     @typography(['element' => 'h2', 'classList' => ['u-margin__top--0']])
         {!!$header!!}
