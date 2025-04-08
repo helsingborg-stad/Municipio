99e4c6c44a624a02aa068227f1a549ac4b0e2f0d
Thor Brink <thor.brink@helsingborg.se>
2025-04-08 11:15:00 +0000

    refactor: deprecate Municipio\PostDecorators\ApplySchemaObject

--- library/PostDecorators/ApplySchemaObject.php
+++ library/PostDecorators/ApplySchemaObject.php
@@ -5,14 +5,29 @@ namespace Municipio\PostDecorators;
 use WP_Post;
 use Municipio\SchemaData\SchemaObjectFromPost\SchemaObjectFromPostInterface;
 
+/**
+ * ApplySchemaObject
+ *
+ * This class is responsible for applying a schema object to a post.
+ * It uses the SchemaObjectFromPostInterface to create the schema object.
+ *
+ * @deprecated Use the SchemaObjectFromPostInterface::getSchemaProperty() directly instead.
+ */
 class ApplySchemaObject implements PostDecorator
 {
+    /**
+     * Constructor.
+     */
     public function __construct(
         private SchemaObjectFromPostInterface $schemaObjectFromPost,
         private ?PostDecorator $inner = new NullDecorator()
     ) {
+        trigger_error('ApplySchemaObject is deprecated. Use the SchemaObjectFromPostInterface::getSchemaProperty() directly instead.', E_USER_DEPRECATED);
     }
 
+    /**
+     * @inheritDoc
+     */
     public function apply(WP_Post $post): WP_Post
     {
         $post->schemaObject = $this->schemaObjectFromPost->create($post);
