971b3b441e51b4eaebb2bd096fc9cfa6c6d7ab40
Thor Brink <thor.brink@helsingborg.se>
2025-04-09 06:57:36 +0000

    feat: add PostObjectWithSchemaObject decorator and corresponding tests

---
+++ library/PostObject/Decorators/PostObjectWithSchemaObject.php
@@ -0,0 +1,141 @@
+<?php
+
+namespace Municipio\PostObject\Decorators;
+
+use Municipio\PostObject\Icon\IconInterface;
+use Municipio\PostObject\PostObjectInterface;
+use Municipio\SchemaData\SchemaObjectFromPost\SchemaObjectFromPostInterface;
+
+/**
+ * Decorator for PostObject that adds schema object functionality.
+ *
+ * @package Municipio\PostObject\Decorators
+ */
+class PostObjectWithSchemaObject implements PostObjectInterface
+{
+    /**
+     * Constructor.
+     */
+    public function __construct(
+        private PostObjectInterface $postObject,
+        private SchemaObjectFromPostInterface $schemaObjectFromPost
+    ) {
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function getSchemaProperty(string $property): mixed
+    {
+        static $schemaObject = null;
+
+        if ($schemaObject === null) {
+            $schemaObject = $this->schemaObjectFromPost->create($this->postObject);
+        }
+
+        if ($property === '@type') {
+            return $schemaObject->getType();
+        }
+
+        return $schemaObject->getProperty($property);
+    }
+
+    /**
+     * @inheritDoc
+     * @codeCoverageIgnore
+     */
+    public function getId(): int
+    {
+        return $this->postObject->getId();
+    }
+
+    /**
+     * @inheritDoc
+     * @codeCoverageIgnore
+     */
+    public function getTitle(): string
+    {
+        return $this->postObject->getTitle();
+    }
+
+    /**
+     * @inheritDoc
+     * @codeCoverageIgnore
+     */
+    public function getPermalink(): string
+    {
+        return $this->postObject->getPermalink();
+    }
+
+    /**
+     * @inheritDoc
+     * @codeCoverageIgnore
+     */
+    public function getCommentCount(): int
+    {
+        return $this->postObject->getCommentCount();
+    }
+
+    /**
+     * @inheritDoc
+     * @codeCoverageIgnore
+     */
+    public function getPostType(): string
+    {
+        return $this->postObject->getPostType();
+    }
+
+    /**
+     * @inheritDoc
+     * @codeCoverageIgnore
+     */
+    public function getBlogId(): int
+    {
+        return $this->postObject->getBlogId();
+    }
+
+    /**
+     * @inheritDoc
+     * @codeCoverageIgnore
+     */
+    public function getIcon(): ?IconInterface
+    {
+        return $this->postObject->getIcon();
+    }
+
+    /**
+     * @inheritDoc
+     * @codeCoverageIgnore
+     */
+    public function getPublishedTime(bool $gmt = false): int
+    {
+        return $this->postObject->getPublishedTime($gmt);
+    }
+
+    /**
+     * @inheritDoc
+     * @codeCoverageIgnore
+     */
+    public function getModifiedTime(bool $gmt = false): int
+    {
+        return $this->postObject->getModifiedTime($gmt);
+    }
+
+    /**
+     * @inheritDoc
+     * @codeCoverageIgnore
+     */
+    public function getArchiveDateTimestamp(): ?int
+    {
+        return $this->postObject->getArchiveDateTimestamp();
+    }
+
+    /**
+     * @inheritDoc
+     * @codeCoverageIgnore
+     */
+    public function getArchiveDateFormat(): string
+    {
+        return $this->postObject->getArchiveDateFormat();
+    }
+}
