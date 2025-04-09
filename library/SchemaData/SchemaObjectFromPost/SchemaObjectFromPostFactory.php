f4ad8c1cacf7a6662466d805ff83cd1e7facd0fd
Thor Brink <thor.brink@helsingborg.se>
2025-04-09 07:44:12 +0000

    refactor: implement FactoryInterface and SchemaObjectFromPostFactory for schema object creation

---
+++ library/SchemaData/SchemaObjectFromPost/SchemaObjectFromPostFactory.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Municipio\SchemaData\SchemaObjectFromPost;
+
+use Municipio\Config\Features\SchemaData\Contracts\TryGetSchemaTypeFromPostType;
+use Municipio\SchemaData\SchemaPropertyValueSanitizer\SchemaPropertyValueSanitizer;
+use Municipio\SchemaData\Utils\GetEnabledSchemaTypesInterface;
+use Municipio\SchemaData\Utils\GetSchemaPropertiesWithParamTypesInterface;
+use WpService\Contracts\GetPostMeta;
+use WpService\Contracts\GetThePostThumbnailUrl;
+
+class SchemaObjectFromPostFactory implements FactoryInterface
+{
+    /**
+     * SchemaObjectFromPost constructor.
+     *
+     * @param TryGetSchemaTypeFromPostType $config
+     */
+    public function __construct(
+        private TryGetSchemaTypeFromPostType $tryGetSchemaTypeFromPostType,
+        private GetThePostThumbnailUrl&GetPostMeta $wpService,
+        private GetSchemaPropertiesWithParamTypesInterface $getSchemaPropertiesWithParamTypes,
+        private SchemaPropertyValueSanitizer $schemaPropSanitizer,
+        private GetEnabledSchemaTypesInterface $getEnabledSchemaTypes,
+    ) {
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function create(): SchemaObjectFromPostInterface
+    {
+        $instance = new SchemaObjectFromPost($this->tryGetSchemaTypeFromPostType);
+        $instance = new SchemaObjectWithNameFromTitle($instance);
+        $instance = new SchemaObjectWithImageFromFeaturedImage($instance, $this->wpService);
+        $instance = new SchemaObjectWithPropertiesFromMetadata($this->getSchemaPropertiesWithParamTypes, $this->wpService, $this->schemaPropSanitizer, $instance);
+
+        return new SchemaObjectWithPropertiesFromExternalContent($this->wpService, $this->getEnabledSchemaTypes, $instance);
+    }
+}
