55cf95e109a39d235d5f841b70079f8648a24039
Thor Brink <thor.brink@helsingborg.se>
2025-05-27 05:33:59 +0000

    docs: add missing function docs

--- library/Public.php
+++ library/Public.php
@@ -3,6 +3,17 @@
 use ComponentLibrary\Init as ComponentLibraryInit;
 
 if (!function_exists('render_blade_view')) {
+
+    /**
+     * Renders a Blade view with the given data and view paths.
+     *
+     * @param string $view The name of the Blade view to render.
+     * @param array $data An associative array of data to pass to the view.
+     * @param bool|array $overrideViewPaths If true, uses the default view paths; if an array, overrides the default view paths.
+     * @param bool $formatError If true, formats the error output if an exception occurs.
+     * @return string The rendered HTML markup of the Blade view.
+     * @throws \Throwable If an error occurs during rendering and $formatError is false.
+     */
     function render_blade_view($view, $data = [], $overrideViewPaths = false, $formatError = true)
     {
         $viewPaths = \Municipio\Helper\Template::getViewPaths();
@@ -43,6 +54,11 @@ if (!function_exists('render_blade_view')) {
 
 
 if (!function_exists('municipio_show_posts_pag')) {
+    /**
+     * Checks if the current query has more than one page of posts.
+     *
+     * @return bool True if there are multiple pages, false otherwise.
+     */
     function municipio_show_posts_pag()
     {
         global $wp_query;
@@ -50,17 +66,17 @@ if (!function_exists('municipio_show_posts_pag')) {
     }
 }
 
-/**
- * Get a posts featured image thumbnail by post id
- *
- * @deprecated 4.6.8
- *
- * @param  int|null $post_id Post id or null
- * @param array $size Set image width/height eg array(400, 300)
- * @param string $ratio Sets the image ratio
- * @return string            Thumbnail url
- */
 if (!function_exists('municipio_get_thumbnail_source')) {
+    /**
+     * Get a posts featured image thumbnail by post id
+     *
+     * @deprecated 4.6.8
+     *
+     * @param  int|null $post_id Post id or null
+     * @param array $size Set image width/height eg array(400, 300)
+     * @param string $ratio Sets the image ratio
+     * @return string            Thumbnail url
+     */
     function municipio_get_thumbnail_source($post_id = null, $size = array(), $ratio = '16:9'): void
     {
         _doing_it_wrong(__FUNCTION__, 'This function is deprecated. Use get_the_post_thumbnail_url instead.', '4.6.8');
@@ -68,16 +84,17 @@ if (!function_exists('municipio_get_thumbnail_source')) {
         return;
     }
 }
-/**
- * Gets the html markup for the logotype
- *
- * @deprecated 4.6.8
- *
- * @param  string  $type    Logotype source
- * @param  boolean $tooltip Show tooltip or not
- * @return string           HTML markup
- */
+
 if (!function_exists('municipio_get_logotype')) {
+    /**
+     * Gets the html markup for the logotype
+     *
+     * @deprecated 4.6.8
+     *
+     * @param  string  $type    Logotype source
+     * @param  boolean $tooltip Show tooltip or not
+     * @return string           HTML markup
+     */
     function municipio_get_logotype($type = 'standard', $tooltip = false, $logo_include = true, $tagline = false, $use_text_replacement = true): void
     {
         _doing_it_wrong(__FUNCTION__, 'This function is deprecated.', '4.6.8');
@@ -86,6 +103,12 @@ if (!function_exists('municipio_get_logotype')) {
 }
 
 if (!function_exists('municipio_human_datediff')) {
+    /**
+     * Returns a human readable time difference from the current time to the given date.
+     *
+     * @param string $date The date to compare against the current time.
+     * @return string A human-readable time difference.
+     */
     function municipio_human_datediff($date)
     {
         $diff = human_time_diff(strtotime($date), current_time('timestamp'));
@@ -94,6 +117,12 @@ if (!function_exists('municipio_human_datediff')) {
 }
 
 if (!function_exists('municipio_get_mime_link_item')) {
+    /**
+     * Returns a CSS class for a link item based on its MIME type.
+     *
+     * @param string $mime The MIME type of the item.
+     * @return string A CSS class for the link item.
+     */
     function municipio_get_mime_link_item($mime)
     {
         $mime = explode('/', $mime);
@@ -107,6 +136,13 @@ if (!function_exists('municipio_get_mime_link_item')) {
 }
 
 if (!function_exists('municipio_to_aspect_ratio')) {
+    /**
+     * Converts a ratio string (e.g., "16:9") to an aspect ratio based on a given size.
+     *
+     * @param string $ratio The aspect ratio in the format "width:height".
+     * @param array $size An array containing the width and height of the image.
+     * @return array An array containing the calculated width and height based on the aspect ratio.
+     */
     function municipio_to_aspect_ratio($ratio, $size)
     {
         if (count($ratio = explode(":", $ratio)) == 2) {
@@ -118,6 +154,12 @@ if (!function_exists('municipio_to_aspect_ratio')) {
 }
 
 if (!function_exists('municiipio_format_currency')) {
+    /**
+     * Formats a currency value by adding spaces every three digits.
+     *
+     * @param string $value The currency value to format.
+     * @return string The formatted currency value with spaces added.
+     */
     function municiipio_format_currency($value)
     {
         $value = str_split(strrev($value), 3);
