61b33708239c75f30bf104486fe33ad77b58836b
NiclasNorin <niclasn@outlook.com>
2025-06-13 11:59:02 +0200

    refactor: customizer component section, also adding card

---
+++ library/Customizer/Sections/Component/Card.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace Municipio\Customizer\Sections\Component;
+
+use Municipio\Customizer\KirkiField;
+
+class Card
+{
+    public function __construct(string $sectionID)
+    {
+        KirkiField::addField([
+            'type'     => 'select',
+            'settings' => 'mod_card_modifier',
+            'label'    => esc_html__('Card', 'municipio'),
+            'description' => 'Affects every component wrapped by a card.',
+            'section'  => $sectionID,
+            'default'  => 'none',
+            'priority' => 10,
+            'choices'  => [
+              'none'      => esc_html__('None', 'municipio'),
+              'panel'     => esc_html__('Panel', 'municipio'),
+              'accented'  => esc_html__('Accented', 'municipio'),
+              'highlight' => esc_html__('Highlight', 'municipio'),
+            ],
+            'output'   => [
+                [
+                    'type'    => 'modifier',
+                    'context' => [
+                        'module.text.box',
+                        'module.map',
+                        'module.posts.index',
+                        'module.posts.list',
+                        'module.manual-input.list',
+                        'module.posts.expandablelist',
+                        'module.manual-input.accordion',
+                        'module.inlay.list',
+                        'module.index',
+                        'module.text.box'
+                    ]
+                ]
+            ],
+        ]);
+    }
+}
