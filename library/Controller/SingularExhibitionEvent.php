4b929e12c22195ca692499b5c12f2daf79555c1a
Thor Brink <thor.brink@helsingborg.se>
2025-07-03 06:03:20 +0000

    wip: add view

---
+++ library/Controller/SingularExhibitionEvent.php
@@ -0,0 +1,220 @@
+<?php
+
+namespace Municipio\Controller;
+
+use DateTime;
+use Municipio\Controller\SingularEvent\Contracts\PriceListItemInterface;
+use Municipio\ImageConvert\Contract\ImageContract;
+use Municipio\ImageConvert\Contract\ImageContractInterface;
+use Municipio\Schema\BaseType;
+use Municipio\Schema\ImageObject;
+use Municipio\Schema\Place;
+
+/**
+ * Class SingularExhibitionEvent
+ * Controller for posts with ExhibitionEvent schema type.
+ */
+class SingularExhibitionEvent extends Singular
+{
+    protected object $postMeta;
+    public string $view = 'single-schema-exhibition-event';
+
+    /**
+     * @inheritDoc
+     */
+    public function init()
+    {
+        parent::init();
+
+        $this->populateLanguageObject();
+
+        $event = $this->post->getSchema();
+
+        $this->data['displayFeaturedImage']             = false;
+        $this->data['placeUrl']                         = $this->getPlaceUrl($event->getProperty('location'));
+        $this->data['placeName']                        = $event->getProperty('location')['name'] ?? $event->getProperty('location')['address'] ?? null;
+        $this->data['placeAddress']                     = $event->getProperty('location')['address'] ?? null;
+        $this->data['priceListItems']                   = $this->getPriceList();
+        $this->data['occassion']                        = $this->getOccassionText($event->getProperty('startDate'), $event->getProperty('endDate'));
+        $this->data['bookingLink']                      = $this->post->getSchemaProperty('offers')[0]['url'] ?? null;
+        $this->data['organizers']                       = $this->post->getSchemaProperty('organizer') ?? [];
+        $this->data['organizers']                       = !is_array($this->data['organizers']) ? [$this->data['organizers']] : $this->data['organizers'];
+        $this->data['physicalAccessibilityFeatures']    = $this->post->getSchemaProperty('physicalAccessibilityFeatures') ?? null;
+        $this->data['eventIsInThePast']                 = $this->eventIsInThePast();
+        $this->data['openingHoursSpecification']        = $event->getProperty('openingHoursSpecification') ?? [];
+        $this->data['specialOpeningHoursSpecification'] = $event->getProperty('specialOpeningHoursSpecification') ?? [];
+        $this->data['galleryComponentAttributes']       = $this->getGalleryComponentAttributes();
+
+        $this->trySetHttpStatusHeader($event);
+    }
+
+    /**
+     * Populate the language object.
+     */
+    private function populateLanguageObject(): void
+    {
+        $this->data['lang']->description        = $this->wpService->__('Description', 'municipio');
+        $this->data['lang']->addToCalendar      = $this->wpService->__('Add to calendar', 'municipio');
+        $this->data['lang']->bookingTitle       = $this->wpService->__('Tickets & registration', 'municipio');
+        $this->data['lang']->bookingButton      = $this->wpService->__('Go to booking page', 'municipio');
+        $this->data['lang']->bookingDisclaimer  = $this->wpService->__('Tickets are sold according to the reseller.', 'municipio');
+        $this->data['lang']->occassionsTitle    = $this->wpService->__('Date and time', 'municipio');
+        $this->data['lang']->moreOccassions     = $this->wpService->__('Other occassions', 'municipio');
+        $this->data['lang']->placeTitle         = $this->wpService->__('Place', 'municipio');
+        $this->data['lang']->priceTitle         = $this->wpService->__('Price', 'municipio');
+        $this->data['lang']->organizersTitle    = $this->wpService->__('Organizers', 'municipio');
+        $this->data['lang']->accessibilityTitle = $this->wpService->__('Accessibility', 'municipio');
+        $this->data['lang']->expiredEventNotice = $this->wpService->__('This event has already taken place.', 'municipio');
+    }
+
+    /**
+     * Get place link attributes
+     *
+     * @return string
+     */
+    public function getPlaceUrl(?Place $place = null): string
+    {
+        if (!$place) {
+            return '';
+        }
+
+        $placeName    = $place['name'] ?? $place['address'] ?? '';
+        $placeAddress = $place['address'] ?? '';
+
+        $googleMapsUrl = 'https://www.google.com/maps/search/?api=1&query=';
+        $placeLink     = $googleMapsUrl . urlencode($placeName . ', ' . $placeAddress);
+
+        return $placeLink;
+    }
+
+    /**
+     * Get date text
+     *
+     * @param DateTime|null $startDate
+     * @param DateTime|null $endDate
+     * @return string
+     */
+    public function getOccassionText(DateTime|null $startDate, DateTime|null $endDate): string
+    {
+        if (!$startDate || !$endDate) {
+            return '';
+        }
+
+        $startDateTimestamp = $startDate->getTimestamp();
+        $endDateTimestamp   = $endDate->getTimestamp();
+
+        $duration = $endDateTimestamp - $startDateTimestamp;
+        $days     = floor($duration / 86400);
+
+        $start = ucfirst($this->wpService->dateI18n('j M', $startDateTimestamp));
+        $end   = ucfirst($this->wpService->dateI18n('j M Y', $endDateTimestamp));
+
+        return "{$start} - {$end}";
+    }
+
+    /**
+     * Get price list
+     *
+     * @return PriceListItemInterface[]
+     */
+    public function getPriceList(): array
+    {
+        $offers = $this->post->getSchemaProperty('offers');
+
+        if (!$offers) {
+            return [];
+        }
+
+        return array_map([$this, 'getPriceListItemFromOffer'], $offers);
+    }
+
+    /**
+     * Get price list item from offer
+     *
+     * @param array $offer
+     * @return PriceListItemInterface
+     */
+    public function getPriceListItemFromOffer(array $offer): PriceListItemInterface
+    {
+        $priceSpecification = $offer['priceSpecification'] ?? [];
+        $name               = $offer['name'] ?? '';
+        $currency           = $offer['priceCurrency'] ?? '';
+
+        if (isset($priceSpecification['minPrice']) && isset($priceSpecification['maxPrice'])) {
+            if ($priceSpecification['minPrice'] === $priceSpecification['maxPrice']) {
+                $price = $priceSpecification['minPrice'] . ' ' . $currency;
+            } else {
+                $price = $priceSpecification['minPrice'] . ' - ' . $priceSpecification['maxPrice'] . ' ' . $currency;
+            }
+        } elseif (isset($priceSpecification['price'])) {
+            $price = $priceSpecification['price'] . ' ' . $currency;
+        } elseif (isset($offer['price'])) {
+            $price = $offer['price'] . ' ' . $currency;
+        } else {
+            $price = $this->wpService->__('Price not available', 'municipio');
+        }
+
+        // Assuming PriceListItemInterface is implemented by a class, e.g., PriceListItem
+        return new \Municipio\Controller\SingularEvent\PriceListItem($name, $price);
+    }
+
+    /**
+     * Determine if the event is in the past.
+     *
+     * @return bool
+     */
+    public function eventIsInThePast(): bool
+    {
+        $event   = $this->post->getSchema();
+        $endDate = $event->getProperty('endDate');
+
+        if ($endDate instanceof \DateTime) {
+            return $endDate->getTimestamp() < time();
+        }
+
+        return false;
+    }
+
+    /**
+     * Try to set HTTP status header
+     * If the event is in the past, set 410 Gone
+     */
+    private function trySetHttpStatusHeader(BaseType $event): void
+    {
+        if ($this->eventIsInThePast($event->getProperty('startDate'))) {
+            $this->wpService->statusHeader(410);
+        }
+    }
+
+    /**
+     * Get gallery component attributes from the event schema image property if the image property is an array of ImageObjects.
+     */
+    private function getGalleryComponentAttributes(): ?array
+    {
+        $imageProperty = $this->post->getSchemaProperty('image');
+
+        if (is_array($imageProperty)) {
+            return [
+                'list' => array_map(fn($image) => $this->getImageAttributes($image), $imageProperty)
+            ];
+        }
+
+        return null;
+    }
+
+    /**
+     * Get image attributes for the gallery component.
+     *
+     * @param ImageObject $image
+     * @return array
+     */
+    private function getImageAttributes(ImageObject $image): array
+    {
+        return [
+            'largeImage' => $image->getProperty('url'),
+            'smallImage' => wp_get_attachment_image_src($image->getProperty('@id'))[0] ,
+            'alt'        => 'foo',
+            'caption'    => null
+        ];
+    }
+}
---
+++ views/v3/single-schema-exhibition-event.blade.php
@@ -0,0 +1,179 @@
+@extends('templates.single', ['showSidebars' => false, 'centerContent' => true])
+
+@section('article.content')
+
+    @section('article.title.before')
+        @hero([
+            "image" => $post->getImage(),
+            "size" => "normal",
+            "classList" => ["u-margin__bottom--4"],
+        ])
+        @endhero
+        @if($eventIsInThePast)
+            @notice([
+                'type' => 'warning',
+                'message' => [
+                    'text' => $lang->expiredEventNotice,
+                ],
+                'icon' => [
+                    'icon' => 'schedule'
+                ]
+            ])@endnotice
+        @endif
+    @stop
+    
+    {!!$post->getSchemaProperty('description')!!}
+
+    @iconSection()
+
+        @if(!empty($organizers))
+            
+            @iconSection__item(['icon' => ['icon' => 'group', 'size' => 'md']])
+                @include('partials.post.schema.event.icon-section-header', ['header' => $lang->organizersTitle])
+                @foreach($organizers as $organizer)
+                    @if(!empty($organizer['name'])) 
+                        
+                        @element([])
+                            @typography(['element' => 'h3', 'variant' => 'h4', 'classList' => ['u-margin__bottom--2']]){!!$organizer['name']!!}@endtypography
+
+                            @if(!empty($organizer['url']))
+                                @element(['classList' => ['u-width--100', 'u-truncate', 'u-margin__bottom--1']])
+                                    @icon(['icon' => 'language'])@endicon @link(['href' => $organizer['url']]) {!!$organizer['url']!!} @endlink
+                                @endelement
+                            @endif
+                            
+                            @if(!empty($organizer['email']))
+                                @element(['classList' => ['u-width--100', 'u-truncate', 'u-margin__bottom--1']])
+                                    @icon(['icon' => 'email'])@endicon @link(['href' => 'mailto:'.$organizer['email']]) {!!$organizer['email']!!} @endlink
+                                @endelement
+                            @endif
+                            @if(!empty($organizer['telephone']))
+                                @element(['classList' => ['u-width--100', 'u-truncate']])
+                                    @icon(['icon' => 'phone'])@endicon @link(['href' => 'tel:'.$organizer['telephone']]) {!!$organizer['telephone']!!} @endlink
+                                @endelement
+                            @endif
+                        @endelement
+
+                    @endif
+                @endforeach
+            @endiconSection__item
+        @endif  
+
+        @if(!empty($occassions))
+            @iconSection__item(['icon' => ['icon' => 'calendar_month', 'size' => 'md']])
+                @include('partials.post.schema.event.icon-section-header', ['header' => $lang->moreOccassions])
+                @element(['componentElement' => 'ul'])
+                    @foreach($occassions as $occassion)
+                        @element(['componentElement' => 'li'])
+                            @typography(){!!$occassion!!}@endtypography
+                        @endelement
+                    @endforeach
+                @endelement
+            @endiconSection__item
+        @endif
+
+        @if(!empty($bookingLink) && !$eventIsInThePast)
+            @iconSection__item(['icon' => ['icon' => 'local_activity', 'size' => 'md']])
+                @include('partials.post.schema.event.icon-section-header', ['header' => $lang->bookingTitle])
+                @button([
+                    'href' => $bookingLink,
+                    'color' => 'primary',
+                    'style' => 'filled',
+                    'size' => 'md',
+                    'icon' => 'arrow_forward',
+                    'fullWidth' => true,
+                    'text' => $lang->bookingButton,
+                ])
+                @endbutton
+                @element(['componentElement' => 'small']){!!$lang->bookingDisclaimer!!}@endelement
+            @endiconSection__item
+        @endif
+
+        @if(!empty($physicalAccessibilityFeatures))
+            @iconSection__item(['icon' => ['icon' => 'accessibility', 'size' => 'md']])
+                @include('partials.post.schema.event.icon-section-header', ['header' => $lang->accessibilityTitle])
+                @element(['componentElement' => 'ul'])
+                    @foreach($physicalAccessibilityFeatures as $feature)
+                        @element(['componentElement' => 'li']){!! $feature !!}@endelement
+                    @endforeach
+                @endelement
+            @endiconSection__item
+        @endif
+
+    @endiconSection
+    
+@stop
+
+@section('sidebar.right-sidebar.before')
+    
+    @paper(['padding' => 2])
+        @collection()
+            @collection__item([])
+                @typography(['element' => 'h4'])
+                    Datum
+                @endtypography
+                @typography([])
+                    {!!$occassion!!}
+                @endtypography
+            @endcollection__item
+        @endcollection
+    @endpaper
+
+    @paper(['padding' => 2])
+        @collection(['classList' => ['u-display--flex']])
+            @collection__item()
+                @typography(['element' => 'h4'])
+                    {!!$lang->placeTitle!!}
+                @endtypography
+                @typography([])
+                    {!!$placeName!!}
+                @endtypography
+            @endcollection__item
+        @endcollection
+    @endpaper
+
+
+    @if(!empty($priceListItems))
+        @paper(['padding' => 2])
+            @collection()
+                    @collection__item([])
+                        @typography(['element' => 'h4'])
+                            Entré
+                        @endtypography
+                        @foreach ($priceListItems as $priceListItem)
+                            @element([])
+                                @element(['componentElement' => 'span']){!!$priceListItem->getName() !!}: @endelement
+                                @element(['componentElement' => 'span', 'classList' => ['u-float--right']]){!! $priceListItem->getPrice() !!}@endelement
+                            @endelement
+                        @endforeach
+                    @endcollection__item
+            @endcollection
+        @endpaper
+    @endif
+
+    @paper(['padding' => 2])
+        @collection()
+                @collection__item([])
+                    @typography(['element' => 'h4'])
+                        Planera ditt besök
+                    @endtypography
+                    @if(!empty($placeUrl))
+                        @element(['classList' => ['u-width--100', 'u-truncate', 'u-margin__bottom--1']])
+                            @icon(['icon' => 'language'])@endicon @link(['href' => $placeUrl]) Hitta hit @endlink
+                        @endelement
+                    @endif
+                @endcollection__item
+        @endcollection
+    @endpaper
+
+@stop
+
+@section('below')
+    @typography(['element' => 'h3'])
+        Galleri
+    @endtypography
+    @if(!empty($galleryComponentAttributes))
+        @gallery($galleryComponentAttributes)@endgallery
+    @endif
+    @includeIf('partials.sidebar', ['id' => 'content-area-bottom', 'classes' => ['o-grid']])
+@stop
