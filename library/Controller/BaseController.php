be028b45ab9fcbda26596e79601d859080278c70
NiclasNorin <niclasn@outlook.com>
2025-08-04 10:33:12 +0200

    refactor: more lang

--- library/Controller/BaseController.php
+++ library/Controller/BaseController.php
@@ -410,6 +410,7 @@ class BaseController
                     'audios' => __('Audios', 'municipio'),
                     'images' => __('Images', 'municipio'),
                     'allowedFiles' => __('Allowed files', 'municipio'),
+                    'followingFilesCouldNotBeUploaded' => __('Following files could not be uploaded', 'municipio')
                 ];
 
                 return (object) array_merge((array) $obj, $lang);
