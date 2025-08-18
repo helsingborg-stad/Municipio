ab6cecb64fa7fae4d38b411c46d856daf0ab5b5a
Thor Brink <thor.brink@helsingborg.se>
2025-08-18 05:56:55 +0000

    feat: add occasion text retrieval and update language labels in SingularExhibitionEvent

--- library/Controller/SingularExhibitionEvent.php
+++ library/Controller/SingularExhibitionEvent.php
@@ -31,18 +31,17 @@ class SingularExhibitionEvent extends Singular
 
         $event = $this->post->getSchema();
 
-        $this->data['displayFeaturedImage']             = false;
-        $this->data['placeUrl']                         = $this->getPlaceUrl($event->getProperty('location'));
-        $this->data['placeName']                        = $this->getPlaceName($event->getProperty('location'));
-        $this->data['placeAddress']                     = $this->getPlaceAddress($event->getProperty('location'));
-        $this->data['priceListItems']                   = $this->getPriceList();
-        $this->data['physicalAccessibilityFeatures']    = $this->getPhysicalAccessibilityFeaturesList($this->post->getSchemaProperty('physicalAccessibilityFeatures'));
-        $this->data['eventIsInThePast']                 = $this->eventIsInThePast();
-        $this->data['openingHoursSpecification']        = $event->getProperty('openingHoursSpecification') ?? [];
-        $this->data['specialOpeningHoursSpecification'] = $event->getProperty('specialOpeningHoursSpecification') ?? [];
-        $this->data['galleryComponentAttributes']       = $this->getGalleryComponentAttributes();
-        $this->data['openingHours']                     = $this->getOpeningHours($event->getProperty('location')?->getProperty('openingHoursSpecification') ?? []);
-        $this->data['specialOpeningHours']              = $this->getOpeningHours($event->getProperty('location')?->getProperty('specialOpeningHoursSpecification') ?? []);
+        $this->data['displayFeaturedImage']          = false;
+        $this->data['placeUrl']                      = $this->getPlaceUrl($event->getProperty('location'));
+        $this->data['placeName']                     = $this->getPlaceName($event->getProperty('location'));
+        $this->data['placeAddress']                  = $this->getPlaceAddress($event->getProperty('location'));
+        $this->data['priceListItems']                = $this->getPriceList();
+        $this->data['occassion']                     = $this->getOccassionText($event->getProperty('startDate'), $event->getProperty('endDate'));
+        $this->data['physicalAccessibilityFeatures'] = $this->getPhysicalAccessibilityFeaturesList($this->post->getSchemaProperty('physicalAccessibilityFeatures'));
+        $this->data['eventIsInThePast']              = $this->eventIsInThePast();
+        $this->data['galleryComponentAttributes']    = $this->getGalleryComponentAttributes();
+        $this->data['openingHours']                  = $this->getOpeningHours($event->getProperty('location')?->getProperty('openingHoursSpecification') ?? []);
+        $this->data['specialOpeningHours']           = $this->getOpeningHours($event->getProperty('location')?->getProperty('specialOpeningHoursSpecification') ?? []);
 
         $this->trySetHttpStatusHeader($event);
     }
@@ -119,6 +118,9 @@ class SingularExhibitionEvent extends Singular
     {
         $lang                           = $this->data['lang'];
         $wp                             = $this->wpService;
+        $lang->bookingTitleLabel        = $wp->__('Tickets & registration', 'municipio');
+        $lang->bookingButtonLabel       = $wp->__('Go to booking page', 'municipio');
+        $lang->bookingDisclaimerLabel   = $wp->__('Tickets are sold according to the reseller.', 'municipio');
         $lang->placeTitle               = $wp->__('Place', 'municipio');
         $lang->expiredEventNoticeLabel  = $wp->__('This event has already taken place.', 'municipio');
         $lang->dateLabel                = $wp->__('Date', 'municipio');
@@ -143,6 +145,28 @@ class SingularExhibitionEvent extends Singular
         }
         $googleMapsUrl = 'https://www.google.com/maps/search/?api=1&query=';
         return $googleMapsUrl . urlencode($place['geo']['address']);
+    }
+
+    /**
+     * Get the occasion text from the event schema.
+     *
+     * @param DateTime|null $startDate
+     * @param DateTime|null $endDate
+     * @return string
+     */
+    public function getOccassionText(?DateTime $startDate, ?DateTime $endDate): string
+    {
+        if (!$startDate || !$endDate) {
+            return '';
+        }
+
+        $startDateTimestamp = $startDate->getTimestamp();
+        $endDateTimestamp   = $endDate->getTimestamp();
+
+        $start = ucfirst($this->wpService->dateI18n('j M', $startDateTimestamp));
+        $end   = ucfirst($this->wpService->dateI18n('j M Y', $endDateTimestamp));
+
+        return "{$start} - {$end}";
     }
 
     /**
