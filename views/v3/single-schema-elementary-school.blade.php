88cd129c6550fb8ed9b643cc904043792dc22fdd
Thor Brink <thor.brink@helsingborg.se>
2025-09-16 13:09:33 +0000

    feat: add EventsGenerator and integrate events into SingularElementarySchool and SingularPreschool views

--- views/v3/single-schema-elementary-school.blade.php
+++ views/v3/single-schema-elementary-school.blade.php
@@ -34,6 +34,38 @@
             @endpaper
         @endif
 
+
+        @if (!empty($events))
+            @element()
+                @if ($lang->eventsLabel)
+                    @typography(['element' => 'h2', 'classList' => ['u-margin__bottom--2']])
+                        {{ $lang->eventsLabel }}
+                    @endtypography
+                @endif
+                @foreach ($events as $event)
+                    @collection([])
+                        @collection__item([
+                            'classList' => ['u-box-shadow--3', 'u-margin__bottom--3', 'u-padding--1', 'u-border--1']
+                        ])
+                            @slot('before')
+                                @datebadge([
+                                    'classList' => ['u-padding--2', 'u-margin__right--2'],
+                                    'date' => $event['timestamp'],
+                                    'size' => 'sm'
+                                ])
+                                @enddatebadge
+                            @endslot
+                                {!! $event['name'] !!}
+                            @typography(['classList' => ['u-padding__bottom--2', 'u-padding__top-2']])
+                                {!! $event['startTimeEndTime'] !!}
+                            @endtypography
+                            {!! $event['description'] !!}
+                        @endcollection__item
+                    @endcollection
+                @endforeach
+            @endelement
+        @endif
+
         @if (!empty($actions))
             @paper(['classList' => ['u-color__bg--complementary-lighter', 'u-padding--2']])
                 @typography(['element' => 'h2'])
--- views/v3/single-schema-preschool.blade.php
+++ views/v3/single-schema-preschool.blade.php
@@ -35,6 +35,37 @@
             @endpaper
         @endif
 
+        @if (!empty($events))
+            @element()
+                @if ($lang->eventsLabel)
+                    @typography(['element' => 'h2', 'classList' => ['u-margin__bottom--2']])
+                        {{ $lang->eventsLabel }}
+                    @endtypography
+                @endif
+                @foreach ($events as $event)
+                    @collection([])
+                        @collection__item([
+                            'classList' => ['u-box-shadow--3', 'u-margin__bottom--3', 'u-padding--1', 'u-border--1']
+                        ])
+                            @slot('before')
+                                @datebadge([
+                                    'classList' => ['u-padding--2', 'u-margin__right--2'],
+                                    'date' => $event['timestamp'],
+                                    'size' => 'sm'
+                                ])
+                                @enddatebadge
+                            @endslot
+                                {!! $event['name'] !!}
+                            @typography(['classList' => ['u-padding__bottom--2', 'u-padding__top-2']])
+                                {!! $event['startTimeEndTime'] !!}
+                            @endtypography
+                            {!! $event['description'] !!}
+                        @endcollection__item
+                    @endcollection
+                @endforeach
+            @endelement
+        @endif
+
         @if(!empty($visitUs))
             @element()
                 @if(!empty($visitUs['heading']))
