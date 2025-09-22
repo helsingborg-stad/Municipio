14909b79ebd125708d68aeb161f73bb4d2a959ee
Sebastian Thulin <sebastian.thulin@helsingborg.se>
2025-09-22 08:40:38 +0200

    fix: Implicitly marking parameter $wpTermQueryArgs as nullable is deprecated

--- library/Helper/WP.php
+++ library/Helper/WP.php
@@ -52,7 +52,7 @@ class WP
      * @param array|null $wpTermQueryArgs The WordPress term query arguments.
      * @return array The terms.
      */
-    public static function getTerms(array $wpTermQueryArgs = null): array
+    public static function getTerms(?array $wpTermQueryArgs = null): array
     {
         $terms = get_terms($wpTermQueryArgs);
 
