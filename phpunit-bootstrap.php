664ee5ecb2412b3ab5bcf864b35cddc3df4db502
Thor Brink <thorbrink@gmail.com>
2025-10-07 10:18:24 +0200

    chore: remove unused dev dependencies (#1547)

--- phpunit-bootstrap.php
+++ phpunit-bootstrap.php
@@ -1,8 +1,5 @@
 <?php // phpcs:ignore PSR1.Files.SideEffects.FoundWithSymbols
 
-use DG\BypassFinals;
-
-BypassFinals::enable();
 define('OBJECT', 'OBJECT');
 include_once __DIR__ . '/vendor/php-stubs/wordpress-stubs/wordpress-stubs.php';
 include_once __DIR__ . '/vendor/php-stubs/acf-pro-stubs/acf-pro-stubs.php';
