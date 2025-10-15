632e3bd13b9ce35d6c8fa85976b3bd4da5e328a1
Thor Brink <thor.brink@helsingborg.se>
2025-10-15 05:34:20 +0000

    refactor: add docs

--- library/Controller/ArchiveEvent.php
+++ library/Controller/ArchiveEvent.php
@@ -19,10 +19,14 @@ class ArchiveEvent extends \Municipio\Controller\Archive
     {
         parent::init();
 
+        /**
+         * Make functions available to view through the data array.
+         * Functions that risk getting called multiple times are memoized.
+         */
         $getSchema                        = new MemoizedFunction(fn(PostObjectInterface $post) => $post->getSchema(), fn(PostObjectInterface $post) => $post->getId());
+        $this->data['getEventPriceRange'] = new MemoizedFunction(fn(PostObjectInterface $post) => ArchiveEvent\GetEventPriceRange::getEventPriceRange($getSchema($post)), fn(PostObjectInterface $post) => $post->getId());
         $this->data['getEventPlaceName']  = fn(PostObjectInterface $post) => ArchiveEvent\GetEventPlaceName::getEventPlaceName($getSchema($post));
         $this->data['getEventDate']       = fn(PostObjectInterface $post) => ArchiveEvent\GetEventDate::getEventDate($getSchema($post));
         $this->data['getDatebadgeDate']   = fn(PostObjectInterface $post) => ArchiveEvent\GetDatebadgeDate::getDatebadgeDate($getSchema($post));
-        $this->data['getEventPriceRange'] = new MemoizedFunction(fn(PostObjectInterface $post) => ArchiveEvent\GetEventPriceRange::getEventPriceRange($getSchema($post)), fn(PostObjectInterface $post) => $post->getId());
     }
 }
