4b361aad83e63183fa5f0b316389e202982fe702
Thor Brink <thor.brink@helsingborg.se>
2025-10-28 13:14:09 +0000

    wip: apply coding standards

--- Modularity/source/php/Module/Archive/Archive.php
+++ Modularity/source/php/Module/Archive/Archive.php
@@ -4,11 +4,19 @@ namespace Modularity\Module\Archive;
 
 use Modularity\Helper\WpService;
 
-class Archive extends \Modularity\Module {
-    public $slug = 'archive';
+/**
+ * Archive module class
+ */
+class Archive extends \Modularity\Module
+{
+    public $slug              = 'archive';
     public $isBlockCompatible = true;
 
-    public function init() {
+    /**
+     * Module init
+     */
+    public function init()
+    {
         $this->nameSingular = WpService::get()->__("Archive", 'municipio');
         $this->namePlural   = WpService::get()->__("Archives", 'municipio');
         $this->description  = WpService::get()->__("Outputs an archive module.", 'municipio');
--- Modularity/source/php/Module/Archive/ArchiveTest.php
+++ Modularity/source/php/Module/Archive/ArchiveTest.php
@@ -6,38 +6,44 @@ use Modularity\Helper\WpService;
 use PHPUnit\Framework\Attributes\TestDox;
 use WpService\Implementations\FakeWpService;
 
-class ArchiveTest extends \PHPUnit\Framework\TestCase {
-
-    protected function setUp(): void {
-        WpService::set(new FakeWpService([ 
+class ArchiveTest extends \PHPUnit\Framework\TestCase
+{
+    protected function setUp(): void
+    {
+        WpService::set(new FakeWpService([
             'addAction' => true,
-            '__' => fn($text) => $text,
+            '__'        => fn($text) => $text,
         ]));
     }
 
     #[TestDox('module exists')]
-    public function testArchiveModuleExists() {
+    public function testArchiveModuleExists()
+    {
         $this->assertInstanceOf(Archive::class, new Archive());
     }
 
     #[TestDox('module has correct slug')]
-    public function testArchiveModuleHasCorrectSlug() {
+    public function testArchiveModuleHasCorrectSlug()
+    {
         $this->assertEquals('archive', (new Archive())->slug);
     }
 
     #[TestDox('module is block compatible by default')]
-    public function testArchiveModuleIsBlockCompatibleByDefault() {
+    public function testArchiveModuleIsBlockCompatibleByDefault()
+    {
         $this->assertTrue((new Archive())->isBlockCompatible);
     }
 
     #[TestDox('module data method returns an array')]
-    public function testArchiveModuleDataMethodReturnsArray() {
+    public function testArchiveModuleDataMethodReturnsArray()
+    {
         $module = new Archive();
         $this->assertIsArray($module->data());
     }
 
     #[TestDox('module has singular, plural and description labels')]
-    public function testArchiveModuleHasLabels() {
+    public function testArchiveModuleHasLabels()
+    {
         $module = new Archive();
         $this->assertNotEmpty($module->nameSingular);
         $this->assertNotEmpty($module->namePlural);
@@ -45,18 +51,19 @@ class ArchiveTest extends \PHPUnit\Framework\TestCase {
     }
 
     #[TestDox('module view template exists')]
-    public function testArchiveModuleHasViewTemplate() {
+    public function testArchiveModuleHasViewTemplate()
+    {
         $module = new Archive();
         $this->assertFileExists(__DIR__ . '/views/' . $module->template());
-
     }
 
-    #[TestDox('module has a acf field group')]
-    public function testArchiveModuleHasAcfFieldGroup() {
-        $module = new Archive();
+    #[TestDox('module has acf field group')]
+    public function testArchiveModuleHasAcfFieldGroup()
+    {
+        $module   = new Archive();
         $fileName = 'mod-' . $module->slug;
-        $acfDir = __DIR__ . '/../../AcfFields';
-        
+        $acfDir   = __DIR__ . '/../../AcfFields';
+
         $this->assertFileExists($acfDir . '/json/' . $fileName . '.json');
         $this->assertFileExists($acfDir . '/php/' . ucfirst($fileName) . '.php');
     }
