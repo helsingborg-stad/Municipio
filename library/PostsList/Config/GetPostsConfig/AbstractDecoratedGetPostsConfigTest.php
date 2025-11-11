5bc4ff9926f6d5450361e49657676c8abe80402d
Thor Brink <thor.brink@helsingborg.se>
2025-11-11 13:52:02 +0000

    wip: fix test coverage

--- library/PostsList/Config/GetPostsConfig/AbstractDecoratedGetPostsConfigTest.php
+++ library/PostsList/Config/GetPostsConfig/AbstractDecoratedGetPostsConfigTest.php
@@ -19,12 +19,22 @@ class AbstractDecoratedGetPostsConfigTest extends TestCase
     #[TestDox('proxies inner config')]
     public function testProxiesInnerConfig(): void
     {
-        $instance = new class extends AbstractDecoratedGetPostsConfig{
-            public function __construct(protected GetPostsConfigInterface $innerConfig = new DefaultGetPostsConfig())
+        $innerConfig = new DefaultGetPostsConfig();
+        $instance    = new class ($innerConfig) extends AbstractDecoratedGetPostsConfig{
+            public function __construct(protected GetPostsConfigInterface $innerConfig)
             {
             }
         };
 
-        $this->assertSame(['post'], $instance->getPostTypes());
+        $this->assertSame($innerConfig->getPostTypes(), $instance->getPostTypes());
+        $this->assertSame($innerConfig->getPostsPerPage(), $instance->getPostsPerPage());
+        $this->assertSame($innerConfig->getPage(), $instance->getPage());
+        $this->assertSame($innerConfig->isFacettingTaxonomyQueryEnabled(), $instance->isFacettingTaxonomyQueryEnabled());
+        $this->assertSame($innerConfig->getSearch(), $instance->getSearch());
+        $this->assertSame($innerConfig->getOrderBy(), $instance->getOrderBy());
+        $this->assertSame($innerConfig->getOrder(), $instance->getOrder());
+        $this->assertSame($innerConfig->getDateFrom(), $instance->getDateFrom());
+        $this->assertSame($innerConfig->getDateTo(), $instance->getDateTo());
+        $this->assertSame($innerConfig->getTerms(), $instance->getTerms());
     }
 }
