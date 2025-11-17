2bbd0085c0e9c0f766f10d811219ebabb159a35f
Thor Brink <thor.brink@helsingborg.se>
2025-11-17 08:42:57 +0000

    refactor: enhance mappers for appearance configuration and add unit tests

--- library/Controller/Archive/Mappers/AppearanceConfigMappers/MapDesign.php
+++ library/Controller/Archive/Mappers/AppearanceConfigMappers/MapDesign.php
@@ -4,18 +4,26 @@ namespace Municipio\Controller\Archive\Mappers\AppearanceConfigMappers;
 
 use Municipio\PostsList\Config\AppearanceConfig\PostDesign;
 
+/**
+ * Mapper for post design in appearance config
+ */
 class MapDesign
 {
+    /**
+     * Map post design from data
+     * @param array $data
+     * @return PostDesign
+     */
     public function map(array $data): PostDesign
     {
         return match ($data['archiveProps']->style ?? 'cards') {
             'cards' => PostDesign::CARD,
-            'compressed' => PostDesign::COMPRESSED,
             'collection' => PostDesign::COLLECTION,
+            'compressed' => PostDesign::COMPRESSED,
             'grid' => PostDesign::BLOCK,
+            'list' => PostDesign::TABLE,
             'newsitem' => PostDesign::NEWSITEM,
             'schema' => PostDesign::SCHEMA,
-            'list' => PostDesign::TABLE,
             default => PostDesign::CARD,
         };
     }
---
+++ library/Controller/Archive/Mappers/AppearanceConfigMappers/MapTaxonomiesToDisplayTest.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Municipio\Controller\Archive\Mappers\AppearanceConfigMappers;
+
+use PHPUnit\Framework\Attributes\TestDox;
+use PHPUnit\Framework\TestCase;
+
+class MapTaxonomiesToDisplayTest extends TestCase
+{
+    #[TestDox('It should return an empty array when no taxonomies are set')]
+    public function testMapTaxonomiesReturnsExpectedArray()
+    {
+        $mapper = new MapTaxonomiesToDisplay();
+        $result = $mapper->map(['archiveProps' => (object) []]);
+        $this->assertEquals([], $result);
+    }
+
+    #[TestDox('It should return the correct taxonomies array when taxonomies are set')]
+    public function testMapTaxonomiesReturnsSetArray()
+    {
+        $mapper   = new MapTaxonomiesToDisplay();
+        $expected = ['category', 'tag'];
+        $result   = $mapper->map(['archiveProps' => (object) ['taxonomiesToDisplay' => $expected]]);
+        $this->assertEquals($expected, $result);
+    }
+}
