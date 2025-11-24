4ff81ef868f462e9ab5df836f7147a2c06cc75eb
Thor Brink <thor.brink@helsingborg.se>
2025-11-24 06:42:12 +0000

    wip: refactor

---
+++ library/PostsList/Block/js/ServerSideRender.tsx
@@ -0,0 +1,30 @@
+import { BlockEditProps } from "@wordpress/blocks";
+import { Shrink } from "./UI/Shrink";
+import { PreventClickOnChildren } from "./UI/PreventClickOnChildren";
+import { PostsListEditProps } from "./Edit";
+
+const { useBlockProps } = window.wp.blockEditor;
+const { __ } = window.wp.i18n;
+
+const LoadingPlaceholder: () => JSX.Element = () => {
+    return <div className="u-preloader" style={{ height: "300px" }}></div>;
+}
+
+export const ServerSideRender: React.FC<PostsListEditProps> = (props) => {
+
+    const { isSelected } = props;
+
+    return (
+        <div {...useBlockProps()}>
+            <PreventClickOnChildren>
+                <Shrink active={isSelected}>
+                    <window.wp.serverSideRender
+                        block={props.name}
+                        attributes={props.attributes}
+                        LoadingResponsePlaceholder={LoadingPlaceholder}
+                    />
+                </Shrink>
+            </PreventClickOnChildren>
+        </div>
+    )
+}
--- library/PostsList/Block/js/edit.tsx
+++ library/PostsList/Block/js/edit.tsx
@@ -1,35 +1,17 @@
 import { BlockEditProps } from "@wordpress/blocks";
 import { SettingsPanel } from "./SettingsPanel/SettingsPanel";
-import { Shrink } from "./UI/Shrink";
-import { PreventClickOnChildren } from "./UI/PreventClickOnChildren";
+import { ServerSideRender } from "./ServerSideRender";
 
-const { useBlockProps } = window.wp.blockEditor;
-const { __ } = window.wp.i18n;
-
-export type PostsListEditProps = BlockEditProps<PostsListAttributes>;
-
-const LoadingPlaceholder: () => JSX.Element = () => {
-    return <div className="u-preloader" style={{ height: "300px" }}></div>;
-}
+export type PostsListEditProps = BlockEditProps<PostsListAttributes> & {
+    name: string;
+};
 
 export const Edit: React.FC<PostsListEditProps> = (props) => {
-
-    const { isSelected } = props;
 
     return (
         <>
             <SettingsPanel {...props} />
-            <div {...useBlockProps()}>
-                <PreventClickOnChildren>
-                    <Shrink active={isSelected}>
-                        <window.wp.serverSideRender
-                            block="municipio/posts-list-block"
-                            attributes={props.attributes}
-                            LoadingResponsePlaceholder={LoadingPlaceholder}
-                        />
-                    </Shrink>
-                </PreventClickOnChildren>
-            </div >
+            <ServerSideRender {...props} />
         </>
     )
 }
