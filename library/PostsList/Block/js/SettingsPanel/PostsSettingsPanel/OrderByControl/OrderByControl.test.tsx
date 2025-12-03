e23ee7cb13cac84ad78280be943ad3d5813b2960
Thor Brink <thor.brink@helsingborg.se>
2025-12-03 13:54:09 +0000

    git Revert "test: update broken fe tests"
    This reverts commit 4be62b02da54cc8ba8017ff57fff766572ba69b4.

--- library/PostsList/Block/js/SettingsPanel/PostsSettingsPanel/OrderByControl/OrderByControl.test.tsx
+++ library/PostsList/Block/js/SettingsPanel/PostsSettingsPanel/OrderByControl/OrderByControl.test.tsx
@@ -1,59 +1,45 @@
-import { findByRole, render } from '@testing-library/react';
-import { userEvent } from '@testing-library/user-event';
-import { PostsListContext } from '../../../PostsListContext';
+const React from 'react';
+import {findByRole, findByText, render} from '@testing-library/react'
+import {userEvent} from '@testing-library/user-event';
 import { OrderByControl } from './OrderByControl';
+import { PostsListContext } from '../../../PostsListContext';
 
 Object.defineProperty(window, 'matchMedia', {
-	writable: true,
-	value: jest.fn().mockImplementation((query) => ({
-		matches: false,
-		media: query,
-		onchange: null,
-		addListener: jest.fn(),
-		removeListener: jest.fn(),
-		addEventListener: jest.fn(),
-		removeEventListener: jest.fn(),
-		dispatchEvent: jest.fn(),
-	})),
+    writable: true,
+    value: jest.fn().mockImplementation((query) => ({
+        matches: false,
+        media: query,
+        onchange: null,
+        addListener: jest.fn(),
+        removeListener: jest.fn(),
+        addEventListener: jest.fn(),
+        removeEventListener: jest.fn(),
+        dispatchEvent: jest.fn(),
+    })),
 });
 
 describe('OrderByControl', () => {
-	it.each(['date', 'title', 'modified'])('allows selecting %s from options', async (targetValue) => {
-		let value = '';
-		const { baseElement } = render(
-			<PostsListContext.Provider value={{ postTypeMetaKeys: async () => [] }}>
-				<OrderByControl
-					postType="post"
-					orderBy={value}
-					onChange={(selectedValue) => {
-						value = selectedValue;
-					}}
-				/>
-			</PostsListContext.Provider>,
-		);
-
-		await userEvent.selectOptions(baseElement.querySelector('select') as HTMLSelectElement, targetValue);
-		expect(value).toBe(targetValue);
-	});
-
-	it('allows selecting meta keys belonging to post type', async () => {
-		let value = '';
-
-		const { baseElement } = render(
-			<PostsListContext.Provider value={{ postTypeMetaKeys: async () => ['meta_key_example'] }}>
-				<OrderByControl
-					postType="custom_post"
-					orderBy={value}
-					onChange={(selectedValue) => {
-						value = selectedValue;
-					}}
-				/>
-			</PostsListContext.Provider>,
-		);
-
-		await findByRole(document.body, 'option', { name: /meta_key_example/ });
-		await userEvent.selectOptions(baseElement.querySelector('select') as Element, 'meta_key_example');
-
-		expect(value).toBe('meta_key_example');
-	});
+    it.each(['date', 'title', 'modified'])('allows selecting %s from options', async (targetValue) => {
+        let value = '';
+        render(
+            <PostsListContext.Provider value={{ postTypeMetaKeys: async (postType: string) => { return []; } }}> 
+                <OrderByControl postType='post' orderBy={value} onChange={(selectedValue) => { value = selectedValue; }} />
+        </PostsListContext.Provider>);
+        await userEvent.selectOptions(document.querySelector('select')!, targetValue);
+        expect(value).toBe(targetValue);
+    });
+    
+    it('allows selecting meta keys belonging to post type', async () => {
+        let value = '';
+        render(
+            <PostsListContext.Provider value={{ postTypeMetaKeys: async (postType: string) => { return ['meta_key_example']; } }}>
+                <OrderByControl postType='custom_post' orderBy={value} onChange={(selectedValue) => { value = selectedValue; }} />
+            </PostsListContext.Provider>
+        );
+        
+        await findByRole(document.body, 'option', { name: /meta_key_example/ });
+        await userEvent.selectOptions(document.querySelector('select')!, 'meta_key_example');
+        
+        expect(value).toBe('meta_key_example');
+    });
 });
