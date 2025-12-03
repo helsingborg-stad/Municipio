839169ca268c452869ee2312647b3911b8b566af
Nikolas Ramstedt <nikolas.ramstedt@helsingborg.se>
2025-12-03 08:38:36 +0000

    format

--- library/Admin/Roles/Subscriber.php
+++ library/Admin/Roles/Subscriber.php
@@ -35,13 +35,11 @@ class Subscriber implements Hookable
     {
         $role = $this->wpService->getRole('subscriber');
 
-        if (is_a($role, 'WP_Role')
-            && !$role->has_cap('read_private_posts')) {
+        if (is_a($role, 'WP_Role') && !$role->has_cap('read_private_posts')) {
             $role->add_cap('read_private_posts');
         }
 
-        if (is_a($role, 'WP_Role')
-            && !$role->has_cap('read_private_pages')) {
+        if (is_a($role, 'WP_Role') && !$role->has_cap('read_private_pages')) {
             $role->add_cap('read_private_pages');
         }
     }
