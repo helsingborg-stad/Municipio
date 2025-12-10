18a5a0d8ea9488acf219bfef1db54323bf852e10
Thor Brink <thor.brink@helsingborg.se>
2025-12-10 08:34:28 +0000

    fix: correct import path for Edit component in test file

--- library/PostsList/Block/js/Edit.test.tsx
+++ library/PostsList/Block/js/Edit.test.tsx
@@ -1,5 +1,5 @@
 import { render } from "@testing-library/react";
-import { Edit, type PostsListEditProps } from "./Edit";
+import { Edit, type PostsListEditProps } from "./Edit.tsx";
 
 jest.mock("./SettingsPanel/SettingsPanel", () => ({
 	SettingsPanel: (props: PostsListEditProps) => (
