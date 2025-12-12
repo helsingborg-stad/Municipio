847f8dd48ede5c73ca8ee62fc3c152a4d0d757bf
Thor Brink <thor.brink@helsingborg.se>
2025-12-12 07:28:59 +0000

    fix: Add Relation enum to FilterDefinition and update RuleSet to use it

--- library/SchemaData/ExternalContent/Filter/FilterDefinition/RuleSetTest.php
+++ library/SchemaData/ExternalContent/Filter/FilterDefinition/RuleSetTest.php
@@ -4,6 +4,7 @@ namespace Municipio\SchemaData\ExternalContent\Filter\FilterDefinition;
 
 use Municipio\SchemaData\ExternalContent\Filter\FilterDefinition\Contracts\Enums\Relation;
 use Municipio\SchemaData\ExternalContent\Filter\FilterDefinition\Contracts\Rule;
+use PHPUnit\Framework\Attributes\TestDox;
 use PHPUnit\Framework\TestCase;
 
 class RuleSetTest extends TestCase
@@ -20,6 +21,20 @@ class RuleSetTest extends TestCase
     {
         $ruleSet = new RuleSet($this->getArrayOfRules(2));
         $this->assertCount(2, $ruleSet->getRules());
+    }
+
+    #[TestDox('getRelation() returns provided relation')]
+    public function testGetRelationReturnsRelation()
+    {
+        $ruleSet = new RuleSet($this->getArrayOfRules(), Relation::AND);
+        $this->assertEquals(Relation::AND, $ruleSet->getRelation());
+    }
+
+    #[TestDox('getRelation() returns OR as default relation')]
+    public function testGetRelationReturnsDefaultRelation()
+    {
+        $ruleSet = new RuleSet($this->getArrayOfRules());
+        $this->assertEquals(Relation::OR, $ruleSet->getRelation());
     }
 
     private function getArrayOfRules(int $nbrOfRulesInArray = 1): array
--- library/SchemaData/ExternalContent/Filter/SchemaObjectsFilter/SchemaObjectsFilterFromFilterDefinitionTest.php
+++ library/SchemaData/ExternalContent/Filter/SchemaObjectsFilter/SchemaObjectsFilterFromFilterDefinitionTest.php
@@ -2,12 +2,14 @@
 
 namespace Municipio\SchemaData\ExternalContent\Filter\SchemaObjectsFilter;
 
+use Municipio\Schema\Schema;
 use Municipio\SchemaData\ExternalContent\Filter\FilterDefinition\Contracts\Enums\Operator;
+use Municipio\SchemaData\ExternalContent\Filter\FilterDefinition\Contracts\Enums\Relation;
 use Municipio\SchemaData\ExternalContent\Filter\FilterDefinition\FilterDefinition;
 use Municipio\SchemaData\ExternalContent\Filter\FilterDefinition\Rule;
 use Municipio\SchemaData\ExternalContent\Filter\FilterDefinition\RuleSet;
+use PHPUnit\Framework\Attributes\TestDox;
 use PHPUnit\Framework\TestCase;
-use Municipio\Schema\Schema;
 
 class SchemaObjectsFilterFromFilterDefinitionTest extends TestCase
 {
@@ -21,11 +23,56 @@ class SchemaObjectsFilterFromFilterDefinitionTest extends TestCase
     #[TestDox('the transform result can be used to filter an array of schema objects')]
     public function testTransformResultCanBeUsedToFilterAnArrayOfSchemaObjects()
     {
-
-        $schemaObjects       = [ Schema::thing()->name('Foo'), Schema::thing()->name('Bar'), Schema::thing()->name('Baz'), ];
-        $filterDefinition    = new FilterDefinition([new RuleSet([new Rule('name', 'Foo', Operator::EQUALS)])]);
+        $schemaObjects = [Schema::thing()->name('Foo'), Schema::thing()->name('Bar'), Schema::thing()->name('Baz')];
+        $filterDefinition = new FilterDefinition([new RuleSet([new Rule('name', 'Foo', Operator::EQUALS)])]);
         $schemaObjectsFilter = new SchemaObjectsFilterFromFilterDefinition($filterDefinition);
 
         $this->assertEquals([Schema::thing()->name('Foo')], $schemaObjectsFilter->filter($schemaObjects));
+    }
+
+    #[TestDox('filter definition with multiple rules in a ruleset respects the provided ruleset operator (OR)')]
+    public function testDefaultFilterBehaviorIsToUseDisjunctionBetweenRulesets()
+    {
+        $schemaObjects = [
+            Schema::thing()->name('Foo')->description('First'),
+            Schema::thing()->name('Bar')->description('Second'),
+            Schema::thing()->name('Baz')->description('Third'),
+        ];
+        $filterDefinition = new FilterDefinition([
+            new RuleSet([new Rule('name', 'Foo', Operator::EQUALS), new Rule('description', 'Third', Operator::EQUALS)], Relation::OR),
+        ]);
+        $schemaObjectsFilter = new SchemaObjectsFilterFromFilterDefinition($filterDefinition);
+
+        $this->assertEquals(
+            [
+                Schema::thing()->name('Foo')->description('First'),
+                Schema::thing()->name('Baz')->description('Third'),
+            ],
+            $schemaObjectsFilter->filter($schemaObjects),
+        );
+    }
+
+    #[TestDox('filter definition with multiple rules in a ruleset respects the provided ruleset operator (AND)')]
+    public function testFilterBehaviorWithMultipleRulesInARulesetIsConjunction()
+    {
+        $schemaObjects = [
+            Schema::thing()->name('Foo')->description('First'),
+            Schema::thing()->name('Foo')->description('Second'),
+            Schema::thing()->name('Baz')->description('Third'),
+        ];
+        $filterDefinition = new FilterDefinition([
+            new RuleSet([
+                new Rule('name', 'Foo', Operator::EQUALS),
+                new Rule('description', 'Second', Operator::EQUALS),
+            ], Relation::AND),
+        ]);
+        $schemaObjectsFilter = new SchemaObjectsFilterFromFilterDefinition($filterDefinition);
+
+        $this->assertEquals(
+            [
+                Schema::thing()->name('Foo')->description('Second'),
+            ],
+            $schemaObjectsFilter->filter($schemaObjects),
+        );
     }
 }
