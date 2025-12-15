61b6c675d6c1747657c81a647973bf826645dd05
Thor Brink <thor.brink@helsingborg.se>
2025-12-15 08:23:24 +0000

    fix: correct variable reference in MapOrderByFromData mapper

--- library/Controller/Archive/Mappers/GetPostsConfig/MapOrderByFromData.php
+++ library/Controller/Archive/Mappers/GetPostsConfig/MapOrderByFromData.php
@@ -17,6 +17,6 @@ class MapOrderByFromData implements MapperInterface
      */
     public function map(array $data): string
     {
-        return $this->data['archiveProps']->dateField ?? 'post_date';
+        return $data['archiveProps']->dateField ?? 'post_date';
     }
 }
