7d17a90fc1794442260bf839bf062d7bfde0bb26
Thor Brink <thor.brink@helsingborg.se>
2025-12-16 13:53:53 +0000

    fix: Correct taxonomy filter config mapping

--- library/Controller/Archive/Mappers/FilterConfigMappers/MapTaxonomyFilterConfigs.php
+++ library/Controller/Archive/Mappers/FilterConfigMappers/MapTaxonomyFilterConfigs.php
@@ -19,9 +19,10 @@ class MapTaxonomyFilterConfigs implements MapperInterface
      * @param array $wpTaxonomies
      * @param ApplyFilters&GetQueriedObject $wpService
      */
-    public function __construct(private array $wpTaxonomies, private ApplyFilters&GetQueriedObject $wpService)
-    {
-    }
+    public function __construct(
+        private array $wpTaxonomies,
+        private ApplyFilters&GetQueriedObject $wpService,
+    ) {}
 
     /**
      * Map taxonomy filter configs
@@ -31,10 +32,14 @@ class MapTaxonomyFilterConfigs implements MapperInterface
      */
     public function map(array $data): mixed
     {
-        $taxonomies = $this->wpService->applyFilters('Municipio/Archive/getTaxonomyFilters/taxonomies', array_diff(
-            $data['archiveProps']->enabledFilters ?? [],
-            [$this->currentTaxonomy()]
-        ), $this->currentTaxonomy());
+        $taxonomies = $this->wpService->applyFilters(
+            'Municipio/Archive/getTaxonomyFilters/taxonomies',
+            array_diff(
+                $data['archiveProps']->enabledFilters ?? [],
+                [$this->currentTaxonomy()],
+            ),
+            $this->currentTaxonomy(),
+        );
 
         if (empty($taxonomies)) {
             return [];
@@ -43,15 +48,13 @@ class MapTaxonomyFilterConfigs implements MapperInterface
         // Wash out invalid taxonomies
         $filteredWpTaxonomies = array_filter(
             $this->wpTaxonomies,
-            fn(\WP_Taxonomy $wpTaxonomy) => in_array($wpTaxonomy->name, $taxonomies)
+            fn(\WP_Taxonomy $wpTaxonomy) => in_array($wpTaxonomy->name, $taxonomies),
         );
 
         // $taxonomies    = array_values(array_intersect($allTaxonomies, $taxonomies));
         return array_map(function (\WP_Taxonomy $taxonomy) use ($data) {
             $camelCasedName = lcfirst(str_replace(' ', '', ucwords(str_replace(['-', '_'], ' ', $taxonomy->name))));
-            $filterType     = isset($this->data['archiveProps']->{$camelCasedName . 'FilterFieldType'}) && $data['archiveProps']->{$camelCasedName . 'FilterFieldType'} === 'multi'
-                ? TaxonomyFilterType::MULTISELECT
-                : TaxonomyFilterType::SINGLESELECT;
+            $filterType = isset($data['archiveProps']->{$camelCasedName . 'FilterFieldType'}) && $data['archiveProps']->{$camelCasedName . 'FilterFieldType'} === 'multi' ? TaxonomyFilterType::MULTISELECT : TaxonomyFilterType::SINGLESELECT;
             return new TaxonomyFilterConfig($taxonomy, $filterType);
         }, $filteredWpTaxonomies);
     }
@@ -62,9 +65,9 @@ class MapTaxonomyFilterConfigs implements MapperInterface
     private function currentTaxonomy()
     {
         $queriedObject = $this->wpService->getQueriedObject();
-        $isTaxArchive  = false;
+        $isTaxArchive = false;
         if (!empty($queriedObject->taxonomy) && isset($_SERVER['REQUEST_URI'])) {
-            $pathParts   = explode('/', trim(parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH), '/'));
+            $pathParts = explode('/', trim(parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH), '/'));
             $trimmedPath = end($pathParts);
             if ($queriedObject->slug == $trimmedPath) {
                 $isTaxArchive = $queriedObject->taxonomy;
