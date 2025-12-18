a337f526997c9c1e18604aeaa1d072df071a973e
Thor Brink <thorbrink@gmail.com>
2025-12-18 13:28:16 +0100

    fix: add organizers card to event schema view (#1711)

---
+++ views/v3/partials/schema/event/organizers-card.blade.php
@@ -0,0 +1,34 @@
+@card([
+    'heading' => $lang->organizersTitle,
+])
+    @if(!empty($place['url']))
+        @slot('belowContent')
+            @element(['classList' => ['u-display--flex', 'o-layout-grid--gap-8', 'u-flex-direction--column']])
+                @foreach($organizers as $organizer)
+                    @element(['classList' => $loop->first ? ['u-margin__top--2'] : []])
+                        @if(!empty($organizer['name']))
+                            @typography(['element' => 'h3', 'variant' => 'h4', 'classList' => ['u-margin__top--0']])
+                                {!! $organizer['name'] !!}
+                            @endtypography
+                        @endif
+                        @if(!empty($organizer['email']))
+                            @link(['href' => 'mailto:' . $organizer['email'], 'classList' => ['u-display--block']])
+                                {!! $organizer['email'] !!}
+                            @endlink
+                        @endif
+                        @if(!empty($organizer['telephone']))
+                            @link(['href' => 'tel:' . $organizer['telephone'], 'classList' => ['u-display--block']])
+                                {!! $organizer['telephone'] !!}
+                            @endlink
+                        @endif
+                        @if(!empty($organizer['url']))
+                            @link(['href' => $organizer['url'], 'classList' => ['u-display--block']])
+                                {!! $organizer['url'] !!}
+                            @endlink
+                        @endif
+                    @endelement
+                @endforeach
+            @endelement
+        @endslot
+    @endif
+@endcard
