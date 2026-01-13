34309e2116759e725741e0fa7ff1e8423fc22c1b
Thor Brink <thor.brink@helsingborg.se>
2026-01-13 09:10:06 +0000

    fix: add reset url to PostsList block filter config mapper

--- library/PostsList/Block/PostsListBlockRenderer/ConfigMappers/BlockAttributesToFilterConfigMapper.php
+++ library/PostsList/Block/PostsListBlockRenderer/ConfigMappers/BlockAttributesToFilterConfigMapper.php
@@ -5,7 +5,6 @@ namespace Municipio\PostsList\Block\PostsListBlockRenderer\ConfigMappers;
 use Municipio\PostsList\Config\FilterConfig\DefaultFilterConfig;
 use Municipio\PostsList\Config\FilterConfig\FilterConfigInterface;
 use Municipio\PostsList\Config\FilterConfig\TaxonomyFilterConfig\TaxonomyFilterConfig;
-use Municipio\PostsList\Config\FilterConfig\TaxonomyFilterConfig\TaxonomyFilterConfigInterface;
 use Municipio\PostsList\Config\FilterConfig\TaxonomyFilterConfig\TaxonomyFilterType;
 use Municipio\PostsList\QueryVars\QueryVarsInterface;
 
@@ -41,12 +40,14 @@ class BlockAttributesToFilterConfigMapper
         }
 
         $showReset = $this->isAnyQueryVarPresent();
+        $resetUrl = $this->getResetUrl();
 
-        return new class($attributes, $taxonomyFilterConfigs, $showReset) extends DefaultFilterConfig {
+        return new class($attributes, $taxonomyFilterConfigs, $showReset, $resetUrl) extends DefaultFilterConfig {
             public function __construct(
                 private array $attributes,
                 private array $taxonomyFilterConfigs,
                 private bool $showReset,
+                private null|string $resetUrl,
             ) {}
 
             public function isTextSearchEnabled(): bool
@@ -73,11 +74,48 @@ class BlockAttributesToFilterConfigMapper
             {
                 return $this->showReset;
             }
+
+            public function getResetUrl(): null|string
+            {
+                return $this->resetUrl;
+            }
         };
     }
 
     private function isAnyQueryVarPresent(): bool
     {
         return !empty($_GET[$this->queryVars->getSearchParameterName()]) || !empty($_GET[$this->queryVars->getDateFromParameterName()]) || !empty($_GET[$this->queryVars->getDateToParameterName()]);
+    }
+
+    private function getResetUrl(): null|string
+    {
+        $currentUrl = (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on' ? 'https' : 'http') . "://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]";
+        $urlParts = parse_url($currentUrl);
+        if (!isset($urlParts['query'])) {
+            return $currentUrl;
+        }
+
+        parse_str($urlParts['query'], $queryParams);
+
+        $filteredQueryParams = array_filter(
+            $queryParams,
+            fn($key) => !in_array($key, [
+                $this->queryVars->getSearchParameterName(),
+                $this->queryVars->getDateFromParameterName(),
+                $this->queryVars->getDateToParameterName(),
+            ]),
+            ARRAY_FILTER_USE_KEY,
+        );
+
+        $resetUrl = $urlParts['scheme'] . '://' . $urlParts['host'];
+        if (isset($urlParts['port'])) {
+            $resetUrl .= ':' . $urlParts['port'];
+        }
+        $resetUrl .= $urlParts['path'] ?? '';
+        if (!empty($filteredQueryParams)) {
+            $resetUrl .= '?' . http_build_query($filteredQueryParams);
+        }
+
+        return $resetUrl;
     }
 }
