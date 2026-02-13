8d62d391508292db0a96c4788c904c69ed488527
NiclasNorin <niclasn@outlook.com>
2026-02-13 09:47:03 +0100

    fix: correcting undefined array key for schools slider

--- views/v3/single-schema-preschool.blade.php
+++ views/v3/single-schema-preschool.blade.php
@@ -13,16 +13,20 @@
         @endelement
         
         @if(!empty($sliderItems))
-            @slider(['repeatSlide' => true, 'autoSlide' => false,'padding' => 11, 'showStepper' => false])
-                @foreach($sliderItems['imageItems'] as $item)
-                    @slider__item($item)
-                    @endslider__item
-                @endforeach
-                @foreach($sliderItems['videoItems'] as $item)
-                    @slider__item()
-                        {!! $item['embed'] !!}
-                    @endslider__item
-                @endforeach
+            @slider(['autoSlide' => false,'padding' => 11, 'showStepper' => false])
+                @if(!empty($sliderItems['imageItems']))
+                    @foreach($sliderItems['imageItems'] as $item)
+                        @slider__item($item)
+                        @endslider__item
+                    @endforeach
+                @endif
+                @if(!empty($sliderItems['videoItems']))
+                    @foreach($sliderItems['videoItems'] as $item)
+                        @slider__item()
+                            {!! $item['embed'] !!}
+                        @endslider__item
+                    @endforeach
+                @endif
             @endslider
         @endif
 
