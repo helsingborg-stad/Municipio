cb96260404e7a584e3ced7c1c660f6a6493fad2e
Nikolas Ramstedt <nikolas.ramstedt@helsingborg.se>
2026-02-19 10:24:49 +0100

    fix(filters): always show reset button

--- library/PostsList/views/parts/filters.blade.php
+++ library/PostsList/views/parts/filters.blade.php
@@ -51,7 +51,7 @@
             @endbutton
         </div>
 
-        @if ($filterConfig->showReset() && $filterConfig->getResetUrl())
+        @if ($filterConfig->getResetUrl())
             <div class="o-grid-fit@xs o-grid-fit@sm o-grid-fit@md u-margin__top--auto">
                 @button([
                     ...$getFilterFormResetButtonArguments(),
