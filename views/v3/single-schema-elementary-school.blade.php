f5355f2fb8a3f91595f4b8a6a66f113437e36cc6
NiclasNorin <niclasn@outlook.com>
2026-02-24 10:57:33 +0100

    refactor: partials

--- views/v3/single-schema-elementary-school.blade.php
+++ views/v3/single-schema-elementary-school.blade.php
@@ -10,68 +10,9 @@
             @includeWhen(!empty($events), 'partials.schema.elementary-school.events')
             @includeWhen(!empty($actions), 'partials.schema.elementary-school.actions')
             @includeWhen(!empty($accordionListItems), 'partials.schema.elementary-school.accordion-list')
-
-        @if(!empty($contactPoints))
-            @paper(['classList' => ['u-padding--2']])
-                @typography(['element' => 'h2', 'classList' => ['u-margin__bottom--2']])
-                    {!! $lang->contactPointsLabel !!}
-                @endtypography
-                @foreach($contactPoints['items'] as $item)
-                    @button([
-                        'text' => $item['name'],
-                        'color' => 'primary',
-                        'size' => 'lg',
-                        'href' => $item['url'],
-                        'icon' => $item['icon'],
-                        'reversePositions' => 'true',
-                        'classList' => ['u-margin__right--1'],
-                    ])
-                    @endbutton
-                @endforeach
-            @endpaper
-        @endif
-
-        @if(!empty($personsAttributes))
-            @element()
-                @typography(['element' => 'h2'])
-                    {!! $lang->contactLabel !!}
-                @endtypography
-                @element(['classList' => ['o-grid', 'o-grid--half-gutter', 'u-margin__top--2']])
-                    @foreach ($personsAttributes as $personAttributes)
-                        @person(array_merge($personAttributes, ['classList' => ['o-grid-12@sm', 'o-grid-6@md']]))@endperson
-                    @endforeach
-                @endelement
-            @endelement
-        @endif
-
-        @if(!empty($addresses))
-            @paper(['classList' => ['u-padding--2']])
-                @element()
-                    @typography(['element' => 'h2'])
-                        {!! $lang->addressLabel !!}
-                    @endtypography
-                    @foreach ($addresses as $address)
-                        @if(!empty($address['address']))
-                            @typography()
-                                {!! $address['address'] !!}
-                            @endtypography
-                        @endif
-                        @if(!empty($address['directionsLink']))
-                            @link(['href' => $address['directionsLink']['href']])
-                                {!! $address['directionsLink']['label'] !!}
-                            @endlink
-                        @endif
-                    @endforeach
-
-                    @openStreetMap([
-                        ...$mapAttributes,
-                        'height' => '400px',
-                        'classList' => ['u-margin__top--2']
-                    ])
-                    @endopenStreetMap
-                @endelement
-            @endpaper
-        @endif
+            @includeWhen(!empty($contactPoints), 'partials.schema.elementary-school.contact-points')
+            @includeWhen(!empty($personsAttributes), 'partials.schema.elementary-school.contact-persons')
+            @includeWhen(!empty($addresses), 'partials.schema.elementary-school.addresses')
     @stop
 
     @include('templates.sections.grid.content', [
