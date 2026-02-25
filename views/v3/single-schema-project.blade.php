f29970448f6c24c644418070a1f3d3a90fca5a93
NiclasNorin <niclasn@outlook.com>
2026-02-25 11:45:19 +0100

    refactor: schema project view to extend grid

--- views/v3/single-schema-project.blade.php
+++ views/v3/single-schema-project.blade.php
@@ -1,53 +1,23 @@
-@extends('templates.single')
+@extends('templates.grid', ['helperNavBeforeContent' => false])
 
 @section('hero-top-sidebar')
-    @hero([ 'image' => $image, 'heroView' => 'twoColumn', 'classList' => ['u-color__bg--lightest'] ])
-        @slot('content')
-            @if (!empty($technology))
-                @typography([ 'element' => 'span', 'classList' => ['page-header_meta'] ]) {!! $technology !!} @endtypography
-            @endif
-            @typography([
-                'element' => 'h1',
-                'variant' => 'h1',
-                'classList' => ['page-header__title', 'u-margin__top--0', 'u-margin__bottom--3']
-            ])
-                {{ $post->postTitle }}
-            @endtypography
-
-            @typography([ 'element' => 'b', ])
-                {{$progressLabel}}
-            @endtypography
-            @progressBar([ 'value' => $progressPercentage ]) @endprogressBar
-
-        @endslot
-    @endhero
+    @include('partials.schema.project.title-area')
+    @include('partials.navigation.helper', [
+        'classList' => ['o-container', 'o-container--helper-nav'],
+    ])
+    @parent
 @stop
 
-@section('article.title')@stop
-@section('article.content')
-    {!!$post->getSchemaProperty('description')!!}
-@stop
+@section('layout')
+    @section('content')
+        @include('partials.schema.project.description')
+    @stop
 
-@section('sidebar.right-sidebar.before')
-    @paper(['padding' => 2])
-        @collection()
-            @foreach ($informationList as $item)
-                @collection__item([])
-                    @typography(['element' => 'h2', 'variant' => 'h3'])
-                        {{$item['label']}}
-                    @endtypography
-                    @if(is_array($item['value']))
-                        @foreach ($item['value'] as $value)
-                            @if(!empty($value))
-                                @typography(){!!$value!!}@endtypography
-                            @endif
-                        @endforeach
-                    @else
-                        @typography(){{$item['value']}}@endtypography
-                    @endif
-                @endcollection__item
-            @endforeach
-        @endcollection
-    @endpaper
+    @section('sidebar-right-content')
+        @include('partials.schema.project.information-list')
+    @stop
 
+    @include('templates.sections.grid.content', [
+        'addToArticleClassList' => ['c-article', 'c-article--readable-width']
+    ])
 @stop
