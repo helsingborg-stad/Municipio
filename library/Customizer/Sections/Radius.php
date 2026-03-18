f713826560a0301cc6391a14b08455f3ac5d089e
Thor Brink <thor.brink@helsingborg.se>
2026-03-18 07:17:27 +0000

    fix: remove unused radius settings from customizer

--- library/Customizer/Sections/Radius.php
+++ library/Customizer/Sections/Radius.php
@@ -10,48 +10,6 @@ class Radius
     {
         KirkiField::addField([
             'type'      => 'slider',
-            'settings'  => 'radius_xs',
-            'label'     => esc_html__('Extra small', 'municipio'),
-            'section'   => $sectionID,
-            'transport' => 'auto',
-            'default'   => 2,
-            'choices'   => [
-                'min'  => 0,
-                'max'  => 12,
-                'step' => 1,
-            ],
-            'output'    => [
-                [
-                    'element'  => ':root',
-                    'property' => '--radius-xs',
-                    'units'    => 'px'
-                ]
-            ],
-        ]);
-
-        KirkiField::addField([
-            'type'      => 'slider',
-            'settings'  => 'radius_sm',
-            'label'     => esc_html__('Small', 'municipio'),
-            'section'   => $sectionID,
-            'transport' => 'auto',
-            'default'   => 4,
-            'choices'   => [
-                'min'  => 0,
-                'max'  => 12,
-                'step' => 1,
-            ],
-            'output'    => [
-                [
-                    'element'  => ':root',
-                    'property' => '--radius-sm',
-                    'units'    => 'px'
-                ]
-            ],
-        ]);
-
-        KirkiField::addField([
-            'type'      => 'slider',
             'settings'  => 'radius_md',
             'label'     => esc_html__('Medium', 'municipio'),
             'section'   => $sectionID,
@@ -66,27 +24,6 @@ class Radius
                 [
                     'element'  => ':root',
                     'property' => '--radius-md',
-                    'units'    => 'px'
-                ]
-            ],
-        ]);
-
-        KirkiField::addField([
-            'type'      => 'slider',
-            'settings'  => 'radius_lg',
-            'label'     => esc_html__('Large', 'municipio'),
-            'section'   => $sectionID,
-            'transport' => 'auto',
-            'default'   => 12,
-            'choices'   => [
-                'min'  => 0,
-                'max'  => 12,
-                'step' => 1,
-            ],
-            'output'    => [
-                [
-                    'element'  => ':root',
-                    'property' => '--radius-lg',
                     'units'    => 'px'
                 ]
             ],
