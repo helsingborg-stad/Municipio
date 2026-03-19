2122c5f5e30cd9fe52d3dc0e418bbf239fd9d81d
NiclasNorin <niclasn@outlook.com>
2026-03-19 10:28:29 +0100

    feat: keep active item when hovering

--- library/BackdropBanner/js/Front/navigation-item.ts
+++ library/BackdropBanner/js/Front/navigation-item.ts
@@ -2,6 +2,8 @@ import type BackgroundImage from "./background-image";
 
 export default class NavigationItem {
     private imageUrlAttribute = "data-js-backdrop-banner-image-url";
+    private navigationItemAttribute = "data-js-backdrop-banner-navigation-item";
+    private activeClass = "is-active";
     private imageUrl: string | null;
 
     constructor(
@@ -9,18 +11,48 @@ export default class NavigationItem {
         private backgroundImage: BackgroundImage,
     ) {
         this.imageUrl = this.navItem.getAttribute(this.imageUrlAttribute);
+        this.setListener();
 
         if (this.imageUrl) {
             const img = new Image();
             img.src = this.imageUrl;
-            this.setListener();
+        }
+
+        if (this.navItem.classList.contains(this.activeClass)) {
+            this.activate();
         }
     }
 
     private setListener(): void {
         this.navItem.addEventListener("mouseenter", () => {
-            if (!this.imageUrl) return;
-            this.backgroundImage.showImage(this.imageUrl);
+            this.activate();
         });
+
+        this.navItem.addEventListener("focusin", () => {
+            this.activate();
+        });
+    }
+
+    private activate(): void {
+        this.setActiveClass();
+
+        if (!this.imageUrl) {
+            return;
+        }
+
+        this.backgroundImage.showImage(this.imageUrl);
+    }
+
+    private setActiveClass(): void {
+        const banner = this.navItem.closest("[data-js-backdrop-banner]");
+        const navigationItems = banner?.querySelectorAll<HTMLElement>(
+            `[${this.navigationItemAttribute}]`,
+        );
+
+        navigationItems?.forEach((item) => {
+            item.classList.remove(this.activeClass);
+        });
+
+        this.navItem.classList.add(this.activeClass);
     }
 }
