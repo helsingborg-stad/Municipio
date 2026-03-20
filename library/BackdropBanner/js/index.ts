569485bcc9a1559c38e2627f309c7634f7368c06
NiclasNorin <niclasn@outlook.com>
2026-03-20 08:22:45 +0100

    test

--- library/BackdropBanner/js/index.ts
+++
@@ -1,7 +0,0 @@
-import blockConfig from '../block.json';
-import { registerBlockType } from "@wordpress/blocks";
-import { Edit as edit } from "./Edit";
-
-registerBlockType<BackdropBannerAttributes>(blockConfig.name, {
-	edit
-});
