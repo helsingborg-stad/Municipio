f100ebf4d8454eb2f430959f4a75731a7647bb49
Thor Brink <thor.brink@helsingborg.se>
2026-03-24 16:12:57 +0000

    fix: migrate color rimary light to primary

--- library/Upgrade/V41/Version41.php
+++ library/Upgrade/V41/Version41.php
@@ -42,12 +42,27 @@ class Version41 implements VersionInterface
             '--color-background' => '--color--background',
             '--c-header-logotype-height' => '--c-header--logotype-height',
             '--c-header-brand-color' => '--c-header--brand-color',
+            '--color--primary-light' => '--color--primary',
+            'height: calc(var(--c-header--logotype-height, 6) * var(--base, 8px));' => 'height: var(--c-header--logotype-height);',
+            'width: calc(var(--c-header--logotype-height, 6) * var(--base, 8px));' => 'width: var(--c-header--logotype-height);',
         ];
 
         $customCss = str_replace(array_keys($searchReplaceMap), array_values($searchReplaceMap), $customCss);
         $acfCustomCss = str_replace(array_keys($searchReplaceMap), array_values($searchReplaceMap), $acfCustomCss);
 
+        $customCss = $this->maybeWrapInCssLayer($customCss);
+        $acfCustomCss = $this->maybeWrapInCssLayer($acfCustomCss);
+
         $this->wpService->wpUpdateCustomCssPost($customCss);
         $this->acfService->updateField('custom_css_input', $acfCustomCss, 'option');
+    }
+
+    private function maybeWrapInCssLayer(string $css): string
+    {
+        if (str_contains($css, '@layer')) {
+            return $css;
+        }
+
+        return "@layer theme {\n" . $css . "\n}";
     }
 }
