fbd4e5ce1cf4d91309548ac4cf76930ec4cae28d
Sebastian Thulin <sebastian.thulin@helsingborg.se>
2026-04-08 14:01:00 +0200

    feat: add a11y scope

--- views/v3/a11y.blade.php
+++ views/v3/a11y.blade.php
@@ -1,89 +1,90 @@
 @extends('templates.single')
 
 @section('content')
-
-    @element([
-        'componentElement' => 'article',
-        'id' => 'article',
-        'classList' => array_merge(
-            (isset($centerContent) && $centerContent) ? ['u-margin__x--auto'] : [],
-            [ 'c-article', 'c-article--readable-width', 's-article', 'u-clearfix', 't-a11y-page']
-        ),
-    ])
-        @typography([
-            'element' => 'h1', 
-            'variant' => 'h1', 
-            'id' => 'page-title',
+    @scope(['name' => ['template', 'a11y']])
+        @element([
+            'componentElement' => 'article',
+            'id' => 'article',
+            'classList' => array_merge(
+                (isset($centerContent) && $centerContent) ? ['u-margin__x--auto'] : [],
+                [ 'c-article', 'c-article--readable-width', 's-article', 'u-clearfix', 't-a11y-page']
+            ),
         ])
-            {!! $heading !!}
-        @endtypography
+            @typography([
+                'element' => 'h1', 
+                'variant' => 'h1', 
+                'id' => 'page-title',
+            ])
+                {!! $heading !!}
+            @endtypography
 
-        {!! $content !!}
+            {!! $content !!}
 
-        {{-- Status pills start --}}
-        @element(['classList' => 'u-display--flex u-flex--row u-align-items--center'])
-            
-            {{-- Compliance level information --}}
-            @if($compliance && $compliance->label)
-                @element([
-                    'componentElement' => 'span', 
-                    'attributeList' => [
-                        'data-tooltip' => $compliance->reference->standard . ", " .$compliance->reference->version,
-                    ]
-                ])
+            {{-- Status pills start --}}
+            @element(['classList' => 'u-display--flex u-flex--row u-align-items--center'])
+                
+                {{-- Compliance level information --}}
+                @if($compliance && $compliance->label)
                     @element([
                         'componentElement' => 'span', 
-                        'classList' => $compliance->class ?? [],
+                        'attributeList' => [
+                            'data-tooltip' => $compliance->reference->standard . ", " .$compliance->reference->version,
+                        ]
                     ])
-                        <span>@icon(['icon' => $compliance->icon])@endicon {{ $compliance->label }}</span>
+                        @element([
+                            'componentElement' => 'span', 
+                            'classList' => $compliance->class ?? [],
+                        ])
+                            <span>@icon(['icon' => $compliance->icon])@endicon {{ $compliance->label }}</span>
+                        @endelement
                     @endelement
-                @endelement
-            @endif
+                @endif
 
-            {{-- Review date information --}}
-            @if($review && $review->date)
-                @element([
-                    'componentElement' => 'span', 
-                    'attributeList' => [
-                        'data-tooltip' => $review->label
-                    ]
-                ])
+                {{-- Review date information --}}
+                @if($review && $review->date)
                     @element([
                         'componentElement' => 'span', 
-                        'classList' => $review->class ?? [],
+                        'attributeList' => [
+                            'data-tooltip' => $review->label
+                        ]
                     ])
-                        <span>@icon(['icon' => $review->icon])@endicon {{ $review->date }}</span>
+                        @element([
+                            'componentElement' => 'span', 
+                            'classList' => $review->class ?? [],
+                        ])
+                            <span>@icon(['icon' => $review->icon])@endicon {{ $review->date }}</span>
+                        @endelement
                     @endelement
-                @endelement
+                @endif
+
+            @endelement 
+            {{-- Status pills end --}}
+
+            {{-- Issues section start --}}
+            @if (!empty($categorizedIssues) && is_array($categorizedIssues) && count($categorizedIssues) > 0)
+                @foreach($categorizedIssues as $key => $category)
+                    @card(['classList' => ['u-margin__top--2']])
+                        @element(['classList' => ['c-card__body']])
+                            @element(['classList' => ['c-card__heading']])
+                                @typography(['element' => 'h4', 'variant' => 'h4'])
+                                    @icon(['icon' => $category['icon'] ?? ''])@endicon
+                                    {{ $category['label'] }}
+                                @endtypography
+                            @endelement
+                        
+                            @element(['classList' => ['u-margin__top--2', 'c-card__content']])
+                                <ul>
+                                    @foreach ($category['issues'] as $issue)
+                                        <li>{{ $issue['label'] }}</li>
+                                    @endforeach
+                                </ul>
+                            @endelement
+                        @endelement
+                    @endcard
+                @endforeach
             @endif
+            {{-- Issues section end --}}
 
-        @endelement 
-        {{-- Status pills end --}}
-
-        {{-- Issues section start --}}
-        @if (!empty($categorizedIssues) && is_array($categorizedIssues) && count($categorizedIssues) > 0)
-            @foreach($categorizedIssues as $key => $category)
-                @card(['classList' => ['u-margin__top--2']])
-                    @element(['classList' => ['c-card__body']])
-                        @element(['classList' => ['c-card__heading']])
-                            @typography(['element' => 'h4', 'variant' => 'h4'])
-                                @icon(['icon' => $category['icon'] ?? ''])@endicon
-                                {{ $category['label'] }}
-                            @endtypography
-                        @endelement
-                    
-                        @element(['classList' => 'u-margin__top--2', 'classList' => ['c-card__content']])
-                            <ul>
-                                @foreach ($category['issues'] as $issue)
-                                    <li>{{ $issue['label'] }}</li>
-                                @endforeach
-                            </ul>
-                        @endelement
-                    @endelement
-                @endcard
-            @endforeach
-        @endif
-        {{-- Issues section end --}}
-
-    @endelement
+        @endelement
+    @endscope
 @stop
