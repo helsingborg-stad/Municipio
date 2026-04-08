00d71a9b28d5d7cbcff85f8ade61e2b605c1fa0d
Sebastian Thulin <sebastian.thulin@helsingborg.se>
2026-04-08 16:43:43 +0200

    fix: check that sidebars actually contain something before printing them.

--- views/v3/partials/sidebar.blade.php
+++ views/v3/partials/sidebar.blade.php
@@ -1,15 +1,29 @@
 @if(isset($id) && $id)
     @section('sidebar.' . $id . '.before')@show
     @if (is_active_sidebar($id))
-        @section('sidebar.active.' . $id . '.before')@show
-        @scope([
-            'id' => 'sidebar-' . $id, 
-            'name' => ['sidebar', 'sidebar-' . $id, $postType . '-sidebar-' . $id],
-            'classList' => $classes ?? []
-        ])
-            @php dynamic_sidebar($id); @endphp
-        @endscope
-        @section('sidebar.active.' . $id . '.after')@show
+        
+        @php
+            ob_start();
+            dynamic_sidebar($id);
+            $sidebarMarkup = trim((string) ob_get_clean());
+        @endphp
+
+        @if(!empty($sidebarMarkup))
+
+            @section('sidebar.active.' . $id . '.before')@show
+
+            @scope([
+                'id' => 'sidebar-' . $id, 
+                'name' => ['sidebar', 'sidebar-' . $id, $postType . '-sidebar-' . $id],
+                'classList' => $classes ?? []
+            ])
+                {!! $sidebarMarkup !!}
+            @endscope
+
+            @section('sidebar.active.' . $id . '.after')@show
+
+        @endif
+        
     @else
         @section('sidebar.inactive.' . $id)@show
     @endif
