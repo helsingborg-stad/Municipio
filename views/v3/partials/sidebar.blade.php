c5cab397d858d0670c069071851bf254a3c9a354
Sebastian Thulin <sebastian.thulin@helsingborg.se>
2026-04-08 11:22:05 +0200

    feat: add scopes to sidebars

--- views/v3/partials/sidebar.blade.php
+++ views/v3/partials/sidebar.blade.php
@@ -2,9 +2,13 @@
     @section('sidebar.' . $id . '.before')@show
     @if (is_active_sidebar($id))
         @section('sidebar.active.' . $id . '.before')@show
-        <div id="sidebar-{{$id}}" class="s-sidebar s-sidebar-{{$id}} sidebar-{{$id}} {{isset($classes) ? is_array($classes) ? implode(' ', $classes) : $classes : ''}}">
+        @scope([
+            'id' => 'sidebar-' . $id, 
+            'name' => ['sidebar', 'sidebar-' . $id, $postType . '-sidebar-' . $id],
+            'classList' => $classes ?? []
+        ])
             @php dynamic_sidebar($id); @endphp
-        </div>
+        @endscope
         @section('sidebar.active.' . $id . '.after')@show
     @else
         @section('sidebar.inactive.' . $id)@show
