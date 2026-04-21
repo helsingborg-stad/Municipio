ad7ec88082334723725197dd26436eb1b5b28e4b
Sebastian Thulin <sebastian.thulin@helsingborg.se>
2026-04-21 14:04:48 +0200

    feat: decodate homr url resolver

--- library/App.php
+++ library/App.php
@@ -226,10 +226,12 @@ class App
         /* Integration: Polylang */
         $resolvePageTreeMenuPageIds = new \Municipio\Integrations\Polylang\ResolvePageTreeMenuPageIds($this->wpService);
         $resolveNavigationCacheKey = new \Municipio\Integrations\Polylang\ResolveNavigationCacheKey($this->wpService);
+        $resolveHomeUrl = new \Municipio\Integrations\Polylang\ResolveHomeUrl($this->wpService);
         $resolvePdfNotFoundUrl = new \Municipio\Integrations\Polylang\ResolvePdfNotFoundUrl($this->wpService);
         $resolveFontAttachmentQueries = new \Municipio\Integrations\Polylang\ResolveFontAttachmentQueries($this->wpService);
         $this->hooksRegistrar->register($resolvePageTreeMenuPageIds);
         $this->hooksRegistrar->register($resolveNavigationCacheKey);
+        $this->hooksRegistrar->register($resolveHomeUrl);
         $this->hooksRegistrar->register($resolvePdfNotFoundUrl);
         $this->hooksRegistrar->register($resolveFontAttachmentQueries);
 
