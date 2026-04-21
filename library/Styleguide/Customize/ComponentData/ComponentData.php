3e4b5e0a01a4cae54f93a8289a61292069971253
Thor Brink <thorbrink@gmail.com>
2026-04-21 10:16:47 +0200

    fix: translate component data (#1895)

--- library/Styleguide/Customize/ComponentData/ComponentData.php
+++ library/Styleguide/Customize/ComponentData/ComponentData.php
@@ -1,37 +1,43 @@
 <?php
 
+declare(strict_types=1);
+
 namespace Municipio\Styleguide\Customize\ComponentData;
 
 use Composer\InstalledVersions;
 use WpService\Contracts\ApplyFilters;
+use WpService\Contracts\_x;
 
 class ComponentData
 {
     private const STYLEGUIDE_PACKAGE = 'helsingborg-stad/styleguide';
 
     public function __construct(
-        private ApplyFilters $wpService,
+        private ApplyFilters&_x $wpService,
     ) {}
 
     public function getComponentData(): array
     {
-        if (!file_exists($this->getFilePath())) {
+        if (!file_exists(static::getFilePath())) {
             return [];
         }
 
-        $contents = file_get_contents($this->getFilePath());
+        $contents = file_get_contents(static::getFilePath());
         $contents = $contents === false ? [] : json_decode($contents, true);
+
+        // Apply local decorators
+        $contents = (new Decorators\ApplyI18n($this->wpService))->decorate($contents);
 
         return $this->wpService->applyFilters('Municipio/Styleguide/Customize/ComponentData', $contents);
     }
 
-    private function getStyleguidePath(): string
+    private static function getStyleguidePath(): string
     {
         return InstalledVersions::getInstallPath(self::STYLEGUIDE_PACKAGE) ?? null;
     }
 
-    private function getFilePath(): string
+    public static function getFilePath(): string
     {
-        return realpath($this->getStyleguidePath()) . '/component-design-tokens.json';
+        return realpath(static::getStyleguidePath()) . '/component-design-tokens.json';
     }
 }
--- library/Styleguide/Customize/ComponentData/ComponentDataTest.php
+++ library/Styleguide/Customize/ComponentData/ComponentDataTest.php
@@ -1,10 +1,13 @@
 <?php
 
+declare(strict_types=1);
+
 namespace Municipio\Styleguide\Customize\ComponentData;
 
 use PHPUnit\Framework\Attributes\TestDox;
 use PHPUnit\Framework\TestCase;
 use WpService\Contracts\ApplyFilters;
+use WpService\Contracts\_x;
 use WpService\Implementations\FakeWpService;
 
 class ComponentDataTest extends TestCase
@@ -15,15 +18,14 @@ class ComponentDataTest extends TestCase
         $componentData = new ComponentData(static::createWpService());
         $result = $componentData->getComponentData();
 
-        $this->assertIsArray($result);
+        static::assertIsArray($result);
     }
 
-    private static function createWpService(): ApplyFilters
+    private static function createWpService(): ApplyFilters&_x
     {
         return new FakeWpService([
-            'applyFilters' => function (string $filterName, $value) {
-                return $value;
-            },
+            'applyFilters' => static fn (string $filterName, $value) => $value,
+            '_x' => static fn (string $text, string $context, string $domain = 'default') => $text,
         ]);
     }
 }
