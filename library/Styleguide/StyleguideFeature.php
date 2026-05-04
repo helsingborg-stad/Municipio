04e562107d6a30d5239ffb054ace35664d975558
Thor Brink <thor.brink@helsingborg.se>
2026-05-04 11:57:18 +0000

    refactor: separate enqueueing of styleguide styles into its own class

--- library/Styleguide/StyleguideFeature.php
+++ library/Styleguide/StyleguideFeature.php
@@ -3,6 +3,7 @@
 namespace Municipio\Styleguide;
 
 use Municipio\HooksRegistrar\Hookable;
+use Municipio\Styleguide\EnqueueStyleguideStyles\EnqueueStyleguideStyles;
 use WpService\WpService;
 
 class StyleguideFeature implements Hookable
@@ -13,26 +14,10 @@ class StyleguideFeature implements Hookable
 
     public function addHooks(): void
     {
-        $this->wpService->addAction('wp_enqueue_scripts', fn() => $this->outputStyleguideCss());
-        $this->wpService->addAction('login_enqueue_scripts', fn() => $this->outputStyleguideCss());
-        $this->wpService->addAction('enqueue_block_editor_assets', fn() => $this->outputStyleguideCss(true));
+        (new EnqueueStyleguideStyles($this->wpService))->addHooks();
         (new ApplyLayerToInlineStyles\ApplyLayerToInlineStyles($this->wpService))->addHooks();
-        (new AddLayerOrderDefinitionToHead\AddLayerOrderDefinitionToHead($this->wpService))->addHooks();
         (new ApplyLayersToEnqueuedStyles\ApplyLayersToEnqueuedStyles($this->wpService))->addHooks();
+        (new AddLayerOrderDefinitionToHead\AddLayerOrderDefinitionToHead($this->wpService))->addHooks();
         (new Customize\Customize($this->wpService))->addHooks();
-    }
-
-    public function outputStyleguideCss(bool $isEditor = false): void
-    {
-        if ($isEditor) {
-            add_editor_style($this->getStyleguideTemplatePath());
-        } else {
-            $this->wpService->wpEnqueueStyle('styleguide-css-variables', get_template_directory_uri() . $this->getStyleguideTemplatePath());
-        }
-    }
-
-    private function getStyleguideTemplatePath(): string
-    {
-        return '/assets/dist/' . \Municipio\Helper\CacheBust::name('css/styleguide.css');
     }
 }
