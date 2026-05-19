d5b1ca520077dccb8286e968447bc2d81eccff4d
Sebastian Thulin <sebastian.thulin@helsingborg.se>
2026-05-19 15:41:31 +0200

    fix: update text module to behave like an article.

--- Modularity/source/php/Module/Text/views/article.blade.php
+++ Modularity/source/php/Module/Text/views/article.blade.php
@@ -1,8 +1,12 @@
 @element([
-    'componentElement' => ((!$hideTitle && !empty($postTitle)) || $hasHeadingsInContent) ? 'article' : 'div',
+    'componentElement' => 'article',
+    'classList' => [
+        'c-article',
+    ],
     'attributeList' => [
-        ...(!empty($font_size) ? ['class' => $font_size] : []),
-        ...(!$hideTitle && !empty($postTitle) ? ['aria-labelledby' => 'mod-text-' . $ID . '-label'] : []),
+        ...(!$hideTitle && !empty($postTitle) ? [
+            'aria-labelledby' => 'mod-text-' . $ID . '-label'] : []
+        ),
     ]
 ])
     @includeWhen(!$hideTitle && !empty($postTitle), 'partials.postTitle', ['variant' => 'h2'])
