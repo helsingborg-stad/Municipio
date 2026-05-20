e25f8b818099c750e7862b8da917ba346370d325
Thor Brink <thor.brink@helsingborg.se>
2026-05-20 05:12:55 +0000

    fix: add missing closing div in language menu partial

--- views/v3/partials/navigation/language.blade.php
+++ views/v3/partials/navigation/language.blade.php
@@ -19,61 +19,63 @@
 
         <div class="c-card__body site-language-menu__body u-padding__top--0">
 
-        @if ($languageMenuOptions->displayCurrentLanguage)
-            @button([
-                'variant' => 'filled',
-                'size' => 'md',
-                'text' => $languageMenuOptions->currentLanguage,
-                'classList' => [
-                    'site-language-menu__default_lang',
-                    'u-margin__bottom--1',
-                    'u-width--100'
-                ],
-                'attributeList' => [
-                    'disabled' => 'disabled',
-                ]
-            ])
-            @endbutton
-        @endif
+            @if ($languageMenuOptions->displayCurrentLanguage)
+                @button([
+                    'variant' => 'filled',
+                    'size' => 'md',
+                    'text' => $languageMenuOptions->currentLanguage,
+                    'classList' => [
+                        'site-language-menu__default_lang',
+                        'u-margin__bottom--1',
+                        'u-width--100'
+                    ],
+                    'attributeList' => [
+                        'disabled' => 'disabled',
+                    ]
+                ])
+                @endbutton
+            @endif
 
-        @nav([
-            'id' => 'menu-language',
-            'items' => $languageMenu['items'],
-            'direction' => 'vertical',
-            'includeToggle' => false,
-            'classList' => ['s-nav-language'],
-            'height' => 'md',
-            'expandLabel' => $lang->expand
-        ])
-        @endnav
-
-        @if($languageMenuOptions->moreLanguageLink)
-            @button([
-                'icon' => 'arrow_forward',
-                'reversePositions' => true,
-                'text' => $languageMenuOptions->moreLanguageLinkLabel,
-                'color' => 'default',
-                'style' => 'basic',
-                'size' => 'sm',
-                'href' => $languageMenuOptions->moreLanguageLink,
-                'classList' => [
-                    'site-language-menu__more'
-                ]
+            @nav([
+                'id' => 'menu-language',
+                'items' => $languageMenu['items'],
+                'direction' => 'vertical',
+                'includeToggle' => false,
+                'classList' => ['s-nav-language'],
+                'height' => 'md',
+                'expandLabel' => $lang->expand
             ])
-            @endbutton
-        @endif
+            @endnav
 
-        @if($languageMenuOptions->disclaimer)
-            @typography([
-                'variant' => 'byline',
-                'classList' => [
-                    'u-border__top--1',
-                    'u-padding__top--2',
-                ]
-            ])
-                {{ $languageMenuOptions->disclaimer }}
-            @endtypography
-        @endif
+            @if($languageMenuOptions->moreLanguageLink)
+                @button([
+                    'icon' => 'arrow_forward',
+                    'reversePositions' => true,
+                    'text' => $languageMenuOptions->moreLanguageLinkLabel,
+                    'color' => 'default',
+                    'style' => 'basic',
+                    'size' => 'sm',
+                    'href' => $languageMenuOptions->moreLanguageLink,
+                    'classList' => [
+                        'site-language-menu__more'
+                    ]
+                ])
+                @endbutton
+            @endif
+
+            @if($languageMenuOptions->disclaimer)
+                @typography([
+                    'variant' => 'byline',
+                    'classList' => [
+                        'u-border__top--1',
+                        'u-padding__top--2',
+                    ]
+                ])
+                    {{ $languageMenuOptions->disclaimer }}
+                @endtypography
+            @endif
+
+        </div>
         
     @endcard
 @endscope
