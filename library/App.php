933d14861561ede2d7ee39a327f189098c55889d
Sebastian Thulin <sebastian.thulin@helsingborg.se>
2026-06-03 08:47:08 +0200

    chore: retire post type design

--- library/App.php
+++ library/App.php
@@ -272,11 +272,6 @@ class App
         });
 
         /**
-         * Imported post type design
-         */
-        $this->setupPostTypeDesign();
-
-        /**
          * Branded emails
          */
         $this->trySetupBrandedEmails();
@@ -863,31 +858,6 @@ class App
         // Hooks
         $hooks = new ImageFocusHooks($this->wpService, $imageFocusManager);
         $hooks->addHooks();
-    }
-
-    /**
-     * Sets up the post type design.
-     *
-     * This method initializes the post type design by creating instances of the
-     * SaveDesigns and SetDesigns classes and passing the option name and the
-     * WordPress service instance.
-     *
-     * @return void
-     */
-    private function setupPostTypeDesign(): void
-    {
-        $optionName = 'post_type_design';
-        $apiUrl = 'https://customizer.municipio.tech/id/';
-        $saveDesigns = new \Municipio\PostTypeDesign\SaveDesigns(
-            $optionName,
-            $this->wpService,
-            new \Municipio\PostTypeDesign\ConfigFromPageId($this->wpService, $apiUrl),
-        );
-
-        $setDesigns = new \Municipio\PostTypeDesign\SetDesigns($optionName, $this->wpService);
-
-        $this->hooksRegistrar->register($saveDesigns);
-        $this->hooksRegistrar->register($setDesigns);
     }
 
     /**
