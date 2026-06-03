b6f68e7d09083bd4514a1d55147598657b1d1e4f
NiclasNorin <niclasn@outlook.com>
2026-06-03 11:31:53 +0200

    refactor: view for profile editor login

--- library/Kulturkortet/ProfileEditor/views/kulturkortet-profile-simple-message.blade.php
+++ library/Kulturkortet/ProfileEditor/views/kulturkortet-profile-simple-message.blade.php
@@ -1,9 +1,24 @@
-@card([
-            'color' => 'info',
-            'heading' => $lang['heading'],
-            'content' => $lang['content'],
-            'buttons' => [
-                    ['type' => 'filled', 'color' => 'primary', 'text' => $lang['url'], 'href' => $url]
-                ]
-        ])
-@endcard
+@element([])
+    @typography([
+        'element' => 'h2',
+        'variant' => 'h4'
+    ])
+        {{ $lang['heading'] }}
+    @endtypography
+
+    @typography([
+        'element' => 'p',
+    ])
+        {{ $lang['content'] }}
+    @endtypography
+
+    @button([
+        'color' => 'primary',
+        'href' => $url,
+        'text' => $lang['actionLabel'],
+        'classList' => ['u-margin__top--2'],
+        'reversePositions' => true,
+        'icon' => 'login'
+    ])
+    @endbutton
+@endelement
