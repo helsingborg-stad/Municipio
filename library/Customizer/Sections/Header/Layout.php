5dbec826d66907244004d9213d7d2afdecf93a8a
Sebastian Thulin <sebastian.thulin@helsingborg.se>
2026-08-07 17:12:05 +0200

    fix: use real popover to break out of stacking context lang. menu

--- library/Customizer/Sections/Header/Layout.php
+++ library/Customizer/Sections/Header/Layout.php
@@ -206,7 +206,7 @@ class Layout
             'choices' => [
                 'min' => 0.25,
                 'max' => 0.85,
-                'step' => 0.1,
+                'step' => 0.05,
             ],
             'active_callback' => $this->getHeaderLogoOverlapMultiplierActiveCallback(),
             'output' => [
