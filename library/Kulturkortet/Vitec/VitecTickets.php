47757a741483141477ef681d43589ab420df90c5
Joakim Larsson <mimmertim@gmail.com>
2026-08-28 15:12:05 +0200

    fix(KulturKortet): validating tickets (+ developer impersonation) (#2149)
    * fix(KulturKortet): validating tickets (+ developer impersonation)
    
    * refactor: remove unnecessary parentheses
    
    ---------
    
    Co-authored-by: Thor Brink <thor.brink@helsingborg.se>

---
+++ library/Kulturkortet/Vitec/VitecTickets.php
@@ -0,0 +1,29 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Municipio\Kulturkortet\Vitec;
+
+class VitecTickets
+{
+    /**
+     * Try to get the active ticket from the list of tickets.
+     * To be active,
+     * - the ticket must have 'Import_Kulturkort' in its template name
+     * - the current date must be within the validFrom and validUntil range
+     *
+     * @param array $tickets List of tickets to check.
+     * @return array|null The active ticket if found, otherwise null.
+     */
+    public static function tryGetActiveTicket(array $tickets): ?array
+    {
+        $now = new \DateTime();
+        return 
+            array_values(
+                array_filter(
+                    $tickets ?? [],
+                    fn($t) => str_contains($t['ticketTemplateName'], 'Import_Kulturkort') && !empty($t['validFrom'] ?? '') && !empty($t['validUntil'] ?? '') && new \DateTime($t['validFrom'] ?? '') <= $now && new \DateTime($t['validUntil'] ?? '') >= $now,
+                ),
+            )[0] ?? null;
+    }
+}
