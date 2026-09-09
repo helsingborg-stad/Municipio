5bdaf8f1493d6d0b0964d8abd1fc2ddb1e6f59f5
Thor Brink <thor.brink@helsingborg.se>
2026-09-09 05:06:59 +0000

    wip: remove dynamic taxonomy

--- library/SchemaData/Taxonomy/TaxonomiesFromSchemaType/TaxonomiesFromSchemaType.php
+++ library/SchemaData/Taxonomy/TaxonomiesFromSchemaType/TaxonomiesFromSchemaType.php
@@ -66,15 +66,7 @@ class TaxonomiesFromSchemaType implements TaxonomiesFromSchemaTypeInterface
      */
     private function getExhibitionEventTaxonomies(): array
     {
-        return [
-            new EventStatusFromDatesTaxonomy($this->wpService, $this->createTaxonomy(
-                'ExhibitionEvent',
-                'startDate',
-                $this->wpService->_x('Status', 'ExhibitionEvent taxonomy name (plural)', 'municipio'),
-                $this->wpService->_x('Status', 'ExhibitionEvent taxonomy name', 'municipio'),
-                ['show_admin_column' => true],
-            )),
-        ];
+        return [];
     }
 
     /**
