b00ac1cceba3d2ee9e76c99315841d4a1c0f30f9
NiclasNorin <niclasn@outlook.com>
2026-09-21 14:53:24 +0200

    fix: language menu position

--- views/v3/partials/header/components/language.blade.php
+++ views/v3/partials/header/components/language.blade.php
@@ -1,4 +1,4 @@
-<div class="site-language-menu">
+<div class="site-language-menu u-display--flex">
     @includeIf('partials.navigation.trigger.language')
     @includeIf('partials.navigation.language')
 </div>
