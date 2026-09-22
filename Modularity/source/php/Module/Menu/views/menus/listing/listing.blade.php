f6627c7300d0cb7f62b6f4b20cd972bdc9ba516b
NiclasNorin <niclasn@outlook.com>
2026-09-22 17:07:56 +0200

    fix: correcting container

--- Modularity/source/php/Module/Menu/views/menus/listing/listing.blade.php
+++ Modularity/source/php/Module/Menu/views/menus/listing/listing.blade.php
@@ -1,11 +1,13 @@
 @element([
-    'classList' => $classList
+    'classList' => array_merge([
+        'o-layout-grid',
+        'o-layout-grid--cq'
+    ], $classList)
 ])
     @element([
         'classList' => array_merge([
             'mod-menu__wrapper',
-            'o-layout-grid',
-            'o-layout-grid--cq'
+            'o-layout-grid'
         ], $wrapperClasses)
     ])
         @foreach ($menu['items'] as $index => $menuItem)
