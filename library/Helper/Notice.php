10bf45f63e4e83d50b466eb918fb700577c83f91
Thor Brink <thor.brink@helsingborg.se>
2026-09-25 13:01:44 +0000

    revert: accidentally changed file

--- library/Helper/Notice.php
+++ library/Helper/Notice.php
@@ -1,8 +1,5 @@
 <?php
 
-declare(strict_types=1);
-
-
 namespace Municipio\Helper;
 
 class Notice
@@ -14,7 +11,7 @@ class Notice
      */
     public static function add($text, $class = 'warning', $icon = null, $action = null, $dismissable = false, $location = 'toast')
     {
-        add_filter('Municipio/viewData', static function ($data) use ($text, $class, $icon, $action, $dismissable, $location) {
+        add_filter('Municipio/viewData', function ($data) use ($text, $class, $icon, $action, $dismissable, $location) {
             $data['notice'][$location][] = [
                 'type'        => $class,
                 'message'     => [
